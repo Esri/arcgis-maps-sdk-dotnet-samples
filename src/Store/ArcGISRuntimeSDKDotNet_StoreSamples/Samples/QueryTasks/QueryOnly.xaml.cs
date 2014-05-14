8aefa637e3f46f43c63799a232e1f52cd3533ed1
Greg DeStigter <gdestigter@esri.com>
2014-05-14 10:28:31 -0700

    Store: Added Query Tasks samples (SDK Issue #14).

--- src/Store/ArcGISRuntimeSDKDotNet_StoreSamples/Samples/QueryTasks/QueryOnly.xaml.cs
+++ src/Store/ArcGISRuntimeSDKDotNet_StoreSamples/Samples/QueryTasks/QueryOnly.xaml.cs
@@ -1,20 +1,18 @@
-﻿using Esri.ArcGISRuntime.Data;
-using Esri.ArcGISRuntime.Layers;
-using Esri.ArcGISRuntime.Tasks.Query;
+﻿using Esri.ArcGISRuntime.Tasks.Query;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace ArcGISRuntimeSDKDotNet_StoreSamples.Samples
 {
-	/// <summary>
-	/// 
-	/// </summary>
+    /// <summary>
+    /// Demonstrates how to use a QueryTask to query a map service without a map.
+    /// </summary>
+    /// <title>Query Only</title>
     /// <category>Query Tasks</category>
-	public sealed partial class QueryOnly : Page
+    public sealed partial class QueryOnly : Page
     {
         public QueryOnly()
         {
@@ -23,38 +21,36 @@ namespace ArcGISRuntimeSDKDotNet_StoreSamples.Samples
 
         private async void QueryButton_Click(object sender, RoutedEventArgs e)
         {
-			try
-			{
-				await RunQuery();
-			}
-			catch(System.Exception ex)
-			{
-				var _ = new Windows.UI.Popups.MessageDialog(ex.Message, "Error").ShowAsync();
-			}
+            try
+            {
+                await RunQuery();
+            }
+            catch (System.Exception ex)
+            {
+                var _ = new Windows.UI.Popups.MessageDialog(ex.Message, "Error").ShowAsync();
+            }
         }
 
         private async Task RunQuery()
         {
-            QueryTask queryTask =
-                new QueryTask(new Uri("http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Census_USA/MapServer/5"));
-
-            Query query = new Query(StateNameTextBox.Text);
-
-            query.OutFields.Add("*");
             try
             {
+                QueryTask queryTask = new QueryTask(
+                    new Uri("http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Census_USA/MapServer/5"));
+
+                Query query = new Query(StateNameTextBox.Text);
+                query.OutFields.Add("*");
+
                 var result = await queryTask.ExecuteAsync(query);
                 itemListView.ItemsSource = result.FeatureSet.Features;
             }
-            catch (TaskCanceledException taskCanceledEx)
+            catch (TaskCanceledException)
             {
-                System.Diagnostics.Debug.WriteLine(taskCanceledEx.Message);
             }
             catch (Exception ex)
             {
-                System.Diagnostics.Debug.WriteLine(ex.Message);
+                var _ = new MessageDialog(ex.Message, "Sample Error").ShowAsync();
             }
         }
     }
-
 }
---
+++ src/Store/ArcGISRuntimeSDKDotNet_StoreSamples/Samples/QueryTasks/QueryRelatedTables.xaml.cs
@@ -0,0 +1,105 @@
+﻿using Esri.ArcGISRuntime.Controls;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Layers;
+using Esri.ArcGISRuntime.Tasks.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+
+namespace ArcGISRuntimeSDKDotNet_StoreSamples.Samples
+{
+    /// <summary>
+    /// Demonstrates how to use a relationship query to display information from a related table for selected features.
+    /// </summary>
+    /// <title>Query Related Tables</title>
+    /// <category>Query Tasks</category>
+	public sealed partial class QueryRelatedTables : Windows.UI.Xaml.Controls.Page
+    {
+        private  GraphicsLayer _wellsLayer;
+
+        public QueryRelatedTables()
+        {
+            this.InitializeComponent();
+
+            _wellsLayer = mapView.Map.Layers["WellsLayer"] as GraphicsLayer;
+                
+            mapView.Map.InitialExtent = new Envelope(-10854000, 4502000, -10829000, 4524000, SpatialReferences.WebMercator);
+        }
+
+        // Select a set of wells near the click point
+        private async void mapView_MapViewTapped(object sender, MapViewInputEventArgs e)
+        {
+            try
+            {
+                _wellsLayer.Graphics.Clear();
+                wellsGrid.ItemsSource = relationshipsGrid.ItemsSource = null;
+
+                QueryTask queryTask =
+                    new QueryTask(new Uri("http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Petroleum/KSPetro/MapServer/0"));
+
+                Query query = new Query("1=1")
+                {
+                    Geometry = Expand(mapView.Extent, e.Location, 0.01),
+                    ReturnGeometry = true,
+                    OutSpatialReference = mapView.SpatialReference,
+                    OutFields = OutFields.All
+                };
+
+                var result = await queryTask.ExecuteAsync(query);
+                if (result.FeatureSet.Features != null && result.FeatureSet.Features.Count > 0)
+                {
+                    _wellsLayer.Graphics.AddRange(result.FeatureSet.Features);
+                    wellsGrid.ItemsSource = result.FeatureSet.Features;
+                    resultsPanel.Visibility = Visibility.Visible;
+                }
+            }
+            catch (Exception ex)
+            {
+                var _ = new MessageDialog(ex.Message, "Sample Error").ShowAsync();
+            }
+        }
+
+        // Query for rows related to the selected well in the wells list view
+        private async void WellsGrid_SelectionChanged(object sender, Windows.UI.Xaml.Controls.SelectionChangedEventArgs e)
+        {
+            try
+            {
+                if (e.AddedItems != null && e.AddedItems.Count > 0)
+                {
+                    QueryTask queryTask =
+                       new QueryTask(new Uri("http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Petroleum/KSPetro/MapServer/0"));
+
+                    //Relationship query
+                    var objectIds = e.AddedItems.OfType<Graphic>()
+                        .Select(g => Convert.ToInt64(g.Attributes["OBJECTID"]));
+
+                    RelationshipParameter parameters = new RelationshipParameter(new List<long>(objectIds), 3)
+                    {
+                        OutSpatialReference = mapView.SpatialReference
+                    };
+
+                    parameters.OutFields.AddRange(new string[] { "OBJECTID, API_NUMBER, ELEVATION, FORMATION, TOP" });
+
+                    var result = await queryTask.ExecuteRelationshipQueryAsync(parameters);
+                    relationshipsGrid.ItemsSource = result.RelatedRecordGroups.FirstOrDefault().Value;
+                }
+            }
+            catch (Exception ex)
+            {
+                var _ = new MessageDialog(ex.Message, "Sample Error").ShowAsync();
+            }
+        }
+
+        private Envelope Expand(Envelope mapExtent, MapPoint point, double pct)
+        {
+            return new Envelope(
+                point.X - mapExtent.Width * (pct / 2), point.Y - mapExtent.Height * (pct / 2),
+                point.X + mapExtent.Width * (pct / 2), point.Y + mapExtent.Height * (pct / 2))
+            {
+                SpatialReference = mapExtent.SpatialReference
+            };
+        }
+    }
+}

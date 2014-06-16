48ee638ebc71a5217e99689164c5debbdb335ed5
greg5999 <gdestigter@esri.com>
2014-06-16 16:39:31 -0700

    [Store] Added offline samples to match desktop - ExportTileCache - GenerateGeodatabase - SyncGeodatabase - OfflineGeocoding - OfflineRouting

---
+++ src/Store/ArcGISRuntimeSDKDotNet_StoreSamples/Samples/Offline/OfflineRouting.xaml.cs
@@ -0,0 +1,178 @@
+﻿using Esri.ArcGISRuntime.Controls;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Layers;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks.NetworkAnalyst;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+
+namespace ArcGISRuntimeSDKDotNet_StoreSamples.Samples
+{
+    /// <summary>
+    /// Demonstrates offline routing with a network analyst LocalRouteTask.
+    /// </summary>
+    /// <title>Routing</title>
+    /// <category>Offline</category>
+    public partial class OfflineRouting : Windows.UI.Xaml.Controls.Page
+    {
+        private const string _localRoutingDatabase = @"networks\san-diego\san-diego-network.geodatabase";
+        private const string _networkName = "Streets_ND";
+
+        private LocalRouteTask _routeTask;
+        private Symbol _directionPointSymbol;
+        private GraphicsLayer _stopsLayer;
+        private GraphicsLayer _routesLayer;
+        private GraphicsLayer _directionsLayer;
+        private bool _isMapReady;
+
+        public OfflineRouting()
+        {
+            InitializeComponent();
+
+            _isMapReady = false;
+            _directionPointSymbol = LayoutRoot.Resources["directionPointSymbol"] as Symbol;
+            _stopsLayer = mapView.Map.Layers["StopsLayer"] as GraphicsLayer;
+            _routesLayer = mapView.Map.Layers["RoutesLayer"] as GraphicsLayer;
+            _directionsLayer = mapView.Map.Layers["DirectionsLayer"] as GraphicsLayer;
+                
+            mapView.Map.InitialExtent = new Envelope(-13044000, 3855000, -13040000, 3858000, SpatialReferences.WebMercator);
+            mapView.ExtentChanged += mapView_ExtentChanged;
+        }
+
+        // Make sure the map is ready for user interaction
+        private async void mapView_ExtentChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                mapView.ExtentChanged -= mapView_ExtentChanged;
+
+                await mapView.LayersLoadedAsync();
+                _isMapReady = true;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        // Get user Stop points
+        private void mapView_MapViewTapped(object sender, MapViewInputEventArgs e)
+        {
+            if (!_isMapReady)
+                return;
+
+            try
+            {
+                e.Handled = true;
+
+                if (_directionsLayer.Graphics.Count() > 0)
+                {
+                    panelResults.Visibility = Visibility.Collapsed;
+
+                    _stopsLayer.Graphics.Clear();
+                    _routesLayer.Graphics.Clear();
+                    _directionsLayer.GraphicsSource = null;
+                }
+
+                _stopsLayer.Graphics.Add(new Graphic(e.Location));
+            }
+            catch (System.Exception ex)
+            {
+                var _ = new MessageDialog(ex.Message, "Sample Error").ShowAsync();
+            }
+        }
+
+        // Calculate the route
+        private async void mapView_MapViewDoubleTapped(object sender, Esri.ArcGISRuntime.Controls.MapViewInputEventArgs e)
+        {
+            if (!_isMapReady || _stopsLayer.Graphics.Count() < 2)
+                return;
+
+            try
+            {
+                e.Handled = true;
+
+                panelResults.Visibility = Visibility.Collapsed;
+                progress.Visibility = Visibility.Visible;
+
+                if (_routeTask == null)
+                {
+                    var path = System.IO.Path.Combine(ApplicationData.Current.LocalFolder.Path, _localRoutingDatabase);
+                    _routeTask = await Task.Run<LocalRouteTask>(() => new LocalRouteTask(path, _networkName));
+                }
+
+                RouteParameters routeParams = await _routeTask.GetDefaultParametersAsync();
+                routeParams.OutSpatialReference = mapView.SpatialReference;
+                routeParams.ReturnDirections = true;
+                routeParams.DirectionsLengthUnit = LinearUnits.Miles;
+                routeParams.DirectionsLanguage = new CultureInfo("en");
+                routeParams.Stops = new FeaturesAsFeature(_stopsLayer.Graphics) { SpatialReference = mapView.SpatialReference };
+
+                var routeResult = await _routeTask.SolveAsync(routeParams);
+                if (routeResult == null || routeResult.Routes == null || routeResult.Routes.Count() == 0)
+                    throw new Exception("No route could be calculated");
+
+                var route = routeResult.Routes.First();
+                _routesLayer.Graphics.Add(new Graphic(route.RouteGraphic.Geometry));
+
+                _directionsLayer.GraphicsSource = route.RouteDirections.Select(rd => GraphicFromRouteDirection(rd));
+
+                var totalTime = route.RouteDirections.Select(rd => rd.Time).Aggregate(TimeSpan.Zero, (p, v) => p.Add(v));
+                var totalLength = route.RouteDirections.Select(rd => rd.GetLength(LinearUnits.Miles)).Sum();
+                txtRouteTotals.Text = string.Format("Time: {0:h':'mm':'ss} / Length: {1:0.00} mi", totalTime, totalLength);
+
+                await mapView.SetViewAsync(route.RouteGraphic.Geometry.Extent.Expand(1.2));
+            }
+            catch (AggregateException ex)
+            {
+                var innermostExceptions = ex.Flatten().InnerExceptions;
+                if (innermostExceptions != null && innermostExceptions.Count > 0)
+                {
+                    var _ = new MessageDialog(innermostExceptions[0].Message, "Sample Error").ShowAsync();
+                }
+                else
+                {
+                    var _ = new MessageDialog(ex.Message, "Sample Error").ShowAsync();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                var _ = new MessageDialog(ex.Message, "Sample Error").ShowAsync();
+            }
+            finally
+            {
+                progress.Visibility = Visibility.Collapsed;
+                if (_directionsLayer.Graphics.Count() > 0)
+                    panelResults.Visibility = Visibility.Visible;
+            }
+        }
+
+        private Graphic GraphicFromRouteDirection(RouteDirection rd)
+        {
+            var graphic = new Graphic(rd.Geometry);
+            graphic.Attributes.Add("Direction", rd.Text);
+            graphic.Attributes.Add("Time", string.Format("{0:h\\:mm\\:ss}", rd.Time));
+            graphic.Attributes.Add("Length", string.Format("{0:0.00}", rd.GetLength(LinearUnits.Miles)));
+            if (rd.Geometry is MapPoint)
+                graphic.Symbol = _directionPointSymbol;
+
+            return graphic;
+        }
+
+        private void listDirections_SelectionChanged(object sender, Windows.UI.Xaml.Controls.SelectionChangedEventArgs e)
+        {
+            _directionsLayer.ClearSelection();
+
+            if (e.AddedItems != null && e.AddedItems.Count == 1)
+            {
+                var graphic = e.AddedItems[0] as Graphic;
+                graphic.IsSelected = true;
+            }
+        }
+    }
+}

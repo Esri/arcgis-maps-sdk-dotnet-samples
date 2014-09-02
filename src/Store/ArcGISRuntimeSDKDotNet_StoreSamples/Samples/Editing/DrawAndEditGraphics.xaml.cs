9150ab0e1cb8776fa787c39cdd99769a220e00cd
Preeti Maske <pmaske@esri.com>
2014-09-02 16:48:23 -0700

    Fixed broken sample. GraphicOverlays were being accessed via x:Name which doesn't work in store. Changed the sample to use ID instead.

--- src/Store/ArcGISRuntimeSDKDotNet_StoreSamples/Samples/Editing/DrawAndEditGraphics.xaml.cs
+++ src/Store/ArcGISRuntimeSDKDotNet_StoreSamples/Samples/Editing/DrawAndEditGraphics.xaml.cs
@@ -9,19 +9,19 @@ using Windows.UI.Xaml.Controls;
 
 namespace ArcGISRuntimeSDKDotNet_StoreSamples.Samples
 {
-	/// <summary>
-	/// Demonstrates drawing and editing map graphics.
-	/// </summary>
-    /// <title>Draw and Edit Graphics</title>
-    /// <category>Editing</category>
-	public sealed partial class DrawAndEditGraphics : Page
-    {
-        Graphic _editGraphic = null;
+  /// <summary>
+  /// Demonstrates drawing and editing map graphics.
+  /// </summary>
+  /// <title>Draw and Edit Graphics</title>
+  /// <category>Editing</category>
+  public sealed partial class DrawAndEditGraphics : Page
+  {
+    Graphic _editGraphic = null;
 
-		public DrawAndEditGraphics()
-        {
-            this.InitializeComponent();
-            DrawShapes.ItemsSource = new DrawShape[]
+    public DrawAndEditGraphics()
+    {
+      this.InitializeComponent();
+      DrawShapes.ItemsSource = new DrawShape[]
             {
                 DrawShape.Freehand,
 				DrawShape.Point,
@@ -33,96 +33,98 @@ namespace ArcGISRuntimeSDKDotNet_StoreSamples.Samples
 				DrawShape.LineSegment,
 				DrawShape.Rectangle
             };
-            DrawShapes.SelectedIndex = 0;
-        }
-
-        private async void OnDrawButtonClicked(object sender, RoutedEventArgs e)
-        {
-            string message = null;
-            var resultGeometry = _editGraphic == null ? null : _editGraphic.Geometry;
-            
-			var editCnfg = MyMapView.Editor.EditorConfiguration;
-            editCnfg.AllowAddVertex = AddVertex.IsChecked.HasValue && AddVertex.IsChecked.Value;
-            editCnfg.AllowDeleteVertex = DeleteVertex.IsChecked.HasValue && DeleteVertex.IsChecked.Value;
-            editCnfg.AllowMoveGeometry = MoveGeometry.IsChecked.HasValue && MoveGeometry.IsChecked.Value;
-            editCnfg.AllowMoveVertex = MoveVertex.IsChecked.HasValue && MoveVertex.IsChecked.Value;
-            editCnfg.AllowRotateGeometry = Rotate.IsChecked.HasValue && Rotate.IsChecked.Value;
-            editCnfg.AllowScaleGeometry = Scale.IsChecked.HasValue && Scale.IsChecked.Value;
-            editCnfg.MaintainAspectRatio = MaintainAspectRatio.IsChecked.HasValue && MaintainAspectRatio.IsChecked.Value;
-            editCnfg.VertexSymbol =
-                    new SimpleMarkerSymbol() { Style = SimpleMarkerStyle.Diamond, Color = Colors.Yellow, Size = 15 };
-
-            try
-            {
-                var drawShape = (DrawShape)DrawShapes.SelectedItem;
-
-                GraphicsOverlay graphicsOverlay;
-				graphicsOverlay = drawShape == DrawShape.Point ? PointGraphicsOverlay :
-                   ((drawShape == DrawShape.Polyline || drawShape == DrawShape.Freehand) ? 
-					PolylineGraphicsOverlay : PolygonGraphicsOverlay);
-
-                var progress = new Progress<GeometryEditStatus>();
-                progress.ProgressChanged += (a, b) =>
-                {
-                    //if (b.GeometryEditAction == GeometryEditAction..CompletedEdit)
-                    //    if (_editGraphic != null)
-                    //        _editGraphic.IsSelected = false;
-
-                };
-
-                var content = (sender as Button).Content.ToString();
-                switch (content)
-                {
-                    case "Draw":
-                        {
-                            var r = await MyMapView.Editor.RequestShapeAsync(drawShape, null, progress);
-                            graphicsOverlay.Graphics.Add(new Graphic() { Geometry = r });
-                            break;
-                        }
-                    case "Edit":
-                        {
-                            if (_editGraphic == null)
-                                return;
-                            var g = _editGraphic;
-                            g.IsVisible = false;
-                            var r = await MyMapView.Editor.EditGeometryAsync(g.Geometry, null, progress);
-                            resultGeometry = r ?? resultGeometry;
-                            _editGraphic.Geometry = resultGeometry;
-                            _editGraphic.IsSelected = false;
-                            _editGraphic.IsVisible = true;
-                            _editGraphic = null;
-                            break;
-                        }
-                }
-
-            }
-            catch (Exception ex)
-            {
-
-                message = ex.Message;
-                if (_editGraphic != null)
-                {
-                    _editGraphic.Geometry = resultGeometry;
-                    _editGraphic.IsVisible = true;
-                }
-            }
-            if (message != null)
-                await new MessageDialog(message).ShowAsync();
-        }
-
-        private async void MyMapView_MapViewTapped(object sender, MapViewInputEventArgs e)
-        {
-            var drawShape = (DrawShape)DrawShapes.SelectedItem;
-            GraphicsLayer graphicsLayer;
-            graphicsLayer = drawShape == DrawShape.Point ? MyMapView.Map.Layers["PointGraphicsLayer"] as GraphicsLayer :
-               ((drawShape == DrawShape.Polyline || drawShape == DrawShape.Freehand) ? MyMapView.Map.Layers["PolylineGraphicsLayer"] as GraphicsLayer : MyMapView.Map.Layers["PolygonGraphicsLayer"] as GraphicsLayer);
-
-            var graphic = await graphicsLayer.HitTestAsync(MyMapView, e.Position);
-            if (graphic != null)
-            {
-                _editGraphic = graphic;
-                _editGraphic.IsSelected = true;
-            }
-        }
+      DrawShapes.SelectedIndex = 0;
     }
+
+    private async void OnDrawButtonClicked(object sender, RoutedEventArgs e)
+    {
+      string message = null;
+      var resultGeometry = _editGraphic == null ? null : _editGraphic.Geometry;
+
+      var editCnfg = MyMapView.Editor.EditorConfiguration;
+      editCnfg.AllowAddVertex = AddVertex.IsChecked.HasValue && AddVertex.IsChecked.Value;
+      editCnfg.AllowDeleteVertex = DeleteVertex.IsChecked.HasValue && DeleteVertex.IsChecked.Value;
+      editCnfg.AllowMoveGeometry = MoveGeometry.IsChecked.HasValue && MoveGeometry.IsChecked.Value;
+      editCnfg.AllowMoveVertex = MoveVertex.IsChecked.HasValue && MoveVertex.IsChecked.Value;
+      editCnfg.AllowRotateGeometry = Rotate.IsChecked.HasValue && Rotate.IsChecked.Value;
+      editCnfg.AllowScaleGeometry = Scale.IsChecked.HasValue && Scale.IsChecked.Value;
+      editCnfg.MaintainAspectRatio = MaintainAspectRatio.IsChecked.HasValue && MaintainAspectRatio.IsChecked.Value;
+      editCnfg.VertexSymbol =
+              new SimpleMarkerSymbol() { Style = SimpleMarkerStyle.Diamond, Color = Colors.Yellow, Size = 15 };
+
+      try
+      {
+        var drawShape = (DrawShape)DrawShapes.SelectedItem;
+
+        GraphicsOverlay graphicsOverlay;
+        graphicsOverlay = drawShape == DrawShape.Point ? MyMapView.GraphicsOverlays["PointGraphicsOverlay"] as GraphicsOverlay :
+                   ((drawShape == DrawShape.Polyline || drawShape == DrawShape.Freehand) ?
+          MyMapView.GraphicsOverlays["PolylineGraphicsOverlay"] as GraphicsOverlay : MyMapView.GraphicsOverlays["PolygonGraphicsOverlay"] as GraphicsOverlay);
+
+        var progress = new Progress<GeometryEditStatus>();
+        progress.ProgressChanged += (a, b) =>
+        {
+          //if (b.GeometryEditAction == GeometryEditAction..CompletedEdit)
+          //    if (_editGraphic != null)
+          //        _editGraphic.IsSelected = false;
+
+        };
+
+        var content = (sender as Button).Content.ToString();
+        switch (content)
+        {
+          case "Draw":
+            {
+              var r = await MyMapView.Editor.RequestShapeAsync(drawShape, null, progress);
+              graphicsOverlay.Graphics.Add(new Graphic() { Geometry = r });
+              break;
+            }
+          case "Edit":
+            {
+              if (_editGraphic == null)
+                return;
+              var g = _editGraphic;
+              g.IsVisible = false;
+              var r = await MyMapView.Editor.EditGeometryAsync(g.Geometry, null, progress);
+              resultGeometry = r ?? resultGeometry;
+              _editGraphic.Geometry = resultGeometry;
+              _editGraphic.IsSelected = false;
+              _editGraphic.IsVisible = true;
+              _editGraphic = null;
+              break;
+            }
+        }
+
+      }
+      catch (Exception ex)
+      {
+
+        message = ex.Message;
+        if (_editGraphic != null)
+        {
+          _editGraphic.Geometry = resultGeometry;
+          _editGraphic.IsVisible = true;
+        }
+      }
+      if (message != null)
+        await new MessageDialog(message).ShowAsync();
+    }
+
+    private async void MyMapView_MapViewTapped(object sender, MapViewInputEventArgs e)
+    {     
+      var drawShape = (DrawShape)DrawShapes.SelectedItem;
+      GraphicsOverlay graphicsOverlay;
+      graphicsOverlay = drawShape == DrawShape.Point ? MyMapView.GraphicsOverlays["PointGraphicsOverlay"] as GraphicsOverlay :
+                 ((drawShape == DrawShape.Polyline || drawShape == DrawShape.Freehand) ?
+        MyMapView.GraphicsOverlays["PolylineGraphicsOverlay"] as GraphicsOverlay : MyMapView.GraphicsOverlays["PolygonGraphicsOverlay"] as GraphicsOverlay);
+
+
+      var graphic = await graphicsOverlay.HitTestAsync(MyMapView, e.Position);
+      if (graphic != null)
+      {
+        _editGraphic = graphic;
+        _editGraphic.IsSelected = true;
+      }
+    }
+  }
 }

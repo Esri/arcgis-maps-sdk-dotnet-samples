220a00a4adb66239b9da1bf3d490f37421ffd2a6
jenn6286 <jnery@esri.com>
2014-09-30 12:53:05 -0700

    Added EditRelatedData sample.

---
+++ src/Desktop/ArcGISRuntimeSDKDotNet_DesktopSamples/StringToDateConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace ArcGISRuntimeSDKDotNet_DesktopSamples
+{
+    public class StringToDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string)
+            {
+                var stringValue = (string)value;
+                DateTime dateTime = DateTime.MinValue;
+                if (DateTime.TryParse(stringValue, out dateTime))
+                    return dateTime;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

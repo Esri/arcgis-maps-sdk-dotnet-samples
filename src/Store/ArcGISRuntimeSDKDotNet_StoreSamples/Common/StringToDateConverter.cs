7a2cc1e735afd1b76df312229db9a49ae8e43f90
jenn6286 <jnery@esri.com>
2014-09-30 13:25:20 -0700

    Added EditRelatedData WinStore.

---
+++ src/Store/ArcGISRuntimeSDKDotNet_StoreSamples/Common/StringToDateConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace ArcGISRuntimeSDKDotNet_StoreSamples.Common
+{
+    public class StringToDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is string)
+            {
+                var stringValue = (string)value;
+                DateTime dateTime = DateTime.MinValue;
+                if (DateTime.TryParse(stringValue, out dateTime))
+                    return dateTime;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

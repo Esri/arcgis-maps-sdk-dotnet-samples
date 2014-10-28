27344e3d36fda40af8a5a29c6b36ef9db96b55e2
unknown <katy4195@kdalton2.esri.com>
2014-10-28 15:03:23 -0600

    Added TOC to display map layers with check boxes to show/hide on map.

--- src/Desktop/ArcGISRuntimeSDKDotNet_DesktopSamples/Samples/DynamicServiceLayers/DynamicLayerAddData.xaml.cs
+++ src/Desktop/ArcGISRuntimeSDKDotNet_DesktopSamples/Samples/DynamicServiceLayers/DynamicLayerAddData.xaml.cs
@@ -18,10 +18,10 @@ namespace ArcGISRuntimeSDKDotNet_DesktopSamples.Samples.DynamicLayers
     /// This sample demonstrates the dynamic layers capability to add Shapefiles and Raster datasets to the map. Single or multiple Shapefiles or Rasters can be selected in the open file dialog from within a folder. These files will be added to a local map service via the dynamic layers capability available in the API. This local map service is then used as the basis for an ArcGISDynamicMapServiceLayer which is added to the map. It would be possible to extend the sample to allows users to specify the symbol/renderer for any shapefiles added. Raster symbology is predetermined by the raster itself.
     /// </summary>
     /// <title>Dynamic Layer Add Data</title>
-	/// <category>Layers</category>
-	/// <subcategory>Dynamic Service Layers</subcategory>
-	/// <requiresLocalServer>true</requiresLocalServer>
-	public partial class DynamicLayerAddData : UserControl
+    /// <category>Layers</category>
+    /// <subcategory>Dynamic Service Layers</subcategory>
+    /// <requiresLocalServer>true</requiresLocalServer>
+    public partial class DynamicLayerAddData : UserControl
     {
         private const string _emptyMapPackage = @"..\..\..\..\..\samples-data\maps\water-distribution-network.mpk";
         private Random _random = new Random();
@@ -53,13 +53,14 @@ namespace ArcGISRuntimeSDKDotNet_DesktopSamples.Samples.DynamicLayers
                     }
 
                     // Call the add dataset method with workspace type, parent directory path and file names (without extensions)
-                    var dynLayer = await AddFileDatasetToDynamicMapServiceLayer(WorkspaceFactoryType.Shapefile, 
+                    var dynLayer = await AddFileDatasetToDynamicMapServiceLayer(WorkspaceFactoryType.Shapefile,
                         Path.GetDirectoryName(openFileDialog.FileName), fileNames);
 
                     // Add the dynamic map service layer to the map
                     if (dynLayer != null)
                     {
-						MyMapView.Map.Layers.Add(dynLayer);
+                        dynLayer.DisplayName = dynLayer.DynamicLayerInfos[0].Name;
+                        MyMapView.Map.Layers.Add(dynLayer);
                         progress.Visibility = Visibility.Collapsed;
                     }
                 }
@@ -83,13 +84,13 @@ namespace ArcGISRuntimeSDKDotNet_DesktopSamples.Samples.DynamicLayers
                 {
                     progress.Visibility = Visibility.Visible;
                     // Call the add dataset method with workspace type, parent directory path and actual file names
-                    var dynLayer = await AddFileDatasetToDynamicMapServiceLayer(WorkspaceFactoryType.Raster, 
+                    var dynLayer = await AddFileDatasetToDynamicMapServiceLayer(WorkspaceFactoryType.Raster,
                         Path.GetDirectoryName(openFileDialog.FileName), new List<string>(openFileDialog.SafeFileNames));
 
                     // Add the dynamic map service layer to the map
                     if (dynLayer != null)
                     {
-						MyMapView.Map.Layers.Add(dynLayer);
+                        MyMapView.Map.Layers.Add(dynLayer);
                         progress.Visibility = Visibility.Collapsed;
                     }
                 }

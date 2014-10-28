396d1674596cf6552cabdffb49ca19426b03319f
unknown <katy4195@kdalton2.esri.com>
2014-10-28 08:13:35 -0600

    Added attribution to Store/Phone for WebTiledLayers sample

--- src/Store/ArcGISRuntimeSDKDotNet_StoreSamples/Samples/TiledLayers/WebTiledLayerSample.xaml.cs
+++ src/Store/ArcGISRuntimeSDKDotNet_StoreSamples/Samples/TiledLayers/WebTiledLayerSample.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Esri.ArcGISRuntime.Controls;
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Layers;
+using System;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -17,22 +19,24 @@ namespace ArcGISRuntimeSDKDotNet_StoreSamples.Samples
         {
             this.InitializeComponent();
 
-            MyMapView.Loaded += MyMapView_Loaded;
+            MyMapView.SpatialReferenceChanged += MyMapView_SpatialReferenceChanged;
         }
 
-        private void MyMapView_Loaded(object sender, RoutedEventArgs e)
+        void MyMapView_SpatialReferenceChanged(object sender, EventArgs e)
         {
-            cboLayers.SelectedIndex = 0;
+             cboLayers.SelectedIndex = 0;
         }
+
 
         private void cboLayers_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-			if (cboLayers == null)
-				return;
 
-			MyMapView.Map.Layers.Remove("MyWebTiledLayer");
+            if (cboLayers == null)
+                return;
 
-			WebTiledLayer webTiledLayer = new WebTiledLayer() { ID = "MyWebTiledLayer" };
+            MyMapView.Map.Layers.Remove("MyWebTiledLayer");
+
+            WebTiledLayer webTiledLayer = new WebTiledLayer() { ID = "MyWebTiledLayer" };
 
             switch (cboLayers.SelectedIndex)
             {
@@ -41,73 +45,86 @@ namespace ArcGISRuntimeSDKDotNet_StoreSamples.Samples
                     webTiledLayer.TemplateUri = "http://{subDomain}.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{level}/{row}/{col}";
                     webTiledLayer.SubDomains = new string[] { "server", "services" };
                     webTiledLayer.CopyrightText = "National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC";
+                    Attribution.Visibility = Visibility.Collapsed; // No attribution info here, so keep dialog collapsed.
                     break;
                 //MapQuest
                 case 1:
-                    webTiledLayer.TemplateUri = "http://mtile01.mqcdn.com/tiles/1.0.0/vx/map/{level}/{col}/{row}.jpg";
+                    webTiledLayer.TemplateUri = "http://otile1.mqcdn.com/tiles/1.0.0/vx/map/{level}/{col}/{row}.jpg";
                     webTiledLayer.CopyrightText = "Map Quest";
+                    Attribution.ContentTemplate = Attribution.Resources["MapQuestAttribution"] as DataTemplate;
+                    Attribution.Visibility = Visibility.Visible;
                     break;
                 //OpenCycleMap
                 case 2:
                     webTiledLayer.TemplateUri = "http://{subDomain}.tile.opencyclemap.org/cycle/{level}/{col}/{row}.png";
                     webTiledLayer.SubDomains = new string[] { "a", "b", "c" };
                     webTiledLayer.CopyrightText = "Open Cycle Map";
+                    Attribution.ContentTemplate = Attribution.Resources["OpenCycleMapAttribution"] as DataTemplate;
+                    Attribution.Visibility = Visibility.Visible;
                     break;
                 //Cloudmade Midnight Commander
                 case 3:
                     webTiledLayer.TemplateUri = "http://{subDomain}.tile.cloudmade.com/1a1b06b230af4efdbb989ea99e9841af/999/256/{level}/{col}/{row}.png";
                     webTiledLayer.SubDomains = new string[] { "a", "b", "c" };
                     webTiledLayer.CopyrightText = "Cloudmade Midnight Commander";
+                    Attribution.Visibility = Visibility.Collapsed; // No attribution info here, so keep dialog collapsed.
                     break;
                 //Cloudmade Pale Dawn
                 case 4:
                     webTiledLayer.TemplateUri = "http://{subDomain}.tile.cloudmade.com/1a1b06b230af4efdbb989ea99e9841af/998/256/{level}/{col}/{row}.png";
                     webTiledLayer.SubDomains = new string[] { "a", "b", "c" };
                     webTiledLayer.CopyrightText = "Cloudmade Pale Dawn";
+                    Attribution.Visibility = Visibility.Collapsed; // No attribution info here, so keep dialog collapsed.
                     break;
                 //MapBox Dark
                 case 5:
                     webTiledLayer.TemplateUri = "http://{subDomain}.tiles.mapbox.com/v3/examples.map-cnkhv76j/{level}/{col}/{row}.png";
                     webTiledLayer.SubDomains = new string[] { "a", "b", "c", "d" };
                     webTiledLayer.CopyrightText = "Mapbox Dark";
-                    break;
-                //Mapbox Streets
-                case 6:
-                    webTiledLayer.TemplateUri = "http://{subDomain}.tiles.mapbox.com/v3/examples.map-vyofok3q/{level}/{col}/{row}.png";
-                    webTiledLayer.SubDomains = new string[] { "a", "b", "c", "d" };
-                    webTiledLayer.CopyrightText = "Mapbox Streets";
+                    Attribution.ContentTemplate = Attribution.Resources["MapboxAttribution"] as DataTemplate;
+                    Attribution.Visibility = Visibility.Visible;
                     break;
                 //Mapbox Terrain
-                case 7:
+                case 6:
                     webTiledLayer.TemplateUri = "http://{subDomain}.tiles.mapbox.com/v3/mapbox.mapbox-warden/{level}/{col}/{row}.png";
                     webTiledLayer.SubDomains = new string[] { "a", "b", "c", "d" };
                     webTiledLayer.CopyrightText = "Mapbox Terrain";
-                    break;
-                //Apple's OpenStreetMap
-                case 8:
-                    webTiledLayer.TemplateUri = "http://gsp2.apple.com/tile?api=1&style=slideshow&layers=default&lang=en_GB&z={level}&x={col}&y={row}&v=9";
-                    webTiledLayer.CopyrightText = "Apple's rendering of OSM data.";
+                    Attribution.ContentTemplate = Attribution.Resources["MapboxAttribution"] as DataTemplate;
+                    Attribution.Visibility = Visibility.Visible;
                     break;
                 //Stamen Terrain
-                case 9:
+                case 7:
                     webTiledLayer.TemplateUri = "http://{subDomain}.tile.stamen.com/terrain/{level}/{col}/{row}.jpg";
                     webTiledLayer.SubDomains = new string[] { "a", "b", "c", "d" };
                     webTiledLayer.CopyrightText = "Stamen Terrain";
+                    Attribution.ContentTemplate = Attribution.Resources["StamenOtherAttribution"] as DataTemplate;
+                    Attribution.Visibility = Visibility.Visible;
                     break;
                 //Stamen Watercolor
-                case 10:
+                case 8:
                     webTiledLayer.TemplateUri = "http://{subDomain}.tile.stamen.com/watercolor/{level}/{col}/{row}.jpg";
                     webTiledLayer.SubDomains = new string[] { "a", "b", "c", "d" };
                     webTiledLayer.CopyrightText = "Stamen Watercolor";
+                    Attribution.ContentTemplate = Attribution.Resources["StamenOtherAttribution"] as DataTemplate;
+                    Attribution.Visibility = Visibility.Visible;
                     break;
                 //Stamen Toner
-                case 11:
+                case 9:
                     webTiledLayer.TemplateUri = "http://{subDomain}.tile.stamen.com/toner/{level}/{col}/{row}.png";
                     webTiledLayer.SubDomains = new string[] { "a", "b", "c", "d" };
                     webTiledLayer.CopyrightText = "Stamen Toner";
+                    Attribution.ContentTemplate = Attribution.Resources["StamenTonerAttribution"] as DataTemplate;
+                    Attribution.Visibility = Visibility.Visible;
                     break;
             }
-			MyMapView.Map.Layers.Add(webTiledLayer);
+
+            MyMapView.Map.Layers.Add(webTiledLayer);
+        }
+
+        private async void Hyperlink_Click(Windows.UI.Xaml.Documents.Hyperlink sender, Windows.UI.Xaml.Documents.HyperlinkClickEventArgs args)
+        {
+            // Navigate to specified Uri
+            await Launcher.LaunchUriAsync(sender.NavigateUri);
         }
     }
 }

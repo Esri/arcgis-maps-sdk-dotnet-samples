fbeefe6dab76fa9e8cf60ede8c2d787184e42d4e
Katy Dalton <kdalton@esri.com>
2014-11-24 12:46:34 -0700

    Removed unused usings.

--- src/Phone/ArcGISRuntimeSDKDotNet_PhoneSamples/Samples/GeocodingTasks/FindPlace.xaml.cs
+++ src/Phone/ArcGISRuntimeSDKDotNet_PhoneSamples/Samples/GeocodingTasks/FindPlace.xaml.cs
@@ -4,23 +4,11 @@ using Esri.ArcGISRuntime.Layers;
 using Esri.ArcGISRuntime.Symbology;
 using Esri.ArcGISRuntime.Tasks.Geocoding;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
-using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 namespace ArcGISRuntimeSDKDotNet_PhoneSamples.Samples
 {

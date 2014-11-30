24d90c8f3071c2293cf7adc99a1f38c9ea8b66e2
Katy Dalton <kdalton@esri.com>
2014-11-29 18:01:05 -0700

    Phone: CsvLayerSample - updates to xmlns and general spacing.

--- src/Phone/ArcGISRuntimeSDKDotNet_PhoneSamples/Samples/GraphicsLayers/CsvLayerSimple.xaml.cs
+++ src/Phone/ArcGISRuntimeSDKDotNet_PhoneSamples/Samples/GraphicsLayers/CsvLayerSimple.xaml.cs
@@ -3,19 +3,16 @@
 
 namespace ArcGISRuntimeSDKDotNet_PhoneSamples.Samples
 {
-
 	/// <summary>
-	/// 
+	/// This sample demonstrates defining a CsvLayer in XAML and setting the serviceUri property to a .csv file hosted online by the United States Geological Survey (USGS).
 	/// </summary>
 	/// <title>CSV Layer</title>
-    /// <category>Graphics Layers</category>
+	/// <category>Graphics Layers</category>
 	public sealed partial class CsvLayerSimple : Page
     {
         public CsvLayerSimple()
         {
             this.InitializeComponent();
         }
-
-
     }
 }

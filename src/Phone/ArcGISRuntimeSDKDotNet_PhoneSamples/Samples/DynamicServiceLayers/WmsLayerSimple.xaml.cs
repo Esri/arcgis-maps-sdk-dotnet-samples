cc2c2a5b0bd0d8e92ca4900166ed2a965cfa2c4a
Katy Dalton <kdalton@esri.com>
2015-01-09 10:40:15 -0700

    Phone: Updated summary for WMSLayer sample.

--- src/Phone/ArcGISRuntimeSDKDotNet_PhoneSamples/Samples/DynamicServiceLayers/WmsLayerSimple.xaml.cs
+++ src/Phone/ArcGISRuntimeSDKDotNet_PhoneSamples/Samples/DynamicServiceLayers/WmsLayerSimple.xaml.cs
@@ -5,16 +5,16 @@ using Windows.UI.Xaml.Controls;
 namespace ArcGISRuntimeSDKDotNet_PhoneSamples.Samples
 {
 	/// <summary>
-	/// 
+	/// This sample demonstrates displaying Open GIS Consortium (OGC) WMS layer on a map.
 	/// </summary>
 	/// <title>WMS Layer</title>
-    /// <category>Dynamic Service Layers</category>
+	/// <category>Dynamic Service Layers</category>
 	public sealed partial class WmsLayerSimple : Page
-    {
-        public WmsLayerSimple()
-        {
-            this.InitializeComponent();
+	{
+		public WmsLayerSimple()
+		{
+			this.InitializeComponent();
 			MyMapView.Map.InitialViewpoint = new Viewpoint(new Envelope(-15000000, 2000000, -7000000, 8000000));
-        }
-    }
+		}
+	}
 }

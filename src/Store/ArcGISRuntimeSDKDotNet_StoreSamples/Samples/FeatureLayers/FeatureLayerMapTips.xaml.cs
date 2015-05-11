bf5b0b894fc5a426360077f36f22a5f3410ea83f
Katy Dalton <kdalton@esri.com>
2015-05-11 15:21:37 -0600

    Update FeatureLayerMapTips sample to use Naperville Shelters FeatureService.

--- src/Store/ArcGISRuntimeSDKDotNet_StoreSamples/Samples/FeatureLayers/FeatureLayerMapTips.xaml.cs
+++ src/Store/ArcGISRuntimeSDKDotNet_StoreSamples/Samples/FeatureLayers/FeatureLayerMapTips.xaml.cs
@@ -9,29 +9,29 @@ using Windows.UI.Xaml.Input;
 
 namespace ArcGISRuntimeSDKDotNet_StoreSamples.Samples
 {
-    /// <summary>
-    /// Demonstrates hit testing in the PointerMoved event of the map view to display feature map tips.
-    /// </summary>
-    /// <title>Map Tips</title>
-    /// <category>Feature Layers</category>
-    public sealed partial class FeatureLayerMapTips : Page
+	/// <summary>
+	/// Demonstrates hit testing in the PointerMoved event of the map view to display feature map tips.
+	/// </summary>
+	/// <title>Map Tips</title>
+	/// <category>Feature Layers</category>
+	public sealed partial class FeatureLayerMapTips : Page
 	{
 		private FeatureLayer _featureLayer;
 		private bool _isMapReady;
 		private FrameworkElement _mapTip;
 
-        public FeatureLayerMapTips()
+		public FeatureLayerMapTips()
 		{
 			this.InitializeComponent();
 
-            _featureLayer = MyMapView.Map.Layers["FeatureLayer"] as FeatureLayer;
-            ((ServiceFeatureTable)_featureLayer.FeatureTable).OutFields = OutFields.All;
+			_featureLayer = MyMapView.Map.Layers["FeatureLayer"] as FeatureLayer;
+			((ServiceFeatureTable)_featureLayer.FeatureTable).OutFields = OutFields.All;
 
 			_mapTip = MyMapView.Overlays.Items[0] as FrameworkElement;
 
 			MyMapView.SpatialReferenceChanged += MyMapView_SpatialReferenceChanged;
 			MyMapView.PointerMoved += MyMapView_PointerMoved;
-        }
+		}
 
 		private async void MyMapView_SpatialReferenceChanged(object sender, System.EventArgs e)
 		{
@@ -39,34 +39,34 @@ namespace ArcGISRuntimeSDKDotNet_StoreSamples.Samples
 			_isMapReady = true;
 		}
 
-        private async void MyMapView_PointerMoved(object sender, PointerRoutedEventArgs e)
-        {
+		private async void MyMapView_PointerMoved(object sender, PointerRoutedEventArgs e)
+		{
 			if (!_isMapReady)
 				return;
 			
 			try
-            {
+			{
 				_isMapReady = false;
 
-                Point screenPoint = e.GetCurrentPoint(MyMapView).Position;
-                var rows = await _featureLayer.HitTestAsync(MyMapView, screenPoint);
-                if (rows != null && rows.Length > 0)
-                {
-                    var features = await _featureLayer.FeatureTable.QueryAsync(rows);
+				Point screenPoint = e.GetCurrentPoint(MyMapView).Position;
+				var rows = await _featureLayer.HitTestAsync(MyMapView, screenPoint);
+				if (rows != null && rows.Length > 0)
+				{
+					var features = await _featureLayer.FeatureTable.QueryAsync(rows);
 					_mapTip.DataContext = features.FirstOrDefault();
-                    _mapTip.Visibility = Visibility.Visible;
-                }
-                else
-                    _mapTip.Visibility = Visibility.Collapsed;
-            }
-            catch
-            {
-                _mapTip.Visibility = Visibility.Collapsed;
-            }
+					_mapTip.Visibility = Visibility.Visible;
+				}
+				else
+					_mapTip.Visibility = Visibility.Collapsed;
+			}
+			catch
+			{
+				_mapTip.Visibility = Visibility.Collapsed;
+			}
 			finally
 			{
 				_isMapReady = true;
 			}
-        }
-    }
+		}
+	}
 }

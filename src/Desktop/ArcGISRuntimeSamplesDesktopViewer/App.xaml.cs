1f40e638f58c25151d20d10fdda6ae1ab1f1398a
Antti Kajanus <akajanus@esri.com>
2015-06-25 10:14:58 +0100

    Added UnhandledException handler.

--- src/Desktop/ArcGISRuntimeSamplesDesktopViewer/App.xaml.cs
+++ src/Desktop/ArcGISRuntimeSamplesDesktopViewer/App.xaml.cs
@@ -1,11 +1,15 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 
-namespace ArcGISRuntime.Samples.DesktopViewerArcGISRuntime
+namespace ArcGISRuntime.Samples.DesktopViewer
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+    	private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+		{
+            Debug.WriteLine("An unhandled exception occured!");
+            Debug.WriteLine(string.Format("Error : {0}", e.Exception));
+            MessageBox.Show(string.Format(e.Exception.Message), "An exception occured");
+		}
+	}
 }

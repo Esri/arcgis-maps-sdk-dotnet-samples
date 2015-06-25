9d44d3279eed772a023727081dcacf6fbf82231f
Katy Dalton <kdalton@esri.com>
2015-06-25 10:33:05 -0600

    Update code and comments. Don't think we really need to account for if pitch is low in this scenario.

--- src/Desktop/ArcGISRuntimeSamplesDesktop/Samples/Scene/FollowTarget.xaml.cs
+++ src/Desktop/ArcGISRuntimeSamplesDesktop/Samples/Scene/FollowTarget.xaml.cs
@@ -22,13 +22,14 @@ namespace ArcGISRuntime.Samples.Desktop
 		public FollowTarget()
 		{
 			InitializeComponent();
+
 			_timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(sldRefresh.Value) };
-			_timer.Tick += _timer_Tick;
+			_timer.Tick += timerTick;
 
 			MySceneView.GraphicsOverlays["TargetOverlay"].Graphics.Add(new Graphic(_point));
 		}
 
-		void _timer_Tick(object sender, EventArgs e)
+		void timerTick(object sender, EventArgs e)
 		{
 			_point = new MapPoint(_point.X + sldSpeed.Value / 10000, _point.Y, _point.Z);
 			MySceneView.GraphicsOverlays["TargetOverlay"].Graphics[0].Geometry = _point;
@@ -37,6 +38,7 @@ namespace ArcGISRuntime.Samples.Desktop
 
 		private void sldInputs_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
 		{
+			// Update the FollowGraphic method as the slider inputs are changed
 			if (sldHeading != null && sldPitch != null && sldDistance != null)
 				FollowGraphic(_point, sldHeading.Value, sldPitch.Value, sldDistance.Value);
 		}
@@ -45,18 +47,14 @@ namespace ArcGISRuntime.Samples.Desktop
 		{
 			try
 			{
+				// Create a new Camera with the specified point and distance values
 				var cam = new Camera(new MapPoint(point.X, point.Y, point.Z + distance), 0, 0);
-				if (pitch > 7.5)
-				{
-					cam = cam.RotateAround(point, heading, pitch - 7.5);
-					cam = new Camera(cam.Location, cam.Heading, cam.Pitch + 7.5);
-				}
-				else
-				{
-					cam = cam.RotateAround(point, heading, 0);
-					cam = new Camera(cam.Location, cam.Heading, cam.Pitch + pitch);
-				}
 
+				// Set the camera to RotateAround the specified point with the user-specified values.
+				// RotateAround returns a new Camera with changes centered at the specified location.
+				cam = cam.RotateAround(point, heading, pitch);
+
+				// Set the SceneView to center the view on the camera's perspective
 				await MySceneView.SetViewAsync(cam, TimeSpan.FromMilliseconds(1), false);
 			}
 			catch (Exception ex)

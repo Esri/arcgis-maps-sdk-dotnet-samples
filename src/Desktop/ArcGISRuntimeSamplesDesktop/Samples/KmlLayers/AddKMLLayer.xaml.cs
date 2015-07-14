777debabb1d7af87ff717c29e51ea89c2b47ca19
Asser Swelam <aswelam@esri.com>
2015-07-13 18:39:26 -0700

    Add Some KML 2D and 3D samples

---
+++ src/Desktop/ArcGISRuntimeSamplesDesktop/Samples/KmlLayers/AddKMLLayer.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Esri.ArcGISRuntime.Layers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArcGISRuntime.Samples.Desktop.Samples.KmlLayers
+{
+    /// <summary>
+    /// This sample show you how to add online kml layer to the map
+    /// </summary>
+    /// <title>Add online KML Layer</title>
+    /// <category>Layers</category>
+    /// <subcategory>Kml Layers</subcategory>
+    public partial class AddKMLLayer : UserControl
+    {
+        public AddKMLLayer()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Desktop/ArcGISRuntimeSamplesDesktop/Samples/Scene/KMLLayerSample3D.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArcGISRuntime.Samples.Desktop.Samples.Scene
+{
+    /// <summary>
+    /// Demonstrates adding a KML to a Scene in XAML.
+    /// </summary>
+    /// <title>3D KML Layer</title>
+    /// <category>Scene</category>
+    /// <subcategory>Layers</subcategory>
+    public partial class KMLLayerSample3D : UserControl
+    {
+        public KMLLayerSample3D()
+        {
+            InitializeComponent();
+        }
+    }
+}

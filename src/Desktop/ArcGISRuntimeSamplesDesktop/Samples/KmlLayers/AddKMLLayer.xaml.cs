3d10e23e6a96d9b3f0159745a3af643ec936126b
Katy Dalton <kdalton@esri.com>
2015-07-15 13:33:54 -0600

    Minor updates to kml samples.

--- src/Desktop/ArcGISRuntimeSamplesDesktop/Samples/KmlLayers/AddKMLLayer.xaml.cs
+++ src/Desktop/ArcGISRuntimeSamplesDesktop/Samples/KmlLayers/AddKMLLayer.xaml.cs
@@ -4,18 +4,17 @@ using Esri.ArcGISRuntime.Layers;
 
 namespace ArcGISRuntime.Samples.Desktop
 {
-    /// <summary>
-    /// This sample demonstrates adding online kml layer to the map
-    /// </summary>
-    /// <title>Add online KML Layer</title>
-    /// <category>Layers</category>
-    /// <subcategory>Kml Layers</subcategory>
-    public partial class AddKMLLayer : UserControl
-    {
-        /// <summary>Construct adding online kml sample control</summary>
-        public AddKMLLayer()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// This sample demonstrates adding an online KML layer to the map
+	/// </summary>
+	/// <title>Add online KML Layer</title>
+	/// <category>Layers</category>
+	/// <subcategory>Kml Layers</subcategory>
+	public partial class AddKMLLayer : UserControl
+	{
+		public AddKMLLayer()
+		{
+			InitializeComponent();
+		}
+	}
 }

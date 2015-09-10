843756ebea34e435fb1b4af21ed3eda3b86fb5c3
Katy Dalton <kdalton@esri.com>
2015-09-10 14:12:04 -0600

    Remove the TokenSecuredServices sample; Add the placeholder page.

---
+++ src/Store/ArcGISRuntimeSamplesStore/Samples/Security/SecuritySamples.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Esri.ArcGISRuntime.Controls;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using Windows.UI.Xaml.Controls;
+
+namespace ArcGISRuntime.Samples.Store.Samples
+{
+	/// <summary>
+	/// This page contains instructions for accessing the Security samples
+	/// </summary>
+	/// <title>Security samples</title>
+	/// <category>Security</category>
+	public partial class SecuritySamples : Page
+	{
+		public SecuritySamples()
+		{
+			InitializeComponent();
+		}
+	}
+}

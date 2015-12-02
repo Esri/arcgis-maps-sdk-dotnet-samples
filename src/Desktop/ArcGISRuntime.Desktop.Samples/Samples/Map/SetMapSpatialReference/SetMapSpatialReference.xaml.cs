14d53404f6f054b0dc162827e308da75922c331b
macca400 <smcilrs73@hotmail.com>
2015-12-02 12:23:57 +0000

    CR fixes

--- src/Desktop/ArcGISRuntime.Desktop.Samples/Samples/Map/SetMapSpatialReference/SetMapSpatialReference.xaml.cs
+++ src/Desktop/ArcGISRuntime.Desktop.Samples/Samples/Map/SetMapSpatialReference/SetMapSpatialReference.xaml.cs
@@ -29,7 +29,7 @@ namespace ArcGISRuntime.Desktop.Samples.SetMapSpatialReference
             LoadMap();
         }
 
-        private async void LoadMap()
+        private void LoadMap()
         {
             try
             {

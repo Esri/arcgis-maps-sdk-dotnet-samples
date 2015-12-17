df18f966efcd4f4a6d0e93d1542418e8cf1b4243
macca400 <smcilrs73@hotmail.com>
2015-12-17 11:12:00 +0000

    Cr fixes

--- src/Desktop/ArcGISRuntime.Desktop.Samples/Samples/Map/OpenExistingMap/OpenExistingMap.xaml.cs
+++ src/Desktop/ArcGISRuntime.Desktop.Samples/Samples/Map/OpenExistingMap/OpenExistingMap.xaml.cs
@@ -23,32 +23,35 @@ namespace ArcGISRuntime.Desktop.Samples.OpenExistingMap
 {
     public partial class OpenExistingMap
     {
+        // Below are three URLs corresponding to Portal Items. In this example the items are Maps. 
         private string itemURL1 = "http://www.arcgis.com/home/item.html?id=2d6fa24b357d427f9c737774e7b0f977";
         private string itemURL2 = "http://www.arcgis.com/home/item.html?id=01f052c8995e4b9e889d73c3e210ebe3";
         private string itemURL3 = "http://www.arcgis.com/home/item.html?id=74a8f6645ab44c4f82d537f1aa0e375d";
 
+        // Corresponding Title strings for the itemUrls.  
         private string title1 = "Housing with Mortgages";
         private string title2 = "USA Tapestry Segmentation";
         private string title3 = "Geology of United States";
 
-        //Construct Load Map sample control
+        // Construct Load Map sample control.
         public OpenExistingMap()
         {
             InitializeComponent();
 
-            Loaded += LoadMap_Loaded;
+            Loaded += OnLoaded;
         }
 
-        // Loads UI elements and an initial Map
-        private async void LoadMap_Loaded(object sender, RoutedEventArgs e)
+        // Loads UI elements and an initial Map.
+        private async void OnLoaded(object sender, RoutedEventArgs e)
         {
+            // Adding items' Titles and URLs to a collection that will be used to populate the combobox's drop down. 
             ICollection<KeyValuePair<String, String>> comboBoxContent =
-            new Dictionary<String, String>()
-            {
-                { title1, itemURL1 },
-                { title2, itemURL2 },
-                { title3, itemURL3 }
-            };
+                new Dictionary<String, String>()
+                {
+                    { title1, itemURL1 },
+                    { title2, itemURL2 },
+                    { title3, itemURL3 }
+                };
 
             try
             {
@@ -63,43 +66,42 @@ namespace ArcGISRuntime.Desktop.Samples.OpenExistingMap
             }
         }
 
-        // Loads a webmap on load button click
-        private async void OnLoadButton_Click(object sender, RoutedEventArgs e)
+        // Loads a webmap on load button click.
+        private async void OnLoadButtonClicked(object sender, RoutedEventArgs e)
         {
             string url = string.Empty;
             if (comboMap.SelectedIndex >= 0)
             {
-                url = comboMap.SelectedValue as string;
-                await LoadMapAsync(url);
+                try
+                {
+                    url = comboMap.SelectedValue as string;
+                    await LoadMapAsync(url);
+                }
+                catch(Exception ex)
+                {
+                    var errorMessage = "Map cannot be loaded." + ex.Message;
+                    MessageBox.Show(errorMessage, "Sample error");
+                }             
             }
         }
 
-        // Loads the given map
+        // Loads the given map.
         private async Task LoadMapAsync(string mapUrl)
         {
-            try
-            {
-                progress.Visibility = Visibility.Visible;
+            progress.Visibility = Visibility.Visible;
 
-                //Initialize map from a portal item URI and load map into MapView 
-                var map = new Map(new Uri(mapUrl));
-                await map.LoadAsync();
-                MyMapView.Map = map;
-               
-                //Get map's info to populate "Map Details" UI element          
-                var item = MyMapView.Map.PortalItem;               
-                detailsPanel.DataContext = item; 
-            }
-            catch (Exception ex)
-            {
-                var errorMessage = "Map cannot be loaded." + ex.Message;
-                MessageBox.Show(errorMessage, "Sample error");
-            }
-            finally
-            {
-                detailsPanel.Visibility = Visibility.Visible;
-                progress.Visibility = Visibility.Hidden;
-            }
+            // Initialize map from a portal item URI and load map into MapView. 
+            var map = new Map(new Uri(mapUrl));
+            // Await LoadAsync so all properties of map will definitely be loaded when interrogated later. i.e Map.PortalItem. 
+            await map.LoadAsync();
+            MyMapView.Map = map;
+            
+            // Get map's info to populate "Map Details" UI element.          
+            var item = MyMapView.Map.PortalItem;             
+            detailsPanel.DataContext = item; 
+  
+            detailsPanel.Visibility = Visibility.Visible;
+            progress.Visibility = Visibility.Hidden;
         }
     }
 }

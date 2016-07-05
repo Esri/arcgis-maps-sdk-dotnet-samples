237e0bb3092eb6901c170501c0c0bde371a23f67
don1760 <dkemlage@esri.com>
2016-07-05 10:45:12 -0700

    Minor updates to the Quartz Beta 1 ArcGISVectorTiledLayerUrl (WPF) sample.

--- src/Desktop/ArcGISRuntime.Desktop.Samples/Samples/Layers/ArcGISVectorTiledLayerUrl/ArcGISVectorTiledLayerUrlVB.xaml.vb
+++ src/Desktop/ArcGISRuntime.Desktop.Samples/Samples/Layers/ArcGISVectorTiledLayerUrl/ArcGISVectorTiledLayerUrlVB.xaml.vb
@@ -11,7 +11,9 @@ Imports Esri.ArcGISRuntime.Mapping
 Imports System.Windows.Controls
 
 Namespace ArcGISVectorTiledLayerUrl
+
     Partial Public Class ArcGISVectorTiledLayerUrlVB
+
         Private _navigationUrl As String = "http://www.arcgis.com/sharing/rest/content/items/00cd8e843bae49b3a040423e5d65416b/resources/styles/root.json"
         Private _streetUrl As String = "http://www.arcgis.com/sharing/rest/content/items/3b8814f6ddbd485cae67e8018992246e/resources/styles/root.json"
         Private _nightUrl As String = "http://www.arcgis.com/sharing/rest/content/items/f96366254a564adda1dc468b447ed956/resources/styles/root.json"
@@ -24,14 +26,17 @@ Namespace ArcGISVectorTiledLayerUrl
         Private _vectorLayerNames As String() = New String() {"Topo", "Streets", "Night", "Navigation"}
 
         Public Sub New()
+
             InitializeComponent()
 
             ' Create the UI, setup the control references and execute initialization 
             Initialize()
+
         End Sub
 
         Private Sub Initialize()
-            ' Create a new ArcGISVectorTiledLayer with the navigation serice Url
+
+            ' Create a new ArcGISVectorTiledLayer with the navigation service Url
             _vectorTiledLayer = New ArcGISVectorTiledLayer(New Uri(_navigationUrl))
 
             ' Create new Map with basemap
@@ -42,38 +47,49 @@ Namespace ArcGISVectorTiledLayerUrl
 
             ' Assign the map to the MapView
             MyMapView.Map = myMap
+
         End Sub
 
         Private Sub OnVectorLayersChooserSelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+
             Dim selectedVectorLayer = e.AddedItems(0).ToString()
 
             Select Case selectedVectorLayer
+
                 Case "Topo"
+
                     _vectorTiledLayerUrl = _topographicUrl
                     Exit Select
 
                 Case "Streets"
+
                     _vectorTiledLayerUrl = _streetUrl
                     Exit Select
 
                 Case "Night"
+
                     _vectorTiledLayerUrl = _nightUrl
                     Exit Select
 
                 Case "Navigation"
+
                     _vectorTiledLayerUrl = _navigationUrl
                     Exit Select
+
                 Case Else
 
                     Exit Select
+
             End Select
 
             ' Create a new ArcGISVectorTiledLayer with the Url Selected by the user
             _vectorTiledLayer = New ArcGISVectorTiledLayer(New Uri(_vectorTiledLayerUrl))
 
-            ' // Create new Map with basemap and assing to the Mapviews Map
+            ' // Create new Map with basemap and assigning to the Mapviews Map
             MyMapView.Map = New Map(New Basemap(_vectorTiledLayer))
 
         End Sub
+
     End Class
+
 End Namespace

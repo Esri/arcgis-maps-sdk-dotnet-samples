96da6bfbb896fa21b3dae54eafd3d4dea7f10391
Thad Tilton <ttilton@esri.com>
2016-07-26 15:54:26 -0700

    UWP OAuth samples for C# and VB

---
+++ src/Windows/ArcGISRuntime.Windows.Samples/Samples/Security/OAuth/MainPage.xaml.cs
@@ -0,0 +1,124 @@
+﻿using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Portal;
+using Esri.ArcGISRuntime.Security;
+using System;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+
+namespace OAuth
+{
+    public sealed partial class MainPage : Page
+    {
+        // Constants for OAuth-related values ...
+        // URL of the server to authenticate with
+        private const string ServerUrl = "https://www.arcgis.com/sharing/rest";
+        // TODO: Provide the client ID for your app (registered with the server)
+        private const string ClientId = "";
+        // TODO: [optional] Provide the client secret for the app (only needed for the OAuthAuthorizationCode auth type)
+        private const string ClientSecret = "";
+        // TODO: Provide a URL registered for the app for redirecting after a successful authorization
+        private const string RedirectUrl = "http://my.redirect.url";
+        // TODO: Provide an ID for a secured web map item hosted on the server
+        private const string WebMapId = "";
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Call a function to set up the AuthenticationManager
+            UpdateAuthenticationManager();
+
+            // Call a function to display a secured web map (will be challenged to log in)
+            DisplayWebMap();
+        }
+        
+        private void UpdateAuthenticationManager()
+        {
+            // Register the server information with the AuthenticationManager
+            Esri.ArcGISRuntime.Security.ServerInfo serverInfo = new ServerInfo
+            {
+                ServerUri = new Uri(ServerUrl),
+                OAuthClientInfo = new OAuthClientInfo
+                {
+                    ClientId = ClientId,
+                    RedirectUri = new Uri(RedirectUrl)
+                }
+            };
+
+            // If a client secret has been configured, set the authentication type to OAuthAuthorizationCode
+            if(!string.IsNullOrEmpty(ClientSecret))
+            {
+                // Use OAuthAuthorizationCode if you need a refresh token (and have specified a valid client secret)
+                serverInfo.TokenAuthenticationType = TokenAuthenticationType.OAuthAuthorizationCode;
+                serverInfo.OAuthClientInfo.ClientSecret = ClientSecret;
+            }
+            else
+            {
+                // Otherwise, use OAuthImplicit
+                serverInfo.TokenAuthenticationType = TokenAuthenticationType.OAuthImplicit;
+            }
+
+            // Register this server with AuthenticationManager
+            AuthenticationManager.Current.RegisterServer(serverInfo);
+            
+            // Use a function in this class to challenge for credentials
+            AuthenticationManager.Current.ChallengeHandler = new ChallengeHandler(CreateCredentialAsync);
+        }
+        
+        private async void DisplayWebMap()
+        {
+            // Display a web map hosted in a portal. If the web map item is secured, AuthenticationManager will
+            // challenge for credentials
+            try
+            {
+                // Connect to a portal (ArcGIS Online, for example)
+                var arcgisPortal = await ArcGISPortal.CreateAsync(new Uri(ServerUrl));
+                // Get a web map portal item using its ID
+                // If the item is secured (not shared publicly) the user will be challenged for credentials at this point
+                var portalItem = await ArcGISPortalItem.CreateAsync(arcgisPortal, WebMapId);
+                // Create a new map with the portal item
+                var myMap = new Map(portalItem);
+
+                // Assign the map to the MapView.Map property to display it in the app
+                MyMapView.Map = myMap;
+                await myMap.RetryLoadAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageDialog dlg = new MessageDialog(ex.Message);
+                await dlg.ShowAsync();
+            }
+        }
+        
+        public async Task<Credential> CreateCredentialAsync(CredentialRequestInfo info)
+        {
+            // ChallengeHandler function for AuthenticationManager that will be called whenever access to a secured
+            // resource is attempted
+            OAuthTokenCredential credential = null;
+            
+            try
+            {
+                // Create generate token options if necessary
+                if (info.GenerateTokenOptions == null)
+                {
+                    info.GenerateTokenOptions = new GenerateTokenOptions();
+                }
+
+                // AuthenticationManager will handle challenging the user for credentials
+                credential = await AuthenticationManager.Current.GenerateCredentialAsync
+                    (
+                            info.ServiceUri,
+                            info.GenerateTokenOptions
+                    ) as OAuthTokenCredential;
+            }
+            catch (Exception ex)
+            {
+                // Exception will be reported in calling function
+                throw (ex);
+            }
+
+            return credential;
+        }
+    }
+}

f3188ce830b38d772a7d225720da275aeea8594a
Thad Tilton <ttilton@esri.com>
2016-09-06 16:56:31 -0700

    Update IntegratedWindowsAuthVB.xaml.vb

--- src/Windows/ArcGISRuntime.Windows.Samples/Samples/Security/IntegratedWindowsAuthVB/IntegratedWindowsAuthVB.xaml.vb
+++ src/Windows/ArcGISRuntime.Windows.Samples/Samples/Security/IntegratedWindowsAuthVB/IntegratedWindowsAuthVB.xaml.vb
@@ -1,4 +1,11 @@
-﻿' The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+' Copyright 2016 Esri.
+'
+' Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+' You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+'
+' Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+' "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+' language governing permissions and limitations under the License.
 
 Imports System.Net
 Imports System.Text
@@ -8,9 +15,7 @@ Imports Esri.ArcGISRuntime.Security
 Imports Esri.ArcGISRuntime.UI
 Imports Windows.ApplicationModel.Core
 Imports Windows.UI.Core
-''' <summary>
-''' An empty page that can be used on its own or navigated to within a Frame.
-''' </summary>
+
 Public NotInheritable Class MainPage
     Inherits Page
     ' TODO - Add the URL for your IWA-secured portal

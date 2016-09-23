095b9ce4abfacc3bb17ff37543722449f9386a6f
thad <ttilton@esri.com>
2016-09-23 11:43:25 -0700

    OAuth updates for UWP (VB)

--- src/UWP/ArcGISRuntime.UWP.Samples/Samples/Security/OAuthVB/OAuth.xaml.vb
+++ src/UWP/ArcGISRuntime.UWP.Samples/Samples/Security/OAuthVB/OAuth.xaml.vb
@@ -1,4 +1,13 @@
-﻿Imports Esri.ArcGISRuntime.Mapping
+﻿' Copyright 2016 Esri.
+'
+' Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+' You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+'
+' Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+' "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+' language governing permissions and limitations under the License.
+
+Imports Esri.ArcGISRuntime.Mapping
 Imports Esri.ArcGISRuntime.Portal
 Imports Esri.ArcGISRuntime.Security
 Imports Windows.UI.Popups
@@ -48,7 +57,7 @@ Public NotInheritable Class MainPage
 
         ' If a client secret has been configured, set the authentication type to OAuthAuthorizationCode
         If Not String.IsNullOrEmpty(ClientSecret) Then
-            ' Use OAuthAuthorizationCode if you need a refresh token (And have specified a valid client secret)
+            ' Use OAuthAuthorizationCode if you need a refresh token (and have specified a valid client secret)
             serverInfo.TokenAuthenticationType = TokenAuthenticationType.OAuthAuthorizationCode
             serverInfo.OAuthClientInfo.ClientSecret = ClientSecret
         Else
@@ -62,22 +71,22 @@ Public NotInheritable Class MainPage
         ' Use a function in this class to challenge for credentials
         AuthenticationManager.Current.ChallengeHandler = New ChallengeHandler(AddressOf CreateCredentialAsync)
 
-        ' Note In a WPF app, you need to associate a custom IOAuthAuthorizeHandler component with the AuthenticationManager to 
+        ' Note: In a WPF app, you need to associate a custom IOAuthAuthorizeHandler component with the AuthenticationManager to 
         '     handle showing OAuth login controls (AuthenticationManager.Current.OAuthAuthorizeHandler = New MyOAuthAuthorize()).
         '     The UWP AuthenticationManager, however, uses a built-in IOAuthAuthorizeHandler (based on WebAuthenticationBroker).
     End Sub
 
     Private Async Sub DisplayWebMap()
-        ' Display a web map hosted in a portal. If the web map item Is secured, AuthenticationManager will
+        ' Display a web map hosted in a portal. If the web map item is secured, AuthenticationManager will
         ' challenge for credentials
         Try
             ' Connect to a portal (ArcGIS Online, for example)
             Dim arcgisPortal As ArcGISPortal = Await ArcGISPortal.CreateAsync(New Uri(ServerUrl))
             ' Get a web map portal item using its ID
-            ' If the item Is secured (Not shared publicly) the user will be challenged for credentials at this point
-            Dim portalItem As ArcGISPortalItem = Await ArcGISPortalItem.CreateAsync(arcgisPortal, WebMapId)
+            ' If the item is secured (not shared publicly) the user will be challenged for credentials at this point
+            Dim portalItm As PortalItem = Await PortalItem.CreateAsync(arcgisPortal, WebMapId)
             ' Create a New map with the portal item
-            Dim myMap As Map = New Map(portalItem)
+            Dim myMap As Map = New Map(portalItm)
 
             ' Assign the map to the MapView.Map property to display it in the app
             MyMapView.Map = myMap
@@ -90,7 +99,7 @@ Public NotInheritable Class MainPage
 
     Public Async Function CreateCredentialAsync(info As CredentialRequestInfo) As Task(Of Credential)
         ' ChallengeHandler function for AuthenticationManager that will be called whenever access to a secured
-        ' resource Is attempted
+        ' resource is attempted
         Dim credenshul As OAuthTokenCredential = Nothing
 
         Try

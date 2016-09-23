363182c6e9b69f3bf47e4445ec84b45d53b585a0
thad <ttilton@esri.com>
2016-09-23 11:38:45 -0700

    Updated UWP OAuth sample (C#)

--- src/UWP/ArcGISRuntime.UWP.Samples/Samples/Security/OAuth/OAuth.xaml.cs
+++ src/UWP/ArcGISRuntime.UWP.Samples/Samples/Security/OAuth/OAuth.xaml.cs
@@ -1,4 +1,13 @@
-﻿using Esri.ArcGISRuntime.Mapping;
+﻿// Copyright 2016 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Portal;
 using Esri.ArcGISRuntime.Security;
 using System;
@@ -84,9 +93,11 @@ namespace OAuth
             {
                 // Connect to a portal (ArcGIS Online, for example)
                 var arcgisPortal = await ArcGISPortal.CreateAsync(new Uri(ServerUrl));
+
                 // Get a web map portal item using its ID
                 // If the item is secured (not shared publicly) the user will be challenged for credentials at this point
-                var portalItem = await ArcGISPortalItem.CreateAsync(arcgisPortal, WebMapId);
+                var portalItem = await PortalItem.CreateAsync(arcgisPortal, WebMapId);
+
                 // Create a new map with the portal item
                 var myMap = new Map(portalItem);
 

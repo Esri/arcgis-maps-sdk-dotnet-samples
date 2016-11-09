be31bbcded4389f4c7ca03ee2602ff62fb7a22ad
Antti Kajanus <anttikajanus@users.noreply.github.com>
2016-11-08 16:24:09 -0800

    Updated vector tiles to use correct services. (#207)

--- src/Forms/Shared/Samples/Layers/ArcGISVectorTiledLayerUrl/ArcGISVectorTiledLayerUrl.xaml.cs
+++ src/Forms/Shared/Samples/Layers/ArcGISVectorTiledLayerUrl/ArcGISVectorTiledLayerUrl.xaml.cs
@@ -15,10 +15,10 @@ namespace ArcGISRuntimeXamarin.Samples.ArcGISVectorTiledLayerUrl
 {
     public partial class ArcGISVectorTiledLayerUrl : ContentPage
     {
-        private string _navigationUrl = "http://www.arcgis.com/sharing/rest/content/items/00cd8e843bae49b3a040423e5d65416b/resources/styles/root.json";
-        private string _streetUrl = "http://www.arcgis.com/sharing/rest/content/items/3b8814f6ddbd485cae67e8018992246e/resources/styles/root.json";
-        private string _nightUrl = "http://www.arcgis.com/sharing/rest/content/items/f96366254a564adda1dc468b447ed956/resources/styles/root.json";
-        private string _topographicUrl = "http://www.arcgis.com/sharing/rest/content/items/be44936bcdd24db588a1ae5076e36f34/resources/styles/root.json";
+        private string _navigationUrl = "http://www.arcgis.com/home/item.html?id=dcbbba0edf094eaa81af19298b9c6247";
+        private string _streetUrl = "http://www.arcgis.com/home/item.html?id=4e1133c28ac04cca97693cf336cd49ad";
+        private string _nightUrl = "http://www.arcgis.com/home/item.html?id=bf79e422e9454565ae0cbe9553cf6471";
+        private string _darkGrayUrl = "http://www.arcgis.com/home/item.html?id=850db44b9eb845d3bd42b19e8aa7a024";
 
         private string _vectorTiledLayerUrl;
         private ArcGISVectorTiledLayer _vectorTiledLayer;
@@ -26,7 +26,7 @@ namespace ArcGISRuntimeXamarin.Samples.ArcGISVectorTiledLayerUrl
         // String array to store some vector layer choices
         private string[] _vectorLayerNames = new string[]
         {
-            "Topo",
+            "Dark gray",
             "Streets",
             "Night",
             "Navigation"
@@ -64,8 +64,8 @@ namespace ArcGISRuntimeXamarin.Samples.ArcGISVectorTiledLayerUrl
 
             switch (selectedLayer)
             {
-                case "Topo":
-                    _vectorTiledLayerUrl = _topographicUrl;
+                case "Dark gray":
+                    _vectorTiledLayerUrl = _darkGrayUrl;
                     break;
 
                 case "Streets":

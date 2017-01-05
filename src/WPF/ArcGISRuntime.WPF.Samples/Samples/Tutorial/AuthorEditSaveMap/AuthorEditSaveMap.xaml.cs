4d99748e5b6f8d09f22329463d06d70f1bef3671
thad <ttilton@esri.com>
2017-01-05 14:00:56 -0800

    WPF version (C# and VB) of the author edit and save webmap tutorial

---
+++ src/WPF/ArcGISRuntime.WPF.Samples/Samples/Tutorial/AuthorEditSaveMap/AuthorEditSaveMap.xaml.cs
@@ -0,0 +1,469 @@
+﻿// Copyright 2016 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Portal;
+using Esri.ArcGISRuntime.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace ArcGISRuntime.WPF.Samples.AuthorEditSaveMap
+{
+    public partial class AuthorEditSaveMap
+    {
+        private MapViewModel _mapViewModel;
+
+        // Constants for OAuth-related values ...
+        // URL of the server to authenticate with (ArcGIS Online)
+        private const string ArcGISOnlineUrl = "https://www.arcgis.com/sharing/rest";
+
+        // Client ID for the app registered with the server (Portal Maps)
+        private const string AppClientId = "2Gh53JRzkPtOENQq";
+
+        // Redirect URL after a successful authorization (configured for the Portal Maps application)
+        private const string OAuthRedirectUrl = "https://developers.arcgis.com";
+
+        public AuthorEditSaveMap()
+        {
+            InitializeComponent();
+
+            // Get the view model (defined as a resource in the XAML)
+            _mapViewModel = this.FindResource("MapViewModel") as MapViewModel;
+
+            // Define a selection handler on the basemap list
+            BasemapListBox.SelectionChanged += OnBasemapsClicked;
+
+            // Define a handler for the Save Map click
+            SaveMapButton.Click += OnSaveMapClick;
+
+            // Define a handler for the New Map click
+            NewMapButton.Click += OnNewMapClicked;
+
+            UpdateAuthenticationManager();
+        }
+
+        private void OnBasemapsClicked(object sender, SelectionChangedEventArgs e)
+        {
+            // Get the text (basemap name) selected in the list box
+            var basemapName = e.AddedItems[0].ToString();
+
+            // Pass the basemap name to the view model method to change the basemap
+            _mapViewModel.ChangeBasemap(basemapName);
+
+        }
+
+        private async void OnSaveMapClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Create a challenge request for portal credentials (OAuth credential request for arcgis.com)
+                CredentialRequestInfo challengeRequest = new CredentialRequestInfo();
+
+                // Use the OAuth implicit grant flow
+                challengeRequest.GenerateTokenOptions = new GenerateTokenOptions
+                {
+                    TokenAuthenticationType = TokenAuthenticationType.OAuthImplicit
+                };
+
+                // Indicate the url (portal) to authenticate with (ArcGIS Online)
+                challengeRequest.ServiceUri = new Uri("https://www.arcgis.com/sharing/rest");
+
+                // Call GetCredentialAsync on the AuthenticationManager to invoke the challenge handler
+                await AuthenticationManager.Current.GetCredentialAsync(challengeRequest, false);
+
+                // Get information for the new portal item
+                var title = TitleTextBox.Text;
+                var description = DescriptionTextBox.Text;
+                var tags = TagsTextBox.Text.Split(',');
+
+                // Throw an exception if the text is null or empty for title or description
+                if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(description))
+                {
+                    throw new Exception("Title and description are required");
+                }
+
+                // Get current map extent (viewpoint) for the map initial extent
+                var currentViewpoint = MyMapView.GetCurrentViewpoint(Esri.ArcGISRuntime.Mapping.ViewpointType.BoundingGeometry);
+                
+                // See if the map has already been saved
+                if (!_mapViewModel.MapIsSaved)
+                {
+                    // Call the SaveNewMapAsync method on the view model, pass in the required info
+                    await _mapViewModel.SaveNewMapAsync(currentViewpoint, title, description, tags);
+
+                    // Report success
+                    MessageBox.Show("Map '" + title + "' was saved to your portal");
+                }
+                else
+                {
+                    // Map has previously been saved as a portal item, update it (title, description, and tags will remain the same)
+                    _mapViewModel.UpdateMapItem();
+
+                    // Report success
+                    MessageBox.Show("Changes to '" + title + "' were updated to the portal.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Report error
+                MessageBox.Show("Error while saving: " + ex.Message);
+            }
+        }
+
+        // Reset (create a new) map
+        private void OnNewMapClicked(object sender, EventArgs e)
+        {
+            _mapViewModel.ResetMap();
+        }
+
+        // ChallengeHandler function that will be called whenever access to a secured resource is attempted
+        public async Task<Credential> CreateCredentialAsync(CredentialRequestInfo info)
+        {
+            Credential credential = null;
+
+            try
+            {
+                // IOAuthAuthorizeHandler will challenge the user for OAuth credentials
+                credential = await AuthenticationManager.Current.GenerateCredentialAsync(info.ServiceUri);
+            }
+            catch (Exception ex)
+            {
+                // Exception will be reported in calling function
+                throw (ex);
+            }
+
+            return credential;
+        }
+
+        private void UpdateAuthenticationManager()
+        {
+            // Define the server information for ArcGIS Online
+            ServerInfo portalServerInfo = new ServerInfo();
+            // ArcGIS Online URI
+            portalServerInfo.ServerUri = new Uri(ArcGISOnlineUrl);
+            // Type of token authentication to use
+            portalServerInfo.TokenAuthenticationType = TokenAuthenticationType.OAuthImplicit;
+
+            // Define the OAuth information
+            OAuthClientInfo oAuthInfo = new OAuthClientInfo
+            {
+                ClientId = AppClientId,
+                RedirectUri = new Uri(OAuthRedirectUrl)
+            };
+            portalServerInfo.OAuthClientInfo = oAuthInfo;
+
+            // Get a reference to the (singleton) AuthenticationManager for the app
+            AuthenticationManager thisAuthenticationManager = AuthenticationManager.Current;
+
+            // Register the ArcGIS Online server information with the AuthenticationManager
+            thisAuthenticationManager.RegisterServer(portalServerInfo);
+
+            // Use the OAuthAuthorize class in this project to create a new web view to show the login UI
+            thisAuthenticationManager.OAuthAuthorizeHandler = new OAuthAuthorize();
+            // Create a new ChallengeHandler that uses a method in this class to challenge for credentials
+            thisAuthenticationManager.ChallengeHandler = new ChallengeHandler(CreateCredentialAsync);
+        }
+    }
+
+    public class MapViewModel : INotifyPropertyChanged
+    {
+        private Map _map = new Map(Basemap.CreateStreetsVector());
+
+        // Gets or sets the map
+        public Map Map
+        {
+            get { return _map; }
+            set { _map = value; OnPropertyChanged(); }
+        }
+
+        // String array to store basemap constructor types
+        private string[] _basemapTypes = new string[]
+        {
+            "Topographic",
+            "Topographic Vector",
+            "Streets",
+            "Streets Vector",
+            "Imagery",
+            "Oceans"
+        };
+
+        // Read-only property to return the available basemap names
+        public string[] BasemapChoices
+        {
+            get { return _basemapTypes; }
+        }
+
+        public void ChangeBasemap(string basemap)
+        {
+            // Apply the selected basemap to the map
+            switch (basemap)
+            {
+                case "Topographic":
+                    // Set the basemap to Topographic
+                    _map.Basemap = Basemap.CreateTopographic();
+                    break;
+                case "Topographic Vector":
+                    // Set the basemap to Topographic (vector)
+                    _map.Basemap = Basemap.CreateTopographicVector();
+                    break;
+                case "Streets":
+                    // Set the basemap to Streets
+                    _map.Basemap = Basemap.CreateStreets();
+                    break;
+                case "Streets Vector":
+                    // Set the basemap to Streets (vector)
+                    _map.Basemap = Basemap.CreateStreetsVector();
+                    break;
+                case "Imagery":
+                    // Set the basemap to Imagery
+                    _map.Basemap = Basemap.CreateImagery();
+                    break;
+                case "Oceans":
+                    // Set the basemap to Oceans
+                    _map.Basemap = Basemap.CreateOceans();
+                    break;
+            }
+        }
+
+        // Save the current map to ArcGIS Online. The initial extent, title, description, and tags are passed in.
+        public async Task SaveNewMapAsync(Viewpoint initialViewpoint, string title, string description, string[] tags)
+        {
+            // Get the ArcGIS Online portal 
+            ArcGISPortal agsOnline = await ArcGISPortal.CreateAsync(new Uri("https://www.arcgis.com/sharing/rest"));
+
+            // Set the map's initial viewpoint using the extent (viewpoint) passed in
+            _map.InitialViewpoint = initialViewpoint;
+
+            // Save the current state of the map as a portal item in the user's default folder
+            await _map.SaveAsAsync(agsOnline, null, title, description, tags, null);
+        }
+
+        public void UpdateMapItem()
+        {
+            // Save the map
+            _map.SaveAsync();
+        }
+
+        public void ResetMap()
+        {
+            // Set the current map to null
+            _map = null;
+
+            // Create a new map with light gray canvas basemap
+            Map newMap = new Map(Basemap.CreateLightGrayCanvasVector());
+
+            // Store the new map 
+            this.Map = newMap;
+        }
+
+        public bool MapIsSaved
+        {
+            // Return True if the current map has a value for the Item property
+            get { return (_map != null && _map.Item != null); }
+        }
+
+        // Raises the "MapViewModel.PropertyChanged" event
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var propertyChangedHandler = PropertyChanged;
+            if (propertyChangedHandler != null)
+                propertyChangedHandler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    #region OAuth handler
+    public class OAuthAuthorize : IOAuthAuthorizeHandler
+    {
+        // Window to contain the OAuth UI
+        private Window _window;
+
+        // Use a TaskCompletionSource to track the completion of the authorization
+        private TaskCompletionSource<IDictionary<string, string>> _tcs;
+
+        // URL for the authorization callback result (the redirect URI configured for your application)
+        private string _callbackUrl;
+
+        // URL that handles the OAuth request
+        private string _authorizeUrl;
+
+        // Function to handle authorization requests, takes the URIs for the secured service, the authorization endpoint, and the redirect URI
+        public Task<IDictionary<string, string>> AuthorizeAsync(Uri serviceUri, Uri authorizeUri, Uri callbackUri)
+        {
+            // If the TaskCompletionSource or Window are not null, authorization is in progress
+            if (_tcs != null || _window != null)
+            {
+                // Allow only one authorization process at a time
+                throw new Exception();
+            }
+
+            // Store the authorization and redirect URLs
+            _authorizeUrl = authorizeUri.AbsoluteUri;
+            _callbackUrl = callbackUri.AbsoluteUri;
+
+            // Create a task completion source
+            _tcs = new TaskCompletionSource<IDictionary<string, string>>();
+
+            // Call a function to show the login controls, make sure it runs on the UI thread for this app
+            var dispatcher = Application.Current.Dispatcher;
+            if (dispatcher == null || dispatcher.CheckAccess())
+                AuthorizeOnUIThread(_authorizeUrl);
+            else
+            {
+                var authorizeOnUIAction = new Action((() => AuthorizeOnUIThread(_authorizeUrl)));
+                dispatcher.BeginInvoke(authorizeOnUIAction);
+            }
+
+            // Return the task associated with the TaskCompletionSource
+            return _tcs.Task;
+        }
+
+        // Challenge for OAuth credentials on the UI thread
+        private void AuthorizeOnUIThread(string authorizeUri)
+        {
+            // Create a WebBrowser control to display the authorize page
+            var webBrowser = new WebBrowser();
+
+            // Handle the navigation event for the browser to check for a response to the redirect URL
+            webBrowser.Navigating += WebBrowserOnNavigating;
+
+            // Display the web browser in a new window 
+            _window = new Window
+            {
+                Content = webBrowser,
+                Height = 430,
+                Width = 395,
+                WindowStartupLocation = WindowStartupLocation.CenterOwner
+            };
+
+            // Set the app's window as the owner of the browser window (if main window closes, so will the browser)
+            if (Application.Current != null && Application.Current.MainWindow != null)
+            {
+                _window.Owner = Application.Current.MainWindow;
+            }
+
+            // Handle the window closed event then navigate to the authorize url
+            _window.Closed += OnWindowClosed;
+            webBrowser.Navigate(authorizeUri);
+
+            // Display the Window
+            _window.ShowDialog();
+        }
+
+        // Handle the browser window closing
+        void OnWindowClosed(object sender, EventArgs e)
+        {
+            // If the browser window closes, return the focus to the main window
+            if (_window != null && _window.Owner != null)
+            {
+                _window.Owner.Focus();
+            }
+
+            // If the task wasn't completed, the user must have closed the window without logging in
+            if (_tcs != null && !_tcs.Task.IsCompleted)
+            {
+                // Set the task completion source exception to indicate a canceled operation
+                _tcs.SetException(new OperationCanceledException());
+            }
+
+            // Set the task completion source and window to null to indicate the authorization process is complete
+            _tcs = null;
+            _window = null;
+        }
+
+        // Handle browser navigation (content changing)
+        void WebBrowserOnNavigating(object sender, NavigatingCancelEventArgs e)
+        {
+            // Check for a response to the callback url
+            const string portalApprovalMarker = "/oauth2/approval";
+            var webBrowser = sender as WebBrowser;
+            Uri uri = e.Uri;
+
+            // If no browser, uri, task completion source, or an empty url, return
+            if (webBrowser == null || uri == null || _tcs == null || string.IsNullOrEmpty(uri.AbsoluteUri))
+                return;
+
+            // Check for redirect
+            bool isRedirected = uri.AbsoluteUri.StartsWith(_callbackUrl) ||
+                _callbackUrl.Contains(portalApprovalMarker) && uri.AbsoluteUri.Contains(portalApprovalMarker);
+
+            if (isRedirected)
+            {
+                // Browser was redirected to the callbackUrl (success!)
+                //    -close the window 
+                //    -decode the parameters (returned as fragments or query)
+                //    -return these parameters as result of the Task
+                e.Cancel = true;
+                var tcs = _tcs;
+                _tcs = null;
+
+                if (_window != null)
+                {
+                    _window.Close();
+                }
+
+                // Call a helper function to decode the response parameters
+                var authResponse = DecodeParameters(uri);
+
+                // Set the result for the task completion source
+                tcs.SetResult(authResponse);
+            }
+        }
+
+        private static IDictionary<string, string> DecodeParameters(Uri uri)
+        {
+            // Create a dictionary of key value pairs returned in an OAuth authorization response URI query string
+            var answer = string.Empty;
+
+            // Get the values from the URI fragment or query string
+            if (!string.IsNullOrEmpty(uri.Fragment))
+            {
+                answer = uri.Fragment.Substring(1);
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(uri.Query))
+                {
+                    answer = uri.Query.Substring(1);
+                }
+            }
+
+            // Parse parameters into key / value pairs
+            var keyValueDictionary = new Dictionary<string, string>();
+            var keysAndValues = answer.Split(new[] { '&' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var kvString in keysAndValues)
+            {
+                var pair = kvString.Split('=');
+                string key = pair[0];
+                string value = string.Empty;
+                if (key.Length > 1)
+                {
+                    value = Uri.UnescapeDataString(pair[1]);
+                }
+
+                keyValueDictionary.Add(key, value);
+            }
+
+            // Return the dictionary of string keys/values
+            return keyValueDictionary;
+        }
+    }
+    #endregion
+}

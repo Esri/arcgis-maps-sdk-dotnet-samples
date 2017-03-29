2b9ce62a52218be13a19c6436f871cd6bc9f3f18
Thad Tilton <ttilton@esri.com>
2017-03-28 20:54:11 -0700

    Forms version of sample

---
+++ src/Forms/Shared/Samples/Map/SearchPortalMaps/SearchPortalMaps.xaml.cs
@@ -0,0 +1,345 @@
+﻿// Copyright 2017 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Portal;
+using Esri.ArcGISRuntime.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+#if __IOS__ 
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Auth;
+#endif
+
+#if __ANDROID__
+using Android.App;
+using Xamarin.Auth;
+#endif
+
+namespace ArcGISRuntimeXamarin.Samples.SearchPortalMaps
+{
+    public partial class SearchPortalMaps : ContentPage, IOAuthAuthorizeHandler
+    {
+        // Constants for OAuth-related values ...
+        // URL of the server to authenticate with (ArcGIS Online)
+        private const string ArcGISOnlineUrl = "https://www.arcgis.com/sharing/rest";
+
+        // Client ID for the app registered with the server (Portal Maps)
+        private const string AppClientId = "2Gh53JRzkPtOENQq";
+
+        // Redirect URL after a successful authorization (configured for the Portal Maps application)
+        private const string OAuthRedirectUrl = "https://developers.arcgis.com";
+
+        public SearchPortalMaps()
+        {
+            InitializeComponent ();
+
+            // Set up the authentication manager and display a default map
+            UpdateAuthenticationManager();
+            DisplayDefaultMap();
+        }
+
+        private async void DisplayDefaultMap()
+        {
+            // Create a new Map instance
+            Map myMap = new Map(Basemap.CreateStreets());
+
+            // Provide Map to the MapView
+            MyMapView.Map = myMap;
+        }
+
+        private async void SearchButton_Click(object sender, EventArgs e)
+        {
+            // Get web map portal items in the current user's folder or from a keyword search
+            IEnumerable<PortalItem> mapItems = null;
+            ArcGISPortal portal;
+
+            var showMapButton = sender as Button;
+
+            // See if the user wants to search public web map items
+            if (showMapButton.Text == "Search Maps")
+            {
+                // Connect to the portal (anonymously)
+                portal = await ArcGISPortal.CreateAsync();
+
+                // Create a query expression that will get public items of type 'web map' with the keyword(s) in the items tags
+                var queryExpression = string.Format("tags:\"{0}\" access:public type: (\"web map\" NOT \"web mapping application\")", "Zone");
+                // Create a query parameters object with the expression and a limit of 10 results
+                PortalQueryParameters queryParams = new PortalQueryParameters(queryExpression, 10);
+
+                // Search the portal using the query parameters and await the results
+                PortalQueryResultSet<PortalItem> findResult = await portal.FindItemsAsync(queryParams);
+                // Get the items from the query results
+                mapItems = findResult.Results;
+            }
+            else
+            {
+                // Call a sub that will force the user to log in to ArcGIS Online (if they haven't already)
+                await EnsureLoggedInAsync();
+
+                // Connect to the portal (will connect using the provided credentials)
+                portal = await ArcGISPortal.CreateAsync(new Uri(ArcGISOnlineUrl));
+
+                // Get the user's content (items in the root folder and a collection of sub-folders)
+                PortalUserContent myContent = await portal.User.GetContentAsync();
+
+                // Get the web map items in the root folder
+                mapItems = from item in myContent.Items where item.Type == PortalItemType.WebMap select item;
+
+                // Loop through all sub-folders and get web map items, add them to the mapItems collection
+                foreach (PortalFolder folder in myContent.Folders)
+                {
+                    IEnumerable<PortalItem> folderItems = await portal.User.GetContentAsync(folder.FolderId);
+                    mapItems.Concat(from item in folderItems where item.Type == PortalItemType.WebMap select item);
+                }
+            }
+
+            // Show the web map portal items in the list box
+           // MapListBox.ItemsSource = mapItems;
+        }
+
+        private async void ShowSearchUI(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void ShowMyMaps(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadMapButton_Click(object sender, EventArgs e)
+        {
+            //// Get the selected web map item in the list box
+            //PortalItem selectedMap = MapListBox.SelectedItem as PortalItem;
+            //if (selectedMap == null) { return; }
+
+            //// Create a new map, pass the web map portal item to the constructor
+            //Map webMap = new Map(selectedMap);
+
+            //// Show the web map in the map view
+            //MyMapView.Map = webMap;
+        }
+
+        private void RadioButtonUnchecked(object sender, EventArgs e)
+        {
+            // When the search/user radio buttons are unchecked, clear the list box
+          //  MapListBox.ItemsSource = null;
+
+            // Set the map to the default (if necessary)
+            if (MyMapView.Map.Item != null)
+            {
+                DisplayDefaultMap();
+            }
+        }
+
+        private async Task EnsureLoggedInAsync()
+        {
+            try
+            {
+                // Create a challenge request for portal credentials (OAuth credential request for arcgis.com)
+                CredentialRequestInfo challengeRequest = new CredentialRequestInfo();
+
+                // Use the OAuth implicit grant flow
+                challengeRequest.GenerateTokenOptions = new GenerateTokenOptions
+                {
+                    TokenAuthenticationType = TokenAuthenticationType.OAuthImplicit
+                };
+
+                // Indicate the url (portal) to authenticate with (ArcGIS Online)
+                challengeRequest.ServiceUri = new Uri(ArcGISOnlineUrl);
+
+                // Call GetCredentialAsync on the AuthenticationManager to invoke the challenge handler
+                var cred = await AuthenticationManager.Current.GetCredentialAsync(challengeRequest, false);
+            }
+            catch (OperationCanceledException ex)
+            {
+                // TODO: handle login canceled
+            }
+            catch (Exception ex)
+            {
+                // TODO: handle login failure
+            }
+        }
+
+        #region OAuth
+        private void UpdateAuthenticationManager()
+        {
+            // Define the server information for ArcGIS Online
+            ServerInfo portalServerInfo = new ServerInfo();
+            // ArcGIS Online URI
+            portalServerInfo.ServerUri = new Uri(ArcGISOnlineUrl);
+            // Type of token authentication to use
+            portalServerInfo.TokenAuthenticationType = TokenAuthenticationType.OAuthImplicit;
+
+            // Define the OAuth information
+            OAuthClientInfo oAuthInfo = new OAuthClientInfo
+            {
+                ClientId = AppClientId,
+                RedirectUri = new Uri(OAuthRedirectUrl)
+            };
+            portalServerInfo.OAuthClientInfo = oAuthInfo;
+
+            // Get a reference to the (singleton) AuthenticationManager for the app
+            AuthenticationManager thisAuthenticationManager = AuthenticationManager.Current;
+
+            // Register the ArcGIS Online server information with the AuthenticationManager
+            thisAuthenticationManager.RegisterServer(portalServerInfo);
+
+            // Create a new ChallengeHandler that uses a method in this class to challenge for credentials
+            thisAuthenticationManager.ChallengeHandler = new ChallengeHandler(CreateCredentialAsync);
+
+            // Set the OAuthAuthorizeHandler component (this class) for Android or iOS platforms
+#if __ANDROID__ || __IOS__
+            thisAuthenticationManager.OAuthAuthorizeHandler = this;
+#endif
+        }
+
+        // ChallengeHandler function that will be called whenever access to a secured resource is attempted
+        public async Task<Credential> CreateCredentialAsync(CredentialRequestInfo info)
+        {
+            Credential credential = null;
+
+            try
+            {
+                // IOAuthAuthorizeHandler will challenge the user for OAuth credentials
+                credential = await AuthenticationManager.Current.GenerateCredentialAsync(info.ServiceUri);
+            }
+            catch (Exception ex)
+            {
+                // Exception will be reported in calling function
+                throw (ex);
+            }
+
+            return credential;
+        }
+
+        #region IOAuthAuthorizationHandler implementation
+        // Use a TaskCompletionSource to track the completion of the authorization
+        private TaskCompletionSource<IDictionary<string, string>> _taskCompletionSource;
+
+        // IOAuthAuthorizeHandler.AuthorizeAsync implementation
+        public Task<IDictionary<string, string>> AuthorizeAsync(Uri serviceUri, Uri authorizeUri, Uri callbackUri)
+        {
+            // If the TaskCompletionSource is not null, authorization is in progress
+            if (_taskCompletionSource != null)
+            {
+                // Allow only one authorization process at a time
+                throw new Exception();
+            }
+
+            // Create a task completion source
+            _taskCompletionSource = new TaskCompletionSource<IDictionary<string, string>>();
+#if __ANDROID__ || __IOS__
+
+#if __ANDROID__
+            // Get the current Android Activity
+            var activity = Xamarin.Forms.Forms.Context as Activity; 
+#endif
+#if __IOS__
+            // Get the current iOS ViewController
+            var viewController = Platform.GetRenderer(this).ViewController;
+#endif
+            // Create a new Xamarin.Auth.OAuth2Authenticator using the information passed in
+            Xamarin.Auth.OAuth2Authenticator authenticator = new Xamarin.Auth.OAuth2Authenticator(
+                clientId: AppClientId,
+                scope: "",
+                authorizeUrl: authorizeUri,
+                redirectUrl: callbackUri);
+
+            // Allow the user to cancel the OAuth attempt
+            authenticator.AllowCancel = true;
+
+            // Define a handler for the OAuth2Authenticator.Completed event
+            authenticator.Completed += (sender, authArgs) =>
+            {
+                try
+                {
+#if __IOS__
+                    // Dismiss the OAuth UI when complete
+                    viewController.DismissViewController(true, null);
+#endif
+
+                    // Check if the user is authenticated
+                    if (authArgs.IsAuthenticated)
+                    {
+                        // If authorization was successful, get the user's account
+                        Xamarin.Auth.Account authenticatedAccount = authArgs.Account;
+
+                        // Set the result (Credential) for the TaskCompletionSource
+                        _taskCompletionSource.SetResult(authenticatedAccount.Properties);
+                    }
+                    else
+                    {
+                        throw new Exception("Unable to authenticate user.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // If authentication failed, set the exception on the TaskCompletionSource
+                    _taskCompletionSource.SetException(ex);
+                }
+                finally
+                {
+                    // Dismiss the OAuth login
+#if __ANDROID__ 
+                    activity.FinishActivity(99);
+#endif
+                }
+            };
+
+            // If an error was encountered when authenticating, set the exception on the TaskCompletionSource
+            authenticator.Error += (sndr, errArgs) =>
+            {
+                // If the user cancels, the Error event is raised but there is no exception ... best to check first
+                if (errArgs.Exception != null)
+                {
+                    _taskCompletionSource.TrySetException(errArgs.Exception);
+                }
+                else
+                {
+                    // Login canceled: dismiss the OAuth login
+                    if (_taskCompletionSource != null)
+                    {
+                        _taskCompletionSource.TrySetCanceled();
+#if __ANDROID__ 
+                        activity.FinishActivity(99);
+#endif
+                    }
+                }
+            };
+
+            // Present the OAuth UI so the user can enter user name and password
+#if __ANDROID__
+            var intent = authenticator.GetUI(activity);
+            activity.StartActivityForResult(intent, 99);
+#endif
+#if __IOS__
+            // Present the OAuth UI (on the app's UI thread) so the user can enter user name and password
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                viewController.PresentViewController(authenticator.GetUI(), true, null);
+            });
+#endif
+
+#endif 
+            // Return completion source task so the caller can await completion
+            return _taskCompletionSource.Task;
+        }
+        #endregion
+        #endregion
+
+    }
+
+
+}

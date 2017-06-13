a7a0b5b98a9410a267e3f0fbff4359874723d3a0
Antti Kajanus <akajanus@esri.com>
2017-06-13 16:01:03 +0100

    Initial implementation for OMT samples.

---
+++ src/WPF/ArcGISRuntime.WPF.Samples/Samples/Offline/OpenMobileMapPackage/OpenMobileMapPackage.xaml.cs
@@ -0,0 +1,56 @@
+﻿// Copyright 2016 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using ArcGISRuntime.Samples.Managers;
+using Esri.ArcGISRuntime.Mapping;
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace ArcGISRuntime.WPF.Samples.OpenMobileMapPackage
+{
+    public partial class OpenMobileMapPackage
+    {
+        public OpenMobileMapPackage()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private async void Initialize()
+        {
+            try
+            {
+                // Get path to the used data. This can be a mobile map package (.mmpk) file or 
+                // an exploded mobile map package (folder that contains .info file).
+                var dataPath = Path.Combine(DataManager.GetDataFolder(), "SampleData", "Open mobile map (map package)");
+
+                // Open a local mobile map package
+                MobileMapPackage myMobileMapPackage = await MobileMapPackage.OpenAsync(dataPath);
+
+                // Find the first map from the package
+                Map myMap = myMobileMapPackage.Maps.First();
+
+                // Assign the map to the MapView
+                MyMapView.Map = myMap;
+
+                // Load the map before hiding the loading UI.
+                await myMap.LoadAsync();
+
+                // Hide loading UI
+                loadingIndicator.Visibility = Visibility.Collapsed;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred. " + ex.ToString(), "Sample error");
+            }
+        }
+    }
+}

6df315736b6ce29bf057da43083680db07f57aac
Nathan Castle <ncastle@esri.com>
2017-07-11 14:09:59 -0700

    Harmonizing with common samples convention
    Added readmes, jpegs, enhanced metadata.jsons

--- src/Android/Xamarin.Android/Samples/Search/FindAddress/FindAddress.cs
+++ src/Android/Xamarin.Android/Samples/Search/FindAddress/FindAddress.cs
@@ -9,9 +9,7 @@
 
 using Android.App;
 using Android.OS;
-using Android.Views;
 using Android.Widget;
-using Esri.ArcGISRuntime.Location;
 using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.UI;
 using Esri.ArcGISRuntime.UI.Controls;

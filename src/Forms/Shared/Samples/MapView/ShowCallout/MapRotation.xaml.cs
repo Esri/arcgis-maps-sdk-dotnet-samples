ee7af63619457a7ef29b943b83ff6eaa4c3dd00e
Nathan Castle <ncastle@esri.com>
2017-07-12 06:27:35 -0700

    Scaffold show callout sample for Xamarin.Forms

---
+++ src/Forms/Shared/Samples/MapView/ShowCallout/MapRotation.xaml.cs
@@ -0,0 +1,35 @@
+﻿// Copyright 2016 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Mapping;
+using Xamarin.Forms;
+
+namespace ArcGISRuntimeXamarin.Samples.ShowCallout
+{
+    public partial class ShowCallout : ContentPage
+    {
+        public ShowCallout()
+        {
+            InitializeComponent();
+
+            Title = "Show callout";
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            // Create a new Map instance with the basemap  
+            Basemap myBasemap = Basemap.CreateStreets();
+            Map myMap = new Map(myBasemap);
+
+            // Assign the map to the MapView
+            MyMapView.Map = myMap;
+        }
+    }
+}

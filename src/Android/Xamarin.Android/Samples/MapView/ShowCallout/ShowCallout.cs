2e53a86cd6b29179f4c6f47d2ab646e4af509ba4
Nathan Castle <ncastle@esri.com>
2017-07-12 06:17:19 -0700

    Scaffold show callout sample for Android

---
+++ src/Android/Xamarin.Android/Samples/MapView/ShowCallout/ShowCallout.cs
@@ -0,0 +1,51 @@
+// Copyright 2016 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Android.App;
+using Android.OS;
+using Android.Widget;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI.Controls;
+
+namespace ArcGISRuntimeXamarin.Samples.ShowCallout
+{
+    [Activity]
+    public class ShowCallout : Activity
+    {
+        // Create and hold reference to the used MapView
+        private MapView _myMapView = new MapView();
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Initialize();
+
+            Title = "Show Callout";
+        }
+
+        void Initialize()
+        {
+            var layout = new LinearLayout(this) { Orientation = Orientation.Vertical };
+
+            // Create a new Map instance with the basemap  
+            Basemap myBasemap = Basemap.CreateImageryWithLabels();
+            Map myMap = new Map(myBasemap);
+
+            // Create a new map view control to display the map
+            _myMapView = new MapView();
+            _myMapView.Map = myMap;
+
+            layout.AddView(_myMapView);
+
+            // Apply the layout to the app
+            SetContentView(layout);
+        }
+    }
+}

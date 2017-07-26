3bec3d1601904b45b842f9c935412af719a93a3d
Nathan Castle <ncastle@esri.com>
2017-07-26 14:02:50 -0700

    initial iOS implementation of Open Mobile Map (mmpk) sample

---
+++ src/iOS/Xamarin.iOS/Samples/Map/OpenMobileMap/OpenMobileMap.cs
@@ -0,0 +1,66 @@
+// Copyright 2017 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI.Controls;
+using Foundation;
+using System.Linq;
+using UIKit;
+
+namespace ArcGISRuntimeXamarin.Samples.OpenMobileMap
+{
+    [Register("OpenMobileMap")]
+    public class OpenMobileMap : UIViewController
+    {
+        // Create and hold reference to the used MapView
+        private MapView _myMapView = new MapView();
+
+        // Hold a reference to a Mobile Map Package
+        private MobileMapPackage _myMapPackage;
+
+        public OpenMobileMap()
+        {
+            Title = "Open mobile map (map package)";
+        }
+
+        public override void ViewDidLoad() {
+            base.ViewDidLoad();
+			CreateLayout();
+			Initialize();
+        }
+
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+
+            // Update the UI to account for new layout
+            _myMapView.Frame = new CoreGraphics.CGRect(0, 0, View.Bounds.Width, View.Bounds.Height);
+        }
+
+        private async void Initialize()
+        {
+            // Load the Mobile Map Package from the Bundle
+            //     File is located in Resources/MobileMapPackages/Yellowstone.mmpk
+            //     Build Action is BundleResource; Do not copy to Output Directory
+            _myMapPackage = await MobileMapPackage.OpenAsync("MobileMapPackages/Yellowstone.mmpk");
+
+            // Display the first map in the package
+            _myMapView.Map = _myMapPackage.Maps.First();
+        }
+
+        private void CreateLayout()
+        {
+            // Setup the visual frame for the MapView
+            _myMapView.Frame = new CoreGraphics.CGRect(0, 0, View.Bounds.Width, View.Bounds.Height);
+
+            // Add MapView to the page
+            View.AddSubview(_myMapView);
+        }
+    }
+}

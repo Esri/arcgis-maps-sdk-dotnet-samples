e5ef759a82f7939caf8a1de2974fe34e78270b17
Nathan Castle <ncastle@esri.com>
2017-07-28 11:03:31 -0700

    generate geodatabase started for UWP

---
+++ src/UWP/ArcGISRuntime.UWP.Samples/Samples/Data/GenerateGeodatabase/GenerateGeodatabase.xaml.cs
@@ -0,0 +1,211 @@
+﻿// Copyright 2017 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks.Offline;
+using Esri.ArcGISRuntime.UI;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Esri.ArcGISRuntime.Tasks;
+
+namespace ArcGISRuntime.UWP.Samples.GenerateGeodatabase
+{
+    public partial class GenerateGeodatabase
+    {
+        // URI for a feature service that supports geodatabase generation
+        private Uri _featureServiceUri = new Uri("https://sampleserver6.arcgisonline.com/arcgis/rest/services/Sync/WildfireSync/FeatureServer");
+
+        // Path to the geodatabase file on disk
+        private string _geoDbPath;
+
+        // Task to be used for generating the geodatabase
+        private GeodatabaseSyncTask gdbSyncTask;
+
+        public GenerateGeodatabase()
+        {
+            InitializeComponent();
+
+            // Create the UI, setup the control references and execute initialization 
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            // Create new Map with basemap
+            Map myMap = new Map(Basemap.CreateTopographic());
+
+            // Create and set initial map location
+            MapPoint initialLocation = new MapPoint(-118.33, 38.00, SpatialReferences.Wgs84);
+            myMap.InitialViewpoint = new Viewpoint(initialLocation, 100000);
+
+            // Assign the map to the MapView
+            MyMapView.Map = myMap;
+
+            // Create a new symbol for the extent graphic
+            SimpleLineSymbol lineSymbol = new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, Colors.Red, 2);
+
+            // Create graphics overlay for the extent graphic and apply a renderer
+            GraphicsOverlay extentOverlay = new GraphicsOverlay();
+            extentOverlay.Renderer = new SimpleRenderer(lineSymbol);
+
+            // Add graphics overlay to the map view
+            MyMapView.GraphicsOverlays.Add(extentOverlay);
+
+            // Set up an event handler for when the viewpoint (extent) changes
+            MyMapView.ViewpointChanged += MapViewExtentChanged;
+
+            // Update the local data path for the geodatabase file
+            SetGdbPath();
+        }
+
+        private void UpdateMapExtent()
+        {
+            // Get the updated extent for the new viewpoint
+            Envelope extent = MyMapView.GetCurrentViewpoint(ViewpointType.BoundingGeometry).TargetGeometry as Envelope;
+
+            // Return if extent is null 
+            if (extent == null) { return; }
+
+            // Create an envelope that is a bit smaller than the extent
+            EnvelopeBuilder envelopeBldr = new EnvelopeBuilder(extent);
+            envelopeBldr.Expand(0.80);
+
+            // Get the (only) graphics overlay in the map view (make sure it exists)
+            var extentOverlay = MyMapView.GraphicsOverlays.FirstOrDefault();
+            if (extentOverlay == null)
+            {
+                return;
+            }
+
+            // Get the extent graphic 
+            Graphic extentGraphic = extentOverlay.Graphics.FirstOrDefault();
+
+            // Create the extent graphic and add it to the overlay if it doesn't exist
+            if (extentGraphic == null)
+            {
+                extentGraphic = new Graphic(envelopeBldr.ToGeometry());
+                extentOverlay.Graphics.Add(extentGraphic);
+            }
+            else
+            {
+                // Otherwise, simply update the graphic's geometry
+                extentGraphic.Geometry = envelopeBldr.ToGeometry();
+            }
+        }
+
+        private async void StartGeodatabaseGeneration()
+        {
+            // Create a task for generating a geodatabase (GeodatabaseSyncTask)
+            gdbSyncTask = await GeodatabaseSyncTask.CreateAsync(_featureServiceUri);
+
+            // Get the current extent of the map view
+            Envelope extent = MyMapView.GetCurrentViewpoint(ViewpointType.BoundingGeometry).TargetGeometry as Envelope;
+
+            // Get the default parameters for the generate geodatabase task
+            GenerateGeodatabaseParameters generateParams = await gdbSyncTask.CreateDefaultGenerateGeodatabaseParametersAsync(extent);
+
+            // Create a generate geodatabase job
+            GenerateGeodatabaseJob _generateGdbJob = gdbSyncTask.GenerateGeodatabase(generateParams, _geoDbPath);
+
+            // Handle the job changed event
+            _generateGdbJob.JobChanged += GenerateGdbJobChanged;
+
+            // Start the job
+            _generateGdbJob.Start();
+        }
+
+        private async void HandleGenerationStatusChange(GenerateGeodatabaseJob job)
+        {
+            JobStatus status = job.Status;
+
+            // If the job completed successfully, add the geodatabase data to the map
+            if (status == JobStatus.Succeeded)
+            {
+                // Get the new geodatabase
+                Geodatabase resultGdb = await job.GetResultAsync();
+
+                // Loop through all feature tables in the geodatabase and add a new layer to the map
+                foreach (GeodatabaseFeatureTable table in resultGdb.GeodatabaseFeatureTables)
+                {
+                    // Create a new feature layer for the table
+                    FeatureLayer lyr = new FeatureLayer(table);
+
+                    // Add the new layer to the map
+                    MyMapView.Map.OperationalLayers.Add(lyr);
+                }
+                // Best practice is to unregister the geodatabase
+                await gdbSyncTask.UnregisterGeodatabaseAsync(resultGdb);
+
+                // Tell the user that the geodatabase was unregistered
+                ShowStatusMessage("Geodatabase was unregistered per best practice");
+            }
+
+            // See if the job failed
+            if (status == JobStatus.Failed)
+            {
+                // Create a message to show the user
+                string message = "Generate geodatabase job failed";
+
+                // Show an error message (if there is one)
+                if (job.Error != null)
+                {
+                    message += ": " + job.Error.Message;
+                }
+                else
+                {
+                    // If no error, show messages from the job
+                    var m = from msg in job.Messages select msg.Message;
+                    message += ": " + string.Join<string>("\n", m);
+                }
+
+                ShowStatusMessage(message);
+            }
+        }
+
+        // Platform-specific functionality:
+
+        private void SetGdbPath()
+        {
+            _geoDbPath = Windows.Storage.ApplicationData.Current.LocalFolder.Path.ToString();
+            _geoDbPath += "\\wildfire.geodatabase";
+        }
+
+        private void ShowStatusMessage(string message)
+        {
+            // Display the message to the user
+            MessageDialog dialog = new MessageDialog(message);
+            dialog.ShowAsync();
+        }
+
+        private async void GenerateButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            StartGeodatabaseGeneration();
+        }
+
+        private async void MapViewExtentChanged(object sender, EventArgs e)
+        {
+            UpdateMapExtent();
+        }
+
+        // Handler for the job changed event
+        private async void GenerateGdbJobChanged(object sender, EventArgs e)
+        {
+            GenerateGeodatabaseJob job = sender as GenerateGeodatabaseJob;
+            HandleGenerationStatusChange(job);
+        }
+
+    }
+}

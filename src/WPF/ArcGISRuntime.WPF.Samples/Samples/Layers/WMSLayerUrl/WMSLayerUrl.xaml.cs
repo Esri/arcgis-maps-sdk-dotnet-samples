cb57f2b722607dc43eb975163e341075ce94a823
Nathan Castle <ncastle@esri.com>
2017-09-13 12:40:18 -0700

    Implementation of WMS Layer URL for WPF

---
+++ src/WPF/ArcGISRuntime.WPF.Samples/Samples/Layers/WMSLayerUrl/WMSLayerUrl.xaml.cs
@@ -0,0 +1,53 @@
+﻿// Copyright 2017 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Ogc;
+using System.IO;
+using System.Collections.Generic;
+using System;
+
+namespace ArcGISRuntime.WPF.Samples.WMSLayerUrl
+{
+    public partial class WMSLayerUrl
+    {
+        private Uri wmsUrl = new Uri("http://certmapper.cr.usgs.gov/arcgis/services/geology/africa/MapServer/WMSServer?request=GetCapabilities&service=WMS");
+        private List<String> wmsLayerNames = new List<string> { "0" };
+
+        public WMSLayerUrl()
+        {
+            InitializeComponent();
+
+            // Execute initialization 
+            Initialize();
+        }
+
+        private async void Initialize()
+        {
+            // Apply an imagery basemap to the map
+            MyMapView.Map = new Map(Basemap.CreateImagery());
+
+            // Create the layer
+            WmsLayer myWmsLayer = new WmsLayer(wmsUrl, wmsLayerNames);
+
+            // Load the layer
+            await myWmsLayer.LoadAsync();
+
+            // Add the layer to the map
+            MyMapView.Map.OperationalLayers.Add(myWmsLayer);
+
+            // Get the full extent of the layer
+            Viewpoint viewExtent = new Viewpoint(myWmsLayer.FullExtent);
+
+            // Zoom to the extent of the layer
+            MyMapView.Map.InitialViewpoint = viewExtent;
+        }
+
+    }
+}

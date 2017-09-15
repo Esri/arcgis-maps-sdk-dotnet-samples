a4e4d0650d30f7cf75a6857132f9d8e75a1dcefa
AVWORLD\don1760 <dkemlage@esri.com>
2017-09-15 11:33:32 -0700

    Initial commit for new RasterRenderingRule sample

---
+++ src/Forms/Shared/Samples/Layers/RasterRenderingRule/RasterRenderingRule.xaml.cs
@@ -0,0 +1,131 @@
+﻿// Copyright 2017 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.ArcGISServices;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Rasters;
+using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ArcGISRuntimeXamarin.Samples.RasterRenderingRule
+{
+    public partial class RasterRenderingRule : ContentPage
+    {
+        public RasterRenderingRule()
+        {
+            InitializeComponent ();
+
+            Title = "Raster rendering rule";
+
+            // Create the UI, setup the control references and execute initialization 
+            Initialize();
+        }
+
+        // Create a global scope empty read-only list for the various rendering rules of the image service raster
+        private IReadOnlyList<RenderingRuleInfo> _myReadOnlyListRenderRuleInfos;
+
+        // Create a global scope Uri for the image server
+        private Uri _myUri;
+
+        // Create a global list to store the names of the rendering rule info for the image service raster
+        private List<string> _names = new List<string>();
+
+        private async void Initialize()
+        {
+            // Assign a new map to the MapView
+            MyMapView.Map = new Map();
+
+            // Set the basemap to Streets
+            MyMapView.Map.Basemap = Basemap.CreateStreets();
+
+            // Set the Uri for the image server
+            _myUri = new Uri("https://sampleserver6.arcgisonline.com/arcgis/rest/services/CharlotteLAS/ImageServer");
+
+            // Create a new image service raster from the Uri
+            ImageServiceRaster myImageServiceRaster = new ImageServiceRaster(_myUri);
+
+            // Load the image service raster
+            await myImageServiceRaster.LoadAsync();
+
+            // Get the ArcGIS image service info (aka. metadata) from the image service raster
+            ArcGISImageServiceInfo myArcGISImageServiceInfo = myImageServiceRaster.ServiceInfo;
+
+            // Get the full extent envelope of the image service raster (the Charlotte, NC area) 
+            Envelope myEnvelope = myArcGISImageServiceInfo.FullExtent;
+
+            // Define a new view point from the full extent envelope
+            Viewpoint myViewPoint = new Viewpoint(myEnvelope);
+
+            // Zoom to the area of the full extent envelope of the image service raster
+            await MyMapView.SetViewpointAsync(myViewPoint);
+
+            // Get the rendering rule info (i.e. definitions of how the image should be drawn) info from the image service raster
+            _myReadOnlyListRenderRuleInfos = myArcGISImageServiceInfo.RenderingRuleInfos;
+
+            // Make sure that we have at least one render rule info to proceed 
+            if (_myReadOnlyListRenderRuleInfos.Count > 0)
+            {
+                // Loop through each rendering rule info
+                foreach (RenderingRuleInfo myRenderingRuleInfo in _myReadOnlyListRenderRuleInfos)
+                {
+                    // Get the name of the rendering rule info
+                    string myRenderingRuleName = myRenderingRuleInfo.Name;
+
+                    // Add the name of the rendering rule info into the global list of names
+                    _names.Add(myRenderingRuleName);
+                }
+            }
+
+            // Call the function to display the image service raster based up on user choice of rendering rules
+           await ChangeRenderingRuleAsync();
+        }
+
+        private async Task ChangeRenderingRuleAsync ()
+        {
+            // Display a picker to the user to choose among the available rendering rules for the image service raster
+            string myRenderingRuleInfoName = await DisplayActionSheet("Select a Rendering Rule", "Cancel", null, _names.ToArray());
+
+            // Loop through each rendering rule info in the image service raster
+            foreach (RenderingRuleInfo myRenderingRuleInfo in _myReadOnlyListRenderRuleInfos)
+            {
+                // Get the name of the rendering rule info
+                string myRenderingRuleName = myRenderingRuleInfo.Name;
+
+                // If the name of the rendering rule info matches what was chosen by the user, proceed 
+                if (myRenderingRuleName == myRenderingRuleInfoName)
+                {
+                    // Create a new rendering rule from the rendering rule info
+                    RenderingRule myRenderingRule = new RenderingRule(myRenderingRuleInfo);
+
+                    // Create a new image service raster from the global scope Uri
+                    ImageServiceRaster myImageServiceRaster = new ImageServiceRaster(_myUri);
+
+                    // Set the image service raster's rendering rule to the rendering rule created earlier
+                    myImageServiceRaster.RenderingRule = myRenderingRule;
+
+                    // Create a new raster layer from the image service raster
+                    RasterLayer myRasterLayer = new RasterLayer(myImageServiceRaster);
+
+                    // Add the raster layer to the operational layers of the  map view
+                    MyMapView.Map.OperationalLayers.Add(myRasterLayer);
+                }
+            }
+        }
+
+        private async void OnChangeRenderingRuleButtonClicked(object sender, EventArgs e)
+        {
+            // Call the function to display the image service raster based up on user choice of rendering rules
+            await ChangeRenderingRuleAsync();
+        }
+
+    }
+}

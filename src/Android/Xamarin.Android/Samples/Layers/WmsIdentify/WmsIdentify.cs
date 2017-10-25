ea975d68b620deadca0cbf5baf4df9a772c0c67a
Nathan Castle <ncastle@esri.com>
2017-10-24 19:05:13 -0700

    Initial implementation for all except Forms

---
+++ src/Android/Xamarin.Android/Samples/Layers/WmsIdentify/WmsIdentify.cs
@@ -0,0 +1,119 @@
+// Copyright 2017 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Android.App;
+using Android.OS;
+using Android.Webkit;
+using Android.Widget;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Ogc;
+using Esri.ArcGISRuntime.UI.Controls;
+using System;
+using System.Collections.Generic;
+
+namespace ArcGISRuntimeXamarin.Samples.WmsIdentify
+{
+    [Activity]
+    public class WmsIdentify : Activity
+    {
+        // Create and hold reference to the used MapView
+        private MapView _myMapView = new MapView();
+
+        // Hold the URL to the WMS service showing EPA water info
+        private Uri wmsUrl = new Uri("https://watersgeo.epa.gov/arcgis/services/OWPROGRAM/SDWIS_WMERC/MapServer/WMSServer?request=GetCapabilities&service=WMS");
+
+        // Hold a list of uniquely-identifying WMS layer names to display
+        private List<String> wmsLayerNames = new List<string> { "4" };
+
+        // Hold the WMS layer
+        WmsLayer myWmsLayer;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Title = "WmsIdentify";
+
+            // Create the UI, setup the control references and execute initialization 
+            CreateLayout();
+            Initialize();
+        }
+
+        private async void Initialize()
+        {
+            // Create new Map with basemap
+            Map myMap = new Map(Basemap.CreateImagery());
+            
+            // Provide used Map to the MapView
+            _myMapView.Map = myMap;
+
+            // Provide used Map to the MapView
+            _myMapView.Map = myMap;
+
+            // Create a new WMS layer displaying the specified layers from the service
+            myWmsLayer = new WmsLayer(wmsUrl, wmsLayerNames);
+
+            // Load the layer
+            await myWmsLayer.LoadAsync();
+
+            // Add the layer to the map
+            _myMapView.Map.OperationalLayers.Add(myWmsLayer);
+
+            // Subscribe to tap events - starting point for feature identification
+            _myMapView.GeoViewTapped += _myMapView_GeoViewTapped;
+        }
+
+        private void CreateLayout()
+        {
+            // Create a new vertical layout for the app
+            var layout = new LinearLayout(this) { Orientation = Orientation.Vertical };
+
+            // Add the map view to the layout
+            layout.AddView(_myMapView);
+
+            // Show the layout in the app
+            SetContentView(layout);
+        }
+
+        private async void _myMapView_GeoViewTapped(object sender, GeoViewInputEventArgs e)
+        {
+            // Perform the identify operation
+            IdentifyLayerResult myIdentifyResult = await _myMapView.IdentifyLayerAsync(myWmsLayer, e.Position, 20, false);
+
+            // Return if there's nothing to show
+            if (myIdentifyResult.GeoElements.Count < 1)
+            {
+                return;
+            }
+
+            // Retrieve the identified feature, which is always a WmsFeature for WMS layers
+            WmsFeature identifiedFeature = myIdentifyResult.GeoElements[0] as WmsFeature;
+
+            // Retrieve teh WmsFeature's HTML content
+            string htmlContent = identifiedFeature.Attributes["HTML"].ToString();
+
+            // Show a callout with the HTML content
+            ShowHtmlCallout(htmlContent, e.Location);
+        }
+
+        private void ShowHtmlCallout(string htmlContent, MapPoint position)
+        {
+            // Create the web browser control
+            WebView htmlView = new WebView(this);
+
+            // Display the string content as an HTML document
+            htmlView.LoadData(htmlContent, "text/html", null);
+
+            // Create the callout with the browser
+            _myMapView.ShowCalloutAt(position, htmlView);
+        }
+    }
+}
---
+++ src/iOS/Xamarin.iOS/Samples/Layers/WmsIdentify/WmsIdentify.cs
@@ -0,0 +1,116 @@
+﻿// Copyright 2017 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Ogc;
+using Esri.ArcGISRuntime.UI.Controls;
+using Foundation;
+using System;
+using System.Collections.Generic;
+using UIKit;
+
+namespace ArcGISRuntimeXamarin.Samples.WmsIdentify
+{
+    [Register("WmsIdentify")]
+    public class WmsIdentify : UIViewController
+    {
+        // Create and hold reference to the used MapView
+        private MapView _myMapView = new MapView();
+
+        // Hold the URL to the WMS service showing EPA water info
+        private Uri wmsUrl = new Uri("https://watersgeo.epa.gov/arcgis/services/OWPROGRAM/SDWIS_WMERC/MapServer/WMSServer?request=GetCapabilities&service=WMS");
+
+        // Hold a list of uniquely-identifying WMS layer names to display
+        private List<String> wmsLayerNames = new List<string> { "4" };
+
+        // Hold the WMS layer
+        WmsLayer myWmsLayer;
+
+        public WmsIdentify()
+        {
+            Title = "WmsIdentify";
+        }
+        private void CreateLayout()
+        {
+            // Add MapView to the page
+            View.AddSubviews(_myMapView);
+        }
+
+        public override void ViewDidLoad()
+        {
+            CreateLayout();
+            Initialize();
+
+            base.ViewDidLoad();
+        }
+        public override void ViewDidLayoutSubviews()
+        {
+            // Setup the visual frame for the MapView
+            _myMapView.Frame = new CoreGraphics.CGRect(0, 0, View.Bounds.Width, View.Bounds.Height);
+
+            base.ViewDidLayoutSubviews();
+        }
+
+        private async void Initialize()
+        {
+            // Create new Map with basemap
+            Map myMap = new Map(Basemap.CreateImagery());
+
+            // Provide used Map to the MapView
+            _myMapView.Map = myMap;
+
+            // Create a new WMS layer displaying the specified layers from the service
+            myWmsLayer = new WmsLayer(wmsUrl, wmsLayerNames);
+
+            // Load the layer
+            await myWmsLayer.LoadAsync();
+
+            // Add the layer to the map
+            _myMapView.Map.OperationalLayers.Add(myWmsLayer);
+
+            // Subscribe to tap events - starting point for feature identification
+            _myMapView.GeoViewTapped += _myMapView_GeoViewTapped;
+        }
+
+        private async void _myMapView_GeoViewTapped(object sender, GeoViewInputEventArgs e)
+        {
+            // Perform the identify operation
+            IdentifyLayerResult myIdentifyResult = await _myMapView.IdentifyLayerAsync(myWmsLayer, e.Position, 20, false);
+
+            // Return if there's nothing to show
+            if (myIdentifyResult.GeoElements.Count < 1)
+            {
+                return;
+            }
+
+            // Retrieve the identified feature, which is always a WmsFeature for WMS layers
+            WmsFeature identifiedFeature = myIdentifyResult.GeoElements[0] as WmsFeature;
+
+            // Retrieve teh WmsFeature's HTML content
+            string htmlContent = identifiedFeature.Attributes["HTML"].ToString();
+
+            // Show a callout with the HTML content
+            ShowHtmlCallout(htmlContent, e.Location);
+        }
+
+        private void ShowHtmlCallout(string htmlContent, MapPoint position)
+        {
+            // Create the web browser control
+            WebKit.WKWebView htmlView = new WebKit.WKWebView(new CoreGraphics.CGRect(0, 0, 100, 200), new WebKit.WKWebViewConfiguration());
+
+            // Display the string content as an HTML document
+            htmlView.LoadHtmlString(new NSString(htmlContent), new NSUrl("/"));
+
+            // Create the callout with the browser
+            _myMapView.ShowCalloutAt(position, htmlView);
+        }
+    }
+}

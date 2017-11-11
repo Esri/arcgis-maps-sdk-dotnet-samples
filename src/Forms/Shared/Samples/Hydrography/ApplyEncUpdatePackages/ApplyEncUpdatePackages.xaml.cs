33ba55db8887d43aa8dc470161d4e072a6c12e60
Nathan Castle <ncastle@esri.com>
2017-11-10 18:31:19 -0800

    Apply ENC Update Packages

---
+++ src/Forms/Shared/Samples/Hydrography/ApplyEncUpdatePackages/ApplyEncUpdatePackages.xaml.cs
@@ -0,0 +1,99 @@
+﻿// Copyright 2017 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using ArcGISRuntimeXamarin.Managers;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Hydrography;
+using Esri.ArcGISRuntime.Mapping;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ArcGISRuntimeXamarin.Samples.ApplyEncUpdatePackages
+{
+    public partial class ApplyEncUpdatePackages : ContentPage
+    {
+        public ApplyEncUpdatePackages()
+        {
+            InitializeComponent();
+
+            Title = "Apply ENC Update Packages";
+
+            // Create the UI, setup the control references and execute initialization 
+            InitializeAsync();
+        }
+
+        private async Task InitializeAsync()
+        {
+            // Initialize the map with an oceans basemap
+            MyMapView.Map = new Map(Basemap.CreateOceans());
+
+            // Get the path to the ENC Exchange Set
+            string encPath = await GetEncPath();
+
+            // Create the Exchange Set
+            // Note: this constructor takes an array of paths because so that update sets can be loaded alongside base data
+            EncExchangeSet _encExchangeSet = new EncExchangeSet(new string[] { encPath });
+
+            // Wait for the exchange set to load
+            await _encExchangeSet.LoadAsync();
+
+            // Store a list of data set extent's - will be used to zoom the mapview to the full extent of the Exchange Set
+            List<Envelope> dataSetExtents = new List<Envelope>();
+
+            // Add each data set as a layer
+            foreach (EncDataSet _encDataSet in _encExchangeSet.DataSets)
+            {
+                var path = _encDataSet.Name.Replace("\\", "/");
+                // Create the cell and layer
+                EncCell cell = new EncCell(Path.Combine(Path.GetDirectoryName(encPath), path));
+                EncLayer _encLayer = new EncLayer(cell);
+
+                // Add the layer to the map
+                MyMapView.Map.OperationalLayers.Add(_encLayer);
+
+                // Wait for the layer to load
+                await _encLayer.LoadAsync();
+
+                // Add the extent to the list of extents
+                dataSetExtents.Add(_encLayer.FullExtent);
+            }
+
+            // Use the geometry engine to compute the full extent of the ENC Exchange Set
+            Envelope fullExtent = GeometryEngine.CombineExtents(dataSetExtents);
+
+            // Set the viewpoint
+            MyMapView.SetViewpoint(new Viewpoint(fullExtent));
+        }
+
+        private async Task<String> GetEncPath()
+        {
+            #region offlinedata
+
+            // The data manager provides a method to get the folder
+            string folder = DataManager.GetDataFolder();
+
+            // Get the full path
+            string filepath = Path.Combine(folder, "SampleData", "AddEncExchangeSet", "ExchangeSet", "ENC_ROOT", "CATALOG.031");
+
+            // Check if the file exists
+            if (!File.Exists(filepath))
+            {
+                // Download the file
+                await DataManager.GetData("9d3ddb20afe3409eae25b3cdeb82215b", "AddEncExchangeSet");
+            }
+
+            return filepath;
+
+            #endregion offlinedata
+        }
+    }
+}

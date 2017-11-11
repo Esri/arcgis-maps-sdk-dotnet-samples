8c6e944e5e7c0e8079259227e23a8d5e2b0b2e35
Nathan Castle <ncastle@esri.com>
2017-11-10 18:01:04 -0800

    Select ENC Features sample

---
+++ src/WPF/ArcGISRuntime.WPF.Samples/Samples/Hydrography/SelectEncFeatures/SelectEncFeatures.xaml.cs
@@ -0,0 +1,147 @@
+﻿// Copyright 2017 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+
+using ArcGISRuntime.Samples.Managers;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Hydrography;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArcGISRuntime.WPF.Samples.SelectEncFeatures
+{
+    public partial class SelectEncFeatures
+    {
+        public SelectEncFeatures()
+        {
+            InitializeComponent();
+
+            // Create the UI, setup the control references and execute initialization
+            InitializeAsync();
+        }
+
+        private async Task InitializeAsync()
+        {
+            // Initialize the map with an oceans basemap
+            MyMapView.Map = new Map(Basemap.CreateOceans());
+
+            // Get the path to the ENC Exchange Set
+            string encPath = await GetEncPath();
+
+            // Create the Exchange Set
+            // Note: this constructor takes an array of paths because so that update sets can be loaded alongside base data
+            EncExchangeSet _encExchangeSet = new EncExchangeSet(new string[] { encPath });
+
+            // Wait for the exchange set to load
+            await _encExchangeSet.LoadAsync();
+
+            // Store a list of data set extent's - will be used to zoom the mapview to the full extent of the Exchange Set
+            List<Envelope> dataSetExtents = new List<Envelope>();
+
+            // Add each data set as a layer
+            foreach (EncDataSet _encDataSet in _encExchangeSet.DataSets)
+            {
+                // Create the cell and layer
+                EncLayer _encLayer = new EncLayer(new EncCell(_encDataSet));
+
+                // Add the layer to the map
+                MyMapView.Map.OperationalLayers.Add(_encLayer);
+
+                // Wait for the layer to load
+                await _encLayer.LoadAsync();
+
+                // Add the extent to the list of extents
+                dataSetExtents.Add(_encLayer.FullExtent);
+            }
+
+            // Use the geometry engine to compute the full extent of the ENC Exchange Set
+            Envelope fullExtent = GeometryEngine.CombineExtents(dataSetExtents);
+
+            // Set the viewpoint
+            MyMapView.SetViewpoint(new Viewpoint(fullExtent));
+
+            // Subscribe to tap events (in order to use them to identify and select features)
+            MyMapView.GeoViewTapped += MyMapView_GeoViewTapped;
+        }
+
+        private void ClearAllSelections()
+        {
+            // For each layer in the operational layers that is an ENC layer
+            foreach (EncLayer layer in MyMapView.Map.OperationalLayers.OfType<EncLayer>())
+            {
+                // Clear the layer's selection
+                layer.ClearSelection();
+            }
+        }
+
+        private async void MyMapView_GeoViewTapped(object sender, Esri.ArcGISRuntime.UI.Controls.GeoViewInputEventArgs e)
+        {
+            // First clear any existing selections
+            ClearAllSelections();
+
+            // Perform the identify operation
+            IReadOnlyList<IdentifyLayerResult> results = await MyMapView.IdentifyLayersAsync(e.Position, 5, false);
+
+            // Return if there are no results
+            if (results.Count < 1) { return; }
+
+            // Select every matching feature
+            foreach (IdentifyLayerResult result in results)
+            {
+                // Get the layer associated with this set of results
+                EncLayer containingLayer = result.LayerContent as EncLayer;
+
+                // Select each ENC feature
+                foreach (EncFeature feature in result.GeoElements.OfType<EncFeature>())
+                {
+                    containingLayer.SelectFeature(feature);
+                }
+            }
+
+            // Get the first identified feature
+            EncFeature firstFeature = results.First().GeoElements.OfType<EncFeature>().First();
+
+            // Note that more checking would be needed if there were more than just ENC layers on the map
+            //     MapView.IdentifyLayersAsync returns results from all layers on the map
+
+            // Create the callout definition
+            CalloutDefinition definition = new CalloutDefinition("Feature", firstFeature.Attributes["FeatureDescription"].ToString());
+
+            // Show the callout
+            MyMapView.ShowCalloutAt(e.Location, definition);
+        }
+
+        private async Task<String> GetEncPath()
+        {
+            #region offlinedata
+
+            // The data manager provides a method to get the folder
+            string folder = DataManager.GetDataFolder();
+
+            // Get the full path
+            string filepath = Path.Combine(folder, "SampleData", "AddEncExchangeSet", "ExchangeSet", "ENC_ROOT", "CATALOG.031");
+
+            // Check if the file exists
+            if (!File.Exists(filepath))
+            {
+                // Download the file
+                await DataManager.GetData("9d3ddb20afe3409eae25b3cdeb82215b", "AddEncExchangeSet");
+            }
+
+            return filepath;
+
+            #endregion offlinedata
+        }
+    }
+}

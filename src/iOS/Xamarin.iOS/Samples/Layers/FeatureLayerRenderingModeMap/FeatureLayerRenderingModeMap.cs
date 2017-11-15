7064e5b11bf1f1caf8cbd8b2ba687bbfefde7941
Stuart McIlreavy <stuartmcilreavy@outlook.com>
2017-11-15 11:11:04 +0000

    iOS sample additions

--- src/iOS/Xamarin.iOS/Samples/Layers/FeatureLayerRenderingModeMap/FeatureLayerRenderingModeMap.cs
+++ src/iOS/Xamarin.iOS/Samples/Layers/FeatureLayerRenderingModeMap/FeatureLayerRenderingModeMap.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2016 Esri.
+﻿// Copyright 2017 Esri.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
@@ -21,17 +21,17 @@ namespace ArcGISRuntimeXamarin.Samples.FeatureLayerRenderingModeMap
     [Register("FeatureLayerRenderingModeMap")]
     public class FeatureLayerRenderingModeMap : UIViewController
     {
-        // Create and hold reference to the used MapView
-        private MapView _myMapViewTop = new MapView();
-        private MapView _myMapViewBottom = new MapView();
+        private MapView _myMapViewTop;
+        private MapView _myMapViewBottom;
 
-        // Viewpoint locations for map view to zoom in and out to.
-        Viewpoint _zoomOutPoint = new Viewpoint(new MapPoint(-118.37, 34.46, SpatialReferences.Wgs84), 650000, 0);
-        Viewpoint _zoomInPoint = new Viewpoint(new MapPoint(-118.45, 34.395, SpatialReferences.Wgs84), 50000, 90);
+        private UIButton _zoomButton;
+
+        private Viewpoint _zoomOutPoint;
+        private Viewpoint _zoomInPoint;
 
         public FeatureLayerRenderingModeMap()
         {
-            Title = "Feature Layer Rendering Mode (Map)";
+            this.Title = "Feature Layer Rendering Mode (Map)";
         }
         
         public override void ViewDidLoad()
@@ -47,24 +47,32 @@ namespace ArcGISRuntimeXamarin.Samples.FeatureLayerRenderingModeMap
 
         public override void ViewDidLayoutSubviews()
         {
-            // Setup the visual frame for the MapView
-            _myMapViewTop.Frame = new CoreGraphics.CGRect(0, 0, View.Bounds.Width/2, View.Bounds.Height/2);
-            _myMapViewBottom.Frame = new CoreGraphics.CGRect(View.Bounds.Width / 2, View.Bounds.Height / 2, View.Bounds.Width/2, View.Bounds.Height/2);
+            // Setup the visual frame for the MapViews
+            _myMapViewTop.Frame = new CoreGraphics.CGRect(0, 0, View.Bounds.Width, View.Bounds.Height/2);
+            _myMapViewBottom.Frame = new CoreGraphics.CGRect(0, View.Bounds.Height / 2, View.Bounds.Width, View.Bounds.Height/2);
 
             base.ViewDidLayoutSubviews();
         }
 
-        private async void Initialize()
+        private  void Initialize()
         {
+            // Viewpoint locations for map view to zoom in and out to.
+            _zoomOutPoint = new Viewpoint(new MapPoint(-118.37, 34.46, SpatialReferences.Wgs84), 650000, 0);
+            _zoomInPoint = new Viewpoint(new MapPoint(-118.45, 34.395, SpatialReferences.Wgs84), 50000, 90);
+
+            // Set the Map property of both of the MapViews
+            _myMapViewTop.Map = new Map();
+            _myMapViewBottom.Map = new Map();
+
             // Set the top map to render all features in static rendering mode
             _myMapViewTop.Map.LoadSettings.PreferredPointFeatureRenderingMode = FeatureRenderingMode.Static;
             _myMapViewTop.Map.LoadSettings.PreferredPolylineFeatureRenderingMode = FeatureRenderingMode.Static;
             _myMapViewTop.Map.LoadSettings.PreferredPolygonFeatureRenderingMode = FeatureRenderingMode.Static;
 
             // Set the bottom map to render all features in dynamic rendering mode
-            _myMapViewBottom.Map.LoadSettings.PreferredPointFeatureRenderingMode = FeatureRenderingMode.Static;
-            _myMapViewBottom.Map.LoadSettings.PreferredPolylineFeatureRenderingMode = FeatureRenderingMode.Static;
-            _myMapViewBottom.Map.LoadSettings.PreferredPolygonFeatureRenderingMode = FeatureRenderingMode.Static;
+            _myMapViewBottom.Map.LoadSettings.PreferredPointFeatureRenderingMode = FeatureRenderingMode.Dynamic;
+            _myMapViewBottom.Map.LoadSettings.PreferredPolylineFeatureRenderingMode = FeatureRenderingMode.Dynamic;
+            _myMapViewBottom.Map.LoadSettings.PreferredPolygonFeatureRenderingMode = FeatureRenderingMode.Dynamic;
 
             // Create service feature table using a point, polyline, and polygon service.
             ServiceFeatureTable poinServiceFeatureTable = new ServiceFeatureTable(new Uri("http://sampleserver6.arcgisonline.com/arcgis/rest/services/Energy/Geology/FeatureServer/0"));
@@ -75,8 +83,6 @@ namespace ArcGISRuntimeXamarin.Samples.FeatureLayerRenderingModeMap
             FeatureLayer pointFeatureLayer = new FeatureLayer(poinServiceFeatureTable);
             FeatureLayer polylineFeatureLayer = new FeatureLayer(polylineServiceFeatureTable);
             FeatureLayer polygonFeatureLayer = new FeatureLayer(polygonServiceFeatureTable);
-
-            pointFeatureLayer.RenderingMode = FeatureRenderingMode.Dynamic;
 
             // Add each layer to top map.
             _myMapViewTop.Map.OperationalLayers.Add(pointFeatureLayer.Clone());
@@ -93,6 +99,29 @@ namespace ArcGISRuntimeXamarin.Samples.FeatureLayerRenderingModeMap
             _myMapViewBottom.SetViewpoint(_zoomOutPoint);
         }
 
+        private void CreateLayout()
+        {
+            // Create and hold reference to the used MapView
+            _myMapViewTop = new MapView();
+            _myMapViewBottom = new MapView();
+
+            // Add a button at the bottom to show webmap choices
+            _zoomButton = new UIButton(UIButtonType.Custom)
+            {
+                Frame = new CoreGraphics.CGRect(
+                    0, View.Bounds.Height - 40, View.Bounds.Width, 40),
+                BackgroundColor = UIColor.White
+            };
+
+            // Create button to show map options
+            _zoomButton.SetTitle("Zoom", UIControlState.Normal);
+            _zoomButton.SetTitleColor(UIColor.Blue, UIControlState.Normal);
+            _zoomButton.TouchUpInside += OnZoomClick;
+
+            // Add MapView to the page
+            View.AddSubviews(_myMapViewTop, _myMapViewBottom, _zoomButton);
+        }
+
         private async void OnZoomClick(object sender, EventArgs e)
         {
             // Initiate task to zoom both map views in.  
@@ -107,28 +136,6 @@ namespace ArcGISRuntimeXamarin.Samples.FeatureLayerRenderingModeMap
             Task t3 = _myMapViewTop.SetViewpointAsync(_zoomOutPoint, TimeSpan.FromSeconds(5));
             Task t4 = _myMapViewBottom.SetViewpointAsync(_zoomOutPoint, TimeSpan.FromSeconds(5));
             await Task.WhenAll(t3, t4);
-
-        }
-
-        private void CreateLayout()
-        {
-            // Add a button at the bottom to show webmap choices
-            UIButton zoomButton = new UIButton(UIButtonType.Custom)
-            {
-                Frame = new CoreGraphics.CGRect(
-                    0, View.Bounds.Height - 40, View.Bounds.Width, 40),
-                BackgroundColor = UIColor.White
-            };
-
-            // Create button to show map options
-            zoomButton.SetTitle("Maps", UIControlState.Normal);
-            zoomButton.SetTitleColor(UIColor.Blue, UIControlState.Normal);
-            zoomButton.TouchUpInside += OnZoomClick;
-
-            // Add MapView to the page
-            View.AddSubviews(_myMapViewTop, _myMapViewBottom, zoomButton);
-
-
         }
     }
 }

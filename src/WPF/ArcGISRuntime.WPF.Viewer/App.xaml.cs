82831fb9c0e049f6fa805f55df35bb763bb49e88
Nathan Castle <ncastle@esri.com>
2017-12-11 13:29:13 -0800

    Should make WPF viewer more resilient
    WIll show a message and shut down if required components are not
    installed.

--- src/WPF/ArcGISRuntime.WPF.Viewer/App.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/App.xaml.cs
@@ -3,12 +3,13 @@
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
 using ArcGISRuntime.Samples.Managers;
 using ArcGISRuntime.Samples.Models;
+using System;
 using System.Windows;
 
 namespace ArcGISRuntime.WPF.Viewer
@@ -19,7 +20,16 @@ namespace ArcGISRuntime.WPF.Viewer
         {
             var selectedLanguage = Language.CSharp;
 
-            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.Initialize();
+            try
+            {
+                Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.Initialize();
+            }
+            catch (Exception ex)
+            {
+                // Show the message and shut down
+                MessageBox.Show(String.Format("There was an error that prevented initializing the runtime. {0}", ex.Message));
+                Current.Shutdown();
+            }
 
             // Check application parameters:
             // parameter definitions:

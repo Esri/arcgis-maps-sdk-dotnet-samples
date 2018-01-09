326841c0e7daecaabcb1917432f6fc56ab70d88f
Nathan Castle <ncastle@esri.com>
2018-01-09 14:24:32 -0800

    WPF implementation

--- src/WPF/ArcGISRuntime.WPF.Samples/Samples/Analysis/QueryFeatureCountAndExtent/QueryFeatureCountAndExtent.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Samples/Samples/Analysis/QueryFeatureCountAndExtent/QueryFeatureCountAndExtent.xaml.cs
@@ -10,23 +10,24 @@
 using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
-using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Media;
 
 namespace ArcGISRuntime.WPF.Samples.QueryFeatureCountAndExtent
 {
     public partial class QueryFeatureCountAndExtent
     {
+        // URL to the feature service
+        private Uri _UsaCitiesSource = new Uri("https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/0");
+        
+        // Statistic definition that returns a count of AREANAME (city names)
+        private StatisticDefinition countStatistic = new StatisticDefinition("AREANAME", StatisticType.Count, "pop");
+
+        // Feature table to query
+        private ServiceFeatureTable _myFeatureTable;
+
         public QueryFeatureCountAndExtent()
         {
             InitializeComponent();
@@ -35,9 +36,64 @@ namespace ArcGISRuntime.WPF.Samples.QueryFeatureCountAndExtent
             Initialize();
         }
 
-        private void Initialize()
+        private async void Initialize()
         {
+            // Create the map with a vector street basemap
+            Map myMap = new Map(Basemap.CreateStreetsVector());
+
+            // Create the feature table from the service URL
+            _myFeatureTable = new ServiceFeatureTable(_UsaCitiesSource);
+
+            // Create the feature layer from the table
+            FeatureLayer myFeatureLayer = new FeatureLayer(_myFeatureTable);
+
+            // Add the feature layer to the map
+            myMap.OperationalLayers.Add(myFeatureLayer);
+
+            // Wait for the feature layer to load
+            await myFeatureLayer.LoadAsync();
+
+            // Set the map initial extent to the extent of the feature layer
+            myMap.InitialViewpoint = new Viewpoint(myFeatureLayer.FullExtent);
+
+            // Add the map to the MapView
+            MyMapView.Map = myMap;
         }
 
+        private async void btnStateCount_Click(object sender, RoutedEventArgs e)
+        {
+            // Create the statistics query parameters to control the query
+            StatisticsQueryParameters statQueryParams = new StatisticsQueryParameters(new List<StatisticDefinition>() { countStatistic });
+
+            // Limit results to matching states
+            statQueryParams.WhereClause = String.Format("upper(ST) LIKE '%{0}%'", txtStateEntry.Text.ToUpper());
+
+            // Execute the statistical query with these parameters and await the results
+            StatisticsQueryResult statQueryResult = await _myFeatureTable.QueryStatisticsAsync(statQueryParams);
+
+            // Display the results in the UI
+            txtResults.Text = statQueryResult.First().Statistics["pop"].ToString();
+        }
+
+        private async void btnExtentCount_Click(object sender, RoutedEventArgs e)
+        {
+            // Create the statistics query parameters, pass in the list of definitions
+            StatisticsQueryParameters statQueryParams = new StatisticsQueryParameters(new List<StatisticDefinition>() { countStatistic });
+
+            // Get the current extent (envelope) from the map view
+            Envelope currentExtent = MyMapView.GetCurrentViewpoint(ViewpointType.BoundingGeometry).TargetGeometry as Envelope;
+
+            // Set the statistics query parameters geometry with the envelope
+            statQueryParams.Geometry = currentExtent;
+
+            // Set the spatial relationship to Intersects (which is the default)
+            statQueryParams.SpatialRelationship = SpatialRelationship.Intersects;
+
+            // Execute the statistical query with these parameters and await the results
+            StatisticsQueryResult statQueryResult = await _myFeatureTable.QueryStatisticsAsync(statQueryParams);
+
+            // Display the results in the UI
+            txtResults.Text = statQueryResult.First().Statistics["pop"].ToString();
+        }
     }
 }

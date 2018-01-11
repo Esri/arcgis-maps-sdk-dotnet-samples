76435b7c99a9324927c3d973a2aebd26714d296b
Nathan Castle <ncastle@esri.com>
2018-01-11 11:40:48 -0800

    Scaffold out list related features sample

---
+++ src/iOS/Xamarin.iOS/Samples/Data/ListRelatedFeatures/ListRelatedFeatures.cs
@@ -0,0 +1,58 @@
+﻿// Copyright 2018 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI.Controls;
+using Foundation;
+using UIKit;
+
+namespace ArcGISRuntimeXamarin.Samples.ListRelatedFeatures
+{
+    [Register("ListRelatedFeatures")]
+    public class ListRelatedFeatures : UIViewController
+    {
+        // Create and hold reference to the used MapView
+        private MapView _myMapView = new MapView();
+
+        public ListRelatedFeatures()
+        {
+            Title = "List related features";
+        }
+
+        private void Initialize()
+        {
+            // Create new Map with basemap
+            Map myMap = new Map(Basemap.CreateImagery());
+
+            // Provide used Map to the MapView
+            _myMapView.Map = myMap;
+        }
+
+        private void CreateLayout()
+        {
+            // Add MapView to the page
+            View.AddSubviews(_myMapView);
+        }
+
+        public override void ViewDidLoad()
+        {
+            CreateLayout();
+            Initialize();
+
+            base.ViewDidLoad();
+        }
+        public override void ViewDidLayoutSubviews()
+        {
+            // Setup the visual frame for the MapView
+            _myMapView.Frame = new CoreGraphics.CGRect(0, 0, View.Bounds.Width, View.Bounds.Height);
+
+            base.ViewDidLayoutSubviews();
+        }
+    }
+}

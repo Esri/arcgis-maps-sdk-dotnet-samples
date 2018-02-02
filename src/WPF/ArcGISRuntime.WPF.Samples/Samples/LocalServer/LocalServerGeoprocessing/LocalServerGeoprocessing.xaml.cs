fbc20195d88b9e794d83a29faaad3dd0c618f7fc
Nathan Castle <ncastle@esri.com>
2018-02-02 15:37:35 -0800

    Fix typo in error message

--- src/WPF/ArcGISRuntime.WPF.Samples/Samples/LocalServer/LocalServerGeoprocessing/LocalServerGeoprocessing.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Samples/Samples/LocalServer/LocalServerGeoprocessing/LocalServerGeoprocessing.xaml.cs
@@ -79,7 +79,7 @@ namespace ArcGISRuntime.WPF.Samples.LocalServerGeoprocessing
             }
             catch (Exception ex)
             {
-                MessageBox.Show(String.Format("Please ensure that local server is installed prior to using the sample. See instructions in readme.me or metadata.json. Message: {0}", ex.Message), "Local Server failed to start");
+                MessageBox.Show(String.Format("Please ensure that local server is installed prior to using the sample. See instructions in readme.md or metadata.json. Message: {0}", ex.Message), "Local Server failed to start");
                 return;
             }
 

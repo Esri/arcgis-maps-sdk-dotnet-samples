c58c45ef1bcea11b96def470e0d5b716170a98b6
Nathan Castle <ncastle@esri.com>
2018-02-12 16:47:51 -0800

    Initial WPF implementation

--- src/WPF/ArcGISRuntime.WPF.Samples/Samples/Analysis/ViewshedLocation/ViewshedLocation.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Samples/Samples/Analysis/ViewshedLocation/ViewshedLocation.xaml.cs
@@ -7,37 +7,169 @@
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
-using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
-using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
+using Esri.ArcGISRuntime.UI.Controls;
+using Esri.ArcGISRuntime.UI.GeoAnalysis;
 
 namespace ArcGISRuntime.WPF.Samples.ViewshedLocation
 {
     public partial class ViewshedLocation
     {
+        // URL to the elevation source
+        private readonly Uri _localElevationImageService = new Uri("http://scene.arcgis.com/arcgis/rest/services/BREST_DTM_1M/ImageServer");
+
+        // URL to the buildings scene layer
+        private readonly Uri _buildingsUrl = new Uri("http://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/Buildings_Brest/SceneServer/layers/0");
+
+        // Reference to the viewshed analysis
+        private LocationViewshed _viewshed;
+
+        // Reference to the analysis overlay that will hold the viewshed analysis
+        private AnalysisOverlay _analysisOverlay;
+
+        // Flag indicating if the viewshed will move with the mouse
+        private bool _subscribedToMouseMoves;
+
         public ViewshedLocation()
         {
             InitializeComponent();
 
-            // Create the UI, setup the control references and execute initialization
+            // Initialize the sample.
             Initialize();
         }
 
         private void Initialize()
         {
+            // Create the scene with the imagery basemap.
+            Scene myScene = new Scene(Basemap.CreateImagery());
+            MySceneView.Scene = myScene;
+
+            // Add the surface elevation.
+            Surface mySurface = new Surface();
+            mySurface.ElevationSources.Add(new ArcGISTiledElevationSource(_localElevationImageService));
+            myScene.BaseSurface = mySurface;
+
+            // Add the scene layer.
+            ArcGISSceneLayer sceneLayer = new ArcGISSceneLayer(_buildingsUrl);
+            myScene.OperationalLayers.Add(sceneLayer);
+
+            // Create the MapPoint representing the initial location.
+            MapPoint initialLocation = new MapPoint(-4.5, 48.4, 100.0);
+
+            // Create the location viewshed analysis.
+            _viewshed = new LocationViewshed(
+                initialLocation, 
+                HeadingSlider.Value, 
+                PitchSlider.Value, 
+                HorizontalAngleSlider.Value, 
+                VerticalAngleSlider.Value, 
+                MinimumDistanceSlider.Value, 
+                MaximumDistanceSlider.Value);
+
+            // Create an initial camera based on the initial location.
+            Camera camera = new Camera(initialLocation, 200.0, 20.0, 70.0, 0.0);
+
+            // Apply the camera to the scene view.
+            MySceneView.SetViewpointCamera(camera);
+
+            // Create an analysis overlay for showing the viewshed analysis.
+            _analysisOverlay = new AnalysisOverlay();
+
+            // Add the viewshed analysis to the overlay.
+            _analysisOverlay.Analyses.Add(_viewshed);
+
+            // Add the analysis overlay to the SceneView.
+            MySceneView.AnalysisOverlays.Add(_analysisOverlay);
+
+            // Update the frustum outline color.
+            Viewshed.FrustumOutlineColor = Color.FromArgb(255, 0, 0, 255);
+
+            // Subscribe to tap events. This enables the 'pick up' and 'drop' workflow for moving the viewpoint.
+            MySceneView.GeoViewTapped += MySceneViewOnGeoViewTapped;
         }
 
+        private void MySceneViewOnGeoViewTapped(object sender, GeoViewInputEventArgs geoViewInputEventArgs)
+        {
+            // The viewshed observer is picked up and moving. Drop it.
+            if (_subscribedToMouseMoves)
+            {
+                MySceneView.MouseMove -= MySceneViewOnMouseMove;
+            }
+            // The viewshed observer is currently pinned. Pick it up.
+            else
+            {
+                MySceneView.MouseMove += MySceneViewOnMouseMove;
+            }
+
+            // Toggle the viewshed movement flag.
+            _subscribedToMouseMoves = !_subscribedToMouseMoves;
+        }
+
+        private void MySceneViewOnMouseMove(object sender, MouseEventArgs mouseEventArgs)
+        {
+            // Get the mouse position.
+            Point cursorSceenPoint = mouseEventArgs.GetPosition(MySceneView);
+
+            // Get the corresponding MapPoint.
+            MapPoint onMapLocation = MySceneView.ScreenToBaseSurface(cursorSceenPoint);
+
+            // Return if the MapPoint is null. This might happen if mouse leaves SceneView area.
+            if (onMapLocation == null)
+            {
+                return;
+            }
+
+            // Update the viewshed.
+            _viewshed.Location = onMapLocation;
+        }
+
+        private void HandleSettingsChange(object sender, RoutedEventArgs e)
+        {
+            // Return if viewshed hasn't been created yet. This happens when the sample is starting.
+            if (_viewshed == null)
+            {
+                return;
+            }
+            
+            // Update the viewshed settings.
+            _viewshed.Heading = HeadingSlider.Value;
+            _viewshed.Pitch = PitchSlider.Value;
+            _viewshed.HorizontalAngle = HorizontalAngleSlider.Value;
+            _viewshed.VerticalAngle = VerticalAngleSlider.Value;
+            _viewshed.MinDistance = MinimumDistanceSlider.Value;
+            _viewshed.MaxDistance = MaximumDistanceSlider.Value;
+
+            // Return if the checkboxes are in an invalid state.
+            if (AnalysisVisibilityCheck.IsChecked == null || FrustumVisibilityCheck.IsChecked == null)
+            {
+                return;
+            }
+
+            // Update visibility of the viewshed analysis.
+            if ((bool)AnalysisVisibilityCheck.IsChecked)
+            {
+                _viewshed.IsVisible = true;
+            }
+            else if (!(bool) AnalysisVisibilityCheck.IsChecked)
+            {
+                _viewshed.IsVisible = false;
+            }
+
+            // Update visibility of the frustum. Note that the frustum will be invisible 
+            //     regardless of this setting if the viewshed analysis is not visible.
+            if ((bool)FrustumVisibilityCheck.IsChecked)
+            {
+                _viewshed.IsFrustumOutlineVisible = true;
+            }
+            else
+            {
+                _viewshed.IsFrustumOutlineVisible = false;
+            }
+        }
     }
 }

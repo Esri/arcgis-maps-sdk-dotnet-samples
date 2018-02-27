5b604d00d99696563dd6c8c296862a9447d4eabf
Nathan Castle <ncastle@esri.com>
2018-02-27 14:43:37 -0800

    Update Android project structure

--- src/Android/Xamarin.Android/Samples/Network Analysis/FindRoute/FindRoute.cs
+++ src/Android/Xamarin.Android/Samples/Network Analysis/FindRoute/FindRoute.cs
@@ -26,7 +26,7 @@ namespace ArcGISRuntime.Samples.FindRoute
     [Activity(Label = "FindRoute")]
     [ArcGISRuntime.Samples.Shared.Attributes.Sample(
         "Find a route",
-        "NetworkAnalysis",
+        "Network Analysis",
         "This sample demonstrates how to solve for the best route between two locations on the map and display driving directions between them.",
         "")]
     public class FindRoute : Activity

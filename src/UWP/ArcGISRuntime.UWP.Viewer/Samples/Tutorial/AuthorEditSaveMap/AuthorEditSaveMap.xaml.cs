b724e129832c30d11e98068868f967de67e50df3
Nathan Castle <ncastle@esri.com>
2018-03-21 10:35:41 -0700

    Add example of additional class file

--- src/UWP/ArcGISRuntime.UWP.Viewer/Samples/Tutorial/AuthorEditSaveMap/AuthorEditSaveMap.xaml.cs
+++ src/UWP/ArcGISRuntime.UWP.Viewer/Samples/Tutorial/AuthorEditSaveMap/AuthorEditSaveMap.xaml.cs
@@ -8,14 +8,9 @@
 // language governing permissions and limitations under the License.
 
 using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.Portal;
 using Esri.ArcGISRuntime.Security;
 using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.UI.Controls;
 using System;
-using System.ComponentModel;
-using System.IO;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
@@ -28,7 +23,7 @@ namespace ArcGISRuntime.UWP.Samples.AuthorEditSaveMap
         "Tutorial",
         "This sample demonstrates how to author and save a map as an ArcGIS portal item (web map). It is also the solution to the [Author, edit, and save maps to your portal tutorial](https://developers.arcgis.com/net/latest/uwp/guide/author-edit-and-save-maps-to-your-portal.htm). Saving a map to arcgis.com requires an ArcGIS Online login.",
         "1. Pan and zoom to the extent you would like for your map.\n2. Choose a basemap from the list of available basemaps.\n3. Click 'Save ...' and provide info for the new portal item (Title, Description, and Tags).\n4. Click 'Save Map to Portal'.\n5. After successfully logging in to your ArcGIS Online account, the map will be saved to your default folder.\n6. You can make additional changes, update the map, and then re-save to store changes in the portal item.")]
-
+    [ArcGISRuntime.Samples.Shared.Attributes.ClassFile("MapViewModel.cs")]
     public partial class AuthorEditSaveMap
     {
         // Constants for OAuth-related values ...
@@ -189,126 +184,5 @@ namespace ArcGISRuntime.UWP.Samples.AuthorEditSaveMap
 
             return credential;
         }
-    }
-    
-    // Provides map data to an application
-    // Note: in a ArcGIS Runtime for .NET template project, this class will be in a separate file: "MapViewModel.cs"
-    public class MapViewModel : INotifyPropertyChanged
-    {        
-        // Store the map view used by the app
-        private MapView _mapView;
-        public MapView AppMapView
-        {
-            set { _mapView = value; }
-        }
-
-        // String array to store basemap constructor types
-        private string[] _basemapTypes = new string[]
-        {
-            "Topographic",
-            "Topographic Vector",
-            "Streets",
-            "Streets Vector",
-            "Imagery",
-            "Oceans"
-        };
-
-        // Read-only property to return the available basemap names
-        public string[] BasemapChoices
-        {
-            get { return _basemapTypes; }
-        }
-
-        public MapViewModel()
-        {
-            // Default constructor
-        }
-
-        // Create a default map with the vector streets basemap
-        private Map _map = new Map(Basemap.CreateStreets());
-        
-        // Gets or sets the map        
-        public Map Map
-        {
-            get { return _map; }
-            set { _map = value; OnPropertyChanged(); }
-        }
-
-        public void ChangeBasemap(string basemap)
-        {
-            // Apply the selected basemap to the map
-            switch (basemap)
-            {
-                case "Topographic":
-                    // Set the basemap to Topographic
-                    _map.Basemap = Basemap.CreateTopographic();
-                    break;
-                case "Topographic Vector":
-                    // Set the basemap to Topographic (vector)
-                    _map.Basemap = Basemap.CreateTopographicVector();
-                    break;
-                case "Streets":
-                    // Set the basemap to Streets
-                    _map.Basemap = Basemap.CreateStreets();
-                    break;
-                case "Streets Vector":
-                    // Set the basemap to Streets (vector)
-                    _map.Basemap = Basemap.CreateStreetsVector();
-                    break;
-                case "Imagery":
-                    // Set the basemap to Imagery
-                    _map.Basemap = Basemap.CreateImagery();
-                    break;
-                case "Oceans":
-                    // Set the basemap to Oceans
-                    _map.Basemap = Basemap.CreateOceans();
-                    break;
-            }
-        }
-
-        // Save the current map to ArcGIS Online. The initial extent, title, description, and tags are passed in.
-        public async Task SaveNewMapAsync(Viewpoint initialViewpoint, string title, string description, string[] tags, RuntimeImage img)
-        {
-            // Get the ArcGIS Online portal 
-            ArcGISPortal agsOnline = await ArcGISPortal.CreateAsync(new Uri("https://www.arcgis.com/sharing/rest"));
-
-            // Set the map's initial viewpoint using the extent (viewpoint) passed in
-            _map.InitialViewpoint = initialViewpoint;
-
-            // Save the current state of the map as a portal item in the user's default folder
-            await _map.SaveAsAsync(agsOnline, null, title, description, tags, img, false);
-        }
-
-        public bool MapIsSaved
-        {
-            // Return True if the current map has a value for the Item property
-            get { return (_map != null && _map.Item != null); }
-        }
-
-        public async void UpdateMapItem()
-        {
-            // Save the map
-            await _map.SaveAsync();
-            
-            // Export the current map view for the item thumbnail
-            RuntimeImage thumbnailImg = await _mapView.ExportImageAsync();
-
-            // Get the file stream from the new thumbnail image
-            Stream imageStream = await thumbnailImg.GetEncodedBufferAsync();
-
-            // Update the item thumbnail
-            (_map.Item as PortalItem).SetThumbnailWithImage(imageStream);
-            await _map.SaveAsync();
-        }
-
-        // Raises the PropertyChanged event for a property
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            var propertyChangedHandler = PropertyChanged;
-            if (propertyChangedHandler != null)
-                propertyChangedHandler(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
     }
 }

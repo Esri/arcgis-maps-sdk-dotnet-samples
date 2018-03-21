a2382f700827860b2f16986aedcbfee618b3c48a
Nathan Castle <ncastle@esri.com>
2018-03-20 18:38:25 -0700

    Removes data manager code in contexts where it won't be used

--- src/ArcGISRuntime.Samples.Shared/Managers/DataManager.cs
+++ src/ArcGISRuntime.Samples.Shared/Managers/DataManager.cs
@@ -6,7 +6,7 @@
 // Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
-
+#if !_SSG_TOOLING_
 using ArcGISRuntime.Samples.Shared.Models;
 using Esri.ArcGISRuntime.Portal;
 using System;
@@ -165,3 +165,4 @@ namespace ArcGISRuntime.Samples.Managers
         }
     }
 }
+#endif

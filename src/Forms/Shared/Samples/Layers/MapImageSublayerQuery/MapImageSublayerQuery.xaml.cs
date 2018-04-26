928d163be8e1f899256f776b63370dfd24aac031
Thad <ttilton@esri.com>
2018-04-26 13:40:45 -0700

    Forms version of map image sublayer query sample.

--- src/Forms/Shared/Samples/Layers/MapImageSublayerQuery/MapImageSublayerQuery.xaml.cs
+++ src/Forms/Shared/Samples/Layers/MapImageSublayerQuery/MapImageSublayerQuery.xaml.cs
@@ -21,7 +21,7 @@ namespace ArcGISRuntime.Samples.MapImageSublayerQuery
         "Query a map image sublayer",
         "Layers",
         "This sample demonstrates how to execute an attribute and spatial query on the sublayers of an ArcGIS map image layer. Sublayers of an ArcGISMapImageLayer may expose a ServiceFeatureTable through a Table property.This allows you to perform the same queries available when working with a table from a FeatureLayer: attribute query, spatial query, statistics query, query for related features, and so on.",
-        "1. Launch the sample, the map displays at an extent where individual states, counties, and cities can be seen clearly.\n2.Provide a numeric value for the population query(values under 1810000 will produce a selection in all layers).\n3.Click the `Query` button to find all features in the current map extent that have a population greater than the value entered.\n   - Any current selection is cleared from the map.\n   - If a non - numeric value was entered, an error message is displayed.\n4.All features(cities, counties, or states) meeting the query criteria are selected in the map.\n   - If no features meet the query criteria, a message displays stating zero features were selected.\n5.Experiment with different map extents and population values and see the results.",
+        "1. Launch the sample, the map displays at an extent where individual states, counties, and cities can be seen clearly.\n2. Provide a numeric value for the population query(values under 1810000 will produce a selection in all layers).\n3. Click the `Query` button to find all features in the current map extent that have a population greater than the value entered.\n   - Any current selection is cleared from the map.\n   - If a non - numeric value was entered, an error message is displayed.\n4. All features(cities, counties, or states) meeting the query criteria are selected in the map.\n   - If no features meet the query criteria, a message displays stating zero features were selected.\n5. Experiment with different map extents and population values and see the results.",
         "Query", "Sublayer", "MapServer", "Table")]
     public partial class MapImageSublayerQuery : ContentPage
     {
@@ -31,7 +31,7 @@ namespace ArcGISRuntime.Samples.MapImageSublayerQuery
         public MapImageSublayerQuery()
         {
             InitializeComponent ();
-
+            
             Title = "Query a map image sublayer";
 
             // Initialize the map to show the USA map image layer. 

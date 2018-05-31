bee5eb6b220d414ff30b85f5cfee86e62673e78a
Nathan Castle <ncastle@esri.com>
2018-05-31 08:04:37 -0700

    Updates per review

--- src/UWP/ArcGISRuntime.UWP.Viewer/Samples/Data/EditAndSyncFeatures/EditAndSyncFeatures.xaml.cs
+++ src/UWP/ArcGISRuntime.UWP.Viewer/Samples/Data/EditAndSyncFeatures/EditAndSyncFeatures.xaml.cs
@@ -31,7 +31,7 @@ namespace ArcGISRuntime.UWP.Samples.EditAndSyncFeatures
         "Edit and sync features",
         "Data",
         "This sample demonstrates how to synchronize offline edits with a feature service.",
-        "1. Pan and zoom to the area you would like to download features for, ensuring that all features are within the rectangle.\n2. Tap the 'generate' button. This will start the process of generating the offline geodatabase.\n3. Tap on a point feature within the area of the generated geodatabase. Then tap on the screen (anywhere within the range of the local geodatabase) to move the point to that location.\n4. Tap the 'Sync Geodatabase' button to synchronize the changes back to the feature service.\n\n Note that the basemap for this sample is downloaded from ArcGIS Online automatically.")]
+        "1. Pan and zoom to the area you would like to download point features for, ensuring that all point features are within the rectangle.\n2. Tap the 'generate' button. This will start the process of generating the offline geodatabase.\n3. Tap on a point feature within the area of the generated geodatabase. Then tap on the screen (anywhere within the range of the local geodatabase) to move the point to that location.\n4. Tap the 'Sync Geodatabase' button to synchronize the changes back to the feature service.\n\n Note that the basemap for this sample is downloaded from ArcGIS Online automatically.")]
     [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("3f1bbf0ec70b409a975f5c91f363fe7d")]
     public partial class EditAndSyncFeatures
     {

e9d515c09154115132d1faf13fe86cf05eb1c324
Nathan Castle <ncastle@esri.com>
2018-06-06 13:50:15 -0700

    New sample: Open scene (Portal item) (#469)
    * New sample: Open scene (Portal item)
    
    This sample demonstrates how to open a web scene from a portal item.
    
    * XAML cleanup per review

---
+++ src/Android/Xamarin.Android/Samples/Map/OpenScene/OpenScene.cs
@@ -0,0 +1,76 @@
+// Copyright 2018 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Android.App;
+using Android.OS;
+using Android.Widget;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Portal;
+using Esri.ArcGISRuntime.UI.Controls;
+using System;
+
+namespace ArcGISRuntime.Samples.OpenScene
+{
+    [Activity]
+    [ArcGISRuntime.Samples.Shared.Attributes.Sample(
+        "Open scene (Portal item)",
+        "Map",
+        "This sample demonstrates how to open a scene from a Portal item. Just like Web Maps are the ArcGIS format for maps, Web Scenes are the ArcGIS format for scenes. These scenes can be stored in ArcGIS Online or Portal.",
+        "The sample will load the scene automatically.")]
+    public class OpenScene : Activity
+    {
+        // Hold the ID of the portal item, which is a web scene.
+        private const string ItemId = "a13c3c3540144967bc933cb5e498b8e4";
+
+        // Create the scene view.
+        private readonly SceneView _mySceneView = new SceneView();
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Title = "Open scene (Portal item)";
+
+            // Create the UI, setup the control references and execute initialization 
+            CreateLayout();
+            Initialize();
+        }
+
+        private async void Initialize()
+        {
+            try
+            {
+                // Try to load the default portal, which will be ArcGIS Online.
+                ArcGISPortal portal = await ArcGISPortal.CreateAsync();
+
+                // Create the portal item.
+                PortalItem websceneItem = await PortalItem.CreateAsync(portal, ItemId);
+
+                // Create and show the scene.
+                _mySceneView.Scene = new Scene(websceneItem);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.ToString());
+            }
+        }
+
+        private void CreateLayout()
+        {
+            // Create a new vertical layout for the app
+            var layout = new LinearLayout(this) {Orientation = Orientation.Vertical};
+
+            // Add the map view to the layout
+            layout.AddView(_mySceneView);
+
+            // Show the layout in the app
+            SetContentView(layout);
+        }
+    }
+}
---
+++ src/iOS/Xamarin.iOS/Samples/Map/OpenScene/OpenScene.cs
@@ -0,0 +1,82 @@
+// Copyright 2018 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Portal;
+using Esri.ArcGISRuntime.UI.Controls;
+using Foundation;
+using System;
+using UIKit;
+
+namespace ArcGISRuntime.Samples.OpenScene
+{
+    [Register("OpenScene")]
+    [ArcGISRuntime.Samples.Shared.Attributes.Sample(
+        "Open scene (Portal item)",
+        "Map",
+        "This sample demonstrates how to open a scene from a Portal item. Just like Web Maps are the ArcGIS format for maps, Web Scenes are the ArcGIS format for scenes. These scenes can be stored in ArcGIS Online or Portal.",
+        "The sample will load the scene automatically.")]
+    public class OpenScene : UIViewController
+    {
+        // Hold the ID of the portal item, which is a web scene.
+        private const string ItemId = "a13c3c3540144967bc933cb5e498b8e4";
+
+        // Create the scene view.
+        private readonly SceneView _mySceneView = new SceneView();
+
+        public OpenScene()
+        {
+            Title = "Open scene (Portal item)";
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            CreateLayout();
+            Initialize();
+        }
+
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+
+            // Update the layout.
+            _mySceneView.Frame = new CoreGraphics.CGRect(0, 0, View.Bounds.Width, View.Bounds.Height);
+
+            // Get the top bar height and adjust the scene insets.
+            nfloat topMargin = NavigationController.NavigationBar.Frame.Height
+                               + UIApplication.SharedApplication.StatusBarFrame.Height;
+            _mySceneView.ViewInsets = new UIEdgeInsets(topMargin, 0, 0, 0);
+        }
+
+        private async void Initialize()
+        {
+            try
+            {
+                // Try to load the default portal, which will be ArcGIS Online.
+                ArcGISPortal portal = await ArcGISPortal.CreateAsync();
+
+                // Create the portal item.
+                PortalItem websceneItem = await PortalItem.CreateAsync(portal, ItemId);
+
+                // Create and show the scene.
+                _mySceneView.Scene = new Scene(websceneItem);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.ToString());
+            }
+        }
+
+        private void CreateLayout()
+        {
+            View.AddSubviews(_mySceneView);
+        }
+    }
+}

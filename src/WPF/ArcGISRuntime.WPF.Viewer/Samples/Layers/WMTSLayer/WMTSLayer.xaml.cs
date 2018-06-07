cb49224c4a171c3014f897ea0b4506af81308532
Zack Allen <zack.m.allen@gmail.com>
2018-06-07 14:24:28 -0700

    added load on start, also added button disabling

--- src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Layers/WMTSLayer/WMTSLayer.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Layers/WMTSLayer/WMTSLayer.xaml.cs
@@ -3,8 +3,8 @@
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
 using Esri.ArcGISRuntime.Mapping;
@@ -25,6 +25,7 @@ namespace ArcGISRuntime.WPF.Samples.WMTSLayer
         public WMTSLayer()
         {
             InitializeComponent();
+            Button1_Click(null, null);
         }
 
         private void Button1_Click(object sender, RoutedEventArgs e)
@@ -51,6 +52,10 @@ namespace ArcGISRuntime.WPF.Samples.WMTSLayer
 
                 // Assign the map to the MapView
                 MyMapView.Map = myMap;
+
+                // Disable and enable the appropriate buttons
+                Button1.IsEnabled = false;
+                Button2.IsEnabled = true;
             }
             catch (Exception ex)
             {
@@ -68,7 +73,7 @@ namespace ArcGISRuntime.WPF.Samples.WMTSLayer
                 // Define a new instance of the WMTS service
                 WmtsService myWmtsService = new WmtsService(myUri);
 
-                // Load the WMTS service 
+                // Load the WMTS service
                 await myWmtsService.LoadAsync();
 
                 // Get the service information (i.e. metadata) about the WMTS service
@@ -94,6 +99,10 @@ namespace ArcGISRuntime.WPF.Samples.WMTSLayer
 
                 // Assign the map to the MapView
                 MyMapView.Map = myMap;
+
+                // Disable and enable the appropriate buttons
+                Button1.IsEnabled = true;
+                Button2.IsEnabled = false;
             }
             catch (Exception ex)
             {

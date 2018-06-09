ec4c4e16509982fa175d0d981ddbcc1516f5867c
Nathan Castle <ncastle@esri.com>
2018-06-09 14:06:24 -0700

    Open map (URL)
    Improvements:
    
    * Switch to form UI & added help label.
    * Reformatted XAML.
    * Reformatted code behind.

--- src/UWP/ArcGISRuntime.UWP.Viewer/Samples/Map/OpenMapURL/OpenMapURL.xaml.cs
+++ src/UWP/ArcGISRuntime.UWP.Viewer/Samples/Map/OpenMapURL/OpenMapURL.xaml.cs
@@ -21,15 +21,15 @@ namespace ArcGISRuntime.UWP.Samples.OpenMapURL
         "")]
     public partial class OpenMapURL
     {
-        // String array to hold urls to publicly available web maps
-        private string[] _itemURLs = new string[]
+        // String array to hold URLs to publicly available web maps.
+        private string[] _itemURLs =
         {
             "http://www.arcgis.com/home/item.html?id=2d6fa24b357d427f9c737774e7b0f977",
             "http://www.arcgis.com/home/item.html?id=01f052c8995e4b9e889d73c3e210ebe3",
             "http://www.arcgis.com/home/item.html?id=92ad152b9da94dee89b9e387dfe21acd"
         };
 
-        // String array to store titles for the webmaps specified above. These titles are in the same order as the urls above
+        // String array to store titles for the webmaps specified above. These titles are in the same order as the URLs above.
         private string[] _titles = new string[]
         {
             "Housing with Mortgages",
@@ -41,33 +41,19 @@ namespace ArcGISRuntime.UWP.Samples.OpenMapURL
         {
             InitializeComponent();
 
-            // Setup the control references and execute initialization 
-            Initialize();
-        }
-
-        private void Initialize()
-        {
-            // Create a new Map instance with url of the webmap that is displayed by default
-            Map myMap = new Map(new Uri(_itemURLs[0]));
-
-            // Provide used Map to the MapView
-            MyMapView.Map = myMap;
-   
-            // Set titles as a items source
-            mapsChooser.ItemsSource = _titles;
-
-            // Update selected item in list
-            mapsChooser.SelectedIndex = 0;
+            // Select the first item.
+            MapList.ItemsSource = _titles;
+            MapList.SelectedIndex = 0;
         }
 
         private void OnMapsChooseSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var selectedMap = e.AddedItems[0].ToString();
+            string selectedMapName = e.AddedItems[0].ToString();
 
-            // Get index that is used to get the selected url
-            var selectedIndex = _titles.ToList().IndexOf(selectedMap);
+            // Get index that is used to get the selected URL.
+            int selectedIndex = _titles.ToList().IndexOf(selectedMapName);
 
-            // Create a new Map instance with url of the webmap that selected
+            // Create a new Map instance with URL of the webmap that selected.
             MyMapView.Map = new Map(new Uri(_itemURLs[selectedIndex]));
         }
     }

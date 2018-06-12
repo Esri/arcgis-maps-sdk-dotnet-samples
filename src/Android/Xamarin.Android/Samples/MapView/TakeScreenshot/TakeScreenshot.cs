e6313cfff32bb41bb3ec43818df5592945db8a51
Zack Allen <zack.m.allen@gmail.com>
2018-06-12 10:27:19 -0700

    Fixed implementation of Take Screenshot for most platforms
    Forms implementation is very different from the others and should probably not be changed in the same way.

--- src/Android/Xamarin.Android/Samples/MapView/TakeScreenshot/TakeScreenshot.cs
+++ src/Android/Xamarin.Android/Samples/MapView/TakeScreenshot/TakeScreenshot.cs
@@ -3,16 +3,17 @@
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
 using Android.App;
+using Android.Graphics;
 using Android.OS;
 using Android.Widget;
 using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.UI.Controls;
 using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.UI.Controls;
 using System;
 
 namespace ArcGISRuntime.Samples.TakeScreenshot
@@ -34,14 +35,14 @@ namespace ArcGISRuntime.Samples.TakeScreenshot
 
             Title = "Take screenshot";
 
-            // Create the UI, setup the control references and execute initialization 
+            // Create the UI, setup the control references and execute initialization
             CreateLayout();
             Initialize();
         }
 
         private void Initialize()
         {
-            // Create a new map instance with the basemap  
+            // Create a new map instance with the basemap
             Basemap myBasemap = Basemap.CreateStreets();
             Map myMap = new Map(myBasemap);
 
@@ -54,29 +55,28 @@ namespace ArcGISRuntime.Samples.TakeScreenshot
             try
             {
                 // Export the image from map view
-                var exportedImage = await _myMapView.ExportImageAsync();
+                Bitmap exportedImage = await (await _myMapView.ExportImageAsync()).ToImageSourceAsync();
 
                 // Create an image button (this will display the exported map view image)
                 var myImageButton = new ImageButton(this);
 
                 // Define the size of the image button to be 2/3 the size of the map view
-                myImageButton.LayoutParameters = new Android.Views.ViewGroup.LayoutParams((int)(_myMapView.Width *.667) , (int)(_myMapView.Height * .667));
+                myImageButton.LayoutParameters = new Android.Views.ViewGroup.LayoutParams((int)(_myMapView.Width * .667), (int)(_myMapView.Height * .667));
 
                 // Set the source of the image button to be that of the exported map view image
-                myImageButton.SetImageBitmap(await exportedImage.ToImageSourceAsync());
+                myImageButton.SetImageBitmap(exportedImage);
 
                 // Make the image that was captured from the map view export to fit within (aka scale-to-fit) the image button
                 myImageButton.SetScaleType(ImageView.ScaleType.FitCenter);
 
-                // Define a popup with a single image button control and make the size of the popup to be 2/3 the size of the map view 
+                // Define a popup with a single image button control and make the size of the popup to be 2/3 the size of the map view
                 var myPopupWindow = new PopupWindow(myImageButton, (int)(_myMapView.Width * .667), (int)(_myMapView.Height * .667));
 
                 // Display the popup in the middle of the map view
                 myPopupWindow.ShowAtLocation(_myMapView, Android.Views.GravityFlags.Center, 0, 0);
 
-                // Define a lambda event handler to close the popup when the user clicks on the image button 
+                // Define a lambda event handler to close the popup when the user clicks on the image button
                 myImageButton.Click += (s, a) => myPopupWindow.Dismiss();
-
             }
             catch (Exception ex)
             {
@@ -86,7 +86,6 @@ namespace ArcGISRuntime.Samples.TakeScreenshot
                 alertBuilder.SetMessage("Capturing image failed. " + ex.Message);
                 alertBuilder.Show();
             }
-
         }
 
         private void CreateLayout()

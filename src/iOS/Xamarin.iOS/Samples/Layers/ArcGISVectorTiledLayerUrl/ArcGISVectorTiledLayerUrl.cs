bdc2f2e22ee0f9dd740091f7487ac3d975e334c2
Nathan Castle <ncastle@esri.com>
2018-06-13 14:33:45 -0700

    ArcGIS vector tiled layer URL
    Changes: small improvements to control sizing.

--- src/iOS/Xamarin.iOS/Samples/Layers/ArcGISVectorTiledLayerUrl/ArcGISVectorTiledLayerUrl.cs
+++ src/iOS/Xamarin.iOS/Samples/Layers/ArcGISVectorTiledLayerUrl/ArcGISVectorTiledLayerUrl.cs
@@ -110,8 +110,8 @@ namespace ArcGISRuntime.Samples.ArcGISVectorTiledLayerUrl
         {
             // Setup the visual frames for the views 
             _myMapView.Frame = new CoreGraphics.CGRect(0, 0, View.Bounds.Width, View.Bounds.Height);
-            _toolbar.Frame = new CoreGraphics.CGRect(0, View.Bounds.Height - 50, View.Bounds.Width, 50);
-            _button.Frame = new CoreGraphics.CGRect(10, _toolbar.Frame.Top + 10, View.Bounds.Width - 20, 30);
+            _toolbar.Frame = new CoreGraphics.CGRect(0, View.Bounds.Height - 40, View.Bounds.Width, 40);
+            _button.Frame = new CoreGraphics.CGRect(5, _toolbar.Frame.Top + 5, View.Bounds.Width - 10, 30);
 
             base.ViewDidLayoutSubviews();
         }

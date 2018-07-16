8897f22136a5bac0d93362cf5368751da69fc9ba
Nathan Castle <ncastle@esri.com>
2018-07-16 15:04:48 -0700

    Feature layer rendering mode UWP improvements

--- src/UWP/ArcGISRuntime.UWP.Viewer/Samples/Layers/FeatureLayerRenderingModeMap/FeatureLayerRenderingModeMap.xaml.cs
+++ src/UWP/ArcGISRuntime.UWP.Viewer/Samples/Layers/FeatureLayerRenderingModeMap/FeatureLayerRenderingModeMap.xaml.cs
@@ -37,6 +37,10 @@ namespace ArcGISRuntime.UWP.Samples.FeatureLayerRenderingModeMap
 
         private void Initialize()
         {
+            // Create the maps.
+            MyStaticMapView.Map = new Map();
+            MyDynamicMapView.Map = new Map();
+
             // Create service feature table using a point, polyline, and polygon service.
             ServiceFeatureTable pointServiceFeatureTable = new ServiceFeatureTable(new Uri("http://sampleserver6.arcgisonline.com/arcgis/rest/services/Energy/Geology/FeatureServer/0"));
             ServiceFeatureTable polylineServiceFeatureTable = new ServiceFeatureTable(new Uri("http://sampleserver6.arcgisonline.com/arcgis/rest/services/Energy/Geology/FeatureServer/8"));
@@ -45,9 +49,9 @@ namespace ArcGISRuntime.UWP.Samples.FeatureLayerRenderingModeMap
             // Create feature layers from service feature tables
             List<FeatureLayer> featureLayers = new List<FeatureLayer>
             {
-                new FeatureLayer(pointServiceFeatureTable),
+                new FeatureLayer(polygonServiceFeatureTable),
                 new FeatureLayer(polylineServiceFeatureTable),
-                new FeatureLayer(polygonServiceFeatureTable)
+                new FeatureLayer(pointServiceFeatureTable)
             };
 
             // Add each layer to the map as a static layer and a dynamic layer
@@ -55,32 +59,32 @@ namespace ArcGISRuntime.UWP.Samples.FeatureLayerRenderingModeMap
             {
                 // Add the static layer to the top map view
                 layer.RenderingMode = FeatureRenderingMode.Static;
-                MyMapViewTop.Map.OperationalLayers.Add(layer);
+                MyStaticMapView.Map.OperationalLayers.Add(layer);
 
                 // Add the dynamic layer to the bottom map view
                 FeatureLayer dynamicLayer = (FeatureLayer)layer.Clone();
                 dynamicLayer.RenderingMode = FeatureRenderingMode.Dynamic;
-                MyMapViewBottom.Map.OperationalLayers.Add(dynamicLayer);
+                MyDynamicMapView.Map.OperationalLayers.Add(dynamicLayer);
             }
 
             // Set the view point of both MapViews.
-            MyMapViewTop.SetViewpoint(_zoomOutPoint);
-            MyMapViewBottom.SetViewpoint(_zoomOutPoint);
+            MyStaticMapView.SetViewpoint(_zoomOutPoint);
+            MyDynamicMapView.SetViewpoint(_zoomOutPoint);
         }
 
         private async void OnZoomClick(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             // Initiate task to zoom both map views in.  
-            Task t1 = MyMapViewTop.SetViewpointAsync(_zoomInPoint, TimeSpan.FromSeconds(5));
-            Task t2 = MyMapViewBottom.SetViewpointAsync(_zoomInPoint, TimeSpan.FromSeconds(5));
+            Task t1 = MyStaticMapView.SetViewpointAsync(_zoomInPoint, TimeSpan.FromSeconds(5));
+            Task t2 = MyDynamicMapView.SetViewpointAsync(_zoomInPoint, TimeSpan.FromSeconds(5));
             await Task.WhenAll(t1, t2);
 
             // Delay start of next set of zoom tasks.
             await Task.Delay(2000);
 
             // Initiate task to zoom both map views out. 
-            Task t3 = MyMapViewTop.SetViewpointAsync(_zoomOutPoint, TimeSpan.FromSeconds(5));
-            Task t4 = MyMapViewBottom.SetViewpointAsync(_zoomOutPoint, TimeSpan.FromSeconds(5));
+            Task t3 = MyStaticMapView.SetViewpointAsync(_zoomOutPoint, TimeSpan.FromSeconds(5));
+            Task t4 = MyDynamicMapView.SetViewpointAsync(_zoomOutPoint, TimeSpan.FromSeconds(5));
             await Task.WhenAll(t3, t4);
 
         }

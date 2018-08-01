e07683d0890ddad8a834d2458689f28d1904f5c9
Thad <ttilton@esri.com>
2018-08-01 14:43:15 -0700

    Android Forms fix for OAuth sample: added static property to the MainActivity class to provide the app context.

--- src/Forms/Android/MainActivity.cs
+++ src/Forms/Android/MainActivity.cs
@@ -16,13 +16,16 @@ namespace ArcGISRuntime.Droid
 	[Activity (Label = "ArcGIS Runtime SDK for .NET", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
-		protected override void OnCreate (Bundle bundle)
+        internal static MainActivity Instance { get; private set; }
+
+        protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
+
+            Instance = this;
 
             Xamarin.Forms.Forms.Init (this, bundle);
 			LoadApplication (new App ());
 		}
-	}
+    }
 }
-

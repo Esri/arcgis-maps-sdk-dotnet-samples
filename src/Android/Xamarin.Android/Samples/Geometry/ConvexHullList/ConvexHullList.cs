c938d96cac75a18bece357b857686d410c4d3a06
Zack Allen <zack.m.allen@gmail.com>
2018-08-02 11:46:05 -0700

    ConvexHullList - Shortened the instructions

--- src/Android/Xamarin.Android/Samples/Geometry/ConvexHullList/ConvexHullList.cs
+++ src/Android/Xamarin.Android/Samples/Geometry/ConvexHullList/ConvexHullList.cs
@@ -236,9 +236,7 @@ namespace ArcGISRuntime.Samples.ConvexHullList
             // Create a TextView for the sample instructions.
             TextView sampleInstructionsTextView = new TextView(this)
             {
-                Text = "Click the 'ConvexHull' button to create convex hull(s) from the polygon graphics. If the 'Union' checkbox " +
-                "is checked, the resulting output will be one polygon being the convex hull for the two input polygons. If the 'Union' checkbox is un-checked, " +
-                "the resulting output will have two convex hull polygons - one for each of the two input polygons."
+                Text = "Click the 'Convex Hull' button to create convex hull(s) from the polygon graphics."
             };
             layout.AddView(sampleInstructionsTextView);
 

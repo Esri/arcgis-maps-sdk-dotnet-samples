76f8b3c6e05a34fa8b91b5102b7e518b9f8da782
Nathan Castle <ncastle@esri.com>
2018-10-10 15:53:58 -0700

    Add missing using

--- src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Layers/IdentifyKmlFeatures/IdentifyKmlFeatures.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Layers/IdentifyKmlFeatures/IdentifyKmlFeatures.xaml.cs
@@ -8,6 +8,7 @@
 // language governing permissions and limitations under the License.
 
 using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Ogc;
 using Esri.ArcGISRuntime.UI.Controls;

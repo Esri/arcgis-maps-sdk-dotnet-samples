dc879d828c224331a2f3bb7c4ca3ade1fae6ad38
Nathan Castle <ncastle@esri.com>
2018-11-26 12:40:09 -0800

    Fix crash in open map URL (Forms)
    If you click the button twice, the action sheet is canceled and no map title is selected.

--- src/Forms/Shared/Samples/Map/OpenMapURL/OpenMapURL.xaml.cs
+++ src/Forms/Shared/Samples/Map/OpenMapURL/OpenMapURL.xaml.cs
@@ -61,7 +61,7 @@ namespace ArcGISRuntime.Samples.OpenMapURL
                 await ((Page)Parent).DisplayActionSheet("Select map", "Cancel",null, titles);
 
             // If selected cancel do nothing
-            if (selectedMapTitle == "Cancel") return;
+            if (selectedMapTitle == null || selectedMapTitle == "Cancel") return;
 
             // Get index that is used to get the selected url
             int selectedIndex = titles.ToList().IndexOf(selectedMapTitle);

144ad46361a1f762ffcbf79034baee1fdaa3dc1f
Nathan Castle <ncastle@esri.com>
2019-01-09 15:49:34 -0800

    Update token secured challenge for iPhone X

--- src/iOS/Xamarin.iOS/Samples/Security/TokenSecuredChallenge/TokenSecuredChallenge.cs
+++ src/iOS/Xamarin.iOS/Samples/Security/TokenSecuredChallenge/TokenSecuredChallenge.cs
@@ -45,9 +45,6 @@ namespace ArcGISRuntimeXamarin.Samples.TokenSecuredChallenge
         private UILabel _publicLayerLabel;
         private UILabel _secureLayerLabel;
 
-        // View containing login controls to display over the map view.
-        private LoginOverlay _loginUI;
-
         public TokenSecuredChallenge()
         {
             Title = "Token Challenge";
@@ -56,50 +53,7 @@ namespace ArcGISRuntimeXamarin.Samples.TokenSecuredChallenge
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-
-            // Call a function to create the user interface.
-            CreateLayout();
-
-            // Call a function to initialize the app.
             Initialize();
-        }
-
-        public override void ViewDidLayoutSubviews()
-        {
-            base.ViewDidLayoutSubviews();
-
-            // Setup the visual frame for the MapView and status labels.
-            CGRect mapViewFrame = new CGRect(0, 120, View.Bounds.Width, View.Bounds.Height - 120);
-            CGRect label1Frame = new CGRect(10, 70, View.Bounds.Width - 10, 20);
-            CGRect label2Frame = new CGRect(10, 95, View.Bounds.Width - 10, 20);
-
-            // Apply the layout frames.
-            _myMapView.Frame = mapViewFrame;
-            _publicLayerLabel.Frame = label1Frame;
-            _secureLayerLabel.Frame = label2Frame;
-        }
-
-        private void CreateLayout()
-        {
-            // Create a label for showing the load status for the public service.
-            _publicLayerLabel = new UILabel()
-            {
-                TextColor = UIColor.Gray,
-                Text = _publicLayerName
-            };
-
-            // Create a label to show the load status of the secured layer.
-            _secureLayerLabel = new UILabel()
-            {
-                TextColor = UIColor.Gray,
-                Text = _secureLayerName
-            };
-            
-            // Create the map view control.
-            _myMapView = new MapView();
-
-            // Add the map view and button to the page.
-            View.AddSubviews(_publicLayerLabel, _secureLayerLabel, _myMapView);
         }
 
         private void Initialize()
@@ -202,27 +156,70 @@ namespace ArcGISRuntimeXamarin.Samples.TokenSecuredChallenge
 
         private void ShowLoginUI()
         {
-            // Get the URL for the service being requested.
-            CredentialRequestInfo info = (CredentialRequestInfo)_loginTaskCompletionSource.Task.AsyncState;
-            string serviceUrl = info.ServiceUri.GetLeftPart(UriPartial.Path);
+            // Prompt for the type of convex hull to create.
+            UIAlertController loginAlert = UIAlertController.Create("Authenticate", "", UIAlertControllerStyle.Alert);
+            loginAlert.AddTextField(field => field.Placeholder = "Username = user1");
+            loginAlert.AddTextField(field => field.Placeholder = "Password = user1");
+            loginAlert.AddAction(UIAlertAction.Create("Log in", UIAlertActionStyle.Default, action => {
+                LoginEntered(loginAlert.TextFields[0].Text, loginAlert.TextFields[1].Text);
+            }));
+            loginAlert.AddAction(UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, null));
 
-            // Create a view to show login controls over the map view.
-            CGRect ovBounds = new CGRect(0, 80, _myMapView.Bounds.Width, _myMapView.Bounds.Height - 80);
-            _loginUI = new LoginOverlay(ovBounds, 0.85f, UIColor.DarkGray, serviceUrl);
+            // Show the alert.
+            PresentViewController(loginAlert, true, null);
+        }
 
-            // Handle the login event to get the login entered by the user.
-            _loginUI.OnLoginInfoEntered += LoginEntered;
+        public override void LoadView()
+        {
+            View = new UIView { BackgroundColor = UIColor.White };
 
-            // Handle the cancel event when the user closes the dialog without entering a login.
-            _loginUI.OnCanceled += LoginCanceled;
+            _publicLayerLabel = new UILabel
+            {
+                Text = "public layer",
+                AdjustsFontSizeToFitWidth = true,
+                TextAlignment = UITextAlignment.Center,
+                BackgroundColor = UIColor.FromWhiteAlpha(0, .6f),
+                TextColor = UIColor.White,
+                Lines = 1,
+                TranslatesAutoresizingMaskIntoConstraints = false
+            };
 
-            // Add the login UI view (will display semi-transparent over the map view)
-            View.Add(_loginUI);
+            _secureLayerLabel = new UILabel
+            {
+                Text = "secure layer",
+                AdjustsFontSizeToFitWidth = true,
+                TextAlignment = UITextAlignment.Center,
+                BackgroundColor = UIColor.FromWhiteAlpha(0, .6f),
+                TextColor = UIColor.White,
+                Lines = 1,
+                TranslatesAutoresizingMaskIntoConstraints = false
+            };
+
+            _myMapView = new MapView();
+            _myMapView.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            View.AddSubviews(_myMapView, _publicLayerLabel, _secureLayerLabel);
+
+            NSLayoutConstraint.ActivateConstraints(new[]
+            {
+                _publicLayerLabel.TopAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.TopAnchor),
+                _publicLayerLabel.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor),
+                _publicLayerLabel.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor),
+                _publicLayerLabel.HeightAnchor.ConstraintEqualTo(40),
+                _secureLayerLabel.TopAnchor.ConstraintEqualTo(_publicLayerLabel.BottomAnchor),
+                _secureLayerLabel.LeadingAnchor.ConstraintEqualTo(_publicLayerLabel.LeadingAnchor),
+                _secureLayerLabel.TrailingAnchor.ConstraintEqualTo(_publicLayerLabel.TrailingAnchor),
+                _secureLayerLabel.HeightAnchor.ConstraintEqualTo(40),
+                _myMapView.TopAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.TopAnchor),
+                _myMapView.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor),
+                _myMapView.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor),
+                _myMapView.BottomAnchor.ConstraintEqualTo(View.BottomAnchor)
+            });
         }
 
         // Handle the OnLoginEntered event from the login UI.
         // LoginEventArgs contains the username and password that were entered.
-        private async void LoginEntered(object sender, LoginEventArgs e)
+        private async void LoginEntered(string username, string password)
         {
             // Make sure the task completion source has all the information needed.
             if (_loginTaskCompletionSource == null ||
@@ -240,8 +237,8 @@ namespace ArcGISRuntimeXamarin.Samples.TokenSecuredChallenge
                 // Create a token credential using the provided username and password.
                 TokenCredential userCredentials = await AuthenticationManager.Current.GenerateCredentialAsync
                                             (requestInfo.ServiceUri,
-                                             e.Username,
-                                             e.Password,
+                                             username,
+                                             password,
                                              requestInfo.GenerateTokenOptions);
 
                 // Set the result on the task completion source.
@@ -252,183 +249,6 @@ namespace ArcGISRuntimeXamarin.Samples.TokenSecuredChallenge
                 // Unable to create credential, set the exception on the task completion source.
                 _loginTaskCompletionSource.TrySetException(ex);
             }
-            finally
-            {
-                // Get rid of the login controls.
-                _loginUI.Hide();
-                _loginUI = null;
-            }
-        }
-
-        private void LoginCanceled(object sender, EventArgs e)
-        {
-            // Remove the login UI.
-            _loginUI.Hide();
-            _loginUI = null;
-
-            // Cancel the task completion source task.
-            _loginTaskCompletionSource.TrySetCanceled();
-        }
-    }
-
-    // View containing login entries (username and password).
-    public class LoginOverlay : UIView
-    {
-        // Event to provide login information when the user dismisses the view.
-        public event EventHandler<LoginEventArgs> OnLoginInfoEntered;
-
-        // Event to report that the login was canceled.
-        public event EventHandler OnCanceled;
-
-        // Store the username and password so the values can be read.
-        private UITextField _usernameTextField;
-        private UITextField _passwordTextField;
-
-        public LoginOverlay(CGRect frame, nfloat transparency, UIColor color, string url) : base(frame)
-        {
-            // Create a semi-transparent overlay with the specified background color.
-            BackgroundColor = color;
-            Alpha = transparency;
-
-            // Set size and spacing for controls.
-            nfloat controlHeight = 25;
-            nfloat rowSpace = 11;
-            nfloat buttonSpace = 15;
-            nfloat textViewWidth = Frame.Width - 60;
-            nfloat buttonWidth = 60;
-
-            // Get the total height and width of the control set (five rows of controls, four sets of space).
-            nfloat totalHeight = (5 * controlHeight) + (4 * rowSpace);
-            nfloat totalWidth = textViewWidth;
-
-            // Find the center x and y of the view.
-            nfloat centerX = Frame.Width / 2;
-            nfloat centerY = Frame.Height / 2;
-
-            // Find the start x and y for the control layout.
-            nfloat controlX = centerX - totalWidth / 2;
-            nfloat controlY = centerY - totalHeight / 2;
-
-            // Set a title.
-            UILabel titleTextBlock = new UILabel(new CGRect(controlX, controlY, textViewWidth, controlHeight))
-            {
-                Text = "Login to:"
-            };
-
-            // Adjust the Y position for the next control.
-            controlY = controlY + controlHeight + rowSpace;
-
-            // Service URL for which the user is logging in.
-            UILabel urlTextBlock = new UILabel(new CGRect(controlX, controlY, textViewWidth, controlHeight))
-            {
-                Text = url,
-                TextColor = UIColor.Blue,
-                Lines = 2,
-                LineBreakMode = UILineBreakMode.CharacterWrap
-            };
-            urlTextBlock.Font = urlTextBlock.Font.WithSize(10);
-
-            // Adjust the Y position for the next control.
-            controlY = controlY + controlHeight + rowSpace;
-
-            // Username text input.
-            _usernameTextField = new UITextField(new CGRect(controlX, controlY, textViewWidth, controlHeight))
-            {
-                Placeholder = "Username = user1",
-                AutocapitalizationType = UITextAutocapitalizationType.None,
-                BackgroundColor = UIColor.LightGray
-            };
-
-            // Adjust the Y position for the next control.
-            controlY = controlY + controlHeight + rowSpace;
-
-            // Password text input
-            _passwordTextField = new UITextField(new CGRect(controlX, controlY, textViewWidth, controlHeight))
-            {
-                SecureTextEntry = true,
-                Placeholder = "Password = user1",
-                AutocapitalizationType = UITextAutocapitalizationType.None,
-                BackgroundColor = UIColor.LightGray
-            };
-
-            // Adjust the Y position for the next control.
-            controlY = controlY + controlHeight + rowSpace;
-
-            // Button to submit the login information.
-            UIButton loginButton = new UIButton(new CGRect(controlX, controlY, buttonWidth, controlHeight));
-            loginButton.SetTitle("Login", UIControlState.Normal);
-            loginButton.SetTitleColor(UIColor.Blue, UIControlState.Normal);
-            loginButton.TouchUpInside += LoginButtonClick;
-
-            // Adjust the X position for the next control.
-            controlX = controlX + buttonWidth + buttonSpace;
-
-            // Button to cancel the login.
-            UIButton cancelButton = new UIButton(new CGRect(controlX, controlY, buttonWidth, controlHeight));
-            cancelButton.SetTitle("Cancel", UIControlState.Normal);
-            cancelButton.SetTitleColor(UIColor.Blue, UIControlState.Normal);
-            cancelButton.TouchUpInside += (s, e) => { OnCanceled?.Invoke(this, null); };
-
-            // Add the controls.
-            AddSubviews(titleTextBlock, urlTextBlock, _usernameTextField, _passwordTextField, loginButton, cancelButton);
-        }
-
-        // Animate increasing transparency to completely hide the view, then remove it.
-        public void Hide()
-        {
-            // Action to make the view transparent.
-            Action makeTransparentAction = () => Alpha = 0;
-
-            // Action to remove the view.
-            Action removeViewAction = () => RemoveFromSuperview();
-
-            // Time to complete the animation (seconds).
-            double secondsToComplete = 0.75;
-
-            // Animate transparency to zero, then remove the view.
-            Animate(secondsToComplete, makeTransparentAction, removeViewAction);
-        }
-
-        private void LoginButtonClick(object sender, EventArgs e)
-        {
-            // Get the values entered in the text fields.
-            string username = _usernameTextField.Text.Trim();
-            string password = _passwordTextField.Text.Trim();
-
-            // Make sure the user entered all values.
-            if (String.IsNullOrEmpty(username) ||
-                String.IsNullOrEmpty(password))
-            {
-                new UIAlertView("Login", "Please enter a username and password", (IUIAlertViewDelegate)null, "OK", null).Show();
-                return;
-            }
-
-            // Fire the OnLoginInfoEntered event and provide the login values.
-            if (OnLoginInfoEntered != null)
-            {
-                // Create a new LoginEventArgs to contain the user's values.
-                LoginEventArgs loginEventArgs = new LoginEventArgs(username, password);
-
-                // Raise the event.
-                OnLoginInfoEntered(sender, loginEventArgs);
-            }
-        }
-    }
-
-    // Custom EventArgs implementation to hold login information (username and password).
-    public class LoginEventArgs : EventArgs
-    {
-        // User name property.
-        public string Username { get; set; }
-
-        // Password property.
-        public string Password { get; set; }
-
-        // Store login values passed into the constructor.
-        public LoginEventArgs(string username, string password)
-        {
-            Username = username;
-            Password = password;
         }
     }
 }

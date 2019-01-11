68ea6fa2a9b3a14108736ec00b3757480df0e397
Nathan Castle <ncastle@esri.com>
2019-01-10 17:18:41 -0800

    Improvements to setting data path

--- src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Local Server/DynamicWorkspaceRaster/DynamicWorkspaceRaster.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Local Server/DynamicWorkspaceRaster/DynamicWorkspaceRaster.xaml.cs
@@ -46,10 +46,16 @@ namespace ArcGISRuntime.WPF.Samples.DynamicWorkspaceRaster
 
             try
             {
-                // Set the local data path - must be done before starting. On most systems, this will be C:\Esri_LS_Data.
+                // LocalServer must not be running when setting the data path.
+                if (LocalServer.Instance.Status == LocalServerStatus.Started)
+                {
+                    await LocalServer.Instance.StopAsync();
+                }
+
+                // Set the local data path - must be done before starting. On most systems, this will be C:\EsriSamples\AppData.
                 // This path should be kept short to avoid Windows path length limitations.
                 string tempDataPathRoot = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.Windows)).FullName;
-                string tempDataPath = Path.Combine(tempDataPathRoot, "Esri_LS_Data");
+                string tempDataPath = Path.Combine(tempDataPathRoot, "EsriSamples", "AppData");
                 Directory.CreateDirectory(tempDataPath); // CreateDirectory won't overwrite if it already exists.
                 LocalServer.Instance.AppDataPath = tempDataPath;
 

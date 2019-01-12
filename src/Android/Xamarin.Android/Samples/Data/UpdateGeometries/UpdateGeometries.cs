ad17bf724db59faeb3fd478f69a4452f3f1d56b9
Nathan Castle <ncastle@esri.com>
2019-01-11 17:05:32 -0800

    Android implementation

--- src/Android/Xamarin.Android/Samples/Data/UpdateGeometries/UpdateGeometries.cs
+++ src/Android/Xamarin.Android/Samples/Data/UpdateGeometries/UpdateGeometries.cs
@@ -13,12 +13,10 @@ using Android.Widget;
 using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
-using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
 using Esri.ArcGISRuntime.UI.Controls;
+using System;
+using System.Linq;
+using Android.Views;
 
 namespace ArcGISRuntimeXamarin.Samples.UpdateGeometries
 {
@@ -30,8 +28,17 @@ namespace ArcGISRuntimeXamarin.Samples.UpdateGeometries
         "")]
     public class UpdateGeometries : Activity
     {
-        // Create and hold reference to the used MapView.
-        private readonly MapView _myMapView = new MapView();
+        // Hold a reference to the MapView.
+        private MapView _myMapView;
+
+        // URL to the feature service.
+        private const string FeatureServiceUrl = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/DamageAssessment/FeatureServer/0";
+
+        // Hold a reference to the feature layer.
+        private FeatureLayer _damageLayer;
+
+        // Hold a reference to the selected feature.
+        private ArcGISFeature _selectedFeature;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -45,17 +52,127 @@ namespace ArcGISRuntimeXamarin.Samples.UpdateGeometries
 
         private void Initialize()
         {
-            // Create new Map with basemap.
-            Map myMap = new Map(Basemap.CreateImagery());
-            
-            // Provide used Map to the MapView.
-            _myMapView.Map = myMap;
+            // Create the map with streets basemap.
+            _myMapView.Map = new Map(Basemap.CreateStreets());
+
+            // Create the feature table, referring to the Damage Assessment feature service.
+            ServiceFeatureTable damageTable = new ServiceFeatureTable(new Uri(FeatureServiceUrl));
+
+            // Create a feature layer to visualize the features in the table.
+            _damageLayer = new FeatureLayer(damageTable);
+
+            // Add the layer to the map.
+            _myMapView.Map.OperationalLayers.Add(_damageLayer);
+
+            // Listen for user taps on the map - on tap, a feature will be selected.
+            _myMapView.GeoViewTapped += MapView_Tapped;
+
+            // Zoom to the United States.
+            _myMapView.SetViewpointCenterAsync(new MapPoint(-10800000, 4500000, SpatialReferences.WebMercator), 3e7);
         }
 
+        private void MapView_Tapped(object sender, GeoViewInputEventArgs e)
+        {
+            // Select the feature if none selected, move the feature otherwise.
+            if (_selectedFeature == null)
+            {
+                // Select the feature.
+                TrySelectFeature(e);
+            }
+            else
+            {
+                // Move the feature.
+                MoveSelectedFeature(e);
+            }
+        }
+
+        private async void MoveSelectedFeature(GeoViewInputEventArgs tapEventDetails)
+        {
+            try
+            {
+                // Get the MapPoint from the EventArgs for the tap.
+                MapPoint destinationPoint = tapEventDetails.Location;
+
+                // Load the feature.
+                await _selectedFeature.LoadAsync();
+
+                // Update the geometry of the selected feature.
+                _selectedFeature.Geometry = destinationPoint;
+
+                // Apply the edit to the feature table.
+                await _selectedFeature.FeatureTable.UpdateFeatureAsync(_selectedFeature);
+
+                // Push the update to the service.
+                ServiceFeatureTable serviceTable = (ServiceFeatureTable) _selectedFeature.FeatureTable;
+                await serviceTable.ApplyEditsAsync();
+                ShowMessage("Success!", $"Moved feature {_selectedFeature.Attributes["objectid"]}");
+            }
+            catch (Exception ex)
+            {
+                ShowMessage("Error when moving feature", ex.ToString());
+            }
+            finally
+            {
+                // Reset the selection.
+                _damageLayer.ClearSelection();
+                _selectedFeature = null;
+            }
+        }
+
+        private async void TrySelectFeature(GeoViewInputEventArgs tapEventDetails)
+        {
+            try
+            {
+                // Perform an identify to determine if a user tapped on a feature.
+                IdentifyLayerResult identifyResult = await _myMapView.IdentifyLayerAsync(_damageLayer, tapEventDetails.Position, 10, false);
+
+                // Do nothing if there are no results.
+                if (!identifyResult.GeoElements.Any())
+                {
+                    return;
+                }
+
+                // Otherwise, get the ID of the first result.
+                long featureId = (long) identifyResult.GeoElements.First().Attributes["objectid"];
+
+                // Get the feature by constructing a query and running it.
+                QueryParameters qp = new QueryParameters();
+                qp.ObjectIds.Add(featureId);
+                FeatureQueryResult queryResult = await _damageLayer.FeatureTable.QueryFeaturesAsync(qp);
+                _selectedFeature = (ArcGISFeature) queryResult.First();
+
+                // Select the feature.
+                _damageLayer.SelectFeature(_selectedFeature);
+            }
+            catch (Exception ex)
+            {
+                ShowMessage("Problem selecting feature", ex.ToString());
+            }
+        }
+
+        private void ShowMessage(string title, string message)
+        {
+            // Display the message to the user.
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.SetMessage(message).SetTitle(title).Show();
+        }
+        
         private void CreateLayout()
         {
             // Create a new vertical layout for the app.
-            var layout = new LinearLayout(this) { Orientation = Orientation.Vertical };
+            var layout = new LinearLayout(this) {Orientation = Orientation.Vertical};
+
+            // Create the help label.
+            TextView helpLabel = new TextView(this);
+            helpLabel.Text = "Tap to select a feature. Tap again to move it.";
+            helpLabel.TextAlignment = TextAlignment.Center;
+            helpLabel.Gravity = GravityFlags.Center;
+
+            // Add the help label to the layout.
+            layout.AddView(helpLabel);
+
+            // Create the map view.
+            _myMapView = new MapView();
 
             // Add the map view to the layout.
             layout.AddView(_myMapView);

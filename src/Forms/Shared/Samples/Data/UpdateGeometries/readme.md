a9bc10291cc4ae144d997ed4c3c2669f11e2f887
Nathan Castle <ncastle@esri.com>
2019-01-11 16:34:21 -0800

    Scaffold and initial implementation for Update Geometries

---
+++ src/Forms/Shared/Samples/Data/UpdateGeometries/readme.md
@@ -0,0 +1,27 @@
+﻿# Update geometries (feature service)
+
+Update a feature's location in an online feature service.
+
+## How to use the sample
+
+Tap a feature to select it. Tap again to set the updated location for that feature. An alert will be shown confirming success or failure.
+
+## How it works
+
+To get a Feature from a ServiceFeatureTable and change it's geometry:
+
+1. Create a service feature table from a URL.
+2. Create a FeatureLayer from the service feature table.
+3. Select features from the feature layer, `FeatureLayer.SelectFeatures`.
+4. Change the selected feature's location using `Feature.Geometry = geometry`.
+5. After a change, update the table on the server using `ServiceFeatureTable.ApplyEditsAsync()`.
+
+## Relevant API
+
+* Map
+* Feature
+* FeatureLayer
+* MapView
+* ServiceFeatureTable
+
+## Tags
---
+++ src/iOS/Xamarin.iOS/Samples/Data/UpdateGeometries/UpdateGeometries.cs
@@ -0,0 +1,69 @@
+﻿// Copyright 2019 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.Tasks.Offline;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.ArcGISServices;
+using Esri.ArcGISRuntime.UI.Controls;
+using Foundation;
+using UIKit;
+
+namespace ArcGISRuntimeXamarin.Samples.UpdateGeometries
+{
+    [Register("UpdateGeometries")]
+    [ArcGISRuntime.Samples.Shared.Attributes.Sample(
+        "Update geometries (feature service)",
+        "Data",
+        "Update a feature's location in an online feature service.",
+        "")]
+    public class UpdateGeometries : UIViewController
+    {
+        // Hold references to UI controls.
+        private MapView _myMapView;
+
+        public UpdateGeometries()
+        {
+            Title = "Update geometries (feature service)";
+        }
+
+        private void Initialize()
+        {
+            // Create new Map with basemap.
+            Map myMap = new Map(Basemap.CreateImagery());
+
+            // Provide used Map to the MapView.
+            _myMapView.Map = myMap;
+        }
+
+        public override void LoadView()
+        {
+            _myMapView = new MapView();
+            _myMapView.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            View = new UIView();
+            View.AddSubviews(_myMapView);
+
+            _myMapView.TopAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.TopAnchor).Active = true;
+            _myMapView.BottomAnchor.ConstraintEqualTo(View.BottomAnchor).Active = true;
+            _myMapView.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor).Active = true;
+            _myMapView.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor).Active = true;
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            Initialize();
+        }
+    }
+}

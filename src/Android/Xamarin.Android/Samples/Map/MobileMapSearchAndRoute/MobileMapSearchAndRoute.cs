d03bbdc63b322126c5671cb11c4fd1b6733551e3
Nathan Castle <nathan.castle@outlook.com>
2019-01-29 18:15:18 -0800

    Implementations for UWP, WPF, and Forms

---
+++ src/Android/Xamarin.Android/Samples/Map/MobileMapSearchAndRoute/MobileMapSearchAndRoute.cs
@@ -0,0 +1,68 @@
+// Copyright 2019 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Android.App;
+using Android.OS;
+using Android.Widget;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.Tasks.Offline;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.ArcGISServices;
+using Esri.ArcGISRuntime.UI.Controls;
+
+namespace ArcGISRuntimeXamarin.Samples.MobileMapSearchAndRoute
+{
+    [Activity]
+    [ArcGISRuntime.Samples.Shared.Attributes.Sample(
+        "Mobile map (search and route)",
+        "Map",
+        "Display maps and use locators to enable search and routing offline using a Mobile Map Package.",
+        "")]
+    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("260eb6535c824209964cf281766ebe43")]
+    public class MobileMapSearchAndRoute : Activity
+    {
+        // Create and hold reference to the used MapView.
+        private readonly MapView _myMapView = new MapView();
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Title = "Mobile map (search and route)";
+
+            CreateLayout();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            // Create new Map with basemap.
+            Map myMap = new Map(Basemap.CreateImagery());
+            
+            // Provide used Map to the MapView.
+            _myMapView.Map = myMap;
+        }
+
+        private void CreateLayout()
+        {
+            // Create a new vertical layout for the app.
+            var layout = new LinearLayout(this) { Orientation = Orientation.Vertical };
+
+            // Add the map view to the layout.
+            layout.AddView(_myMapView);
+
+            // Show the layout in the app.
+            SetContentView(layout);
+        }
+    }
+}

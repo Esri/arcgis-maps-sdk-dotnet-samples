908f31c50ff080b2ad9574609cca3b52ef824b44
Nathan Castle <ncastle@esri.com>
2019-03-11 13:51:43 -0700

    New sample: Add point scene layer

---
+++ src/Android/Xamarin.Android/Samples/Layers/AddPointSceneLayer/AddPointSceneLayer.cs
@@ -0,0 +1,68 @@
+// Copyright 2019 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Android.App;
+using Android.OS;
+using Android.Widget;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI.Controls;
+using System;
+
+namespace ArcGISRuntimeXamarin.Samples.AddPointSceneLayer
+{
+    [Activity (ConfigurationChanges=Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
+    [ArcGISRuntime.Samples.Shared.Attributes.Sample(
+        "Add a point scene layer",
+        "Layers",
+        "View a point scene layer from a scene service.",
+        "")]
+    public class AddPointSceneLayer : Activity
+    {
+        // Hold references to the UI controls.
+        private SceneView _mySceneView;
+
+        // URL for the service with the point scene layer.
+        private const string PointSceneServiceUri = "https://tiles.arcgis.com/tiles/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Airports_PointSceneLayer/SceneServer/layers/0";
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Title = "Add a point scene layer";
+
+            CreateLayout();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            // Create the scene.
+            _mySceneView.Scene = new Scene(Basemap.CreateImagery());
+
+            // Create the layer.
+            ArcGISSceneLayer pointSceneLayer = new ArcGISSceneLayer(new Uri(PointSceneServiceUri));
+
+            // Show the layer in the scene.
+            _mySceneView.Scene.OperationalLayers.Add(pointSceneLayer);
+        }
+
+        private void CreateLayout()
+        {
+            // Create a new vertical layout for the app.
+            var layout = new LinearLayout(this) {Orientation = Orientation.Vertical};
+
+            // Add the map view to the layout.
+            _mySceneView = new SceneView();
+            layout.AddView(_mySceneView);
+
+            // Show the layout in the app.
+            SetContentView(layout);
+        }
+    }
+}

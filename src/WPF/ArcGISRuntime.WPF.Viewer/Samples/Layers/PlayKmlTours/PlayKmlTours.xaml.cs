92588e72e9e549731efebfeace0b0fb9316db3df
Nathan Castle <ncastle@esri.com>
2019-03-13 19:02:57 -0700

    New sample: Display KML tour (#613)
    * New sample: Display KML tour
    
    * Update for sample design change
    
    * Fix issues from last commit
    
    * Readme update

---
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Layers/PlayKmlTours/PlayKmlTours.xaml.cs
@@ -0,0 +1,161 @@
+﻿// Copyright 2019 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Ogc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using ArcGISRuntime.Samples.Managers;
+
+namespace ArcGISRuntime.WPF.Samples.PlayKmlTours
+{
+    [ArcGISRuntime.Samples.Shared.Attributes.Sample(
+        "Play a KML tour",
+        "Layers",
+        "Play tours in KML files.",
+        "")]
+    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("f10b1d37fdd645c9bc9b189fb546307c")]
+    public partial class PlayKmlTours
+    {
+        // The KML tour controller provides player controls for KML tours.
+        private readonly KmlTourController _tourController = new KmlTourController();
+
+        public PlayKmlTours()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private async void Initialize()
+        {
+            // Load the scene with a basemap and a terrain surface.
+            MySceneView.Scene = new Scene(Basemap.CreateImagery());
+            MySceneView.Scene.BaseSurface.ElevationSources.Add(new ArcGISTiledElevationSource(new Uri("https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")));
+
+            // Get the URL to the data.
+            string filePath = DataManager.GetDataFolder("f10b1d37fdd645c9bc9b189fb546307c", "Esri_tour.kmz");
+            Uri kmlUrl = new Uri(filePath);
+
+            // Create the KML dataset and layer.
+            KmlDataset dataset = new KmlDataset(kmlUrl);
+            KmlLayer layer = new KmlLayer(dataset);
+
+            // Add the layer to the map.
+            MySceneView.Scene.OperationalLayers.Add(layer);
+
+            try
+            {
+                // Load the dataset.
+                await dataset.LoadAsync();
+
+                // Find the first KML tour.
+                FindKmlTour(dataset.RootNodes);
+
+                // Handle absence of tour gracefully.
+                if (_tourController.Tour == null)
+                {
+                    throw new InvalidOperationException("No tour found. Can't enable touring for a KML file with no tours.");
+                }
+
+                // Enable the play button.
+                PlayButton.IsEnabled = true;
+
+                // Hide the status bar.
+                LoadingStatusBar.Visibility = Visibility.Collapsed;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+                MessageBox.Show(e.ToString(), "Error");
+            }
+        }
+
+        private void FindKmlTour(IEnumerable<KmlNode> rootNodes)
+        {
+            // Hold a list of nodes to explore.
+            Queue<KmlNode> nodesToExplore = new Queue<KmlNode>();
+
+            // Add each root node to the list.
+            foreach (KmlNode rootNode in rootNodes)
+            {
+                nodesToExplore.Enqueue(rootNode);
+            }
+
+            // Keep exploring until a tour is found or there are no more nodes.
+            while (nodesToExplore.Any())
+            {
+                // Remove a node from the queue.
+                KmlNode currentNode = nodesToExplore.Dequeue();
+
+                // If the node is a tour, use it.
+                if (currentNode is KmlTour)
+                {
+                    _tourController.Tour = (KmlTour) currentNode;
+                    return;
+                }
+
+                // If the node is a container, add all of its children to the list of nodes to explore.
+                if (currentNode is KmlContainer)
+                {
+                    KmlContainer container = (KmlContainer) currentNode;
+                    foreach (KmlNode node in container.ChildNodes)
+                    {
+                        nodesToExplore.Enqueue(node);
+                    }
+                }
+
+                // Otherwise, continue.
+            }
+        }
+
+        private void Play_Clicked(object sender, EventArgs e)
+        {
+            if (_tourController != null)
+            {
+                // Play the tour.
+                _tourController.Play();
+
+                // Configure the UI.
+                PauseButton.IsEnabled = true;
+                PlayButton.IsEnabled = false;
+                ResetButton.IsEnabled = true;
+            }
+        }
+
+        private void Pause_Clicked(object sender, EventArgs e)
+        {
+            if (_tourController != null)
+            {
+                // Pause the tour.
+                _tourController.Pause();
+
+                // Configure the UI.
+                PlayButton.IsEnabled = true;
+                PauseButton.IsEnabled = false;
+            }
+        }
+
+        private void Reset_Clicked(object sender, EventArgs e)
+        {
+            if (_tourController != null)
+            {
+                // Reset the tour.
+                _tourController.Reset();
+
+                // Configure the UI.
+                PlayButton.IsEnabled = true;
+                PauseButton.IsEnabled = false;
+                ResetButton.IsEnabled = false;
+            }
+        }
+    }
+}
---
+++ src/iOS/Xamarin.iOS/Samples/Layers/PlayKmlTours/PlayKmlTours.cs
@@ -0,0 +1,240 @@
+﻿// Copyright 2019 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using ArcGISRuntime.Samples.Managers;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Ogc;
+using Esri.ArcGISRuntime.UI.Controls;
+using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UIKit;
+
+namespace ArcGISRuntimeXamarin.Samples.PlayKmlTours
+{
+    [Register("PlayKmlTours")]
+    [ArcGISRuntime.Samples.Shared.Attributes.Sample(
+        "Play a KML tour",
+        "Layers",
+        "Display tours in KML files.",
+        "")]
+    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("f10b1d37fdd645c9bc9b189fb546307c")]
+    public class PlayKmlTours : UIViewController
+    {
+        // Hold references to UI controls.
+        private SceneView _mySceneView;
+        private UIBarButtonItem _playButton;
+        private UIBarButtonItem _pauseButton;
+        private UIBarButtonItem _resetButton;
+        private UIActivityIndicatorView _loadingIndicator;
+
+        // The KML tour controller provides player controls for KML tours.
+        private readonly KmlTourController _tourController = new KmlTourController();
+
+        public PlayKmlTours()
+        {
+            Title = "Play a KML tour";
+        }
+
+        private async void Initialize()
+        {
+            // Load the scene with a basemap and a terrain surface.
+            _mySceneView.Scene = new Scene(Basemap.CreateImagery());
+            _mySceneView.Scene.BaseSurface.ElevationSources.Add(new ArcGISTiledElevationSource(new Uri("https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")));
+
+            // Get the URL to the data.
+            string filePath = DataManager.GetDataFolder("f10b1d37fdd645c9bc9b189fb546307c", "Esri_tour.kmz");
+            Uri kmlUrl = new Uri(filePath);
+
+            // Create the KML dataset and layer.
+            KmlDataset dataset = new KmlDataset(kmlUrl);
+            KmlLayer layer = new KmlLayer(dataset);
+
+            // Add the layer to the map.
+            _mySceneView.Scene.OperationalLayers.Add(layer);
+
+            try
+            {
+                // Load the dataset.
+                await dataset.LoadAsync();
+
+                // Find the first KML tour.
+                FindKmlTour(dataset.RootNodes);
+
+                // Handle absence of tour gracefully.
+                if (_tourController.Tour == null)
+                {
+                    throw new InvalidOperationException("No tour found. Can't enable touring for a KML file with no tours.");
+                }
+
+                // Enable the play button.
+                _playButton.Enabled = true;
+
+                // Hide the activity indicator.
+                _loadingIndicator.StopAnimating();
+            }
+            catch (Exception e)
+            {
+                new UIAlertView("Error", e.ToString(), (IUIAlertViewDelegate) null, "OK", null).Show();
+            }
+        }
+
+        private void FindKmlTour(IEnumerable<KmlNode> rootNodes)
+        {
+            // Hold a list of nodes to explore.
+            Queue<KmlNode> nodesToExplore = new Queue<KmlNode>();
+
+            // Add each root node to the list.
+            foreach (KmlNode rootNode in rootNodes)
+            {
+                nodesToExplore.Enqueue(rootNode);
+            }
+
+            // Keep exploring until a tour is found or there are no more nodes.
+            while (nodesToExplore.Any())
+            {
+                // Remove a node from the queue.
+                KmlNode currentNode = nodesToExplore.Dequeue();
+
+                // If the node is a tour, use it.
+                if (currentNode is KmlTour)
+                {
+                    _tourController.Tour = (KmlTour) currentNode;
+                    return;
+                }
+
+                // If the node is a container, add all of its children to the list of nodes to explore.
+                if (currentNode is KmlContainer)
+                {
+                    KmlContainer container = (KmlContainer) currentNode;
+                    foreach (KmlNode node in container.ChildNodes)
+                    {
+                        nodesToExplore.Enqueue(node);
+                    }
+                }
+
+                // Otherwise, continue.
+            }
+        }
+
+        private void Play_Clicked(object sender, EventArgs routedEventArgs)
+        {
+            if (_tourController != null)
+            {
+                // Play the tour.
+                _tourController.Play();
+
+                // Configure the UI.
+                _pauseButton.Enabled = true;
+                _playButton.Enabled = false;
+                _resetButton.Enabled = true;
+            }
+        }
+
+        private void Pause_Clicked(object sender, EventArgs routedEventArgs)
+        {
+            if (_tourController != null)
+            {
+                // Pause the tour.
+                _tourController.Pause();
+
+                // Configure the UI.
+                _playButton.Enabled = true;
+                _pauseButton.Enabled = false;
+            }
+        }
+
+        private void Reset_Clicked(object sender, EventArgs routedEventArgs)
+        {
+            if (_tourController != null)
+            {
+                // Reset the tour.
+                _tourController.Reset();
+
+                // Configure the UI.
+                _playButton.Enabled = true;
+                _pauseButton.Enabled = false;
+                _resetButton.Enabled = false;
+            }
+        }
+
+        public override void LoadView()
+        {
+            // Create the views.
+            View = new UIView {BackgroundColor = UIColor.White};
+
+            _mySceneView = new SceneView();
+            _mySceneView.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            UIToolbar toolbar = new UIToolbar();
+            toolbar.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            _playButton = new UIBarButtonItem(UIBarButtonSystemItem.Play, Play_Clicked) {Enabled = false};
+            _pauseButton = new UIBarButtonItem(UIBarButtonSystemItem.Pause, Pause_Clicked) {Enabled = false};
+            _resetButton = new UIBarButtonItem(UIBarButtonSystemItem.Refresh, Reset_Clicked) {Enabled = false};
+
+            toolbar.Items = new UIBarButtonItem[]
+            {
+                new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace),
+                _playButton,
+                _pauseButton,
+                _resetButton
+            };
+
+            UILabel helpLabel = new UILabel
+            {
+                Text = "Use the buttons to play the tour. Contains audio. 🎧",
+                AdjustsFontSizeToFitWidth = true,
+                TextAlignment = UITextAlignment.Center,
+                BackgroundColor = UIColor.FromWhiteAlpha(0, .6f),
+                TextColor = UIColor.White,
+                Lines = 1,
+                TranslatesAutoresizingMaskIntoConstraints = false
+            };
+
+            _loadingIndicator = new UIActivityIndicatorView(UIActivityIndicatorViewStyle.WhiteLarge)
+            {
+                TranslatesAutoresizingMaskIntoConstraints = false,
+                HidesWhenStopped = true,
+                BackgroundColor = UIColor.FromWhiteAlpha(0, .6f)
+            };
+            _loadingIndicator.StartAnimating();
+
+            // Add the views.
+            View.AddSubviews(_mySceneView, toolbar, helpLabel, _loadingIndicator);
+
+            // Lay out the views.
+            NSLayoutConstraint.ActivateConstraints(new[]
+            {
+                _mySceneView.TopAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.TopAnchor),
+                _mySceneView.BottomAnchor.ConstraintEqualTo(toolbar.TopAnchor),
+                _mySceneView.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor),
+                _mySceneView.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor),
+                toolbar.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor),
+                toolbar.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor),
+                toolbar.BottomAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.BottomAnchor),
+                helpLabel.TopAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.TopAnchor),
+                helpLabel.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor),
+                helpLabel.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor),
+                helpLabel.HeightAnchor.ConstraintEqualTo(40),
+                _loadingIndicator.TopAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.TopAnchor),
+                _loadingIndicator.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor),
+                _loadingIndicator.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor),
+                _loadingIndicator.BottomAnchor.ConstraintEqualTo(View.BottomAnchor)
+            });
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            Initialize();
+        }
+    }
+}

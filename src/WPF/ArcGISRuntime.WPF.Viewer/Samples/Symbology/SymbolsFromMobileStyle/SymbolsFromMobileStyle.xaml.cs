1faf149609cebc2cd19ae16c2aaa78f7e7b021d1
Thad Tilton <ttilton@esri.com>
2019-04-19 16:44:51 -0600

    Polished up code and comments for WPF version of the sample.

--- src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Symbology/SymbolsFromMobileStyle/SymbolsFromMobileStyle.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Symbology/SymbolsFromMobileStyle/SymbolsFromMobileStyle.xaml.cs
@@ -7,24 +7,19 @@
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
-using Esri.ArcGISRuntime.Data;
-using Esri.ArcGISRuntime.Geometry;
+using ArcGISRuntime.Samples.Managers;
 using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
 using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
 using Esri.ArcGISRuntime.UI.Controls;
 using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Drawing;
 using System.Windows.Data;
-using ArcGISRuntime.Samples.Managers;
+using WinMedia = System.Windows.Media;
 
 namespace ArcGISRuntime.WPF.Samples.SymbolsFromMobileStyle
 {
@@ -36,8 +31,7 @@ namespace ArcGISRuntime.WPF.Samples.SymbolsFromMobileStyle
     [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("1bd036f221f54a99abc9e46ff3511cbf")]
     public partial class SymbolsFromMobileStyle
     {
-        // The file path to the mobile style file (.stylx) and the symbol style object.
-        private string _mobileStyleFilePath;
+        // A variable for the symbol style object.
         private SymbolStyle _emojiStyle;
 
         // The unique identifier (key) for the background face symbol in the mobile style.
@@ -46,118 +40,164 @@ namespace ArcGISRuntime.WPF.Samples.SymbolsFromMobileStyle
         public SymbolsFromMobileStyle()
         {
             InitializeComponent();
+
+            // Call a function that will create the map and fill UI controls with symbols in a mobile style.
             Initialize();
         }
 
         private async Task Initialize()
         {
-            GraphicsOverlay overlay = new GraphicsOverlay();
+            // Create a new topographic basemap and assign it to the map view.
             Map map = new Map(Basemap.CreateTopographic());
             MyMapView.Map = map;
+
+            // Create a graphics overlay for showing point graphics and add it to the map view.
+            GraphicsOverlay overlay = new GraphicsOverlay();
             MyMapView.GraphicsOverlays.Add(overlay);
 
+            // Fill the symbol color combo box with some color choices.
             FaceColorComboBox.Items.Add(Color.Yellow);
             FaceColorComboBox.Items.Add(Color.LightGreen);
             FaceColorComboBox.Items.Add(Color.LightPink);
             FaceColorComboBox.SelectedIndex = 0;
 
             // Get the full path to the downloaded mobile style file (.stylx).
-            _mobileStyleFilePath = DataManager.GetDataFolder("1bd036f221f54a99abc9e46ff3511cbf", "emoji-mobile.stylx");
+            string mobileStyleFilePath = DataManager.GetDataFolder("1bd036f221f54a99abc9e46ff3511cbf", "emoji-mobile.stylx");
 
-            await ReadMobileStyle();
+            // Call a function that will read the mobile style file and populate list boxes with symbol layers.
+            await ReadMobileStyle(mobileStyleFilePath);
 
+            // Handle the tapped event on the map view to draw point graphics with the chosen symbol.
             MyMapView.GeoViewTapped += GeoViewTapped;
         }
 
-        private async Task ReadMobileStyle()
+        private async Task ReadMobileStyle(string stylePath)
         {
-            // Open a mobile style file.
-            _emojiStyle = await SymbolStyle.OpenAsync(_mobileStyleFilePath);
-
-            // Get the default style search parameters.
-            SymbolStyleSearchParameters searchParams = await _emojiStyle.GetDefaultSearchParametersAsync();
-
-            // Search the style with the default parameters to return all symbol results.
-            IList<SymbolStyleSearchResult> styleResults = await _emojiStyle.SearchSymbolsAsync(searchParams);
-
-            List<SymbolLayerInfo> eyeSymbolInfos = new List<SymbolLayerInfo>();
-            List<SymbolLayerInfo> mouthSymbolInfos = new List<SymbolLayerInfo>();
-            List<SymbolLayerInfo> hatSymbolInfos = new List<SymbolLayerInfo>();
-
-            // Loop through the results and put symbols into the appropriate list according to category.
-            foreach (SymbolStyleSearchResult result in styleResults)
+            try
             {
-                MultilayerPointSymbol multiLayerSym = result.Symbol as MultilayerPointSymbol;
-                RuntimeImage swatch = await multiLayerSym.CreateSwatchAsync();
-                System.Windows.Media.ImageSource symbolImage = await swatch.ToImageSourceAsync();
+                // Open the mobile style file at the provided path.
+                _emojiStyle = await SymbolStyle.OpenAsync(stylePath);
 
-                SymbolLayerInfo symbolInfo = new SymbolLayerInfo(result.Name, symbolImage, result.Key);
-                switch (result.Category)
+                // Get the default style search parameters.
+                SymbolStyleSearchParameters searchParams = await _emojiStyle.GetDefaultSearchParametersAsync();
+
+                // Search the style with the default parameters to return all symbol results.
+                IList<SymbolStyleSearchResult> styleResults = await _emojiStyle.SearchSymbolsAsync(searchParams);
+
+                // Create an empty placeholder image to represent "no symbol" for each category.
+                WinMedia.ImageSource emptyImage = WinMedia.Imaging.BitmapSource.Create
+                    (2, 2, 96, 96,
+                     WinMedia.PixelFormats.Indexed1,
+                     new WinMedia.Imaging.BitmapPalette(new List<WinMedia.Color> { WinMedia.Colors.Transparent }),
+                     new byte[4],
+                     1);
+
+                // Create lists to contain the available symbol layers for each category of symbol and add an empty entry as default.
+                List<SymbolLayerInfo> eyeSymbolInfos = new List<SymbolLayerInfo> { new SymbolLayerInfo("", emptyImage, "") };
+                List<SymbolLayerInfo> mouthSymbolInfos = new List<SymbolLayerInfo> { new SymbolLayerInfo("", emptyImage, "") };
+                List<SymbolLayerInfo> hatSymbolInfos = new List<SymbolLayerInfo>() { new SymbolLayerInfo("", emptyImage, "") };
+
+                // Loop through the results and put symbols into the appropriate list according to category.
+                foreach (SymbolStyleSearchResult result in styleResults)
                 {
-                    case "Eyes":
-                        {
-                            eyeSymbolInfos.Add(symbolInfo);
-                            break;
-                        }
-                    case "Mouth":
-                        {
-                            mouthSymbolInfos.Add(symbolInfo);
-                            break;
-                        }
-                    case "Hat":
-                        {
-                            hatSymbolInfos.Add(symbolInfo);
-                            break;
-                        }
-                }
-            }
+                    // Get the symbol for this result.
+                    MultilayerPointSymbol multiLayerSym = result.Symbol as MultilayerPointSymbol;
 
-            // Show the symbols in the category list boxes.
-            EyeSymbolList.ItemsSource = eyeSymbolInfos;
-            MouthSymbolList.ItemsSource = mouthSymbolInfos;
-            HatSymbolList.ItemsSource = hatSymbolInfos;
+                    // Create a swatch image from the symbol.
+                    RuntimeImage swatch = await multiLayerSym.CreateSwatchAsync();
+                    WinMedia.ImageSource symbolImage = await swatch.ToImageSourceAsync();
+
+                    // Create a symbol layer info object to represent the symbol in the list.
+                    // The symbol key will be used to retrieve the symbol from the style.
+                    SymbolLayerInfo symbolInfo = new SymbolLayerInfo(result.Name, symbolImage, result.Key);
+
+                    // Add the symbol layer info to the correct list for its category.
+                    switch (result.Category)
+                    {
+                        case "Eyes":
+                            {
+                                eyeSymbolInfos.Add(symbolInfo);
+                                break;
+                            }
+                        case "Mouth":
+                            {
+                                mouthSymbolInfos.Add(symbolInfo);
+                                break;
+                            }
+                        case "Hat":
+                            {
+                                hatSymbolInfos.Add(symbolInfo);
+                                break;
+                            }
+                    }
+                }
+
+                // Show the symbols in the category list boxes.
+                EyeSymbolList.ItemsSource = eyeSymbolInfos;
+                MouthSymbolList.ItemsSource = mouthSymbolInfos;
+                HatSymbolList.ItemsSource = hatSymbolInfos;
+
+                // Call a function to construct the current symbol (default yellow circle).
+                Symbol faceSymbol = await GetCurrentSymbol();
+
+                // Call a function to show a preview image of the symbol.
+                await UpdateSymbolPreview(faceSymbol);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error reading symbols from style: " + ex.Message);
+            }
         }
 
-        private async void GeoViewTapped(object sender, Esri.ArcGISRuntime.UI.Controls.GeoViewInputEventArgs e)
+        // Handler for the tapped event on the map view.
+        private async void GeoViewTapped(object sender, GeoViewInputEventArgs e)
         {
+            // Call a function to get the currently defined multilayer point symbol.
             MultilayerPointSymbol faceSymbol = await GetCurrentSymbol();
+
+            // Create a graphic for the tapped location using the current symbol and add it to the map view.
             Graphic graphic = new Graphic(e.Location, faceSymbol);
             MyMapView.GraphicsOverlays.First().Graphics.Add(graphic);
         }
 
-        private void ListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        // An event handler for list box and combo box selection changes that will update the current symbol.
+        private async void SymbolPropertyChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
-            UpdateSymbolPreview();
+            // Call a function that will construct the current symbol.
+            Symbol faceSymbol = await GetCurrentSymbol();
+
+            // Call a function to update the symbol preview.
+            UpdateSymbolPreview(faceSymbol);
         }
 
         private async Task<MultilayerPointSymbol> GetCurrentSymbol()
         {
-            if(_emojiStyle == null) { return null; }
+            // If the style hasn't been opened, return.
+            if (_emojiStyle == null) { return null; }
 
+            MultilayerPointSymbol faceSymbol = null;
+            try
+            {
+                // Get the key that identifies the selected eye symbol (or an empty string if none selected).
+                SymbolLayerInfo eyeLayerInfo = (SymbolLayerInfo)EyeSymbolList.SelectedItem;
+                string eyeLayerKey = eyeLayerInfo != null ? eyeLayerInfo.Key : string.Empty;
+
+                // Get the key that identifies the selected mouth symbol (or an empty string if none selected).
+                SymbolLayerInfo mouthLayerInfo = (SymbolLayerInfo)MouthSymbolList.SelectedItem;
+                string mouthLayerKey = mouthLayerInfo != null ? mouthLayerInfo.Key : string.Empty;
+
+                // Get the key that identifies the selected hat symbol (or an empty string if none selected).
+                SymbolLayerInfo hatLayerInfo = (SymbolLayerInfo)HatSymbolList.SelectedItem;
+                string hatLayerKey = hatLayerInfo != null ? hatLayerInfo.Key : string.Empty;
+            
+            // Create a list of the symbol keys that identify the selected symbol layers, including the base (circle) symbol.
             List<string> symbolKeys = new List<string>
             {
-                _baseSymbolKey
+                _baseSymbolKey, eyeLayerKey, mouthLayerKey, hatLayerKey
             };
 
-            if (EyeSymbolList.SelectedItem != null)
-            {
-                SymbolLayerInfo eyeLayerInfo = (SymbolLayerInfo)EyeSymbolList.SelectedItem;
-                symbolKeys.Add(eyeLayerInfo.Key);
-            }
-
-            if (MouthSymbolList.SelectedItem != null)
-            {
-                SymbolLayerInfo mouthLayerInfo = (SymbolLayerInfo)MouthSymbolList.SelectedItem;
-                symbolKeys.Add(mouthLayerInfo.Key);
-            }
-
-            if (HatSymbolList.SelectedItem != null)
-            {
-                SymbolLayerInfo hatLayerInfo = (SymbolLayerInfo)HatSymbolList.SelectedItem;
-                symbolKeys.Add(hatLayerInfo.Key);
-            }
-
-            MultilayerPointSymbol faceSymbol = await _emojiStyle.GetSymbolAsync(symbolKeys) as MultilayerPointSymbol;
+            // Get a multilayer point symbol from the style that contains the selected symbol layers.
+            faceSymbol = await _emojiStyle.GetSymbolAsync(symbolKeys) as MultilayerPointSymbol;
 
             // Loop through all symbol layers and lock the color.
             foreach (SymbolLayer lyr in faceSymbol.SymbolLayers)
@@ -177,48 +217,70 @@ namespace ArcGISRuntime.WPF.Samples.SymbolsFromMobileStyle
 
             // Set the symbol size from the slider.
             faceSymbol.Size = SizeSlider.Value;
-            
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            // Return the multilayer point symbol.
             return faceSymbol;
         }
 
-        private async void UpdateSymbolPreview()
+        private async Task UpdateSymbolPreview(Symbol symbolToShow)
         {
-            MultilayerPointSymbol faceSymbol = await GetCurrentSymbol();
-            if (faceSymbol != null)
-            {
-                RuntimeImage swatch = await faceSymbol.CreateSwatchAsync(80, 80, 96, Color.White);
-                System.Windows.Media.ImageSource symbolImage = await swatch.ToImageSourceAsync();
-                SymbolPreviewImage.Source = symbolImage;
-            }
+            if (symbolToShow == null) { return; }
+
+            // Create a swatch from the symbol with a white background.
+            RuntimeImage swatch = await symbolToShow.CreateSwatchAsync(80, 80, 96, Color.White);
+
+            // Convert the swatch to an image source and show it in the Image control.
+            WinMedia.ImageSource symbolImage = await swatch.ToImageSourceAsync();
+            SymbolPreviewImage.Source = symbolImage;
         }
 
-        private void ClearGraphicsButton_Click(object sender, RoutedEventArgs e)
+        // A handler for the click event on the clear graphics button.
+        private void ClearGraphicsClick(object sender, RoutedEventArgs e)
         {
+            // Clear all graphics from the first (only) graphics overlay in the map view.
             MyMapView.GraphicsOverlays.FirstOrDefault()?.Graphics.Clear();
         }
+    }
 
-        }
-        public class SymbolLayerInfo
+    // A class for storing information about a symbol layer.
+    public class SymbolLayerInfo
     {
-        public System.Windows.Media.ImageSource ImgSrc { get; set; }
-        public string Name { get; set; }
-        public string Key { get; set; }
+        // An image source for a preview image of the symbol.
+        public WinMedia.ImageSource ImageSrc { get; private set; }
 
-        public SymbolLayerInfo(string name, System.Windows.Media.ImageSource source, string key)
+        // The name of the symbol as it appears in the mobile style.
+        public string Name { get; private set; }
+
+        // A key that uniquely identifies the symbol in the style.
+        public string Key { get; private set; }
+
+        // Take all symbol info in the constructor.
+        public SymbolLayerInfo(string name, WinMedia.ImageSource source, string key)
         {
             Name = name;
-            ImgSrc = source;
+            ImageSrc = source;
             Key = key;
         }
     }
 
+    // A class that converts a System.Drawing.Color object to a solid brush for setting background color for UI controls. 
     public class ColorToSolidBrushConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
+            // Get the input value as a System.Drawing.Color.
             Color inColor = (Color)value;
-            System.Windows.Media.Color outColor = System.Windows.Media.Color.FromArgb(inColor.A, inColor.R, inColor.G, inColor.B);
-            System.Windows.Media.SolidColorBrush brush = new System.Windows.Media.SolidColorBrush(outColor);
+
+            // Convert the input System.Drawing.Color to a System.Windows.Media.Color.
+            WinMedia.Color outColor = WinMedia.Color.FromArgb(inColor.A, inColor.R, inColor.G, inColor.B);
+
+            // Create a solid color brush using the color and return it.
+            WinMedia.SolidColorBrush brush = new WinMedia.SolidColorBrush(outColor);
             return brush;
         }
 

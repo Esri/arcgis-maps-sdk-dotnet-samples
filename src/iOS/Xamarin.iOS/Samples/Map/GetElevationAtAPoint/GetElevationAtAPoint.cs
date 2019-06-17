d58d8d9721ecb03cdb3c4a78973d7ff875392cc3
Zack Allen <zack.m.allen@gmail.com>
2019-06-17 15:11:06 -0700

    Fixed faulty UI in iOS and retook screenshot

--- src/iOS/Xamarin.iOS/Samples/Map/GetElevationAtAPoint/GetElevationAtAPoint.cs
+++ src/iOS/Xamarin.iOS/Samples/Map/GetElevationAtAPoint/GetElevationAtAPoint.cs
@@ -3,18 +3,18 @@
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Symbology;
 using Esri.ArcGISRuntime.UI;
-using System;
-using System.Drawing;
 using Esri.ArcGISRuntime.UI.Controls;
 using Foundation;
+using System;
+using System.Drawing;
 using UIKit;
 
 namespace ArcGISRuntimeXamarin.Samples.GetElevationAtAPoint
@@ -101,30 +101,34 @@ namespace ArcGISRuntimeXamarin.Samples.GetElevationAtAPoint
             UILabel helpLabel = new UILabel
             {
                 Text = "Tap to find the elevation for a point.",
-                AdjustsFontSizeToFitWidth = true,
-                TextAlignment = UITextAlignment.Center,
-                BackgroundColor = UIColor.FromWhiteAlpha(0, .6f),
+                BackgroundColor = UIColor.FromWhiteAlpha(0f, .6f),
                 TextColor = UIColor.White,
-                Lines = 1,
+                TextAlignment = UITextAlignment.Center,
                 TranslatesAutoresizingMaskIntoConstraints = false
             };
 
             // Add the views.
-            View.AddSubviews(_mySceneView);
+            View.AddSubviews(_mySceneView, helpLabel);
 
             // Lay out the views.
-            NSLayoutConstraint.ActivateConstraints(new []{
+            NSLayoutConstraint.ActivateConstraints(new[]{
                 _mySceneView.TopAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.TopAnchor),
                 _mySceneView.BottomAnchor.ConstraintEqualTo(View.BottomAnchor),
                 _mySceneView.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor),
-                _mySceneView.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor)
-            });            
+                _mySceneView.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor),
+
+                helpLabel.TopAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.TopAnchor),
+                helpLabel.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor),
+                helpLabel.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor),
+                helpLabel.HeightAnchor.ConstraintEqualTo(40)
+            });
         }
 
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
             Initialize();
+
             // Handle taps on the scene view for getting elevation.
             _mySceneView.GeoViewTapped += SceneViewTapped;
         }

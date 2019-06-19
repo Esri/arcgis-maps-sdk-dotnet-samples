662db662a93dd1cad00aca3f7fc391211f1a277d
Zack Allen <zack.m.allen@gmail.com>
2019-06-19 09:49:11 -0700

    Generated templates

---
+++ src/iOS/Xamarin.iOS/Samples/MapView/ChooseCameraController/ChooseCameraController.cs
@@ -0,0 +1,71 @@
+﻿// Copyright 2019 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.Tasks.Offline;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.ArcGISServices;
+using Esri.ArcGISRuntime.UI.Controls;
+using Foundation;
+using UIKit;
+
+namespace ArcGISRuntimeXamarin.Samples.ChooseCameraController
+{
+    [Register("ChooseCameraController")]
+    [ArcGISRuntime.Samples.Shared.Attributes.Sample(
+        "Choose camera controller",
+        "MapView",
+        "Control the behavior of the camera in a scene.",
+        "")]
+    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
+    public class ChooseCameraController : UIViewController
+    {
+        // Hold references to UI controls.
+        private SceneView _mySceneView;
+
+        public ChooseCameraController()
+        {
+            Title = "Choose camera controller";
+        }
+
+        private void Initialize()
+        {
+        }
+
+        public override void LoadView()
+        {
+            // Create the views.
+            View = new UIView();
+
+            _mySceneView = new SceneView();
+            _mySceneView.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            // Add the views.
+            View.AddSubviews(_mySceneView);
+
+            // Lay out the views.
+            NSLayoutConstraint.ActivateConstraints(new []{
+                _mySceneView.TopAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.TopAnchor),
+                _mySceneView.BottomAnchor.ConstraintEqualTo(View.BottomAnchor),
+                _mySceneView.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor),
+                _mySceneView.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor)
+            });            
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            Initialize();
+        }
+    }
+}

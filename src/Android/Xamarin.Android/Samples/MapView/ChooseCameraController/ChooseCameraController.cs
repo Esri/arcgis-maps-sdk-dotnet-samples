d0710752cfd812bc7460373344ef1d183a0a8d14
Zack Allen <zack.m.allen@gmail.com>
2019-06-21 09:26:13 -0700

    Started android

--- src/Android/Xamarin.Android/Samples/MapView/ChooseCameraController/ChooseCameraController.cs
+++ src/Android/Xamarin.Android/Samples/MapView/ChooseCameraController/ChooseCameraController.cs
@@ -9,16 +9,14 @@
 
 using Android.App;
 using Android.OS;
-using Android.Widget;
-using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
 using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
 using Esri.ArcGISRuntime.UI.Controls;
+using System;
+using ArcGISRuntime.Samples.Managers;
+using Android;
 
 namespace ArcGISRuntimeXamarin.Samples.ChooseCameraController
 {
@@ -28,7 +26,8 @@ namespace ArcGISRuntimeXamarin.Samples.ChooseCameraController
         "MapView",
         "Control the behavior of the camera in a scene.",
         "")]
-    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
+    [ArcGISRuntime.Samples.Shared.Attributes.AndroidLayout("ChooseCameraController.axml")]
+    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("681d6f7694644709a7c830ec57a2d72b")]
     public class ChooseCameraController : Activity
     {
         // Hold references to the UI controls.
@@ -43,22 +42,124 @@ namespace ArcGISRuntimeXamarin.Samples.ChooseCameraController
             CreateLayout();
             Initialize();
         }
-
-        private void Initialize()
-        {
-        }
-
         private void CreateLayout()
         {
-            // Create a new vertical layout for the app.
-            var layout = new LinearLayout(this) { Orientation = Orientation.Vertical };
+            //SetContentView(2130903071);
+            SetContentView(Resource.Layout.ChooseCameraController);
+        }
 
-            // Add the map view to the layout.
-            _mySceneView = new SceneView();
-            layout.AddView(_mySceneView);
+        // Path for elevation data.
+        private readonly Uri _elevationUri = new Uri("http://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer");
 
-            // Show the layout in the app.
-            SetContentView(layout);
+        // Path for the plane model.
+        private readonly Uri _modelUri = new Uri(DataManager.GetDataFolder("681d6f7694644709a7c830ec57a2d72b", "Bristol.dae"));
+
+        // Geo element camera controller.
+        private OrbitGeoElementCameraController _orbitPlaneCameraController;
+
+        // Location camera controller.
+        private OrbitLocationCameraController _orbitCraterCameraController;
+
+        public ChooseCameraController()
+        {
+            Initialize();
+        }
+
+        private async void Initialize()
+        {
+            // Create a scene.
+            Scene myScene = new Scene(Basemap.CreateImageryWithLabels());
+
+            // Create a surface for elevation data.
+            Surface surface = new Surface();
+            surface.ElevationSources.Add(new ArcGISTiledElevationSource(_elevationUri));
+
+            // Add the surface to the scene.
+            myScene.BaseSurface = surface;
+
+
+            // Create a graphics overlay for the scene.
+            GraphicsOverlay sceneGraphicsOverlay = new GraphicsOverlay()
+            {
+                SceneProperties = new LayerSceneProperties(SurfacePlacement.Absolute)
+            };
+            //_mySceneView.GraphicsOverlays.Add(sceneGraphicsOverlay);
+
+            // Location at the crater.
+            MapPoint craterLocation = new MapPoint(-109.929589, 38.437304, 1700, SpatialReferences.Wgs84);
+
+            // Create the plane symbol and make it 10x larger (to be the right size relative to the scene).
+            ModelSceneSymbol planeSymbol;
+            try
+            {
+                planeSymbol = await ModelSceneSymbol.CreateAsync(_modelUri, 10.0);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                CreateErrorDialog("Loading plane model failed. Sample failed to initialize.");
+                return;
+            }
+
+            // Create a graphic using the plane symbol.
+            Graphic planeGraphic = new Graphic(new MapPoint(craterLocation.X, craterLocation.Y, 5000.0, SpatialReferences.Wgs84), planeSymbol);
+            sceneGraphicsOverlay.Graphics.Add(planeGraphic);
+
+            // Instantiate a new camera controller which orbits a geo element.
+            _orbitPlaneCameraController = new OrbitGeoElementCameraController(planeGraphic, 300.0)
+            {
+                CameraPitchOffset = 30,
+                CameraHeadingOffset = 150
+            };
+
+            // Instantiate a new camera controller which orbits a location.
+            _orbitCraterCameraController = new OrbitLocationCameraController(craterLocation, 6000.0)
+            {
+                CameraPitchOffset = 3,
+                CameraHeadingOffset = 150
+            };
+
+            // Set the starting camera controller.
+            //_mySceneView.CameraController = _orbitPlaneCameraController;
+
+            // Enable all of the radio buttons.
+           // OrbitPlaneButton.IsEnabled = true;
+           // OrbitCraterButton.IsEnabled = true;
+            //FreePanButton.IsEnabled = true;
+
+            // Add the scene to the view.
+            //_mySceneView.Scene = myScene;
+        }
+        /*
+        private void Setting_Checked(object sender, EventArgs e)
+        {
+            switch (((RadioButton)sender).Name)
+            {
+                case nameof(OrbitPlaneButton):
+                    // Switch to the plane camera controller.
+                    MySceneView.CameraController = _orbitPlaneCameraController;
+                    break;
+
+                case nameof(OrbitCraterButton):
+                    // Switch to the crater camera controller.
+                    MySceneView.CameraController = _orbitCraterCameraController;
+                    break;
+
+                case nameof(FreePanButton):
+                    // Switch to a globe camera controller, which is free pan.
+                    MySceneView.CameraController = new GlobeCameraController();
+                    break;
+            }
+        }
+        */
+
+        
+        private void CreateErrorDialog(string message)
+        {
+            // Create a dialog to show message to user.
+            AlertDialog alert = new AlertDialog.Builder(this).Create();
+            alert.SetMessage(message);
+            alert.Show();
         }
     }
 }

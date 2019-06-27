d75429acf88a544bcaf158ccb6f5d9585ae2a883
dotMorten <mn@iter.dk>
2019-06-26 22:19:32 -0700

    Undo changes to assembly info

--- src/WPF/ArcGISRuntime.WPF.Viewer/Properties/AssemblyInfo.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Properties/AssemblyInfo.cs
@@ -5,18 +5,15 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-#if !NETCOREAPP
 [assembly: AssemblyTitle("ArcGIS Runtime SDK for .NET Samples Viewer")]
+[assembly: AssemblyDescription("Viewer application to browse ArcGIS Runtime SDK for .NET samples.")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Esri Inc.")]
 [assembly: AssemblyProduct("ArcGIS Runtime SDK for .NET Samples Viewer")]
 [assembly: AssemblyCopyright("Copyright © Esri Inc. 2018")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyCompany("Esri Inc.")]
-#endif
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyDescription("Viewer application to browse ArcGIS Runtime SDK for .NET samples.")]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -40,3 +37,17 @@ using System.Windows;
                                               //(used if a resource is not found in the page, 
                                               // app, or any theme specific resource dictionaries)
 )]
+
+
+// Version information for an assembly consists of the following four values:	
+//	
+//      Major Version	
+//      Minor Version 	
+//      Build Number	
+//      Revision	
+//	
+// You can specify all the values or you can default the Build and Revision Numbers 	
+// by using the '*' as shown below:	
+// [assembly: AssemblyVersion("1.0.*")]	
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

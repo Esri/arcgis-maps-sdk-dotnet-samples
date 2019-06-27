1ad68723cee578468858f449c1e6ef3279fe080a
dotMorten <mn@iter.dk>
2019-06-26 22:21:13 -0700

    Whitespace

--- src/WPF/ArcGISRuntime.WPF.Viewer/Properties/AssemblyInfo.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Properties/AssemblyInfo.cs
@@ -42,11 +42,11 @@ using System.Windows;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 	
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]

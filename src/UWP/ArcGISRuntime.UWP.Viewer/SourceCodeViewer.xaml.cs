72fd2b680db823af8859ee41f4a4fd0a49ba96b2
Zack Allen <zack.m.allen@gmail.com>
2019-07-01 13:12:00 -0700

    SourceCodeViewer improvements

--- src/UWP/ArcGISRuntime.UWP.Viewer/SourceCodeViewer.xaml.cs
+++ src/UWP/ArcGISRuntime.UWP.Viewer/SourceCodeViewer.xaml.cs
@@ -8,18 +8,17 @@
 // language governing permissions and limitations under the License.
 
 using ArcGISRuntime.Samples.Managers;
-using System;
+using Microsoft.Toolkit.Uwp.UI.Controls;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using Windows.UI.Xaml.Controls;
+using System.Linq;
 
 namespace ArcGISRuntime.UWP.Viewer
 {
     public partial class SourceCode
     {
-        // Holds source files (as strings of html).
-        private Dictionary<string, string> _sourceFiles;
+        // List of all of the tabs that will load.
+        private List<TabViewItem> tabs;
 
         public SourceCode()
         {
@@ -30,114 +29,49 @@ namespace ArcGISRuntime.UWP.Viewer
         {
             string folderPath = SampleManager.Current.SelectedSample.Path;
 
-            // Filepaths for the css and js files used for syntax highlighting.
-            string cssPath = "ms-appx-web:///" + "Resources\\SyntaxHighlighting/highlight.css";
-            string jsPath = "ms-appx-web:///" + "Resources\\SyntaxHighlighting/highlight.pack.js";
-
-            // Dictionary holds html strings for source code as values. Keys are strings of filepaths.
-            _sourceFiles = new Dictionary<string, string>();
-
-            // Clear out any old items when loading for a new sample.
-            FileSelection.Items.Clear();
-
-            // Start of each html string.
-            string htmlStart =
-                "<html>" +
-                "<head>" +
-                "<link rel=\"stylesheet\" href=\"" + cssPath + "\">" +
-                "<script type=\"text/javascript\" src=\"" + jsPath + "\"></script>" +
-                "<script>hljs.initHighlightingOnLoad();</script>" +
-                "</head>" +
-                "<body>" +
-                "<pre>";
-
-            // End of each html string.
-            string htmlEnd =
-                "</pre>" +
-                "</body>" +
-                "</html>";
-
-            // Index of the .cs file. -1 will result in no selection if a .cs file is not found.
-            int csIndex = -1;
-
             // Source code of the file.
             string source;
 
-            // Add every .cs and .xaml file in the directory of the sample.
-            foreach (string filepath in Directory.GetFiles(folderPath))
+            // Create a new list of TabViewItems.
+            tabs = new List<TabViewItem>();
+
+            // Loop through all .cs and .xaml files.
+            foreach (string filepath in Directory.GetFiles(folderPath)
+                .Where(candidate => candidate.EndsWith(".cs") || candidate.EndsWith(".xaml")))
             {
-                Console.WriteLine(filepath);
-                try
+                // Get the source text from the file.
+                source = File.ReadAllText(filepath);
+
+                // Create a new tab.
+                TabViewItem newTab = new TabViewItem();
+
+                // Set the tab text to the file name.
+                newTab.Header = Path.GetFileName(filepath);
+
+                // Create the code viewer.
+                Monaco.CodeEditor viewer = new Monaco.CodeEditor();
+                viewer.Options.ReadOnly = true;
+                viewer.Text = source;
+
+                // Change Monaco language for C# files.
+                if (filepath.EndsWith(".cs")) { viewer.CodeLanguage = "csharp"; }
+
+                // Adjust tabs for dark mode.
+                if (App.Current.RequestedTheme == Windows.UI.Xaml.ApplicationTheme.Dark)
                 {
-                    if (filepath.EndsWith(".cs"))
-                    {
-                        // Get the source text from the file.
-                        source = File.ReadAllText(filepath);
-
-                        // Build the html.
-                        source =
-                            htmlStart +
-                            "<code class=\"csharp\">" +
-                            source +
-                            "</code>" +
-                            htmlEnd;
-
-                        // Set the index of the .cs file.
-                        csIndex = _sourceFiles.Count;
-                    }
-                    else if (filepath.EndsWith(".xaml"))
-                    {
-                        // Get the source text from the file.
-                        source = File.ReadAllText(filepath);
-
-                        // Replace the tag characters so that the html renders correctly.
-                        source = source.Replace("<", "&lt;");
-                        source = source.Replace(">", "&gt;");
-
-                        // Build the html.
-                        source =
-                            htmlStart +
-                            "<code class=\"xml\">" +
-                            source +
-                            "</code>" +
-                            htmlEnd;
-                    }
-                    else
-                    {
-                        // Continue looping over other files if file is not .cs or .xaml.
-                        continue;
-                    }
-                    // Add the source html to the _sourceFiles dictionary.
-                    _sourceFiles[filepath] = source;
-
-                    // Add the file as a selectable item in the combobox.
-                    FileSelection.Items.Add(filepath);
+                    Tabs.RequestedTheme = Windows.UI.Xaml.ElementTheme.Dark;
+                    newTab.RequestedTheme = Windows.UI.Xaml.ElementTheme.Dark;
                 }
-                catch (Exception e)
-                {
-                    // Any files that failed to be read will have error messages printed to the console for debugging.
-                    Debug.WriteLine(e.Message);
-                }
+
+                // Set the tabs content to the code viewer.
+                newTab.Content = viewer;
+
+                // Add the tab to the beginning of the list.
+                tabs.Insert(0, newTab);
             }
 
-            // Default to the index of the last .cs file.
-            FileSelection.SelectedIndex = csIndex;
-
-            // Check if any source files were found.
-            if (FileSelection.Items.Count == 0)
-            {
-                SourceCodeBrowser.NavigateToString("Source files not found.");
-            }
-        }
-
-        private void SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            // Check that there are Items in the combobox. Prevents error when switching between samples.
-            if (FileSelection.Items.Count != 0)
-            {
-                // Set the web browser to display the source code of the selected file.
-                SourceCodeBrowser.NavigateToString(_sourceFiles[FileSelection.SelectedValue.ToString()]);
-            }
+            // Set the Tab source to the list of tabs.
+            Tabs.ItemsSource = tabs;
         }
     }
 }

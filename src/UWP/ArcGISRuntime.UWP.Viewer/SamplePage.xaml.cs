104ef30328bd2089bc21d041d3f9c13a238c77b4
Zack Allen <zack.m.allen@gmail.com>
2019-07-11 15:38:20 -0700

    Cleanup

--- src/UWP/ArcGISRuntime.UWP.Viewer/SamplePage.xaml.cs
+++ src/UWP/ArcGISRuntime.UWP.Viewer/SamplePage.xaml.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2018 Esri.
+﻿// Copyright 2019 Esri.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
@@ -72,7 +72,6 @@ namespace ArcGISRuntime.UWP.Viewer
 
         private void TabChanged(object sender, Windows.UI.Xaml.Controls.SelectionChangedEventArgs e)
         {
-            
             switch (((TabViewItem)Tabs.SelectedItem).Header.ToString())
             {
                 case "Live Sample":
--- src/UWP/ArcGISRuntime.UWP.Viewer/SettingsWindow.xaml.cs
+++ src/UWP/ArcGISRuntime.UWP.Viewer/SettingsWindow.xaml.cs
@@ -1,4 +1,13 @@
-﻿using ArcGISRuntime.Samples.Managers;
+﻿// Copyright 2019 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+
+using ArcGISRuntime.Samples.Managers;
 using ArcGISRuntime.Samples.Shared.Models;
 using ArcGISRuntime.UWP.Viewer;
 using Esri.ArcGISRuntime;
@@ -15,13 +24,8 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace ArcGISRuntime
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class SettingsWindow
     {
         private static string _runtimeVersion = "";
@@ -59,7 +63,7 @@ namespace ArcGISRuntime
             }
 
             // Set margin for Back Button
-            BackButton.Margin = new Thickness(Window.Current.Bounds.Width-BackButton.Width, 0, 0,0);
+            BackButton.Margin = new Thickness(Window.Current.Bounds.Width - BackButton.Width, 0, 0, 0);
             this.SizeChanged += MainPage_SizeChanged;
         }
 

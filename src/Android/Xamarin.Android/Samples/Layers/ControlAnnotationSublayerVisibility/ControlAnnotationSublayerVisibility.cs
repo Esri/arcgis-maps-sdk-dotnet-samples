fe411fc9a5bef712bb89102826d392ab148c0b37
Zack Allen <zack.m.allen@gmail.com>
2019-08-26 16:45:31 -0700

    Added Android

--- src/Android/Xamarin.Android/Samples/Layers/ControlAnnotationSublayerVisibility/ControlAnnotationSublayerVisibility.cs
+++ src/Android/Xamarin.Android/Samples/Layers/ControlAnnotationSublayerVisibility/ControlAnnotationSublayerVisibility.cs
@@ -3,36 +3,44 @@
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
 using Android.App;
+using Android.Graphics;
 using Android.OS;
 using Android.Widget;
-using Esri.ArcGISRuntime.Data;
-using Esri.ArcGISRuntime.Geometry;
+using ArcGISRuntime;
+using ArcGISRuntime.Samples.Managers;
 using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
-using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
 using Esri.ArcGISRuntime.UI.Controls;
+using System.Linq;
 
 namespace ArcGISRuntimeXamarin.Samples.ControlAnnotationSublayerVisibility
 {
-    [Activity (ConfigurationChanges=Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
+    [Activity(ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
     [ArcGISRuntime.Samples.Shared.Attributes.Sample(
         "Control annotation sublayer visibility",
         "Layers",
         "Use annotation sublayers to gain finer control of annotation layer subtypes.",
         "")]
+    [ArcGISRuntime.Samples.Shared.Attributes.AndroidLayout("ControlAnnotationSublayerVisibility.axml")]
     [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("b87307dcfb26411eb2e92e1627cb615b")]
     public class ControlAnnotationSublayerVisibility : Activity
     {
         // Hold references to the UI controls.
         private MapView _myMapView;
+        private CheckBox _openCheckBox;
+        private CheckBox _closedCheckBox;
+        private TextView _status;
+
+        // Mobile map package that contains annotation layers.
+        private MobileMapPackage _mobileMapPackage;
+
+        // Sub layers of the annotation layer.
+        private AnnotationSublayer _openSublayer;
+        private AnnotationSublayer _closedSublayer;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -44,21 +52,77 @@ namespace ArcGISRuntimeXamarin.Samples.ControlAnnotationSublayerVisibility
             Initialize();
         }
 
-        private void Initialize()
+        private async void Initialize()
         {
+            // Load the mobile map package.
+            _mobileMapPackage = new MobileMapPackage(DataManager.GetDataFolder("b87307dcfb26411eb2e92e1627cb615b", "GasDeviceAnno.mmpk"));
+            await _mobileMapPackage.LoadAsync();
+
+            // Set the mapview to display the map from the package.
+            _myMapView.Map = _mobileMapPackage.Maps.First();
+
+            // Get the annotation layer from the MapView operational layers.
+            AnnotationLayer annotationLayer = (AnnotationLayer)_myMapView.Map.OperationalLayers.Where(layer => layer is AnnotationLayer).First();
+
+            // Load the annotation layer.
+            await annotationLayer.LoadAsync();
+
+            // Get the annotation sub layers.
+            _closedSublayer = (AnnotationSublayer)annotationLayer.SublayerContents[0];
+            _openSublayer = (AnnotationSublayer)annotationLayer.SublayerContents[1];
+
+            // Set the label content.
+            _openCheckBox.Text = $"{_openSublayer.Name} (1:{_openSublayer.MaxScale} - 1:{_openSublayer.MinScale})";
+            _closedCheckBox.Text = _closedSublayer.Name;
+
+            // Enable the check boxes.
+            _openCheckBox.Enabled = true;
+            _closedCheckBox.Enabled = true;
+
+            // Add event handler for changing the text to indicate whether the "open" sublayer is visible at the current scale.
+            _myMapView.ViewpointChanged += (s, e) =>
+            {
+                // Check if the sublayer is visible at the current map scale.
+                if (_openSublayer.IsVisibleAtScale(_myMapView.MapScale))
+                {
+                    _openCheckBox.SetTextColor(Color.Black);
+                }
+                else
+                {
+                    _openCheckBox.SetTextColor(Color.Gray);
+                }
+
+                // Set the current map scale text.
+                _status.Text = "Current map scale: 1:" + (int)_myMapView.MapScale;
+            };
+        }
+
+        private void OpenCheckBoxChanged(object sender, CompoundButton.CheckedChangeEventArgs e)
+        {
+            // Set the visibility of the sub layer.
+            if (_openSublayer != null) _openSublayer.IsVisible = _openCheckBox.Checked;
+        }
+
+        private void ClosedCheckBoxChanged(object sender, CompoundButton.CheckedChangeEventArgs e)
+        {
+            // Set the visibility of the sub layer.
+            if (_closedSublayer != null) _closedSublayer.IsVisible = _closedCheckBox.Checked;
         }
 
         private void CreateLayout()
         {
-            // Create a new vertical layout for the app.
-            var layout = new LinearLayout(this) { Orientation = Orientation.Vertical };
+            // Load the UI from the axml file.
+            SetContentView(Resource.Layout.ControlAnnotationSublayerVisibility);
 
-            // Add the map view to the layout.
-            _myMapView = new MapView();
-            layout.AddView(_myMapView);
+            // Get the UI elements from the axml resource.
+            _myMapView = FindViewById<MapView>(Resource.Id.MapView);
+            _openCheckBox = FindViewById<CheckBox>(Resource.Id.openCheckBox);
+            _closedCheckBox = FindViewById<CheckBox>(Resource.Id.closedCheckBox);
+            _status = FindViewById<TextView>(Resource.Id.statusLabel);
 
-            // Show the layout in the app.
-            SetContentView(layout);
+            // Add listeners for the checkboxes.
+            _openCheckBox.CheckedChange += OpenCheckBoxChanged;
+            _closedCheckBox.CheckedChange += ClosedCheckBoxChanged;
         }
     }
 }

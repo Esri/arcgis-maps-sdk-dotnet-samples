20ef2e3d88e05b0d51d08ca95f1f4e1296b3932a
Zack Allen <zack.m.allen@gmail.com>
2019-09-04 14:43:23 -0700

    Added WPF

--- src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Map/HonorMobileMapPackageExpiration/HonorMobileMapPackageExpiration.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Map/HonorMobileMapPackageExpiration/HonorMobileMapPackageExpiration.xaml.cs
@@ -7,22 +7,10 @@
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
-using Esri.ArcGISRuntime.Data;
-using Esri.ArcGISRuntime.Geometry;
+using ArcGISRuntime.Samples.Managers;
 using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
-using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
-using Esri.ArcGISRuntime.UI.Controls;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Media;
 
 namespace ArcGISRuntime.WPF.Samples.HonorMobileMapPackageExpiration
 {
@@ -34,14 +22,64 @@ namespace ArcGISRuntime.WPF.Samples.HonorMobileMapPackageExpiration
     [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("174150279af74a2ba6f8b87a567f480b")]
     public partial class HonorMobileMapPackageExpiration
     {
+        private readonly string _mobileMapPackagePath = DataManager.GetDataFolder("174150279af74a2ba6f8b87a567f480b", "LothianRiversAnno.mmpk");
+
         public HonorMobileMapPackageExpiration()
         {
             InitializeComponent();
             Initialize();
         }
 
-        private void Initialize()
+        private async void Initialize()
         {
+            try
+            {
+                // Create a mobile map package.
+                MobileMapPackage mobileMapPackage = new MobileMapPackage(_mobileMapPackagePath);
+
+                // Load the mobile map package.
+                await mobileMapPackage.LoadAsync();
+
+                // Check if the map package is expired.
+                if (mobileMapPackage.Expiration?.IsExpired == true)
+                {
+                    // Get the expiration of the mobile map package.
+                    Expiration expiration = mobileMapPackage.Expiration;
+
+                    // Get the expiration message.
+                    string expirationMessage = expiration.Message;
+
+                    // Get the expiration date.
+                    string expirationDate = expiration.DateTime.ToString("F");
+
+                    // Set the expiration message.
+                    ExpirationLabel.Text = expirationMessage + "\nExpiration date: " + expirationDate;
+
+                    // Check if the map is accessible after expiration.
+                    if (expiration.Type == ExpirationType.AllowExpiredAccess && mobileMapPackage.Maps.Count > 0)
+                    {
+                        // Set the mapview to the map from the mobile map package.
+                        MyMapView.Map = mobileMapPackage.Maps[0];
+                    }
+                    else if (expiration.Type == ExpirationType.PreventExpiredAccess)
+                    {
+                        MessageBox.Show("The author of this mobile map package has disallowed access after the expiration date.", "Error");
+                    }
+                }
+                else if (mobileMapPackage.Maps.Count > 0)
+                {
+                    // Set the mapview to the map from the mobile map package.
+                    MyMapView.Map = mobileMapPackage.Maps[0];
+                }
+                else
+                {
+                    MessageBox.Show("Failed to load the mobile map package.", "Error");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }

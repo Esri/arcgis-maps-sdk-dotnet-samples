7901ba1c2e168f277126eadfd0ac29317e6a2ebb
Zack Allen <zack.m.allen@gmail.com>
2019-09-04 15:12:43 -0700

    Added Xamarin Forms

--- src/Forms/Shared/Samples/Map/HonorMobileMapPackageExpiration/HonorMobileMapPackageExpiration.xaml.cs
+++ src/Forms/Shared/Samples/Map/HonorMobileMapPackageExpiration/HonorMobileMapPackageExpiration.xaml.cs
@@ -3,19 +3,13 @@
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
-using Esri.ArcGISRuntime.Data;
-using Esri.ArcGISRuntime.Geometry;
+using ArcGISRuntime.Samples.Managers;
 using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
-using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
-using Esri.ArcGISRuntime.UI.Controls;
+using System;
 using Xamarin.Forms;
 
 namespace ArcGISRuntimeXamarin.Samples.HonorMobileMapPackageExpiration
@@ -28,14 +22,64 @@ namespace ArcGISRuntimeXamarin.Samples.HonorMobileMapPackageExpiration
     [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("174150279af74a2ba6f8b87a567f480b")]
     public partial class HonorMobileMapPackageExpiration : ContentPage
     {
+        private readonly string _mobileMapPackagePath = DataManager.GetDataFolder("174150279af74a2ba6f8b87a567f480b", "LothianRiversAnno.mmpk");
+
         public HonorMobileMapPackageExpiration()
         {
             InitializeComponent();
             Initialize();
         }
 
-        private void Initialize()
+        private async void Initialize()
         {
+            try
+            {
+                // Create a mobile map package.
+                MobileMapPackage mobileMapPackage = new MobileMapPackage(_mobileMapPackagePath);
+
+                // Load the mobile map package.
+                await mobileMapPackage.LoadAsync();
+
+                // Check if the map package is expired.
+                if (mobileMapPackage.Expiration?.IsExpired == true)
+                {
+                    // Get the expiration of the mobile map package.
+                    Expiration expiration = mobileMapPackage.Expiration;
+
+                    // Get the expiration message.
+                    string expirationMessage = expiration.Message;
+
+                    // Get the expiration date.
+                    string expirationDate = expiration.DateTime.ToString("F");
+
+                    // Set the expiration message.
+                    ExpirationLabel.Text = expirationMessage + "\nExpiration date: " + expirationDate;
+
+                    // Check if the map is accessible after expiration.
+                    if (expiration.Type == ExpirationType.AllowExpiredAccess && mobileMapPackage.Maps.Count > 0)
+                    {
+                        // Set the mapview to the map from the mobile map package.
+                        MyMapView.Map = mobileMapPackage.Maps[0];
+                    }
+                    else if (expiration.Type == ExpirationType.PreventExpiredAccess)
+                    {
+                        await Application.Current.MainPage.DisplayAlert("Error", "The author of this mobile map package has disallowed access after the expiration date.", "OK");
+                    }
+                }
+                else if (mobileMapPackage.Maps.Count > 0)
+                {
+                    // Set the mapview to the map from the mobile map package.
+                    MyMapView.Map = mobileMapPackage.Maps[0];
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Error", "Failed to load the mobile map package.", "OK");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }

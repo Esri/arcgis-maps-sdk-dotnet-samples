ba9024a38a6ef5804a272491aa28d7f06232f0cd
Zack Allen <zack.m.allen@gmail.com>
2019-09-05 17:06:00 -0700

    Started Forms implementation

--- src/Forms/Shared/Samples/Network Analysis/NavigateRoute/NavigateRoute.xaml.cs
+++ src/Forms/Shared/Samples/Network Analysis/NavigateRoute/NavigateRoute.xaml.cs
@@ -17,6 +17,30 @@ using Esri.ArcGISRuntime.UI;
 using Esri.ArcGISRuntime.ArcGISServices;
 using Esri.ArcGISRuntime.UI.Controls;
 using Xamarin.Forms;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Location;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Navigation;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks.NetworkAnalysis;
+using Esri.ArcGISRuntime.UI;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows;
+using Color = System.Drawing.Color;
+using Android.Runtime;
+#if __IOS__
+
+#endif
+#if __ANDROID__
+using Android.Speech.Tts;
+#endif
+#if WINDOWS_UWP
+
+#endif
+
 
 namespace ArcGISRuntimeXamarin.Samples.NavigateRoute
 {
@@ -28,14 +52,264 @@ namespace ArcGISRuntimeXamarin.Samples.NavigateRoute
     [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
     public partial class NavigateRoute : ContentPage
     {
+        private RouteTracker _tracker;
+        private RouteResult _routeResult;
+        private Route _route;
+
+        private IReadOnlyList<DirectionManeuver> _directionsList;
+
+#if __IOS__
+
+#endif
+#if __ANDROID__
+        //private TextToSpeech _speechSynthesizer = new TextToSpeech(Android.App.Application.Context, );
+#endif
+#if WINDOWS_UWP
+private SpeechSynthesizer _speechSynthesizer = new SpeechSynthesizer();
+#endif
+
+        //
+
+        private Graphic _routeAheadGraphic;
+        private Graphic _routeTraveledGraphic;
+
+        // San Diego Convention Center.
+        private readonly MapPoint _startLocation = new MapPoint(-117.160386727, 32.706608, SpatialReferences.Wgs84);
+
+        // RH Fleet Aerospace Museum.
+        private readonly MapPoint _destinationLocation = new MapPoint(-117.147230, 32.730467, SpatialReferences.Wgs84);
+
+        // Feature service for routing in San Diego.
+        private readonly Uri _routingUri = new Uri("http://sampleserver6.arcgisonline.com/arcgis/rest/services/NetworkAnalysis/SanDiego/NAServer/Route");
+
         public NavigateRoute()
         {
             InitializeComponent();
             Initialize();
         }
 
-        private void Initialize()
+        private async void Initialize()
         {
+            try
+            {
+                // Add event handler for when this sample is unloaded.
+                //Unloaded += SampleUnloaded;
+
+                // Create the map view.
+                MyMapView.Map = new Map(Basemap.CreateNavigationVector());
+
+                // Create the route task, using the online routing service.
+                RouteTask routeTask = await RouteTask.CreateAsync(_routingUri);
+
+                // Get the default route parameters.
+                RouteParameters routeParams = await routeTask.CreateDefaultParametersAsync();
+
+                // Explicitly set values for parameters.
+                routeParams.ReturnDirections = true;
+                routeParams.ReturnStops = true;
+                routeParams.ReturnRoutes = true;
+                routeParams.OutputSpatialReference = SpatialReferences.Wgs84;
+
+                // Create a Stop for first location.
+                Stop stop1 = new Stop(_startLocation);
+
+                // Create a Stop for the destination (RH Fleet Aerospace Museum).
+                Stop stop2 = new Stop(_destinationLocation);
+
+                // Assign the stops to the route parameters.
+                List<Stop> stopPoints = new List<Stop> { stop1, stop2 };
+                routeParams.SetStops(stopPoints);
+
+                // Get the route results.
+                _routeResult = await routeTask.SolveRouteAsync(routeParams);
+                _route = _routeResult.Routes[0];
+
+                // Add a graphics overlay for the route graphics.
+                MyMapView.GraphicsOverlays.Add(new GraphicsOverlay());
+
+                // Add graphics for the start and end points.
+                MyMapView.GraphicsOverlays[0].Graphics.Add(new Graphic(_startLocation, new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Cross, Color.Green, 25)));
+                MyMapView.GraphicsOverlays[0].Graphics.Add(new Graphic(_destinationLocation, new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.X, Color.Red, 25)));
+
+                // Create a graphic (with a dashed line symbol) to represent the route.
+                _routeAheadGraphic = new Graphic(_route.RouteGeometry) { Symbol = new SimpleLineSymbol(SimpleLineSymbolStyle.Dash, Color.BlueViolet, 5) };
+
+                // Create a graphic (solid) to represent the route that's been traveled (initially empty).
+                _routeTraveledGraphic = new Graphic { Symbol = new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, Color.LightBlue, 3) };
+
+                // Add the route graphics to the map view.
+                MyMapView.GraphicsOverlays[0].Graphics.Add(_routeAheadGraphic);
+                MyMapView.GraphicsOverlays[0].Graphics.Add(_routeTraveledGraphic);
+
+                // Set the map viewpoint to show the entire route.
+                await MyMapView.SetViewpointGeometryAsync(_route.RouteGeometry, 100);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
+
+        private void StartNavigation(object sender, EventArgs e)
+        {
+            // Disable the start navigation button.
+            StartNavigationButton.IsEnabled = false;
+
+            // Get the directions for the route.
+            _directionsList = _route.DirectionManeuvers;
+
+            // Create a route tracker.
+            _tracker = new RouteTracker(_routeResult, 0);
+            _tracker.NewVoiceGuidance += SpeakDirection;
+
+            // Handle route tracking status changes.
+            _tracker.TrackingStatusChanged += TrackingStatusUpdated;
+
+            // Turn on navigation mode for the map view.
+            MyMapView.LocationDisplay.AutoPanMode = LocationDisplayAutoPanMode.Navigation;
+            MyMapView.LocationDisplay.AutoPanModeChanged += AutoPanModeChanged;
+
+            // Add a data source for the location display.
+            MyMapView.LocationDisplay.DataSource = new RouteTrackerLocationDataSource(new FakeLocationProvider(_route.RouteGeometry), _tracker);
+            // Use this instead if you want real location:
+            // MyMapView.LocationDisplay.DataSource = new RouteTrackerLocationDataSource(new SystemLocationDataSource(), _tracker);
+
+            // Enable the location display (this wil start the location data source).
+            MyMapView.LocationDisplay.IsEnabled = true;
+        }
+
+        private void TrackingStatusUpdated(object sender, RouteTrackerTrackingStatusChangedEventArgs e)
+        {
+            TrackingStatus status = e.TrackingStatus;
+
+            // Start building a status message for the UI.
+            System.Text.StringBuilder statusMessageBuilder = new System.Text.StringBuilder();
+
+            // Check the destination status.
+            if (status.DestinationStatus == DestinationStatus.NotReached || status.DestinationStatus == DestinationStatus.Approaching)
+            {
+                statusMessageBuilder.AppendLine("Distance remaining: " +
+                                            status.RouteProgress.RemainingDistance.DisplayText + " " +
+                                            status.RouteProgress.RemainingDistance.DisplayTextUnits.PluralDisplayName);
+
+                statusMessageBuilder.AppendLine("Time remaining: " +
+                                                status.RouteProgress.RemainingTime.ToString(@"hh\:mm\:ss"));
+
+                if (status.CurrentManeuverIndex + 1 < _directionsList.Count)
+                {
+                    statusMessageBuilder.AppendLine("Next direction: " + _directionsList[status.CurrentManeuverIndex + 1].DirectionText);
+                }
+
+                // Set geometries for progress and the remaining route.
+                _routeAheadGraphic.Geometry = status.RouteProgress.RemainingGeometry;
+                _routeTraveledGraphic.Geometry = status.RouteProgress.TraversedGeometry;
+            }
+            else if (status.DestinationStatus == DestinationStatus.Reached)
+            {
+                statusMessageBuilder.AppendLine("Destination reached.");
+
+                // Set the route geometries to reflect the completed route.
+                _routeAheadGraphic.Geometry = null;
+                _routeTraveledGraphic.Geometry = status.RouteResult.Routes[0].RouteGeometry;
+            }
+
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                // Show the status information in the UI.
+                MessagesTextBlock.Text = statusMessageBuilder.ToString().TrimEnd('\n').TrimEnd('\r');
+            });
+        }
+
+        private void SpeakDirection(object sender, RouteTrackerNewVoiceGuidanceEventArgs e)
+        {
+            // Say the direction using voice synthesis.
+#if __IOS__
+
+#endif
+#if __ANDROID__
+            //var p = new Dictionary<string, string>();
+            //_speechSynthesizer.Speak(e.VoiceGuidance.Text, QueueMode.Flush, p);
+#endif
+#if WINDOWS_UWP
+private SpeechSynthesizer _speechSynthesizer = new SpeechSynthesizer();
+#endif
+            //_speechSynthesizer.SpeakAsyncCancelAll();
+            //_speechSynthesizer.SpeakAsync(e.VoiceGuidance.Text);
+        }
+
+        private void AutoPanModeChanged(object sender, LocationDisplayAutoPanMode e)
+        {
+            // Turn the recenter button on or off when the location display changes to or from navigation mode.
+            RecenterButton.IsEnabled = e != LocationDisplayAutoPanMode.Navigation;
+        }
+
+        private void RecenterButton_Click(object sender, EventArgs e)
+        {
+            // Change the mapview to use navigation mode.
+            MyMapView.LocationDisplay.AutoPanMode = LocationDisplayAutoPanMode.Navigation;
+        }
+
+        private void SampleUnloaded(object sender, EventArgs e)
+        {
+            // Stop the speech synthesizer.
+            //_speechSynthesizer.SpeakAsyncCancelAll();
+            //_speechSynthesizer.Dispose();
+
+            // Stop the tracker.
+            if (_tracker != null)
+            {
+                _tracker.TrackingStatusChanged -= TrackingStatusUpdated;
+                _tracker.NewVoiceGuidance -= SpeakDirection;
+                _tracker = null;
+            }
+
+            // Stop the location data source.
+            MyMapView.LocationDisplay?.DataSource?.StopAsync();
+        }
+}
+
+    /*
+     * This location data source uses an input data source and a route tracker.
+     * The location source that it updates is based on the snapped-to-route location from the route tracker.
+     */
+    public class RouteTrackerLocationDataSource : LocationDataSource
+    {
+        private LocationDataSource _inputDataSource;
+        private RouteTracker _routeTracker;
+
+        public RouteTrackerLocationDataSource(LocationDataSource dataSource, RouteTracker routeTracker)
+        {
+            // Set the data source
+            _inputDataSource = dataSource ?? throw new ArgumentNullException(nameof(dataSource));
+
+            // Set the route tracker.
+            _routeTracker = routeTracker ?? throw new ArgumentNullException(nameof(routeTracker));
+
+            // Change the tracker location when the source location changes.
+            _inputDataSource.LocationChanged += InputLocationChanged;
+
+            // Update the location output when the tracker location updates.
+            _routeTracker.TrackingStatusChanged += TrackingStatusChanged;
+        }
+
+        private void InputLocationChanged(object sender, Location e)
+        {
+            // Update the tracker location with the new location from the source (simulation or GPS).
+            _routeTracker.TrackLocationAsync(e);
+        }
+
+        private void TrackingStatusChanged(object sender, RouteTrackerTrackingStatusChangedEventArgs e)
+        {
+            // Check if the tracking status has a location.
+            if (e.TrackingStatus.DisplayLocation != null)
+            {
+                // Call the base method for LocationDataSource to update the location with the tracked (snapped to route) location.
+                UpdateLocation(e.TrackingStatus.DisplayLocation);
+            }
+        }
+
+        protected override Task OnStartAsync() => _inputDataSource.StartAsync();
+
+        protected override Task OnStopAsync() => _inputDataSource.StartAsync();
     }
 }

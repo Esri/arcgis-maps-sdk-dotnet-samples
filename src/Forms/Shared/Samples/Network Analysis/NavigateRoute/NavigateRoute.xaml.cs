df59ffdfcacc28831c5d069adc9220600d34d449
Zack Allen <zack.m.allen@gmail.com>
2019-09-10 10:35:25 -0700

    Cleanup on Forms sample code

--- src/Forms/Shared/Samples/Network Analysis/NavigateRoute/NavigateRoute.xaml.cs
+++ src/Forms/Shared/Samples/Network Analysis/NavigateRoute/NavigateRoute.xaml.cs
@@ -3,26 +3,23 @@
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
 using Esri.ArcGISRuntime.Geometry;
-using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.UI;
-using Xamarin.Forms;
 using Esri.ArcGISRuntime.Location;
+using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Navigation;
+using Esri.ArcGISRuntime.Symbology;
 using Esri.ArcGISRuntime.Tasks.NetworkAnalysis;
+using Esri.ArcGISRuntime.UI;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Color = System.Drawing.Color;
+using Xamarin.Forms;
 using static Xamarin.Essentials.TextToSpeech;
-//using Map = Esri.ArcGISRuntime.Mapping.Map;
-//using Location = Esri.ArcGISRuntime.Location.Location;
-using System.Threading;
+using Color = System.Drawing.Color;
 
 namespace ArcGISRuntimeXamarin.Samples.NavigateRoute
 {
@@ -32,7 +29,6 @@ namespace ArcGISRuntimeXamarin.Samples.NavigateRoute
         "Use a routing service to navigate between two points.",
         "")]
     [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
-
     public partial class NavigateRoute : ContentPage, IDisposable
     {
         private RouteTracker _tracker;
@@ -43,7 +39,6 @@ namespace ArcGISRuntimeXamarin.Samples.NavigateRoute
 
         private Graphic _routeAheadGraphic;
         private Graphic _routeTraveledGraphic;
-        
 
         // San Diego Convention Center.
         private readonly MapPoint _startLocation = new MapPoint(-117.160386727, 32.706608, SpatialReferences.Wgs84);
@@ -53,8 +48,6 @@ namespace ArcGISRuntimeXamarin.Samples.NavigateRoute
 
         // Feature service for routing in San Diego.
         private readonly Uri _routingUri = new Uri("http://sampleserver6.arcgisonline.com/arcgis/rest/services/NetworkAnalysis/SanDiego/NAServer/Route");
-
-        public IntPtr Handle => throw new NotImplementedException();
 
         public NavigateRoute()
         {
@@ -66,10 +59,6 @@ namespace ArcGISRuntimeXamarin.Samples.NavigateRoute
         {
             try
             {
-                // Add event handler for when this sample is unloaded.
-                //Unloaded += SampleUnloaded;
-                
-
                 // Create the map view.
                 MyMapView.Map = new Map(Basemap.CreateNavigationVector());
 

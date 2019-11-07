668cdb6d149b27c9f68d4484f0586791c78aa119
Zack Allen <zack.m.allen@gmail.com>
2019-11-07 11:21:17 -0800

    Added pipe placer for View Hidden

---
+++ src/Forms/AugmentedReality/Samples/ViewHiddenInfrastructureAR/PipeViewer.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Esri.ArcGISRuntime.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ArcGISRuntimeXamarin.Samples.ViewHiddenInfrastructureAR
+{
+    public partial class PipeViewer : ContentPage
+    {
+        // Pipe graphics that have been passed in by the PipePlacer class.
+        public static IEnumerable<Graphic> PipeGraphics;
+        public PipeViewer()
+        {
+            InitializeComponent();
+        }
+    }
+}

73b0d7cf24edc02b16c7674b899d21380674253e
Zack Allen <zack.m.allen@gmail.com>
2019-11-11 18:47:55 -0800

    added about and license markdown files

--- src/iOS/Xamarin.iOS/ViewControllers/SettingsViewController.cs
+++ src/iOS/Xamarin.iOS/ViewControllers/SettingsViewController.cs
@@ -1,6 +1,8 @@
 ﻿using Esri.ArcGISRuntime;
+using Foundation;
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Reflection;
 using UIKit;
 using WebKit;
@@ -15,6 +17,9 @@ namespace ArcGISRuntime
 
         private UISegmentedControl _switcher;
 
+        // Directory for loading HTML locally.
+        private string _contentDirectoryPath = Path.Combine(NSBundle.MainBundle.BundlePath, "Content/");
+
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
@@ -25,6 +30,42 @@ namespace ArcGISRuntime
         {
             NavigationItem.TitleView = _switcher;
             _switcher.ValueChanged += TabChanged;
+
+            var runtimeTypeInfo = typeof(ArcGISRuntimeEnvironment).GetTypeInfo();
+            var rtVersionString = FileVersionInfo.GetVersionInfo(runtimeTypeInfo.Assembly.Location).FileVersion;
+            string aboutPath = Path.Combine(NSBundle.MainBundle.BundlePath, "about.md");
+            string aboutContent = File.ReadAllText(aboutPath) + rtVersionString;
+            string aboutHTML = MarkdownString(aboutContent);
+
+            _aboutView.LoadHtmlString(aboutHTML, new NSUrl(_contentDirectoryPath, true));
+
+            string licensePath = Path.Combine(NSBundle.MainBundle.BundlePath, "licenses.md");
+            string licenseContent = File.ReadAllText(licensePath);
+            string licenseHTML = MarkdownString(licenseContent);
+
+            _licensesView.LoadHtmlString(licenseHTML, new NSUrl(_contentDirectoryPath, true));
+
+        }
+
+        private string MarkdownString(string rawMarkdown)
+        {
+            string markdownCSSPath = Path.Combine(NSBundle.MainBundle.BundlePath, "SyntaxHighlighting/github-markdown.css");
+            string parsedMarkdown = new MarkedNet.Marked().Parse(rawMarkdown);
+
+            string markdowntHTML = "<!doctype html>" +
+                "<head>" +
+                "<link rel=\"stylesheet\" href=\"" +
+                markdownCSSPath +
+                "\" />" +
+                "<meta name=\"viewport\" content=\"width=" +
+                UIScreen.MainScreen.Bounds.Width.ToString() +
+                ", shrink-to-fit=YES\">" +
+                "</head>" +
+                "<body class=\"markdown-body\">" +
+                parsedMarkdown +
+                "</body>";
+
+            return markdowntHTML;
         }
 
         private void TabChanged(object sender, EventArgs e)
@@ -59,12 +100,6 @@ namespace ArcGISRuntime
             _aboutView.TranslatesAutoresizingMaskIntoConstraints = false;
             _aboutView.NavigationDelegate = new BrowserLinksNavigationDelegate();
 
-            UILabel versionLabel = new UILabel();
-            var runtimeTypeInfo = typeof(ArcGISRuntimeEnvironment).GetTypeInfo();
-            var rtVersionString = FileVersionInfo.GetVersionInfo(runtimeTypeInfo.Assembly.Location).FileVersion;
-
-            _aboutView.AddSubview(versionLabel);
-
             _licensesView = new WKWebView(new CoreGraphics.CGRect(), new WKWebViewConfiguration()) { Hidden = true };
             _licensesView.TranslatesAutoresizingMaskIntoConstraints = false;
             _licensesView.NavigationDelegate = new BrowserLinksNavigationDelegate();
@@ -74,21 +109,21 @@ namespace ArcGISRuntime
             _downloadView.TranslatesAutoresizingMaskIntoConstraints = false;
 
             // Add sub views to main view.
-            View.AddSubviews(_switcher, _aboutView, _licensesView, _downloadView);
+            View.AddSubviews(_aboutView, _licensesView, _downloadView);
 
             NSLayoutConstraint.ActivateConstraints(new[]
             {
-                 _aboutView.TopAnchor.ConstraintEqualTo(_switcher.TopAnchor),
+                 _aboutView.TopAnchor.ConstraintEqualTo(View.TopAnchor),
                  _aboutView.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor),
                  _aboutView.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor),
                  _aboutView.BottomAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.BottomAnchor),
 
-                 _licensesView.TopAnchor.ConstraintEqualTo(_switcher.TopAnchor),
+                 _licensesView.TopAnchor.ConstraintEqualTo(View.TopAnchor),
                  _licensesView.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor),
                  _licensesView.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor),
                  _licensesView.BottomAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.BottomAnchor),
 
-                 _downloadView.TopAnchor.ConstraintEqualTo(_switcher.TopAnchor),
+                 _downloadView.TopAnchor.ConstraintEqualTo(View.TopAnchor),
                  _downloadView.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor),
                  _downloadView.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor),
                  _downloadView.BottomAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.BottomAnchor),
@@ -107,25 +142,6 @@ namespace ArcGISRuntime
             base.ViewDidDisappear(animated);
 
             // Unsubscribe from events, per best practice.
-        }
-    }
-    public class BrowserLinksNavigationDelegate : WKNavigationDelegate
-    {
-        public override void DecidePolicy(WKWebView webView, WKNavigationAction navigationAction, Action<WKNavigationActionPolicy> decisionHandler)
-        {
-            if (navigationAction.NavigationType == WKNavigationType.LinkActivated)
-            {
-                // Open links in browser.
-                if (UIApplication.SharedApplication.CanOpenUrl(navigationAction.Request.Url))
-                {
-                    UIApplication.SharedApplication.OpenUrl(navigationAction.Request.Url);
-                }
-                decisionHandler(WKNavigationActionPolicy.Cancel);
-            }
-            else
-            {
-                decisionHandler(WKNavigationActionPolicy.Allow);
-            }
         }
     }
 }

f5f7bb287d01b41f3c45c93fbc5dee4fd360981b
Zack Allen <zack.m.allen@gmail.com>
2019-11-12 10:00:00 -0800

    Added download UI components

--- src/iOS/Xamarin.iOS/ViewControllers/SettingsViewController.cs
+++ src/iOS/Xamarin.iOS/ViewControllers/SettingsViewController.cs
@@ -12,8 +12,14 @@ namespace ArcGISRuntime
     public class SettingsViewController : UIViewController
     {
         private WKWebView _aboutView;
+
         private WKWebView _licensesView;
+
         private UIView _downloadView;
+        private UILabel _downloadLabel;
+        private UIBarButtonItem _downloadAllButton;
+        private UIBarButtonItem _deleteAllButton;
+        private UITableView _downloadTable;
 
         private UISegmentedControl _switcher;
 
@@ -35,19 +41,19 @@ namespace ArcGISRuntime
             var rtVersionString = FileVersionInfo.GetVersionInfo(runtimeTypeInfo.Assembly.Location).FileVersion;
             string aboutPath = Path.Combine(NSBundle.MainBundle.BundlePath, "about.md");
             string aboutContent = File.ReadAllText(aboutPath) + rtVersionString;
-            string aboutHTML = MarkdownString(aboutContent);
+            string aboutHTML = MarkdownToHTML(aboutContent);
 
             _aboutView.LoadHtmlString(aboutHTML, new NSUrl(_contentDirectoryPath, true));
 
             string licensePath = Path.Combine(NSBundle.MainBundle.BundlePath, "licenses.md");
             string licenseContent = File.ReadAllText(licensePath);
-            string licenseHTML = MarkdownString(licenseContent);
+            string licenseHTML = MarkdownToHTML(licenseContent);
 
             _licensesView.LoadHtmlString(licenseHTML, new NSUrl(_contentDirectoryPath, true));
 
         }
 
-        private string MarkdownString(string rawMarkdown)
+        private string MarkdownToHTML(string rawMarkdown)
         {
             string markdownCSSPath = Path.Combine(NSBundle.MainBundle.BundlePath, "SyntaxHighlighting/github-markdown.css");
             string parsedMarkdown = new MarkedNet.Marked().Parse(rawMarkdown);
@@ -66,6 +72,16 @@ namespace ArcGISRuntime
                 "</body>";
 
             return markdowntHTML;
+        }
+
+        private void DeleteAll(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void DownloadAll(object sender, EventArgs e)
+        {
+            
         }
 
         private void TabChanged(object sender, EventArgs e)
@@ -94,19 +110,41 @@ namespace ArcGISRuntime
             // Create and configure the views.
             View = new UIView { BackgroundColor = UIColor.White };
 
+            // Used to switch between the different views.
             _switcher = new UISegmentedControl(new string[] { "About", "Licenses", "Offline Data" }) { SelectedSegment = 0 };
 
+            // Displays the about.md in a web view.
             _aboutView = new WKWebView(new CoreGraphics.CGRect(), new WKWebViewConfiguration());
             _aboutView.TranslatesAutoresizingMaskIntoConstraints = false;
             _aboutView.NavigationDelegate = new BrowserLinksNavigationDelegate();
 
+            // Displays the licenses.md in a web view.
             _licensesView = new WKWebView(new CoreGraphics.CGRect(), new WKWebViewConfiguration()) { Hidden = true };
             _licensesView.TranslatesAutoresizingMaskIntoConstraints = false;
             _licensesView.NavigationDelegate = new BrowserLinksNavigationDelegate();
 
-
+            // View for managing offline data for samples.
             _downloadView = new UIView { BackgroundColor = UIColor.White, Hidden = true };
             _downloadView.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            _downloadLabel = new UILabel() { Text = "Download" };
+            _downloadLabel.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            _downloadAllButton = new UIBarButtonItem() { Title = "Download all"};
+            _deleteAllButton = new UIBarButtonItem() { Title = "Delete all" };
+            var toolbar = new UIToolbar();
+            toolbar.TranslatesAutoresizingMaskIntoConstraints = false;
+            toolbar.Items = new[]
+            {
+                _downloadAllButton,
+                new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace),
+                _deleteAllButton,
+            };
+
+            _downloadTable = new UITableView();
+            _downloadTable.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            _downloadView.AddSubviews(_downloadLabel, toolbar, _downloadTable);
 
             // Add sub views to main view.
             View.AddSubviews(_aboutView, _licensesView, _downloadView);
@@ -127,6 +165,21 @@ namespace ArcGISRuntime
                  _downloadView.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor),
                  _downloadView.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor),
                  _downloadView.BottomAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.BottomAnchor),
+
+                 _downloadLabel.TopAnchor.ConstraintEqualTo(_downloadView.TopAnchor),
+                 _downloadLabel.LeadingAnchor.ConstraintEqualTo(_downloadView.LeadingAnchor),
+                 _downloadLabel.TrailingAnchor.ConstraintEqualTo(_downloadView.TrailingAnchor),
+                 _downloadLabel.BottomAnchor.ConstraintEqualTo(toolbar.TopAnchor),
+
+                 toolbar.TopAnchor.ConstraintEqualTo(_downloadLabel.BottomAnchor),
+                 toolbar.LeadingAnchor.ConstraintEqualTo(_downloadView.LeadingAnchor),
+                 toolbar.TrailingAnchor.ConstraintEqualTo(_downloadView.TrailingAnchor),
+                 toolbar.BottomAnchor.ConstraintEqualTo(_downloadTable.TopAnchor),
+
+                 _downloadTable.TopAnchor.ConstraintEqualTo(toolbar.BottomAnchor),
+                 _downloadTable.LeadingAnchor.ConstraintEqualTo(_downloadView.LeadingAnchor),
+                 _downloadTable.TrailingAnchor.ConstraintEqualTo(_downloadView.TrailingAnchor),
+                 _downloadTable.BottomAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.BottomAnchor),
             });
         }
 
@@ -135,13 +188,19 @@ namespace ArcGISRuntime
             base.ViewWillAppear(animated);
 
             // Subscribe to events.
+            _downloadAllButton.Clicked += DownloadAll;
+            _deleteAllButton.Clicked += DeleteAll;
         }
+
+        
 
         public override void ViewDidDisappear(bool animated)
         {
             base.ViewDidDisappear(animated);
 
             // Unsubscribe from events, per best practice.
+            _downloadAllButton.Clicked -= DownloadAll;
+            _deleteAllButton.Clicked -= DeleteAll;
         }
     }
 }

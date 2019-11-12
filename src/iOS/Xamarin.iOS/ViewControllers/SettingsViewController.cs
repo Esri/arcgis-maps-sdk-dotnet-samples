e33c6d735869a49b046b395e283687bd9bf7f987
Zack Allen <zack.m.allen@gmail.com>
2019-11-12 15:08:36 -0800

    Implemented shell of TableViewSource for sample downloads

--- src/iOS/Xamarin.iOS/ViewControllers/SettingsViewController.cs
+++ src/iOS/Xamarin.iOS/ViewControllers/SettingsViewController.cs
@@ -1,9 +1,15 @@
-﻿using Esri.ArcGISRuntime;
+﻿using ArcGISRuntime.Samples.Managers;
+using ArcGISRuntime.Samples.Shared.Models;
+using CoreGraphics;
+using Esri.ArcGISRuntime;
 using Foundation;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Threading;
 using UIKit;
 using WebKit;
 
@@ -50,7 +56,6 @@ namespace ArcGISRuntime
             string licenseHTML = MarkdownToHTML(licenseContent);
 
             _licensesView.LoadHtmlString(licenseHTML, new NSUrl(_contentDirectoryPath, true));
-
         }
 
         private string MarkdownToHTML(string rawMarkdown)
@@ -76,12 +81,10 @@ namespace ArcGISRuntime
 
         private void DeleteAll(object sender, EventArgs e)
         {
-            
         }
 
         private void DownloadAll(object sender, EventArgs e)
         {
-            
         }
 
         private void TabChanged(object sender, EventArgs e)
@@ -130,7 +133,7 @@ namespace ArcGISRuntime
             _downloadLabel = new UILabel() { Text = "Download" };
             _downloadLabel.TranslatesAutoresizingMaskIntoConstraints = false;
 
-            _downloadAllButton = new UIBarButtonItem() { Title = "Download all"};
+            _downloadAllButton = new UIBarButtonItem() { Title = "Download all" };
             _deleteAllButton = new UIBarButtonItem() { Title = "Delete all" };
             var toolbar = new UIToolbar();
             toolbar.TranslatesAutoresizingMaskIntoConstraints = false;
@@ -142,7 +145,10 @@ namespace ArcGISRuntime
             };
 
             _downloadTable = new UITableView();
+            _downloadTable.Source = new SamplesTableSource();
             _downloadTable.TranslatesAutoresizingMaskIntoConstraints = false;
+            _downloadTable.RowHeight = 50;
+            _downloadTable.AllowsSelection = false;
 
             _downloadView.AddSubviews(_downloadLabel, toolbar, _downloadTable);
 
@@ -179,7 +185,8 @@ namespace ArcGISRuntime
                  _downloadTable.TopAnchor.ConstraintEqualTo(toolbar.BottomAnchor),
                  _downloadTable.LeadingAnchor.ConstraintEqualTo(_downloadView.LeadingAnchor),
                  _downloadTable.TrailingAnchor.ConstraintEqualTo(_downloadView.TrailingAnchor),
-                 _downloadTable.BottomAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.BottomAnchor),
+                 _downloadTable.HeightAnchor.ConstraintEqualTo(_downloadTable.RowHeight*10),
+                 _downloadTable.BottomAnchor.ConstraintEqualTo(_downloadView.SafeAreaLayoutGuide.BottomAnchor),
             });
         }
 
@@ -192,8 +199,6 @@ namespace ArcGISRuntime
             _deleteAllButton.Clicked += DeleteAll;
         }
 
-        
-
         public override void ViewDidDisappear(bool animated)
         {
             base.ViewDidDisappear(animated);
@@ -201,6 +206,124 @@ namespace ArcGISRuntime
             // Unsubscribe from events, per best practice.
             _downloadAllButton.Clicked -= DownloadAll;
             _deleteAllButton.Clicked -= DeleteAll;
+        }
+
+        private class SamplesTableSource : UITableViewSource
+        {
+            private CancellationTokenSource _cancellationTokenSource;
+            private List<SampleInfo> _samples;
+
+            private UIImage _downloadImage = UIImage.FromBundle("InfoIcon");
+            private UIImage _globeImage = UIImage.FromBundle("InfoIcon");
+
+            public SamplesTableSource()
+            {
+                // Set up offline data.
+                _samples = SampleManager.Current.AllSamples.Where(m => m.OfflineDataItems?.Any() ?? false).ToList();
+                _cancellationTokenSource = new CancellationTokenSource();
+            }
+
+            public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+            {
+                // If the cell represents an attachment, get an attachment cell.
+                if (indexPath.Row < _samples.Count)
+                {
+                    // Gets a cell for the specified row.
+                    UITableViewCell cell;
+
+                    // Make the cell.
+                    cell = new UITableViewCell(UITableViewCellStyle.Default, "SampleCell");
+                    cell.TextLabel.Text = _samples[indexPath.Row].SampleName;
+
+                    // Make the accessory view
+                    UIStackView accessoryView = new UIStackView();
+                    accessoryView.Spacing = 5;
+                    accessoryView.Axis = UILayoutConstraintAxis.Horizontal;
+                    accessoryView.LayoutMarginsRelativeArrangement = true;
+                    accessoryView.Alignment = UIStackViewAlignment.Fill;
+                    accessoryView.LayoutMargins = new UIEdgeInsets(8, 8, 8, 8);
+
+                    UIButton agolButton = new UIButton() { TranslatesAutoresizingMaskIntoConstraints = false };
+                    agolButton.SetImage(_globeImage, UIControlState.Normal);
+                    agolButton.TouchUpInside += (s, e) => OpenInAGOL(indexPath);
+                    CGRect agolFrame = new CGRect(0, 0, _downloadImage.Size.Width, _downloadImage.Size.Height);
+                    agolButton.Frame = agolFrame;
+
+                    UIButton dlButton = new UIButton() { TranslatesAutoresizingMaskIntoConstraints = false };
+                    dlButton.SetImage(_downloadImage, UIControlState.Normal);
+                    dlButton.TouchUpInside += (s, e) => Download(indexPath);
+                    CGRect frame = new CGRect(0, 0, _downloadImage.Size.Width, _downloadImage.Size.Height);
+                    dlButton.Frame = frame;
+
+                    accessoryView.AddSubviews(agolButton, dlButton);
+                    
+                    accessoryView.Frame = new CGRect(0, 0, _downloadImage.Size.Width + _globeImage.Size.Width + 20, Math.Max(_downloadImage.Size.Height , _globeImage.Size.Height));
+
+                    NSLayoutConstraint.ActivateConstraints(new[]
+                    {
+                         agolButton.TopAnchor.ConstraintEqualTo(accessoryView.TopAnchor),
+                         agolButton.LeadingAnchor.ConstraintEqualTo(accessoryView.LeadingAnchor),
+                         agolButton.TrailingAnchor.ConstraintEqualTo(dlButton.LeadingAnchor),
+                         agolButton.BottomAnchor.ConstraintEqualTo(accessoryView.BottomAnchor),
+
+                         dlButton.TopAnchor.ConstraintEqualTo(accessoryView.TopAnchor),
+                         dlButton.LeadingAnchor.ConstraintEqualTo(agolButton.TrailingAnchor),
+                         dlButton.TrailingAnchor.ConstraintEqualTo(accessoryView.TrailingAnchor),
+                         dlButton.BottomAnchor.ConstraintEqualTo(accessoryView.BottomAnchor),
+                    });
+
+                    cell.Accessory = UITableViewCellAccessory.Checkmark;
+                    cell.AccessoryView = accessoryView;
+
+                    return cell;
+                }
+                return null;
+            }
+
+            private void OpenInAGOL(NSIndexPath indexPath)
+            {
+            }
+
+            private void Download(NSIndexPath indexPath)
+            {
+            }
+
+            public override UITableViewRowAction[] EditActionsForRow(UITableView tableView, NSIndexPath indexPath)
+            {
+                var actions = new UITableViewRowAction[1];
+                actions[0] = UITableViewRowAction.Create(UITableViewRowActionStyle.Destructive, "Delete", DeleteHandler);
+                return actions;
+            }
+
+            private void DeleteHandler(UITableViewRowAction action, NSIndexPath indexPath)
+            {
+                try
+                {
+                    SampleInfo sample = _samples[indexPath.Row];
+
+                    foreach (string offlineItemId in sample.OfflineDataItems)
+                    {
+                        string offlineDataPath = DataManager.GetDataFolder(offlineItemId);
+
+                        Directory.Delete(offlineDataPath, true);
+                    }
+                    //await new MessageDialog($"Offline data deleted for {sample.SampleName}").ShowAsync();
+                }
+                catch (Exception exception)
+                {
+                    Debug.WriteLine(exception);
+                    //await new MessageDialog($"Couldn't delete offline data.", "Error").ShowAsync();
+                }
+            }
+
+            public override nint RowsInSection(UITableView tableview, nint section)
+            {
+                if (section == 0)
+                {
+                    return _samples?.Count ?? 0;
+                }
+                return 0;
+            }
         }
     }
 }

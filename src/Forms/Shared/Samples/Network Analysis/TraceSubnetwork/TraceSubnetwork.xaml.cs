f31458df98d44c21844e724cbb29ed9ae6bb00aa
Zack Allen <zack.m.allen@gmail.com>
2019-11-14 11:36:14 -0800

    Templated

---
+++ src/Forms/Shared/Samples/Network Analysis/TraceSubnetwork/TraceSubnetwork.xaml.cs
@@ -0,0 +1,41 @@
+﻿// Copyright 2019 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.Tasks.Offline;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.ArcGISServices;
+using Esri.ArcGISRuntime.UI.Controls;
+using Xamarin.Forms;
+
+namespace ArcGISRuntimeXamarin.Samples.TraceSubnetwork
+{
+    [ArcGISRuntime.Samples.Shared.Attributes.Sample(
+        "Trace a subnetwork",
+        "Network Analysis",
+        "Discover all the features participating in a subnetwork with subnetwork, upstream, and downstream trace types.",
+        "")]
+    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
+    public partial class TraceSubnetwork : ContentPage
+    {
+        public TraceSubnetwork()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+        }
+    }
+}

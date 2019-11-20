e84f1fcee2c2d2415132e7952e5760210579b866
Zack Allen <zack.m.allen@gmail.com>
2019-11-20 10:14:19 -0800

    Cleanup

--- src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Network Analysis/TraceSubnetwork/TraceSubnetwork.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Network Analysis/TraceSubnetwork/TraceSubnetwork.xaml.cs
@@ -20,7 +20,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using SelectionMode = Esri.ArcGISRuntime.Mapping.SelectionMode;
 
 namespace ArcGISRuntime.WPF.Samples.TraceSubnetwork
 {

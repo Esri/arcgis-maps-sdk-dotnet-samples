e6b242b20f76cb6d4c9e9f03fc046280bc1d18e3
Zack Allen <zack.m.allen@gmail.com>
2019-12-06 15:58:41 -0800

    Started android sample

--- src/Android/Xamarin.Android/Samples/Layers/DisplaySubtypeFeatureLayer/DisplaySubtypeFeatureLayer.cs
+++ src/Android/Xamarin.Android/Samples/Layers/DisplaySubtypeFeatureLayer/DisplaySubtypeFeatureLayer.cs
@@ -19,20 +19,42 @@ using Esri.ArcGISRuntime.Tasks.Offline;
 using Esri.ArcGISRuntime.UI;
 using Esri.ArcGISRuntime.ArcGISServices;
 using Esri.ArcGISRuntime.UI.Controls;
+using ArcGISRuntime;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using System;
+using System.ComponentModel;
 
 namespace ArcGISRuntimeXamarin.Samples.DisplaySubtypeFeatureLayer
 {
     [Activity (ConfigurationChanges=Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
     [ArcGISRuntime.Samples.Shared.Attributes.Sample(
-        "Display a subtype feature layer",
+        "Display subtype feature layer",
         "Layers",
         "Displays a composite layer of all the subtype values in a feature class.",
         "")]
-    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
     public class DisplaySubtypeFeatureLayer : Activity
     {
         // Hold references to the UI controls.
         private MapView _myMapView;
+        private Button _minScaleButton;
+        private Button _changeRendererButton;
+        private RadioButton _visibleButton;
+        private RadioButton _notVisibleButton;
+        private TextView _mapScaleLabel;
+        private TextView _sublayerScaleLabel;
+
+        // Reference to a sublayer.
+        private SubtypeSublayer _sublayer;
+
+        // JSON for labeling features from the sublayer.
+        private const string _labelJSON = "{ \"labelExpression\":\"[nominalvoltage]\",\"labelPlacement\":\"esriServerPointLabelPlacementAboveRight\",\"useCodedValues\":true,\"symbol\":{\"angle\":0,\"backgroundColor\":[0,0,0,0],\"borderLineColor\":[0,0,0,0],\"borderLineSize\":0,\"color\":[0,0,255,255],\"font\":{\"decoration\":\"none\",\"size\":10.5,\"style\":\"normal\",\"weight\":\"normal\"},\"haloColor\":[255,255,255,255],\"haloSize\":2,\"horizontalAlignment\":\"center\",\"kerning\":false,\"type\":\"esriTS\",\"verticalAlignment\":\"middle\",\"xoffset\":0,\"yoffset\":0}}";
+
+        // Renderers for the sublayer.
+        private Renderer _defaultRenderer;
+        private Renderer _customRenderer;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -44,21 +66,80 @@ namespace ArcGISRuntimeXamarin.Samples.DisplaySubtypeFeatureLayer
             Initialize();
         }
 
-        private void Initialize()
+        private async void Initialize()
         {
+            try
+            {
+                // Starting viewpoint for the map view.
+                Viewpoint _startingViewpoint = new Viewpoint(new Envelope(-9812691.11079696, 5128687.20710657, -9812377.9447607, 5128865.36767282, SpatialReferences.WebMercator));
+
+                // Create the map.
+                _myMapView.Map = new Map(Basemap.CreateStreetsNightVector()) { InitialViewpoint = _startingViewpoint };
+
+                // NOTE: This layer supports any ArcGIS Feature Table that define subtypes.
+                SubtypeFeatureLayer subtypeFeatureLayer = new SubtypeFeatureLayer(new ServiceFeatureTable(new Uri("https://sampleserver7.arcgisonline.com/arcgis/rest/services/UtilityNetwork/NapervilleElectric/FeatureServer/100")));
+                _myMapView.Map.OperationalLayers.Add(subtypeFeatureLayer);
+
+                // Select sublayer to control.
+                await subtypeFeatureLayer.LoadAsync();
+
+                // Select the sublayer of street lights by name.
+                _sublayer = subtypeFeatureLayer.GetSublayerBySubtypeName("Street Light");
+
+                // Set the label definitions using the JSON.
+                _sublayer.LabelDefinitions.Add(LabelDefinition.FromJson(_labelJSON));
+
+                // Enable labels for the sub layer.
+                _sublayer.LabelsEnabled = true;
+
+                // Get the default renderer for the sublayer.
+                _defaultRenderer = _sublayer.Renderer.Clone();
+
+                // Create a custom renderer for the sublayer.
+                _customRenderer = new SimpleRenderer()
+                {
+                    Symbol = new SimpleMarkerSymbol()
+                    {
+                        Color = System.Drawing.Color.Salmon,
+                        Style = SimpleMarkerSymbolStyle.Diamond,
+                        Size = 20,
+                    }
+                };
+
+                // Update the UI for displaying the current map scale.
+                _myMapView.ViewpointChanged += ViewpointChanged;
+                _mapScaleLabel.Text = $"Current map scale: 1:{(int)_myMapView.MapScale}";
+            }
+            catch (Exception ex)
+            {
+                new AlertDialog.Builder(this).SetMessage(ex.Message).SetTitle(ex.GetType().ToString()).Show();
+            }
+        }
+
+        private void ViewpointChanged(object sender, EventArgs e)
+        {
+            // Update the label showing the current map scale.
+            _mapScaleLabel.Text = $"Current map scale: 1:{(int)_myMapView.MapScale}";
         }
 
         private void CreateLayout()
         {
-            // Create a new vertical layout for the app.
-            var layout = new LinearLayout(this) { Orientation = Orientation.Vertical };
+            // Load the layout from the axml resource.
+            SetContentView(Resource.Layout.DisplaySubtypeFeatureLayer);
 
-            // Add the map view to the layout.
-            _myMapView = new MapView(this);
-            layout.AddView(_myMapView);
+            _myMapView = FindViewById<MapView>(Resource.Id.MapView);
 
-            // Show the layout in the app.
-            SetContentView(layout);
+            _minScaleButton = FindViewById<Button>(Resource.Id.minScaleButton);
+            _changeRendererButton = FindViewById<Button>(Resource.Id.rendererButton);
+
+            _visibleButton = FindViewById<RadioButton>(Resource.Id.visibleButton);
+           _notVisibleButton = FindViewById<RadioButton>(Resource.Id.notVisibleButton);
+
+            _mapScaleLabel = FindViewById<TextView>(Resource.Id.mapScaleLabel);
+            _sublayerScaleLabel = FindViewById<TextView>(Resource.Id.sublayerScaleLabel);
+
+            // Add listeners for all of the buttons.
+            
         }
     }
 }

ea2c7f59f24a505025167421877fb036ca4c2d08
Zack Allen <zack.m.allen@gmail.com>
2020-01-02 12:01:43 -0800

    Updated Xamarin.Forms

--- src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Layers/BrowseWfsLayers/BrowseWfsLayers.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Layers/BrowseWfsLayers/BrowseWfsLayers.xaml.cs
@@ -17,7 +17,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace ArcGISRuntime.WPF.Samples.BrowseWfsLayers

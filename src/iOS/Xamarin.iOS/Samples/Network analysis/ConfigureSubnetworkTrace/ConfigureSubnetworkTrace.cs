f42dec63fd1727fb83204776f2276a2e605aa2f7
Zack Allen <zack.m.allen@gmail.com>
2020-01-23 15:25:46 -0800

    Added UIPickerView

--- src/iOS/Xamarin.iOS/Samples/Network analysis/ConfigureSubnetworkTrace/ConfigureSubnetworkTrace.cs
+++ src/iOS/Xamarin.iOS/Samples/Network analysis/ConfigureSubnetworkTrace/ConfigureSubnetworkTrace.cs
@@ -10,6 +10,8 @@
 using Esri.ArcGISRuntime.UtilityNetworks;
 using Foundation;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UIKit;
 
 namespace ArcGISRuntimeXamarin.Samples.ConfigureSubnetworkTrace
@@ -61,8 +63,57 @@ namespace ArcGISRuntimeXamarin.Samples.ConfigureSubnetworkTrace
             Title = "Configure subnetwork trace";
         }
 
-        private void Initialize()
+        private async void Initialize()
         {
+            try
+            {
+                //Configuration.IsEnabled = false;
+
+                // Create and load the utility network.
+                _utilityNetwork = await UtilityNetwork.CreateAsync(new Uri(FeatureServiceUrl));
+
+                // Build the choice lists for network attribute comparison.
+                _attributePicker.Model = new AttributeComparisonPickerModel(_utilityNetwork.Definition.NetworkAttributes.Where(i => i.IsSystemDefined == false));
+                //ComparisonOperators.ItemsSource = Enum.GetValues(typeof(UtilityAttributeComparisonOperator));
+
+                //// Create a default starting location.
+                //UtilityNetworkSource networkSource = _utilityNetwork.Definition.GetNetworkSource(DeviceTableName);
+                //UtilityAssetGroup assetGroup = networkSource.GetAssetGroup(AssetGroupName);
+                //UtilityAssetType assetType = assetGroup.GetAssetType(AssetTypeName);
+                //Guid globalId = Guid.Parse(GlobalId);
+                //_startingLocation = _utilityNetwork.CreateElement(assetType, globalId);
+
+                //// Set the terminal for this location. (For our case, we use the 'Load' terminal.)
+                //_startingLocation.Terminal = _startingLocation.AssetType.TerminalConfiguration?.Terminals.Where(t => t.Name == "Load").FirstOrDefault();
+
+                //// Get a default trace configuration from a tier to update the UI.
+                //UtilityDomainNetwork domainNetwork = _utilityNetwork.Definition.GetDomainNetwork(DomainNetworkName);
+                //UtilityTier sourceTier = domainNetwork.GetTier(TierName);
+
+                //// Set the trace configuration.
+                //_configuration = sourceTier.TraceConfiguration;
+
+                //// Set the default expression (if provided).
+                //if (sourceTier.TraceConfiguration.Traversability.Barriers is UtilityTraceConditionalExpression expression)
+                //{
+                //    ConditionBarrierExpression.Text = ExpressionToString(expression);
+                //    _initialExpression = expression;
+                //}
+
+                //// Setting DataContext will resolve the data-binding in XAML.
+                //Configuration.DataContext = _configuration;
+
+                //// Set the traversability scope.
+                //sourceTier.TraceConfiguration.Traversability.Scope = UtilityTraversabilityScope.Junctions;
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message, ex.Message.GetType().Name, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                //Configuration.IsEnabled = true;
+            }
         }
 
         public override void LoadView()
@@ -143,6 +194,7 @@ namespace ArcGISRuntimeXamarin.Samples.ConfigureSubnetworkTrace
                 buttonContainer.WidthAnchor.ConstraintEqualTo(scrollView.WidthAnchor),
             });
         }
+
         private UIStackView GetRowStackView(UIView[] views)
         {
             UIStackView row = new UIStackView(views);
@@ -157,6 +209,73 @@ namespace ArcGISRuntimeXamarin.Samples.ConfigureSubnetworkTrace
         {
             base.ViewDidLoad();
             Initialize();
+        }
+
+        private class AttributeComparisonPickerModel : UIPickerViewModel
+        {
+            private UtilityNetworkAttribute[] _attributes;
+            private Array _comparisons;
+            public UtilityNetworkAttribute SelectedAttribute;
+            public int SelectedComparison;
+
+            // Constructor takes the default values for RGB.
+            public AttributeComparisonPickerModel(IEnumerable<UtilityNetworkAttribute> attributes)
+            {
+                _attributes = attributes.ToArray();
+                _comparisons = Enum.GetValues(typeof(UtilityAttributeComparisonOperator));
+            }
+
+            // Return the number of picker components.
+            public override nint GetComponentCount(UIPickerView pickerView)
+            {
+                return 2;
+            }
+
+            // Return the number of attributes.
+            public override nint GetRowsInComponent(UIPickerView pickerView, nint component)
+            {
+                return component == 0 ? _attributes.Count() : _comparisons.Length;
+            }
+
+            // Get the title to display in each picker component.
+            public override string GetTitle(UIPickerView pickerView, nint row, nint component)
+            {
+                if (component == 0)
+                {
+                    return _attributes[row].Name;
+                }
+                else
+                {
+                    return Enum.GetName(typeof(UtilityAttributeComparisonOperator), (int)row);
+                }
+            }
+
+            // Handle the selection event for the picker.
+            public override void Selected(UIPickerView pickerView, nint row, nint component)
+            {
+                // Get the selected RGB values.
+                if(component == 0)
+                {
+                    SelectedAttribute = _attributes[row];
+                }
+                else
+                {
+                    SelectedComparison = (int)row;
+                }
+            }
+
+            //// Return the desired width for each component in the picker.
+            //public override nfloat GetComponentWidth(UIPickerView pickerView, nint component)
+            //{
+            //    // All components display the same range of values (largest is 3 digits).
+            //    return 60f;
+            //}
+
+            //// Return the desired height for rows in the picker.
+            //public override nfloat GetRowHeight(UIPickerView pickerView, nint component)
+            //{
+            //    return 30f;
+            //}
         }
     }
 }

aa5b701121e1abd17f1ee499f49515dd6c421e7c
Zack Allen <zack.m.allen@gmail.com>
2020-02-10 12:54:14 -0800

    Added working list of samples with offline data

--- src/Forms/Shared/SampleSettingsPage.xaml.cs
+++ src/Forms/Shared/SampleSettingsPage.xaml.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using ArcGISRuntime.Samples.Managers;
+using ArcGISRuntime.Samples.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using Xamarin.Forms;
 
 namespace ArcGISRuntime
 {
     public partial class SampleSettingsPage : TabbedPage
     {
+        private static string _runtimeVersion = "";
+        private CancellationTokenSource _cancellationTokenSource;
+        private List<SampleInfo> OfflineDataSamples;
+        private readonly MarkedNet.Marked _markdownRenderer = new MarkedNet.Marked();
+
         public SampleSettingsPage()
         {
             InitializeComponent();
@@ -14,6 +24,35 @@ namespace ArcGISRuntime
         private void Initialize()
         {
             LicensePage.Source = "https://www.google.com";
+
+            // Set up offline data.
+            OfflineDataSamples = SampleManager.Current.AllSamples.Where(m => m.OfflineDataItems?.Any() ?? false).ToList();
+            OfflineDataView.ItemsSource = OfflineDataSamples;
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        private void DownloadClicked(object sender, EventArgs e)
+        {
+            if(((Button)sender).CommandParameter is SampleInfo sampleInfo)
+            {
+                // Download data for that sample.
+            }
+        }
+
+        private void AGOLClicked(object sender, EventArgs e)
+        {
+            if (((Button)sender).CommandParameter is SampleInfo sampleInfo)
+            {
+                // Open data for that sample in AGOL.
+            }
+        }
+
+        private void DeleteClicked(object sender, EventArgs e)
+        {
+            if (((Button)sender).CommandParameter is SampleInfo sampleInfo)
+            {
+                // Delete data for that sample.
+            }
         }
     }
 }

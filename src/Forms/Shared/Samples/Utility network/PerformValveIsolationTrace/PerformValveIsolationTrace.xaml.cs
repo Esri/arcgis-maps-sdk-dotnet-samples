3a807005eb2ef054ec6630a3ed80ef2bf254903b
Zack Allen <zack.m.allen@gmail.com>
2020-02-18 15:29:58 -0800

    Templated sample

---
+++ src/Forms/Shared/Samples/Utility network/PerformValveIsolationTrace/PerformValveIsolationTrace.xaml.cs
@@ -0,0 +1,41 @@
+﻿// Copyright 2020 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.Tasks.Offline;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.ArcGISServices;
+using Esri.ArcGISRuntime.UI.Controls;
+using Xamarin.Forms;
+
+namespace ArcGISRuntimeXamarin.Samples.PerformValveIsolationTrace
+{
+    [ArcGISRuntime.Samples.Shared.Attributes.Sample(
+        "Perform valve isolation trace",
+        "Utility network",
+        "Run a filtered trace to locate operable features that will isolate an area from the flow of network resources.",
+        "")]
+    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
+    public partial class PerformValveIsolationTrace : ContentPage
+    {
+        public PerformValveIsolationTrace()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+        }
+    }
+}

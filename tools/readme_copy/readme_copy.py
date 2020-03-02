ce645b9437ab3bd9c594137938e52b86831e287f
Zack Allen <zack.m.allen@gmail.com>
2020-03-02 12:18:43 -0800

    Added python script for copying readmes from WPF

--- tools/readme_copy/readme_copy.py
+++ tools/readme_copy/readme_copy.py
@@ -31,28 +31,33 @@ def replace_readmes(category, formal_name, sample_root):
         for platform in plats:
             platform_path = os.path.join(get_platform_samples_root(platform, sample_root), category, formal_name, ("readme.md"))
             
+            # Fix the guide doc url for the platform
+            platform_lines = [line.replace("wpf/guide", str.lower(platform)+"/guide") for line in wpflines]
+
             # Write the WPF readme to other platform
             with open(platform_path, "r+") as file:
                 file.seek(0)
-                file.writelines(wpflines)
+                file.writelines(platform_lines)
                 file.truncate()
                 print(f"{formal_name} written to {platform}")
     except OSError as e:
         print(e.errno)
 def main():
-    if len(sys.argv) < 4:
-        print("Usage: python readme_copy.py {category} {formal name of sample} {path_to_samples (ends in src)}")
+    if len(sys.argv) is 4:
+        # Get the user arguments.
+        category = sys.argv[1]
+        formal_name = sys.argv[2]        
+        sample_root = sys.argv[3]
+        replace_readmes(category, formal_name, sample_root)
+    elif len(sys.argv) is 2:
+        for category in os.listdir(get_platform_samples_root("WPF", sample_root)):
+            for sample in os.listdir( os.path.join(get_platform_samples_root("WPF", sample_root), category) ):
+                replace_readmes(category, sample, sample_root)
+    else:
+        print("Usage for single sample: python readme_copy.py {category} {formal name of sample} {path_to_samples (ends in src)}")
+        print("Usage for all samples: python readme_copy.py {path_to_samples (ends in src)}")
         return
-    # Get the user arguments.
-    category = sys.argv[1]
-    formal_name = sys.argv[2]        
-    sample_root = sys.argv[3]
 
-    for category in os.listdir(get_platform_samples_root("WPF", sample_root)):
-        for sample in os.listdir( os.path.join(get_platform_samples_root("WPF", sample_root), category) ):
-            replace_readmes(category, sample, sample_root)
 
-    replace_readmes(category, formal_name, sample_root)
-    
 if __name__=="__main__":
     main()

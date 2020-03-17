9a2737da8abaa07534c1eedba0cc750d5af3117c
Zack Allen <zack.m.allen@gmail.com>
2020-03-17 15:12:09 -0700

    Renamed raster layer raster function

--- src/iOS/Xamarin.iOS/Samples/Layers/RasterLayerRasterFunction/RasterLayerRasterFunction.cs
+++ src/iOS/Xamarin.iOS/Samples/Layers/RasterLayerRasterFunction/RasterLayerRasterFunction.cs
@@ -20,7 +20,7 @@ namespace ArcGISRuntime.Samples.RasterLayerRasterFunction
 {
     [Register("RasterLayerRasterFunction")]
     [ArcGISRuntime.Samples.Shared.Attributes.Sample(
-        "ArcGIS raster function (service)",
+        "Apply raster function to raster from service",
         "Layers",
         "Show a raster layer from an image service with a raster function applied.",
         "")]

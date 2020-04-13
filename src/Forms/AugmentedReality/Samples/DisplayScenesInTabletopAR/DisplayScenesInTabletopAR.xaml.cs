246a49f6e662702e6b35aac611fdacfa8e7defe6
Zack Allen <zack.m.allen@gmail.com>
2020-04-13 15:30:45 -0700

    Capitalization fix

---
+++ src/Forms/AugmentedReality/Samples/DisplayScenesInTabletopAR/DisplayScenesInTabletopAR.xaml.cs
@@ -0,0 +1,147 @@
+﻿// Copyright 2020 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+
+using ArcGISRuntime.Samples.Managers;
+using Esri.ArcGISRuntime.Mapping;
+using System;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace ArcGISRuntimeXamarin.Samples.DisplayScenesInTabletopAR
+{
+    [ArcGISRuntime.Samples.Shared.Attributes.Sample(
+        "Display scenes in tabletop AR",
+        "Augmented reality",
+        "Use augmented reality (AR) to pin a scene to a table or desk for easy exploration.",
+        "")]
+    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("7dd2f97bb007466ea939160d0de96a9d")]
+    public partial class DisplayScenesInTabletopAR : ContentPage, IARSample
+    {
+        // Scene to be displayed on the tabletop.
+        private Scene _tabletopScene;
+
+        public DisplayScenesInTabletopAR()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            // Display an empty scene to enable tap-to-place.
+            MyARSceneView.Scene = new Scene(SceneViewTilingScheme.Geographic);
+
+            // Render the scene invisible to start.
+            MyARSceneView.Scene.BaseSurface.Opacity = 0;
+
+            // Get notification when planes are detected
+            MyARSceneView.PlanesDetectedChanged += ARSceneView_PlanesDetectedChanged;
+        }
+
+        // This method is called by the sample viewer when the AR sample has appeared.
+        async void IARSample.StartAugmentedReality()
+        {
+            // Start device tracking.
+            try
+            {
+                await MyARSceneView.StartTrackingAsync();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private void ARSceneView_PlanesDetectedChanged(object sender, bool planeDetected)
+        {
+            if (planeDetected)
+            {
+                EnableTapToPlace();
+                MyARSceneView.PlanesDetectedChanged -= ARSceneView_PlanesDetectedChanged;
+            }
+        }
+
+        private void EnableTapToPlace()
+        {
+            // Show the help label.
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                HelpLabel.Text = "Tap to place the scene.";
+            });
+
+            // Wait for the user to tap.
+            MyARSceneView.GeoViewTapped += ARGeoViewTapped;
+        }
+
+        private void ARGeoViewTapped(object sender, Esri.ArcGISRuntime.Xamarin.Forms.GeoViewInputEventArgs e)
+        {
+            // If the tapped position is valid, display the scene.
+            if (MyARSceneView.SetInitialTransformation(e.Position)) DisplayScene();
+        }
+
+        private async void DisplayScene()
+        {
+            // Hide the help label.
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                HelpLabel.IsVisible = false;
+            });
+
+            if (_tabletopScene == null)
+            {
+                // Get the downloaded mobile scene package.
+                MobileScenePackage package =
+                    await MobileScenePackage.OpenAsync(DataManager.GetDataFolder("7dd2f97bb007466ea939160d0de96a9d",
+                        "philadelphia.mspk"));
+
+                // Load the package.
+                await package.LoadAsync();
+
+                // Get the first scene.
+                _tabletopScene = package.Scenes.First();
+
+                // Hide the base surface.
+                _tabletopScene.BaseSurface.Opacity = 0;
+
+                // Enable subsurface navigation. This allows you to look at the scene from below.
+                _tabletopScene.BaseSurface.NavigationConstraint = NavigationConstraint.None;
+
+                // Set the AR scene to the tabletop scene.
+                MyARSceneView.Scene = _tabletopScene;
+            }
+
+            // Create a camera at the bottom and center of the scene.
+            //    This camera is the point at which the scene is pinned to the real-world surface.
+            Camera originCamera = new Camera(39.95787000283599,
+                -75.16996728256345,
+                8.813445091247559,
+                0, 90, 0);
+
+            // Set the origin camera.
+            MyARSceneView.OriginCamera = originCamera;
+
+            // The width of the scene content is about 800 meters.
+            double geographicContentWidth = 800;
+
+            // Set the clipping distance for the scene.
+            MyARSceneView.ClippingDistance = geographicContentWidth/2;
+
+            // The desired physical width of the scene is 1 meter.
+            double tableContainerWidth = 1;
+
+            // Set the translation factor based on the scene content width and desired physical size.
+            MyARSceneView.TranslationFactor = geographicContentWidth / tableContainerWidth;
+        }
+
+        void IARSample.StopAugmentedReality()
+        {
+            MyARSceneView.StopTrackingAsync();
+        }
+    }
+}
---
+++ src/Forms/AugmentedReality/Samples/DisplayScenesInTabletopAR/readme.md
@@ -0,0 +1,48 @@
+# Display scenes in tabletop AR
+
+Use augmented reality (AR) to pin a scene to a table or desk for easy exploration.
+
+![Scene content shown sitting on a surface, as if it were a 3D printed model](DisplayScenesInTabletopAR.jpg)
+
+## Use case
+
+Tabletop scenes allow you to use your device to interact with scenes as if they are 3D-printed model models sitting on your desk. You could use this to virtually explore a proposed development without needing to create a physical model.
+
+## How to use the sample
+
+You'll see a feed from the camera when you open the sample. Tap on any flat, horizontal surface (like a desk or table) to place the scene. With the scene placed, you can move the camera around the scene to explore. You can also pan and zoom with touch to adjust the position of the scene.
+
+## How it works
+
+1. Create an `ARSceneView` and add it to the view.
+    * Note: this sample uses content in the WGS 84 geographic tiling scheme, rather than the web mercator tiling scheme. Once a scene has been displayed, the scene view cannot display another scene with a non-matching tiling scheme. To avoid that, the sample starts by showing a blank scene with an invisible base surface. Touch events will not be raised for the scene view unless a scene is displayed.
+2. Wait for plane detection using `arView.PlanesDetectedChanged` before allowing the user to tap to place the scene.
+3. Wait for the user to tap the view, then use `arView.SetInitialTransforamtion(tappedPoint)` to set the initial transformation, which allows you to place the scene. This method uses ARKit's built-in plane detection.
+4. If the call to `SetInitialTransformation` returns `true`, proceed to load the scene and display it.
+5. To enable looking at the scene from below, set `scene.BaseSurface.NavigationConstraint` to `0`.
+6. Set the origin camera to the point in the scene where it should be anchored to the real-world surface you tapped. Typically that is the point at the center of the scene, with the altitude of the lowest point in the scene.
+7. Set `arView.TranslationFactor` such that the user can view the entire scene by moving the device around it. The translation factor defines how far the virtual camera moves when the physical camera moves.
+    * A good formula for determining translation factor to use in a tabletop map experience is **translationFactor = sceneWidth / tableTopWidth**. The scene width is the width/length of the scene content you wish to display in meters. The tabletop width is the length of the area on the physical surface that you want the scene content to fill. For simplicity, the sample assumes a scene width of 800 meters and physical size of 1 meter.
+
+## Relevant API
+
+* ARSceneView
+* Surface
+
+## Offline data
+
+This sample uses offline data, available as an [item on ArcGIS Online](https://www.arcgis.com/home/item.html?id=7dd2f97bb007466ea939160d0de96a9d).
+
+## About the data
+
+This sample uses the [Philadelphia Mobile Scene Package](https://www.arcgis.com/home/item.html?id=7dd2f97bb007466ea939160d0de96a9d). It was chosen because it is a compact scene ideal for tabletop use. Note that tabletop mapping experiences work best with small, focused scenes. The small, focused area with basemap tiles defines a clear boundary for the scene.
+
+## Additional information
+
+**Tabletop AR** is one of three main patterns for working with geographic information in augmented reality. See [Display scenes in augmented reality](https://developers.arcgis.com/net/latest/forms/guide/display-scenes-in-augmented-reality.htm) in the guide for more information.
+
+This sample uses the ArcGIS Runtime Toolkit. See [Display scenes in augmented reality](https://developers.arcgis.com/net/latest/forms/guide/display-scenes-in-augmented-reality.htm) in the guide to learn about the toolkit and how to add it to your app.
+
+## Tags
+
+augmented reality, drop, mixed reality, model, pin, place, table-top, tabletop

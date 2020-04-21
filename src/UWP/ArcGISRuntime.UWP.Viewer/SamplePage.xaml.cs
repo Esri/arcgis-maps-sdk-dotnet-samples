66d94d1788bc71da5f3369be93734143e57cb06d
Zack Allen <zack.m.allen@gmail.com>
2020-04-21 12:17:46 -0700

    Fixed bug where tab switching was disabled after going to settings

--- src/UWP/ArcGISRuntime.UWP.Viewer/SamplePage.xaml.cs
+++ src/UWP/ArcGISRuntime.UWP.Viewer/SamplePage.xaml.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2019 Esri.
+﻿// Copyright 2020 Esri.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
@@ -22,8 +22,9 @@ namespace ArcGISRuntime.UWP.Viewer
         {
             InitializeComponent();
 
-            // Add event for cleaning up sample page when closed.
+            // Add events for cleaning up sample page when closed or opened.
             Unloaded += SamplePage_Unloaded;
+            Loaded += SamplePage_Loaded;
 
             // Get selected sample and set that as the DataContext.
             DataContext = SampleManager.Current.SelectedSample;
@@ -92,12 +93,13 @@ namespace ArcGISRuntime.UWP.Viewer
             e.Handled = true;
         }
 
-        private static void SamplePage_Unloaded(object sender, RoutedEventArgs e)
+        private void SamplePage_Loaded(object sender, RoutedEventArgs e)
         {
-            ((SamplePage)sender).RemoveSamplePageHandlers();
+            Tabs.SelectionChanged += TabChanged;
+            DescriptionBlock.ImageResolving += MarkDownBlock_ImageResolving;
         }
 
-        private void RemoveSamplePageHandlers()
+        private void SamplePage_Unloaded(object sender, RoutedEventArgs e)
         {
             Tabs.SelectionChanged -= TabChanged;
             DescriptionBlock.ImageResolving -= MarkDownBlock_ImageResolving;

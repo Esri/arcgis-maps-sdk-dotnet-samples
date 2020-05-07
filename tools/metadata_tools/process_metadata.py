4f4eba34b38830e08dbee88dd49d1ef183f4817a
Zack Allen <zack.m.allen@gmail.com>
2020-05-07 15:50:07 -0700

    Refactoring and updating usage

--- tools/metadata_tools/process_metadata.py
+++ tools/metadata_tools/process_metadata.py
@@ -86,63 +86,6 @@ def write_samples_toc(platform_dir, relative_path_to_samples, samples_in_categor
     with open(readme_path, 'w+') as file:
         file.write(readme_text)
 
-def main():
-    '''
-    Usage: python process_metadata.py {operation} {path_to_samples (ends in src)} {path_to_secondary}
-    Operations: toc; secondary path is empty
-                improve; secondary path is common readme
-                sync; keep metadata in sync with readme
-    '''
-
-    if len(sys.argv) < 3:
-        print("Usage: python process_metadata.py {operation} {path_to_samples (ends in src)} {path_to_secondary}")
-        print("Operations are toc, improve, attributes, and sync; secondary path is path to common readme source for the improve operation.")
-        return
-
-    operation = sys.argv[1]        
-    sample_root = sys.argv[2]
-    common_dir_path = ""
-    if operation == "improve":
-        if len(sys.argv) < 4:
-            print("Usage: python process_metadata.py improve {path_to_samples (ends in src)} {path_to_readme_source}")
-            return
-        common_dir_path = sys.argv[3]
-
-    for platform in ["UWP", "WPF", "Android", "Forms", "iOS"]:
-        # make a list of samples, so that build_all_csproj.bat can be produced
-        list_of_sample_dirs = []
-        list_of_samples = {}
-        skipped_categories = False
-        for r, d, f in os.walk(get_platform_samples_root(platform, sample_root)):
-            if not skipped_categories:
-                skipped_categories = True
-                continue
-            for sample_dir in d:
-                # skip category directories
-                sample = sample_metadata()
-                path_to_readme = os.path.join(r, sample_dir, "readme.md")
-                if not os.path.exists(path_to_readme):
-                    print(f"skipping path; does not exist: {path_to_readme}")
-                    continue
-                sample.populate_from_readme(platform, path_to_readme)
-                sample.populate_snippets_from_folder(platform, path_to_readme)
-                if operation == "improve":
-                    sample.try_replace_with_common_readme(platform, common_dir_path, path_to_readme)
-                if operation in ["improve", "sync"]:
-                    sample.flush_to_json(os.path.join(r, sample_dir, "readme.metadata.json"))
-                if operation == "attributes":
-                    update_attribute(sample, os.path.join(r, sample_dir))
-                list_of_sample_dirs.append(sample_dir)
-                # track samples in each category to enable TOC generation
-                if sample.category in list_of_samples.keys():
-                    list_of_samples[sample.category].append(sample)
-                else:
-                    list_of_samples[sample.category] = [sample]
-        # write out samples TOC
-        if operation in ["toc", "improve", "sync"]:
-            write_samples_toc(get_platform_samples_root(platform, sample_root), get_relative_path_to_samples_from_platform_root(platform), list_of_samples)
-    return
-
 def update_attribute(sample, sample_dir):
     try:
         # Get the formal name of the sample
@@ -224,6 +167,64 @@ def update_attribute(sample, sample_dir):
             file.truncate()
     except:
         print("Error with sample: "+sample_dir)
+
+def main():
+    '''
+    Usage: python process_metadata.py {operation} {path_to_samples (ends in src)} {path_to_secondary}
+    Operations: toc; secondary path is empty
+                improve; secondary path is common readme
+                attributes; keep attributes in code in sync with readme
+                sync; keep metadata in sync with readme
+    '''
+
+    if len(sys.argv) < 3:
+        print("Usage: python process_metadata.py {operation} {path_to_samples (ends in src)} {path_to_secondary}")
+        print("Operations are toc, improve, attributes, and sync; secondary path is path to common readme source for the improve operation.")
+        return
+
+    operation = sys.argv[1]        
+    sample_root = sys.argv[2]
+    common_dir_path = ""
+    if operation == "improve":
+        if len(sys.argv) < 4:
+            print("Usage: python process_metadata.py improve {path_to_samples (ends in src)} {path_to_readme_source}")
+            return
+        common_dir_path = sys.argv[3]
+
+    for platform in ["UWP", "WPF", "Android", "Forms", "iOS"]:
+        # make a list of samples, so that build_all_csproj.bat can be produced
+        list_of_sample_dirs = []
+        list_of_samples = {}
+        skipped_categories = False
+        for r, d, f in os.walk(get_platform_samples_root(platform, sample_root)):
+            if not skipped_categories:
+                skipped_categories = True
+                continue
+            for sample_dir in d:
+                # skip category directories
+                sample = sample_metadata()
+                path_to_readme = os.path.join(r, sample_dir, "readme.md")
+                if not os.path.exists(path_to_readme):
+                    print(f"skipping path; does not exist: {path_to_readme}")
+                    continue
+                sample.populate_from_readme(platform, path_to_readme)
+                sample.populate_snippets_from_folder(platform, path_to_readme)
+                if operation == "improve":
+                    sample.try_replace_with_common_readme(platform, common_dir_path, path_to_readme)
+                if operation in ["improve", "sync"]:
+                    sample.flush_to_json(os.path.join(r, sample_dir, "readme.metadata.json"))
+                if operation == "attributes":
+                    update_attribute(sample, os.path.join(r, sample_dir))
+                list_of_sample_dirs.append(sample_dir)
+                # track samples in each category to enable TOC generation
+                if sample.category in list_of_samples.keys():
+                    list_of_samples[sample.category].append(sample)
+                else:
+                    list_of_samples[sample.category] = [sample]
+        # write out samples TOC
+        if operation in ["toc", "improve", "sync"]:
+            write_samples_toc(get_platform_samples_root(platform, sample_root), get_relative_path_to_samples_from_platform_root(platform), list_of_samples)
+    return
 
 if __name__ == "__main__":
     main()

508f7ac90becc616d94489eb5df0c3726cac71da
Zack Allen <zack.m.allen@gmail.com>
2020-08-03 16:06:06 -0700

    Updated UWP

--- src/UWP/ArcGISRuntime.UWP.Viewer/Samples/Layers/ShowPopup/ShowPopup.xaml.cs
+++ src/UWP/ArcGISRuntime.UWP.Viewer/Samples/Layers/ShowPopup/ShowPopup.xaml.cs
@@ -3,22 +3,18 @@
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
 using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
-using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
+using Esri.ArcGISRuntime.Mapping.Popups;
 using Esri.ArcGISRuntime.UI.Controls;
 using System;
+using System.Diagnostics;
 using System.Linq;
-using Windows.UI.Xaml.Controls;
 
 namespace ArcGISRuntime.UWP.Samples.ShowPopup
 {
@@ -27,7 +23,6 @@ namespace ArcGISRuntime.UWP.Samples.ShowPopup
         "Layers",
         "Show predefined popups from a web map.",
         "")]
-    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
     public partial class ShowPopup
     {
         public ShowPopup()
@@ -38,6 +33,42 @@ namespace ArcGISRuntime.UWP.Samples.ShowPopup
 
         private void Initialize()
         {
+            // Load the map.
+            MyMapView.Map = new Map(new Uri("https://runtime.maps.arcgis.com/home/webmap/viewer.html?webmap=e4c6eb667e6c43b896691f10cc2f1580"));
+        }
+
+        private async void MapViewTapped(object sender, GeoViewInputEventArgs e)
+        {
+            try
+            {
+                // Get the feature layer from the map.
+                FeatureLayer incidentLayer = MyMapView.Map.OperationalLayers.First() as FeatureLayer;
+
+                // Identify the tapped on feature.
+                IdentifyLayerResult result = await MyMapView.IdentifyLayerAsync(incidentLayer, e.Position, 12, true);
+
+                if (result != null && result.Popups.Any())
+                {
+                    // Get the first popup from the identify result.
+                    Popup popup = result.Popups.First();
+
+                    // Create a new popup manager for the popup.
+                    MyPopupViewer.PopupManager = new PopupManager(popup);
+
+                    QueryParameters queryParams = new QueryParameters
+                    {
+                        // Set the geometry to selection envelope for selection by geometry.
+                        Geometry = new Envelope((MapPoint)popup.GeoElement.Geometry, 6, 6)
+                    };
+
+                    // Select the features based on query parameters defined above.
+                    await incidentLayer.SelectFeaturesAsync(queryParams, SelectionMode.New);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.Message);
+            }
         }
     }
 }

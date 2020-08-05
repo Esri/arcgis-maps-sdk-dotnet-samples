dac3f43b6c0f6000c94a2994e8c5ac4910b19f17
Zack Allen <zack.m.allen@gmail.com>
2020-08-04 18:46:54 -0700

    Implemented Android

--- src/Android/Xamarin.Android/Samples/Layers/ShowPopup/ShowPopup.cs
+++ src/Android/Xamarin.Android/Samples/Layers/ShowPopup/ShowPopup.cs
@@ -3,36 +3,36 @@
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
 using Android.App;
 using Android.OS;
-using Android.Widget;
+using ArcGISRuntime;
 using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
-using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
+using Esri.ArcGISRuntime.Mapping.Popups;
+using Esri.ArcGISRuntime.Toolkit.UI.Controls;
 using Esri.ArcGISRuntime.UI.Controls;
+using System;
+using System.Linq;
 
 namespace ArcGISRuntimeXamarin.Samples.ShowPopup
 {
-    [Activity (ConfigurationChanges=Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
+    [Activity(ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
     [ArcGISRuntime.Samples.Shared.Attributes.Sample(
         "Show popup",
         "Layers",
         "Show predefined popups from a web map.",
         "")]
-    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
+    [ArcGISRuntime.Samples.Shared.Attributes.AndroidLayout("ShowPopup.xml")]
     public class ShowPopup : Activity
     {
         // Hold references to the UI controls.
         private MapView _myMapView;
+        private PopupViewer _popupViewer;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -46,19 +46,62 @@ namespace ArcGISRuntimeXamarin.Samples.ShowPopup
 
         private void Initialize()
         {
+            // Load the map.
+            _myMapView.Map = new Map(new Uri("https://runtime.maps.arcgis.com/home/webmap/viewer.html?webmap=e4c6eb667e6c43b896691f10cc2f1580"));
+        }
+
+        private async void MapViewTapped(object sender, GeoViewInputEventArgs e)
+        {
+            try
+            {
+                // Get the feature layer from the map.
+                FeatureLayer incidentLayer = _myMapView.Map.OperationalLayers.First() as FeatureLayer;
+
+                // Identify the tapped on feature.
+                IdentifyLayerResult result = await _myMapView.IdentifyLayerAsync(incidentLayer, e.Position, 12, true);
+
+                if (result != null && result.Popups.Any())
+                {
+                    // Get the first popup from the identify result.
+                    Popup popup = result.Popups.First();
+
+                    // Create a new popup manager for the popup.
+                    _popupViewer.PopupManager = new PopupManager(popup);
+
+                    QueryParameters queryParams = new QueryParameters
+                    {
+                        // Set the geometry to selection envelope for selection by geometry.
+                        Geometry = new Envelope((MapPoint)popup.GeoElement.Geometry, 6, 6)
+                    };
+
+                    // Select the features based on query parameters defined above.
+                    await incidentLayer.SelectFeaturesAsync(queryParams, SelectionMode.New);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print(ex.Message);
+            }
         }
 
         private void CreateLayout()
         {
             // Create a new vertical layout for the app.
-            var layout = new LinearLayout(this) { Orientation = Orientation.Vertical };
+            SetContentView(Resource.Layout.ShowPopup);
 
-            // Add the map view to the layout.
-            _myMapView = new MapView(this);
-            layout.AddView(_myMapView);
+            _myMapView = FindViewById<MapView>(Resource.Id.MapView);
+            _popupViewer = FindViewById<PopupViewer>(Resource.Id.popupViewer);
 
-            // Show the layout in the app.
-            SetContentView(layout);
+            // Add event handlers.
+            _myMapView.GeoViewTapped += MapViewTapped;
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            // Unhook event handlers.
+            _myMapView.GeoViewTapped -= MapViewTapped;
         }
     }
 }

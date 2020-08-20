f6c366874d8d8a17edeb31c9d00216f9c63a0374
Zack Allen <zack.m.allen@gmail.com>
2020-08-19 17:40:43 -0700

    Added support for .xml layouts

--- tools/metadata_tools/csproj_utils.py
+++ tools/metadata_tools/csproj_utils.py
@@ -45,6 +45,10 @@ def get_csproj_xml_for_android_layout(snippets_list):
             stripped_name = file.strip("../../../").replace("/", "\\")
             cs_file_include = f'<AndroidResource Include="{stripped_name}" />\n'
             xml_string += cs_file_include
+        elif file.endswith(".xml"):
+            stripped_name = file.strip("../../../").replace("/", "\\")
+            cs_file_include = f'<AndroidResource Include="{stripped_name}" />\n'
+            xml_string += cs_file_include
         elif "Attrs.xml" in file:
             stripped_name = file.strip("../../../").replace("/", "\\")
             cs_file_include = f'<AndroidResource Include="{stripped_name}"><Generator>MSBuild:UpdateGeneratedFiles</Generator></AndroidResource>\n'

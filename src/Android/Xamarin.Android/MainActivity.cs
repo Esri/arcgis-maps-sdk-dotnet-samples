78f4cb9be638b2fc77b71f0a2668457e6ec32bed
Zack Allen <zallen@esri.com>
2020-12-14 14:30:18 -0800

    Added AR check for Xamarin.Android

--- src/Android/Xamarin.Android/MainActivity.cs
+++ src/Android/Xamarin.Android/MainActivity.cs
@@ -1,22 +1,24 @@
-﻿// Copyright 2018 Esri.
+﻿// Copyright 2020 Esri.
+// Copyright 2020 Esri.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
-using System;
 using Android.App;
-using Android.Widget;
+using Android.Content;
 using Android.OS;
-using System.Collections.Generic;
+using Android.Widget;
 using ArcGISRuntime.Samples.Managers;
 using ArcGISRuntime.Samples.Shared.Models;
-using Android.Content;
-using System.Linq;
 using Esri.ArcGISRuntime.Security;
+using Google.AR.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ArcGISRuntime
 {
@@ -38,6 +40,17 @@ namespace ArcGISRuntime
                 // Initialize the SampleManager and create the Sample Categories.
                 SampleManager.Current.Initialize();
                 _sampleCategories = SampleManager.Current.FullTree.Items.OfType<SearchableTreeNode>().ToList();
+
+                // Remove AR category if device does not support AR.
+                var arSession = new Session(this);
+                var arConfig = new Config(arSession);
+#pragma warning disable CS0618 // Type or member is obsolete
+                if (!arSession.IsSupported(arConfig))
+                {
+                    _sampleCategories.RemoveAll(category => category.Name == "Augmented reality");
+                }
+#pragma warning restore CS0618 // Type or member is obsolete
+
                 _filteredSampleCategories = _sampleCategories;
 
                 // Set up the custom ArrayAdapter for displaying the Categories.
@@ -79,7 +92,7 @@ namespace ArcGISRuntime
             {
                 _filteredSampleCategories = new List<SearchableTreeNode>();
             }
-                
+
             _categoriesListView.SetAdapter(new CategoriesAdapter(this, _filteredSampleCategories));
 
             // Expand all entries; makes it easier to see search results.
@@ -152,4 +165,3 @@ namespace ArcGISRuntime
         }
     }
 }
-

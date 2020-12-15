e03d74aae5553a8be23671bbc265f7b8a533a85b
Morten Nielsen <mn@iter.dk>
2020-12-15 09:11:56 -0800

    Fix resource load issues

--- src/WinUI/ArcGISRuntime.WinUI.Viewer/Samples/Symbology/RenderPictureMarkers/RenderPictureMarkers.xaml.cs
+++ src/WinUI/ArcGISRuntime.WinUI.Viewer/Samples/Symbology/RenderPictureMarkers/RenderPictureMarkers.xaml.cs
@@ -99,7 +99,7 @@ namespace ArcGISRuntime.WinUI.Samples.RenderPictureMarkers
             // Get image as a stream from the resources
             // Picture is defined as EmbeddedResource and DoNotCopy
             Stream resourceStream = currentAssembly.GetManifestResourceStream(
-                "ArcGISRuntime.Resources.PictureMarkerSymbols.pin_star_blue.png");
+                "ArcGISRuntime.WinUI.Viewer.Resources.PictureMarkerSymbols.pin_star_blue.png");
 
             // Create new symbol using asynchronous factory method from stream
             PictureMarkerSymbol pinSymbol = await PictureMarkerSymbol.CreateAsync(resourceStream);

bdc3fc2b75f5ebb0b0781f09ec94b86bf89948c7
Zack Allen <zallen@esri.com>
2021-01-13 21:12:01 -0800

    Update CategoryListPage.xaml.cs

--- src/Forms/Shared/CategoryListPage.xaml.cs
+++ src/Forms/Shared/CategoryListPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2020 Esri.
+﻿// Copyright 2021 Esri.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
@@ -17,7 +17,6 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 #if XAMARIN_ANDROID

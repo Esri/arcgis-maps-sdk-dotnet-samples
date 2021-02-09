2d386f46a984beab35c6aa302a17e1a9af1425b6
Zack Allen <zallen@esri.com>
2021-02-09 13:54:21 -0800

    Templated

---
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Layers/ApplyMosaicRule/ApplyMosaicRule.xaml.cs
@@ -0,0 +1,47 @@
+﻿// Copyright 2021 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.Tasks.Offline;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.ArcGISServices;
+using Esri.ArcGISRuntime.UI.Controls;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ArcGISRuntime.WPF.Samples.ApplyMosaicRule
+{
+    [ArcGISRuntime.Samples.Shared.Attributes.Sample(
+        "Apply mosaic rule to rasters",
+        "Layers",
+        "Apply mosaic rule to a mosaic dataset of rasters.",
+        "")]
+    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
+    public partial class ApplyMosaicRule
+    {
+        public ApplyMosaicRule()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+        }
+    }
+}

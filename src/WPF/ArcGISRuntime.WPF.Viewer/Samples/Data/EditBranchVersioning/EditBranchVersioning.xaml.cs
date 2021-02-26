3d119d83de38b31b1ded474ac560f9389be2d80f
Zack Allen <zallen@esri.com>
2021-02-25 23:51:29 -0800

    Got everything working

--- src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Data/EditBranchVersioning/EditBranchVersioning.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Data/EditBranchVersioning/EditBranchVersioning.xaml.cs
@@ -15,6 +15,7 @@ using Esri.ArcGISRuntime.UI.Controls;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -30,10 +31,14 @@ namespace ArcGISRuntime.WPF.Samples.EditBranchVersioning
     {
         //https://sampleserver7.arcgisonline.com/arcgis/rest/services/DamageAssessment/FeatureServer/0
 
-        private Feature _selectedFeature;
+        private ArcGISFeature _selectedFeature;
         private FeatureLayer _featureLayer;
         private ServiceFeatureTable _featureTable;
         private ServiceGeodatabase _serviceGeodatabase;
+
+        //private string _defaultVersionName;
+        private string _userCreatedVersionName;
+        private string _attributeFieldName = "TYPDAMAGE";
 
         public EditBranchVersioning()
         {
@@ -94,29 +99,193 @@ namespace ArcGISRuntime.WPF.Samples.EditBranchVersioning
             }
         }
 
-        private void SwitchVersion(object sender, RoutedEventArgs e)
+        private void VersionButtonPressed(object sender, RoutedEventArgs e)
         {
-            VersionPicker.Visibility = Visibility.Visible;
+            // Check if user version has been created.
+            if (_userCreatedVersionName != null)
+            {
+                SwitchVersion();
+            }
+            else
+            {
+                VersionCreator.Visibility = Visibility.Visible;
+            }
         }
 
-        private void MyMapView_GeoViewTapped(object sender, GeoViewInputEventArgs e)
+        private async void SwitchVersion()
         {
-            
+            if (_serviceGeodatabase.VersionName == _serviceGeodatabase.DefaultVersionName)
+            {
+                await _serviceGeodatabase.SwitchVersionAsync(_userCreatedVersionName);
+                CurrentVersionLabel.Content = $"Current version: {_serviceGeodatabase.VersionName}";
+            }
+            else
+            {
+                if (_serviceGeodatabase.HasLocalEdits())
+                {
+                    var edits = await _serviceGeodatabase.ApplyEditsAsync();
+                    if (!(edits?.Count > 0))
+                    {
+                        if (!edits.ToArray()[0].EditResults[0].CompletedWithErrors)
+                        {
+                            ShowAlert("Applied edits successfully on the server");
+                        }
+                        else
+                        {
+                            ShowAlert(edits.ToArray()[0].EditResults[0].Error.Message);
+                            return;
+                        }
+                    }
+                }
+
+                await _serviceGeodatabase.SwitchVersionAsync(_serviceGeodatabase.DefaultVersionName);
+                CurrentVersionLabel.Content = $"Current version: {_serviceGeodatabase.VersionName}";
+            }
         }
 
-        private void DamageBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        private bool TextInputValid(string inputText)
         {
+            if (inputText.Contains(".") || inputText.Contains(";") || inputText.Contains("'") || inputText.Contains("\""))
+            {
+                ShowAlert("Please enter a valid version name.\nThe name cannot contain the following characters:\n. ; ' \" ");
+                return false;
+            }
+            else if (inputText.Length > 0 && inputText.StartsWith(" "))
+            {
+                ShowAlert("Version name cannot begin with a space");
+                return false;
+            }
+            else if (inputText.Length > 62)
+            {
+                ShowAlert("Version name must not exceed 62 characters");
+                return false;
+            }
+            else if (inputText.Length == 0)
+            {
+                ShowAlert("Please enter a version name");
+                return false;
+            }
+            else return true;
         }
 
-        private void ConfirmClick(object sender, RoutedEventArgs e)
+        private void ShowAlert(string alertText)
+        {
+            MessageBox.Show(alertText, "Alert", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
+        private async void MyMapView_GeoViewTapped(object sender, GeoViewInputEventArgs e)
+        {
+            // Clear any existing selection.
+            _featureLayer.ClearSelection();
+
+            // Dismiss any existing callouts.
+            MyMapView.DismissCallout();
+
+            try
+            {
+                // Perform an identify to determine if a user tapped on a feature.
+                IdentifyLayerResult identifyResult = await MyMapView.IdentifyLayerAsync(_featureLayer, e.Position, 2, false);
+
+                // Do nothing if there are no results.
+                if (!identifyResult.GeoElements.Any())
+                {
+                    return;
+                }
+
+                // Get the tapped feature.
+                _selectedFeature = (ArcGISFeature)identifyResult.GeoElements.First();
+
+                // Select the feature.
+                _featureLayer.SelectFeature(_selectedFeature);
+
+                // Get the current value.
+                string currentAttributeValue = _selectedFeature.Attributes[_attributeFieldName].ToString();
+
+                // Update the combobox selection without triggering the event.
+                DamageBox.SelectionChanged -= DamageBox_SelectionChanged;
+                DamageBox.SelectedValue = currentAttributeValue;
+                DamageBox.SelectionChanged += DamageBox_SelectionChanged;
+
+                // Update the UI for the selection.
+                AttributePicker.Visibility = Visibility.Visible;
+
+                DamageBox.IsEnabled = _serviceGeodatabase.VersionName != _serviceGeodatabase.DefaultVersionName;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().Name, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private async void DamageBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             try
             {
+                // Get the new value.
+                string selectedAttributeValue = DamageBox.SelectedValue.ToString();
 
+                // Load the feature.
+                await _selectedFeature.LoadAsync();
+
+                // Update the attribute value.
+                _selectedFeature.SetAttributeValue(_attributeFieldName, selectedAttributeValue);
+
+                // Update the table.
+                await _selectedFeature.FeatureTable.UpdateFeatureAsync(_selectedFeature);
+
+                // Update the service.
+                ServiceFeatureTable table = (ServiceFeatureTable)_selectedFeature.FeatureTable;
+                await table.ApplyEditsAsync();
+
+                //ShowAlert("Edited feature " + _selectedFeature.Attributes["objectid"]);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Failed to edit feature");
             }
             finally
             {
-                VersionPicker.Visibility = Visibility.Collapsed;
+                // Clear the selection.
+                //_featureLayer.ClearSelection();
+                //_selectedFeature = null;
+
+                // Update the UI.
+                //AttributePicker.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private async void ConfirmClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Validate name and access input.
+                if (!TextInputValid(NameEntryBox.Text)) return;
+
+                if (!(AccessBox.SelectedItem is VersionAccess))
+                {
+                    ShowAlert("Please select an access level");
+                    return;
+                }
+
+                // Set the user defined name, access level and description as service version parameters
+                ServiceVersionParameters newVersionParameters = new ServiceVersionParameters();
+                newVersionParameters.Name = NameEntryBox.Text;
+                newVersionParameters.Access = (VersionAccess)AccessBox.SelectedItem;
+                newVersionParameters.Description = DescriptionBox.Text;
+
+                ServiceVersionInfo newVersion = await _serviceGeodatabase.CreateVersionAsync(newVersionParameters);
+                _userCreatedVersionName = newVersion.Name;
+                SwitchVersion();
+
+                CreateVersionButton.Content = "Switch version";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().Name, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                VersionCreator.Visibility = Visibility.Collapsed;
             }
         }
 
@@ -124,12 +293,16 @@ namespace ArcGISRuntime.WPF.Samples.EditBranchVersioning
         {
             try
             {
-
             }
             finally
             {
-                VersionPicker.Visibility = Visibility.Collapsed;
+                VersionCreator.Visibility = Visibility.Collapsed;
             }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            AttributePicker.Visibility = Visibility.Collapsed;
         }
     }
 }

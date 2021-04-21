1da43c568a8dde4e271f64b4efeacd1be202cbb9
Zack Allen <zack.m.allen@gmail.com>
2021-04-21 15:01:36 -0700

    Improved UI slightly

--- src/Android/Xamarin.Android/Samples/MapView/DisplayDrawingStatus/DisplayDrawingStatus.cs
+++ src/Android/Xamarin.Android/Samples/MapView/DisplayDrawingStatus/DisplayDrawingStatus.cs
@@ -1,10 +1,10 @@
-// Copyright 2016 Esri.
+// Copyright 2021 Esri.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
 using Android.App;
@@ -15,11 +15,10 @@ using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.UI;
 using Esri.ArcGISRuntime.UI.Controls;
 using System;
-using Android.Views;
 
 namespace ArcGISRuntime.Samples.DisplayDrawingStatus
 {
-    [Activity (ConfigurationChanges=Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
+    [Activity(ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
     [ArcGISRuntime.Samples.Shared.Attributes.Sample(
         name: "Display draw status",
         category: "MapView",
@@ -32,7 +31,7 @@ namespace ArcGISRuntime.Samples.DisplayDrawingStatus
         private MapView _myMapView;
 
         // Waiting popup
-        private AlertDialog _progressDialog;
+        private ProgressBar _progressBar;
 
         // Status label
         private TextView _statusLabel;
@@ -43,7 +42,7 @@ namespace ArcGISRuntime.Samples.DisplayDrawingStatus
 
             Title = "Display drawing status";
 
-            // Create the UI, setup the control references and execute initialization 
+            // Create the UI, setup the control references and execute initialization
             CreateLayout();
             Initialize();
         }
@@ -79,12 +78,12 @@ namespace ArcGISRuntime.Samples.DisplayDrawingStatus
                 // Show the activity indicator if the map is drawing
                 if (e.Status == DrawStatus.InProgress)
                 {
-                    _progressDialog.Show();
+                    _progressBar.Visibility = Android.Views.ViewStates.Visible;
                     _statusLabel.Text = "Drawing status: In progress";
                 }
                 else
                 {
-                    _progressDialog.Hide();
+                    _progressBar.Visibility = Android.Views.ViewStates.Invisible;
                     _statusLabel.Text = "Drawing status: Complete";
                 }
             });
@@ -98,20 +97,14 @@ namespace ArcGISRuntime.Samples.DisplayDrawingStatus
             _statusLabel = new TextView(this);
             _statusLabel.Text = "Drawing status: unknown";
 
-            // Add the views to the layout
-            layout.AddView(_statusLabel);
-            _myMapView = new MapView(this);
-            layout.AddView(_myMapView);
+            _progressBar = new ProgressBar(this) { Indeterminate = true };
 
-            // Create an activity indicator
-            // Show the waiting dialog.
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.SetView(new ProgressBar(this)
-            {
-                Indeterminate = true
-            });
-            builder.SetMessage("Drawing in progress.");
-            _progressDialog = builder.Create();
+            _myMapView = new MapView(this);
+
+            // Add the views to the layout
+            layout.AddView(_progressBar);
+            layout.AddView(_statusLabel);
+            layout.AddView(_myMapView);
 
             // Show the layout in the app
             SetContentView(layout);

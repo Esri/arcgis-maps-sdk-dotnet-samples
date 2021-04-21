7060d853fa878a47c4f2731fd55a1c1551e9d24f
Zack Allen <zack.m.allen@gmail.com>
2021-04-20 17:02:54 -0700

    U11: WPF fixes (#876)
    * Fixed .NET core error for edit feature attachments
    
    * Updated shell execute pattern
    
    * Copyright cleanup

--- src/WPF/ArcGISRuntime.WPF.Viewer/SettingsWindow.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/SettingsWindow.xaml.cs
@@ -1,4 +1,13 @@
-﻿using ArcGISRuntime.Samples.Managers;
+﻿// Copyright 2021 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+
+using ArcGISRuntime.Samples.Managers;
 using ArcGISRuntime.Samples.Shared.Models;
 using ArcGISRuntime.WPF.Viewer;
 using Esri.ArcGISRuntime;
@@ -58,7 +67,7 @@ namespace ArcGISRuntime
             {
                 string onlinePath = $"https://www.arcgis.com/home/item.html?id={offlineItem}";
 
-                Process.Start(onlinePath);
+                Process.Start(new ProcessStartInfo(onlinePath) { UseShellExecute = true });
             }
         }
 
@@ -152,7 +161,7 @@ namespace ArcGISRuntime
         {
             // Button for this was removed because it doesn't work with
             // folder redirection when packaged with desktop bridge.
-            Process.Start(DataManager.GetDataFolder());
+            Process.Start(new ProcessStartInfo(DataManager.GetDataFolder()) { UseShellExecute = true });
         }
 
         private void DeleteData_Click(object sender, RoutedEventArgs e)

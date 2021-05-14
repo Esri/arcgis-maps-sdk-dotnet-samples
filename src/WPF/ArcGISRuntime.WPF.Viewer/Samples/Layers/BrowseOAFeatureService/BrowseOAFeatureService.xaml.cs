cb659637b6ed334d11220127ba9f4afc9067acdd
Zack Allen <zack.m.allen@gmail.com>
2021-05-13 17:27:11 -0700

    WPF

--- src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Layers/BrowseOAFeatureService/BrowseOAFeatureService.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Layers/BrowseOAFeatureService/BrowseOAFeatureService.xaml.cs
@@ -10,19 +10,13 @@
 using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Ogc;
 using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
-using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
-using Esri.ArcGISRuntime.UI.Controls;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Media;
+using Color = System.Drawing.Color;
 
 namespace ArcGISRuntime.WPF.Samples.BrowseOAFeatureService
 {
@@ -32,9 +26,12 @@ namespace ArcGISRuntime.WPF.Samples.BrowseOAFeatureService
         description: "Browse an OGC API feature service for layers and add them to the map.",
         instructions: "Select a layer to display from the list of layers shown in an OGC API service.",
         tags: new[] { "OGC", "OGC API", "browse", "catalog", "feature", "layers", "service", "web" })]
-    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
     public partial class BrowseOAFeatureService
     {
+        
+        // Landing URL to the OAFeat service.
+        private const string ServiceUrl = "https://demo.ldproxy.net/daraa";
+
         public BrowseOAFeatureService()
         {
             InitializeComponent();
@@ -43,6 +40,131 @@ namespace ArcGISRuntime.WPF.Samples.BrowseOAFeatureService
 
         private void Initialize()
         {
+            // Init the UI.
+            ServiceTextBox.Text = ServiceUrl;
+            // Create the map with topographic basemap.
+            MyMapView.Map = new Map(Basemap.CreateTopographic());
+            LoadService();
+        }
+
+        private async void LoadService()
+        {
+            try
+            {
+                LoadingProgressBar.Visibility = Visibility.Visible;
+                LoadLayersButton.IsEnabled = false;
+                LoadServiceButton.IsEnabled = false;
+
+                // Create the OGC API - Features service using the landing URL.
+                OgcFeatureService service = new OgcFeatureService(new Uri(ServiceTextBox.Text));
+
+                // Load the OAFeat service.
+                await service.LoadAsync();
+
+                // Get the service metadata.
+                OgcFeatureServiceInfo serviceInfo = service.ServiceInfo;
+
+                // Get a list of available collections.
+                IEnumerable<OgcFeatureCollectionInfo> layerListReversed = serviceInfo.FeatureCollectionInfos;
+
+                // Show the layers in the UI.
+                OgcFeatureCollectionList.ItemsSource = layerListReversed;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                MessageBox.Show(ex.Message, "Error loading service");
+            }
+            finally
+            {
+                // Update the UI.
+                LoadingProgressBar.Visibility = Visibility.Collapsed;
+                LoadLayersButton.IsEnabled = true;
+                LoadServiceButton.IsEnabled = true;
+            }
+        }
+
+        private async void LoadLayers_Clicked(object sender, RoutedEventArgs e)
+        {
+            // Skip if nothing selected.
+            if (OgcFeatureCollectionList.SelectedItems.Count < 1)
+            {
+                return;
+            }
+
+            // Show the progress bar.
+            LoadingProgressBar.Visibility = Visibility.Visible;
+
+            // Clear the existing layers.
+            MyMapView.Map.OperationalLayers.Clear();
+
+            try
+            {
+                // Get the selected collection.
+                OgcFeatureCollectionInfo selectedCollectionInfo = (OgcFeatureCollectionInfo)OgcFeatureCollectionList.SelectedItems[0];
+
+                // Create the OGC feature collection table.
+                OgcFeatureCollectionTable table = new OgcFeatureCollectionTable(selectedCollectionInfo);
+
+                // Set the feature request mode to manual (only manual is currently supported).
+                // In this mode, you must manually populate the table - panning and zooming won't request features automatically.
+                table.FeatureRequestMode = FeatureRequestMode.ManualCache;
+
+                // Populate the OGC feature collection table.
+                QueryParameters queryParamaters = new QueryParameters();
+                queryParamaters.MaxFeatures = 1000;
+                await table.PopulateFromServiceAsync(queryParamaters, false, null);
+
+                // Create a feature layer from the OGC feature collection table.
+                FeatureLayer ogcFeatureLayer = new FeatureLayer(table);
+
+                // Choose a renderer for the layer based on the table.
+                ogcFeatureLayer.Renderer = GetRendererForTable(table) ?? ogcFeatureLayer.Renderer;
+
+                // Add the layer to the map.
+                MyMapView.Map.OperationalLayers.Add(ogcFeatureLayer);
+
+                // Zoom to the extent of the selected collection.
+                Envelope collectionExtent = selectedCollectionInfo.Extent;
+                if (collectionExtent != null && !collectionExtent.IsEmpty)
+                {
+                    await MyMapView.SetViewpointGeometryAsync(collectionExtent, 100);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                MessageBox.Show(ex.Message, "Error loading service");
+            }
+            finally
+            {
+                // Hide the progress bar.
+                LoadingProgressBar.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private Renderer GetRendererForTable(FeatureTable table)
+        {
+            switch (table.GeometryType)
+            {
+                case GeometryType.Point:
+                case GeometryType.Multipoint:
+                    return new SimpleRenderer(new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, Color.Blue, 5));
+
+                case GeometryType.Polygon:
+                case GeometryType.Envelope:
+                    return new SimpleRenderer(new SimpleFillSymbol(SimpleFillSymbolStyle.Solid, Color.Blue, null));
+
+                case GeometryType.Polyline:
+                    return new SimpleRenderer(new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, Color.Blue, 1));
+            }
+
+            return null;
+        }
+
+        private void LoadServiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadService();
         }
     }
 }

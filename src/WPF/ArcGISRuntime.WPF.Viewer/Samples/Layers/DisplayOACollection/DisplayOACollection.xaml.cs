28553c763c0a1667929881cce35ab25deb7bdb93
Zack Allen <zack.m.allen@gmail.com>
2021-06-02 15:55:49 -0400

    WPF

--- src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Layers/DisplayOACollection/DisplayOACollection.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Layers/DisplayOACollection/DisplayOACollection.xaml.cs
@@ -11,18 +11,10 @@ using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
-using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
-using Esri.ArcGISRuntime.UI.Controls;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Media;
+using Color = System.Drawing.Color;
 
 namespace ArcGISRuntime.WPF.Samples.DisplayOACollection
 {
@@ -35,14 +27,96 @@ namespace ArcGISRuntime.WPF.Samples.DisplayOACollection
     [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
     public partial class DisplayOACollection
     {
+        // Hold a reference to the OGC feature collection table.
+        private OgcFeatureCollectionTable _featureTable;
+
+        // Constants for the service URL and collection id.
+        private const string ServiceUrl = "https://demo.ldproxy.net/daraa";
+
+        // Note that the service defines the collection id which can be accessed via OgcFeatureCollectionInfo.CollectionId.
+        private const string CollectionId = "TransportationGroundCrv";
+
         public DisplayOACollection()
         {
             InitializeComponent();
             Initialize();
         }
 
-        private void Initialize()
+        private async void Initialize()
         {
+            // Create the map with topographic basemap.
+            MyMapView.Map = new Map(BasemapStyle.ArcGISTopographic);
+
+            try
+            {
+                // Create the feature table from URI and collection id.
+                _featureTable = new OgcFeatureCollectionTable(new Uri(ServiceUrl), CollectionId);
+
+                // Set the feature request mode to manual (only manual is currently supported).
+                // In this mode, you must manually populate the table - panning and zooming won't request features automatically.
+                _featureTable.FeatureRequestMode = FeatureRequestMode.ManualCache;
+
+                // Load the table.
+                await _featureTable.LoadAsync();
+
+                // Create a feature layer to visualize the OAFeat features.
+                FeatureLayer ogcFeatureLayer = new FeatureLayer(_featureTable);
+
+                // Apply a renderer.
+                ogcFeatureLayer.Renderer = new SimpleRenderer(new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, Color.Blue, 3));
+
+                // Add the layer to the map.
+                MyMapView.Map.OperationalLayers.Add(ogcFeatureLayer);
+
+                // Use the navigation completed event to populate the table with the features needed for the current extent.
+                MyMapView.NavigationCompleted += MapView_NavigationCompleted;
+
+                // Zoom to a small area within the dataset by default.
+                Envelope datasetExtent = _featureTable.Extent;
+                if (datasetExtent != null && !datasetExtent.IsEmpty)
+                {
+                    await MyMapView.SetViewpointGeometryAsync(new Envelope(datasetExtent.GetCenter(), datasetExtent.Width / 3, datasetExtent.Height / 3));
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Couldn't load sample.");
+                Debug.WriteLine(e);
+            }
+        }
+
+        private async void MapView_NavigationCompleted(object sender, EventArgs e)
+        {
+            // Show the loading bar.
+            LoadingProgressBar.Visibility = Visibility.Visible;
+
+            // Get the current extent.
+            Envelope currentExtent = MyMapView.VisibleArea.Extent;
+
+            // Create a query based on the current visible extent.
+            QueryParameters visibleExtentQuery = new QueryParameters();
+            visibleExtentQuery.Geometry = currentExtent;
+            visibleExtentQuery.SpatialRelationship = SpatialRelationship.Intersects;
+
+            // Set a limit of 5000 on the number of returned features per request,
+            // because the default on some services could be as low as 10.
+            visibleExtentQuery.MaxFeatures = 5000;
+
+            try
+            {
+                // Populate the table with the query, leaving existing table entries intact.
+                // Setting outFields to null requests all fields.
+                await _featureTable.PopulateFromServiceAsync(visibleExtentQuery, false, null);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.ToString(), "Couldn't populate table.");  
+            }
+            finally
+            {
+                // Hide the loading bar.
+                LoadingProgressBar.Visibility = Visibility.Collapsed;
+            }
         }
     }
 }

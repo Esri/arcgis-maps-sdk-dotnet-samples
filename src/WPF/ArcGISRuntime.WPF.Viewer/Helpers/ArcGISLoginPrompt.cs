e5de5966c1f878dcd3e298b76605dac6d1571841
Zack Allen <zack.m.allen@gmail.com>
2021-06-15 14:13:16 -0400

    Copyright and new URLs for login prompt

--- src/WPF/ArcGISRuntime.WPF.Viewer/Helpers/ArcGISLoginPrompt.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Helpers/ArcGISLoginPrompt.cs
@@ -1,4 +1,13 @@
-﻿using Esri.ArcGISRuntime.Security;
+﻿// Copyright 2021 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Security;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -14,10 +23,10 @@ namespace ArcGISRuntime.Helpers
         private const string ArcGISOnlineUrl = "https://www.arcgis.com/sharing/rest";
 
         // Client ID for the app registered with the server.
-        private const string AppClientId = "2Gh53JRzkPtOENQq";
+        private const string AppClientId = "lgAdHkYZYlwwfAhC";
 
         // Redirect URL after a successful authorization.
-        private const string OAuthRedirectUrl = "https://developers.arcgis.com";
+        private const string OAuthRedirectUrl = "my-ags-app://auth";
 
         public static async Task<bool> EnsureAGOLCredentialAsync()
         {
@@ -153,8 +162,8 @@ namespace ArcGISRuntime.Helpers
                 _window = new Window
                 {
                     Content = webBrowser,
-                    Width = 450,
-                    Height = 400,
+                    Width = 430,
+                    Height = 395,
                     WindowStartupLocation = WindowStartupLocation.CenterOwner
                 };
 

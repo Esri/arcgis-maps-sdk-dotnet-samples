92561cb8b7fd74419c22cfd74fe5c45872b63e23
mike0000 <mbranscomb@esri.com>
2021-06-25 16:36:51 -0700

    Updated samples for recent changes.

--- src/WinUI/ArcGISRuntime.WinUI.Viewer/Samples/Map/SearchPortalMaps/SearchPortalMaps.xaml.cs
+++ src/WinUI/ArcGISRuntime.WinUI.Viewer/Samples/Map/SearchPortalMaps/SearchPortalMaps.xaml.cs
@@ -234,22 +234,12 @@ namespace ArcGISRuntime.WinUI.Samples.SearchPortalMaps
         private void UpdateAuthenticationManager()
         {
             // Define the server information for ArcGIS Online
-            ServerInfo portalServerInfo = new ServerInfo
+            ServerInfo portalServerInfo = new ServerInfo(new Uri(ArcGISOnlineUrl))
             {
-                // ArcGIS Online URI
-                ServerUri = new Uri(ArcGISOnlineUrl),
-
                 // Type of token authentication to use
-                TokenAuthenticationType = TokenAuthenticationType.OAuthImplicit
+                TokenAuthenticationType = TokenAuthenticationType.OAuthAuthorizationCode,
+                OAuthClientInfo = new OAuthClientInfo(_appClientId, new Uri(_oAuthRedirectUrl))
             };
-
-            // Define the OAuth information
-            OAuthClientInfo oAuthInfo = new OAuthClientInfo
-            {
-                ClientId = _appClientId,
-                RedirectUri = new Uri(_oAuthRedirectUrl)
-            };
-            portalServerInfo.OAuthClientInfo = oAuthInfo;
 
             // Get a reference to the (singleton) AuthenticationManager for the app
             AuthenticationManager thisAuthenticationManager = AuthenticationManager.Current;
--- src/WinUI/ArcGISRuntime.WinUI.Viewer/Samples/Search/FindPlace/FindPlace.xaml.cs
+++ src/WinUI/ArcGISRuntime.WinUI.Viewer/Samples/Search/FindPlace/FindPlace.xaml.cs
@@ -23,6 +23,7 @@ using System.Threading.Tasks;
 using Windows.UI.Popups;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using ArcGISRuntime.Samples.Shared.Managers;
 
 namespace ArcGISRuntime.WinUI.Samples.FindPlace
 {
@@ -39,7 +40,7 @@ namespace ArcGISRuntime.WinUI.Samples.FindPlace
         private LocatorTask _geocoder;
 
         // Service Uri to be provided to the LocatorTask (geocoder)
-        private Uri _serviceUri = new Uri("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
+        private Uri _serviceUri = new Uri("https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer");
 
         public FindPlace()
         {
@@ -51,6 +52,12 @@ namespace ArcGISRuntime.WinUI.Samples.FindPlace
 
         private async void Initialize()
         {
+            if (await ApiKeyManager.CheckKeyValidity() != ApiKeyStatus.Valid)
+            {
+                await new MessageDialog2("Please use the settings dialog to configure an API Key.", "Error").ShowAsync();
+                return;
+            }
+
             // Add event handler for when this sample is unloaded.
             Unloaded += SampleUnloaded;
 
@@ -120,7 +127,7 @@ namespace ArcGISRuntime.WinUI.Samples.FindPlace
             else
             {
                 // Get the current device location
-                return MyMapView.LocationDisplay.Location.Position;
+                return MyMapView.LocationDisplay.Location?.Position;
             }
         }
 

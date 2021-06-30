f9d271a7b46776b165820f4fa94598fef4bd5328
Zack Allen <zack.m.allen@gmail.com>
2021-06-29 18:37:19 -0700

    Update RoutePlanner.cs

--- src/Android/Xamarin.Android/Samples/Augmented reality/NavigateAR/RoutePlanner.cs
+++ src/Android/Xamarin.Android/Samples/Augmented reality/NavigateAR/RoutePlanner.cs
@@ -37,6 +37,7 @@ namespace ArcGISRuntimeXamarin.Samples.NavigateAR
         description: "Use a route displayed in the real world to navigate.",
         instructions: "The sample opens with a map centered on the current location. Tap the map to add an origin and a destination; the route will be shown as a line. When ready, click 'Confirm' to start the AR navigation. Calibrate the heading before starting to navigate. When you start, route instructions will be displayed and spoken. As you proceed through the route, new directions will be provided until you arrive.",
         tags: new[] { "augmented reality", "directions", "full-scale", "guidance", "mixed reality", "navigate", "navigation", "real-scale", "route", "routing", "world-scale" })]
+    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("628e8e3521cf45e9a28a12fe10c02c4d")]
     public class NavigateARRoutePlanner : AppCompatActivity
     {
         // Hold references to the UI controls.

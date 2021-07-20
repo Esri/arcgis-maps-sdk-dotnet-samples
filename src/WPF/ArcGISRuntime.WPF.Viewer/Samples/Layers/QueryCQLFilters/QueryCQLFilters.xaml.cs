23ad901d2caf834b3be1347ed285eae719273578
Zack Allen <zack.m.allen@gmail.com>
2021-07-20 16:58:24 -0700

    Added WPF from design

--- src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Layers/QueryCQLFilters/QueryCQLFilters.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Layers/QueryCQLFilters/QueryCQLFilters.xaml.cs
@@ -11,18 +11,12 @@ using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
-using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
-using Esri.ArcGISRuntime.UI.Controls;
 using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Media;
+using Color = System.Drawing.Color;
 
 namespace ArcGISRuntime.WPF.Samples.QueryCQLFilters
 {
@@ -32,9 +26,30 @@ namespace ArcGISRuntime.WPF.Samples.QueryCQLFilters
         description: "Query data from an OGC API feature service using CQL filters.",
         instructions: "Enter a CQL query. Press the \"Apply query\" button to see the query applied to the OGC API features shown on the map.",
         tags: new[] { "CQL", "OGC", "OGC API", "browse", "catalog", "common query language", "feature table", "filter", "query", "service", "web" })]
-    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
     public partial class QueryCQLFilters
     {
+        public IReadOnlyList<string> DefaultWhereClause { get; } = new[]
+        {
+            // Sample Query 1: Query for features with an F_CODE attribute property of "AP010".
+            "F_CODE = 'AP010'", // cql-text query
+            "{ \"eq\" : [ { \"property\" : \"F_CODE\" }, \"AP010\" ] }", // cql-json query
+
+            // Sample Query 2: Query for features with an F_CODE attribute property similar to "AQ".
+            "F_CODE LIKE 'AQ%'", // cql-text query
+
+            // Sample Query 3: use cql-json to combine "before" and "eq" operators with the logical "and" operator
+           "{\"and\":[{\"eq\":[{ \"property\" : \"F_CODE\" }, \"AP010\"]},{ \"before\":[{ \"property\" : \"ZI001_SDV\"},\"2013-01-01\"]}]}"
+        };
+
+        // Hold a reference to the OGC feature collection table.
+        private OgcFeatureCollectionTable _featureTable;
+
+        // Constants for the service URL and collection id.
+        private const string ServiceUrl = "https://demo.ldproxy.net/daraa";
+
+        // Note that the service defines the collection id which can be accessed via OgcFeatureCollectionInfo.CollectionId.
+        private const string CollectionId = "TransportationGroundCrv";
+
         public QueryCQLFilters()
         {
             InitializeComponent();
@@ -43,6 +58,153 @@ namespace ArcGISRuntime.WPF.Samples.QueryCQLFilters
 
         private void Initialize()
         {
+            // Populate the UI.
+            WhereClauseBox.ItemsSource = DefaultWhereClause;
+            MaxFeaturesBox.Text = "3000";
+            StartDatePicker.SelectedDate = new DateTime(2011, 6, 13);
+            EndDatePicker.SelectedDate = new DateTime(2012, 1, 7);
+
+            // Create the map with topographic basemap.
+            MyMapView.Map = new Map(Basemap.CreateTopographic());
+            LoadService();
+        }
+
+        private async void LoadService()
+        {
+            try
+            {
+                LoadingProgressBar.Visibility = Visibility.Visible;
+
+                // Create the feature table from URI and collection id.
+                _featureTable = new OgcFeatureCollectionTable(new Uri(ServiceUrl), CollectionId);
+
+                // Set the feature request mode to manual (only manual is currently supported).
+                // In this mode, you must manually populate the table - panning and zooming won't request features automatically.
+                _featureTable.FeatureRequestMode = FeatureRequestMode.ManualCache;
+
+                // Load the table.
+                await _featureTable.LoadAsync();
+
+                // Populate the OGC feature collection table.
+                QueryParameters queryParamaters = new QueryParameters();
+                queryParamaters.MaxFeatures = 3000;
+                await _featureTable.PopulateFromServiceAsync(queryParamaters, false, null);
+
+                // Create a feature layer from the OGC feature collection
+                // table to visualize the OAFeat features.
+                FeatureLayer ogcFeatureLayer = new FeatureLayer(_featureTable);
+
+                // Choose a renderer for the layer based on the table.
+                ogcFeatureLayer.Renderer = GetRendererForTable(_featureTable) ?? ogcFeatureLayer.Renderer;
+
+                // Add the layer to the map.
+                MyMapView.Map.OperationalLayers.Add(ogcFeatureLayer);
+
+                // Zoom to the extent of the feature layer/table.
+                Envelope tableExtent = _featureTable.Extent;
+                if (tableExtent != null && !tableExtent.IsEmpty)
+                {
+                    await MyMapView.SetViewpointGeometryAsync(tableExtent, 20);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                MessageBox.Show(ex.Message, "Error loading/populating service");
+            }
+            finally
+            {
+                // Update the UI.
+                LoadingProgressBar.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private Renderer GetRendererForTable(FeatureTable table)
+        {
+            switch (table.GeometryType)
+            {
+                case GeometryType.Point:
+                case GeometryType.Multipoint:
+                    return new SimpleRenderer(new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, Color.Blue, 4));
+
+                case GeometryType.Polygon:
+                case GeometryType.Envelope:
+                    return new SimpleRenderer(new SimpleFillSymbol(SimpleFillSymbolStyle.Solid, Color.Green, null));
+
+                case GeometryType.Polyline:
+                    return new SimpleRenderer(new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, Color.Red, 1.5));
+            }
+
+            return null;
+        }
+
+        public QueryParameters CreateQueryParameters()
+        {
+            // Create new QueryParameters and assign its WhereClause property
+            // to the content of the WhereClauseBox
+            var queryParameters = new QueryParameters
+            {
+                WhereClause = WhereClauseBox.Text ?? ""
+            };
+
+            // Set the MaxFeatures property to MaxFeaturesBox content
+            if (int.TryParse(MaxFeaturesBox.Text, out int parsedMaxFeatures))
+                queryParameters.MaxFeatures = parsedMaxFeatures;
+
+            // Set a TimeExtent variable to match the inputted DateTime content.
+
+            // Set user date times if provided.
+            DateTime startDate = (StartDateBox.IsChecked == true && StartDatePicker.SelectedDate is DateTime userStart) ? userStart : DateTime.MinValue;
+            DateTime endDate = (EndDateBox.IsChecked == true && EndDatePicker.SelectedDate is DateTime userEnd) ? userEnd : new DateTime(9999, 12, 31);
+
+            // Use the newly created startDate and endDate to create the TimeExtent.
+            queryParameters.TimeExtent = new Esri.ArcGISRuntime.TimeExtent(startDate, endDate);
+
+            return queryParameters;
+        }
+
+        private void LoadServiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadService();
+        }
+
+        private async void ApplyQuery_Click(object sender, RoutedEventArgs e)
+        {
+            if (_featureTable.LoadStatus != Esri.ArcGISRuntime.LoadStatus.Loaded)
+            {
+                return;
+            }
+
+            try
+            {
+                QueryingProgressBar.Visibility = Visibility.Visible;
+
+                // Set queryParameters to the user's input
+                var queryParameters = CreateQueryParameters();
+
+                // Populate the table with the query, clearing the exist content of the table.
+                // Setting outFields to null requests all fields.
+                var result = await _featureTable.PopulateFromServiceAsync(queryParameters, true, outFields: null);
+
+                // Report the number of returned features by the query
+                NumberOfReturnedFeatures.Content = $"Query returned {result.Count()} features.";
+
+                // Zoom to the extent of the returned features.
+                Envelope tableExtent = _featureTable.Extent;
+                if (tableExtent != null && !tableExtent.IsEmpty)
+                {
+                    await MyMapView.SetViewpointGeometryAsync(tableExtent, 20);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error querying/populating service");
+            }
+            finally
+            {
+                // Update the UI.
+                QueryingProgressBar.Visibility = Visibility.Hidden;
+            }
         }
     }
 }

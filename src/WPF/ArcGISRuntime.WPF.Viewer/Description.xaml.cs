6e321e4a4ab09ad67f9473aeb978cf22bfa8d2ce
Zack Allen <zack.m.allen@gmail.com>
2021-09-16 10:08:02 -0700

    Fix hyperlink behavior in WPF viewer (#924)

--- src/WPF/ArcGISRuntime.WPF.Viewer/Description.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Description.xaml.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2018 Esri.
+﻿// Copyright 2021 Esri.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
@@ -7,6 +7,7 @@
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
 // language governing permissions and limitations under the License.
 
+using System.Diagnostics;
 using System.IO;
 using ArcGISRuntime.Samples.Shared.Models;
 
@@ -26,14 +27,38 @@ namespace ArcGISRuntime.WPF.Viewer
             DataContext = sample;
 
             string folderPath = sample.Path;
-            string cssPath = System.IO.Path.Combine(App.ResourcePath, "Resources", "github-markdown.css");
-            string readmePath = System.IO.Path.Combine(folderPath, "Readme.md");
-            string readmeContent = System.IO.File.ReadAllText(readmePath);
-            string overrideCssPath = System.IO.Path.Combine(App.ResourcePath, "Resources", "hide-header.css");
+            string cssPath = Path.Combine(App.ResourcePath, "Resources", "github-markdown.css");
+            string readmePath = Path.Combine(folderPath, "Readme.md");
+            string readmeContent = File.ReadAllText(readmePath);
+            string overrideCssPath = Path.Combine(App.ResourcePath, "Resources", "hide-header.css");
             readmeContent = _markdownRenderer.Parse(readmeContent);
 
             string htmlString = "<!doctype html><head><base href=\"" + readmePath + "\"><link rel=\"stylesheet\" href=\"" + cssPath + "\" /><link rel=\"stylesheet\" href=\"" + overrideCssPath + "\" /></head><body class=\"markdown-body\">" + readmeContent + "</body>";
-            DescriptionView.NavigateToString(htmlString);
+            
+            // Set the html in web browser.
+            DescriptionView.DocumentText = htmlString;
+
+            // Add an event handler for hyperlink clicks.
+            DescriptionView.Document.Click += HyperlinkClick;
+
+            // Create an event handler for canceling the default hyperlink behavior.
+            DescriptionView.NewWindow += (s, e) => { e.Cancel = true; };
+        }
+
+        private void HyperlinkClick(object sender, System.Windows.Forms.HtmlElementEventArgs e)
+        {
+            // Get the html element that the user clicked on.
+            System.Windows.Forms.HtmlElement src = DescriptionView.Document?.GetElementFromPoint(e.ClientMousePosition);
+
+            // Check if the element is a hyperlink.
+            if (src?.OuterHtml.Contains("http") == true)
+            {
+                // Parse the url from the hyperlink html.
+                string url = src.OuterHtml.Split('\"')[1];
+
+                // Open the url.
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
         }
     }
 }

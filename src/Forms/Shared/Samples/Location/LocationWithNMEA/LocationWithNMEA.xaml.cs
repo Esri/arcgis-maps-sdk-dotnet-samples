d7781d1960987800ca7573015678720a3425fd8c
Zack Allen <zack.m.allen@gmail.com>
2022-01-11 21:07:03 -0800

    Templates

---
+++ src/Forms/Shared/Samples/Location/LocationWithNMEA/LocationWithNMEA.xaml.cs
@@ -0,0 +1,42 @@
+﻿// Copyright 2022 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.Tasks.Offline;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.ArcGISServices;
+using Esri.ArcGISRuntime.UI.Controls;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ArcGISRuntimeXamarin.Samples.LocationWithNMEA
+{
+    [ArcGISRuntime.Samples.Shared.Attributes.Sample(
+        "Display device location with NMEA data sources",
+        "Location",
+        "Parse NMEA sentences and use the results to show device location on the map.",
+        "")]
+    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("d5bad9f4fee9483791e405880fb466da")]
+    public partial class LocationWithNMEA : ContentPage
+    {
+        public LocationWithNMEA()
+        {
+            InitializeComponent();
+            _ = Initialize();
+        }
+
+        private async Task Initialize()
+        {
+        }
+    }
+}

d9aba96bfc0aaabacc06db279ead8275fcd4b284
Zack Allen <zack.m.allen@gmail.com>
2022-01-12 08:34:35 -0800

    Started sample

--- src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Location/LocationWithNMEA/LocationWithNMEA.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Location/LocationWithNMEA/LocationWithNMEA.xaml.cs
@@ -7,22 +7,17 @@
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
-using Esri.ArcGISRuntime.Data;
+using ArcGISRuntime.Samples.Managers;
 using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Location;
 using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
-using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
-using Esri.ArcGISRuntime.UI.Controls;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
-using System.Windows.Media;
 
 namespace ArcGISRuntime.WPF.Samples.LocationWithNMEA
 {
@@ -34,6 +29,12 @@ namespace ArcGISRuntime.WPF.Samples.LocationWithNMEA
     [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("d5bad9f4fee9483791e405880fb466da")]
     public partial class LocationWithNMEA
     {
+        private NmeaLocationDataSource _nmeaSource;
+
+        private SimulatedNMEADataSource _simulatedNMEADataSource;
+
+        private Location[] _mockLocations;
+
         public LocationWithNMEA()
         {
             InitializeComponent();
@@ -42,6 +43,109 @@ namespace ArcGISRuntime.WPF.Samples.LocationWithNMEA
 
         private async Task Initialize()
         {
+            // Create the map.
+            MyMapView.Map = new Map(BasemapStyle.ArcGISNavigation);
+            MyMapView.SetViewpoint(new Viewpoint(new MapPoint(-117.191, 34.0306, SpatialReferences.Wgs84), 100000));
+
+            // Create the simulated data.
+            string nmeaPath = DataManager.GetDataFolder("d5bad9f4fee9483791e405880fb466da", "Redlands.nmea");
+            _simulatedNMEADataSource = new SimulatedNMEADataSource(nmeaPath);
+
+            // Create the NMEA data source.
+            _nmeaSource = new NmeaLocationDataSource(SpatialReferences.Wgs84);
+            _nmeaSource.NmeaDataStream = _simulatedNMEADataSource.DataStream;
+            _nmeaSource.SatellitesChanged += SatellitesChanged;
+
+            MyMapView.LocationDisplay.DataSource = _nmeaSource;
+
+            await _nmeaSource.StartAsync();
+        }
+
+        private void SatellitesChanged(object sender, IReadOnlyList<NmeaSatelliteInfo> infos)
+        {
+            if (infos.Count == 0) return;
+            SortedSet<int> uniqueSatelliteIds = new SortedSet<int>();
+            foreach (var info in infos)
+            {
+                uniqueSatelliteIds.Add(info.Id);
+            }
+
+            Dispatcher.BeginInvoke((Action)delegate ()
+            {
+                // Show the status information in the UI.
+                InfoLabel.Content = $"Satellite count: {infos.Count} Satellites: {string.Join(", ", uniqueSatelliteIds)} System: {infos.First().System} ";
+
+            });
+        }
+
+        private void StartClick(object sender, RoutedEventArgs e)
+        {
+            _simulatedNMEADataSource.Start();
+        }
+
+        private void RecenterClick(object sender, RoutedEventArgs e)
+        {
+            MyMapView.LocationDisplay.AutoPanMode = Esri.ArcGISRuntime.UI.LocationDisplayAutoPanMode.Recenter;
+        }
+
+        private void ResetClick(object sender, RoutedEventArgs e)
+        {
+            _simulatedNMEADataSource.Stop();
+            _simulatedNMEADataSource.Reset();
+        }
+    }
+
+    public class SimulatedNMEADataSource : IDisposable
+    {
+        private Timer _timer;
+
+        public Stream DataStream;
+
+        private int _count = 0;
+        private const int DefaultInterval = 100;
+
+        private string[] _nmeaStrings;
+
+        public SimulatedNMEADataSource(string nmeaSourceFilePath, double speed = 1.0)
+        {
+            _timer = new Timer((int)(DefaultInterval * speed));
+
+            _nmeaStrings = File.ReadAllText(nmeaSourceFilePath).Split('\n');
+
+            DataStream = new MemoryStream();
+
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+        public void Reset()
+        {
+            _timer.Stop();
+            _count = 0;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string nmeaString = $"{_nmeaStrings[_count]}\n";
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(nmeaString);
+            DataStream.Write(data, 0, data.Length);
+
+            _count = (_count + 1) % _nmeaStrings.Length;
+        }
+
+        public void Dispose()
+        {
+            _timer.Dispose();
+            DataStream.Dispose();
         }
     }
 }

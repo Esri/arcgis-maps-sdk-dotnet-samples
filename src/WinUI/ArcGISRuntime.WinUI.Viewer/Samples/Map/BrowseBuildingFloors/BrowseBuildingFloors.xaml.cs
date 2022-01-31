2d5b87f6df649b13083a26f95d2c1a61bad7f0a2
Armando Santana <69874675+asantana917@users.noreply.github.com>
2022-01-31 15:16:23 -0800

    WinUI update for BrowseBuildingFloors

--- src/WinUI/ArcGISRuntime.WinUI.Viewer/Samples/Map/BrowseBuildingFloors/BrowseBuildingFloors.xaml.cs
+++ src/WinUI/ArcGISRuntime.WinUI.Viewer/Samples/Map/BrowseBuildingFloors/BrowseBuildingFloors.xaml.cs
@@ -21,6 +21,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Input;
+using Esri.ArcGISRuntime.Portal;
+using Esri.ArcGISRuntime.Mapping.Floor;
+using System.Collections.Generic;
+using Esri.ArcGISRuntime;
+using Microsoft.UI.Xaml.Controls;
 
 namespace ArcGISRuntime.WinUI.Samples.BrowseBuildingFloors
 {
@@ -33,6 +38,12 @@ namespace ArcGISRuntime.WinUI.Samples.BrowseBuildingFloors
     [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
     public partial class BrowseBuildingFloors
     {
+        private const string _portalItem = "f133a698536f44c8884ad81f80b6cfc7";
+        private FloorManager _floorManager;
+
+        // Collection of floors.
+        private readonly Dictionary<string, FloorLevel> _floorOptions = new Dictionary<string, FloorLevel>();
+
         public BrowseBuildingFloors()
         {
             InitializeComponent();
@@ -41,6 +52,53 @@ namespace ArcGISRuntime.WinUI.Samples.BrowseBuildingFloors
 
         private async Task Initialize()
         {
+            try
+            {
+                ArcGISPortal portal = await ArcGISPortal.CreateAsync();
+
+                // Get the portal item for a web map using its unique item id.
+                PortalItem mapItem = await PortalItem.CreateAsync(portal, _portalItem);
+                Map map = new Map(mapItem);
+
+                MyMapView.Map = map;
+
+                // Map needs to be loaded in order for floormanager to be used.
+                await MyMapView.Map.LoadAsync();
+                await MyMapView.Map.FloorManager.LoadAsync();
+
+                if (MyMapView.Map.FloorManager.LoadStatus.Equals(LoadStatus.Loaded))
+                {
+                    _floorManager = MyMapView.Map.FloorManager;
+                    foreach (FloorLevel floors in _floorManager.Levels)
+                    {
+                        _floorOptions.Add(floors.ShortName, floors);
+                    }
+                }
+
+                FloorChooser.ItemsSource = _floorOptions.Keys;
+                FloorChooser.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                await new MessageDialog2(ex.Message, "Error").ShowAsync();
+            }
+        }
+
+        private void OnFloorChooserSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Get the name of the selected floor.
+            string selectedFloorName = e.AddedItems[0].ToString();
+
+            // Set all existing floors visibility to false.
+            foreach (FloorLevel level in _floorManager.Levels)
+            {
+                level.IsVisible = false;
+            }
+
+            // Set the selected floor visibility to true.
+            _floorOptions[selectedFloorName].IsVisible = true;
+
+            MyMapView.SetViewpoint(MyMapView.Map.InitialViewpoint);
         }
     }
 }

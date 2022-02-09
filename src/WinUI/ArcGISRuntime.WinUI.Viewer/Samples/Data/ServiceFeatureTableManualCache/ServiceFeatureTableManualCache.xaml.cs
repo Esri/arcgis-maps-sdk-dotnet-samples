a69d8e10abac54b22224bebce632c3ed9f1ef40b
Zack Allen <zack.m.allen@gmail.com>
2022-02-09 12:53:55 -0800

    Update SF 311 data in samples (#962)

--- src/WinUI/ArcGISRuntime.WinUI.Viewer/Samples/Data/ServiceFeatureTableManualCache/ServiceFeatureTableManualCache.xaml.cs
+++ src/WinUI/ArcGISRuntime.WinUI.Viewer/Samples/Data/ServiceFeatureTableManualCache/ServiceFeatureTableManualCache.xaml.cs
@@ -47,7 +47,7 @@ namespace ArcGISRuntime.WinUI.Samples.ServiceFeatureTableManualCache
 
             // Create uri to the used feature service
             Uri serviceUri = new Uri(
-               "https://sampleserver6.arcgisonline.com/arcgis/rest/services/SF311/FeatureServer/0");
+               "https://services2.arcgis.com/ZQgQTuoyBrtmoGdP/arcgis/rest/services/SF_311_Incidents/FeatureServer/0");
 
             // Create feature table for the incident feature service
             _incidentsFeatureTable = new ServiceFeatureTable(serviceUri)

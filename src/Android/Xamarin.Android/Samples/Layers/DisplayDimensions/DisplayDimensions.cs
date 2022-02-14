c19d679fab68fc79a38f9c1c0b4a8ec02f9a264f
Hamish Duff <hduff@esri.com>
2022-02-14 12:04:53 +0000

    generated templates

---
+++ src/Android/Xamarin.Android/Samples/Layers/DisplayDimensions/DisplayDimensions.cs
@@ -0,0 +1,64 @@
+// Copyright 2022 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Android.App;
+using Android.OS;
+using Android.Widget;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.Tasks.Offline;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.ArcGISServices;
+using Esri.ArcGISRuntime.UI.Controls;
+
+namespace ArcGISRuntimeXamarin.Samples.DisplayDimensions
+{
+    [Activity (ConfigurationChanges=Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
+    [ArcGISRuntime.Samples.Shared.Attributes.Sample(
+        "Display dimensions",
+        "Layers",
+        "Display dimension features from a mobile map package",
+        "")]
+    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("f5ff6f5556a945bca87ca513b8729a1e")]
+    public class DisplayDimensions : Activity
+    {
+        // Hold references to the UI controls.
+        private MapView _myMapView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Title = "Display dimensions";
+
+            CreateLayout();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+        }
+
+        private void CreateLayout()
+        {
+            // Create a new vertical layout for the app.
+            var layout = new LinearLayout(this) { Orientation = Orientation.Vertical };
+
+            // Add the map view to the layout.
+            _myMapView = new MapView();
+            layout.AddView(_myMapView);
+
+            // Show the layout in the app.
+            SetContentView(layout);
+        }
+    }
+}
---
+++ src/UWP/ArcGISRuntime.UWP.Viewer/Samples/Layers/DisplayDimensions/DisplayDimensions.xaml.cs
@@ -0,0 +1,43 @@
+﻿// Copyright 2022 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.Tasks.Offline;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.ArcGISServices;
+using Esri.ArcGISRuntime.UI.Controls;
+using System;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+
+namespace ArcGISRuntime.UWP.Samples.DisplayDimensions
+{
+    [ArcGISRuntime.Samples.Shared.Attributes.Sample(
+        "Display dimensions",
+        "Layers",
+        "Display dimension features from a mobile map package",
+        "")]
+    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("f5ff6f5556a945bca87ca513b8729a1e")]
+    public partial class DisplayDimensions
+    {
+        public DisplayDimensions()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+        }
+    }
+}

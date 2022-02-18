fb745c751ebf983fac0b49f7eeb3c142a4f313de
Hamish Duff <hduff@esri.com>
2022-02-18 10:47:06 +0000

    Updated SceneLayerSelection WPF sample

--- src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Layers/SceneLayerSelection/SceneLayerSelection.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Layers/SceneLayerSelection/SceneLayerSelection.xaml.cs
@@ -14,6 +14,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using Esri.ArcGISRuntime.Geometry;
+using System.Threading.Tasks;
 
 namespace ArcGISRuntime.WPF.Samples.SceneLayerSelection
 {
@@ -31,10 +32,10 @@ namespace ArcGISRuntime.WPF.Samples.SceneLayerSelection
             Initialize();
         }
 
-        private async void Initialize()
+        private async Task Initialize()
         {
             // Create a new Scene with an imagery basemap.
-            Scene scene = new Scene(BasemapStyle.ArcGISImageryStandard);
+            Scene scene = new Scene(BasemapStyle.ArcGISTopographic);
 
             // Add a base surface with elevation data.
             Surface elevationSurface = new Surface();
@@ -43,7 +44,7 @@ namespace ArcGISRuntime.WPF.Samples.SceneLayerSelection
             scene.BaseSurface = elevationSurface;
 
             // Add a scene layer.
-            Uri buildingsService = new Uri("https://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/Buildings_Berlin/SceneServer");
+            Uri buildingsService = new Uri("https://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/Buildings_Brest/SceneServer/layers/0");
             ArcGISSceneLayer buildingsLayer = new ArcGISSceneLayer(buildingsService);
             scene.OperationalLayers.Add(buildingsLayer);
 
@@ -53,9 +54,7 @@ namespace ArcGISRuntime.WPF.Samples.SceneLayerSelection
             try
             {
                 // Create a camera with an interesting view.
-                await buildingsLayer.LoadAsync();
-                MapPoint center = (MapPoint)GeometryEngine.Project(buildingsLayer.FullExtent.GetCenter(), SpatialReferences.Wgs84);
-                Camera viewCamera = new Camera(center.Y, center.X, 600, 120, 60, 0);
+                Camera viewCamera = new Camera(48.378, -4.494, 200, 345, 65, 0);
 
                 // Set the viewpoint with the camera.
                 MySceneView.SetViewpointCamera(viewCamera);

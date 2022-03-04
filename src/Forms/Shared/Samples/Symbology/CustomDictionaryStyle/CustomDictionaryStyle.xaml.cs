1d1aaf3fe1c3ba9a6fc08aff39b28298efd94913
Hamish Duff <hduff@esri.com>
2022-03-04 15:56:22 +0000

    Updated CustomDictionaryStyles Xamarin.Forms Sample

--- src/Forms/Shared/Samples/Symbology/CustomDictionaryStyle/CustomDictionaryStyle.xaml.cs
+++ src/Forms/Shared/Samples/Symbology/CustomDictionaryStyle/CustomDictionaryStyle.xaml.cs
@@ -9,6 +9,7 @@
 
 using ArcGISRuntime.Samples.Managers;
 using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Portal;
 using Esri.ArcGISRuntime.Symbology;
 using System;
 using Xamarin.Forms;
@@ -20,7 +21,7 @@ namespace ArcGISRuntimeXamarin.Samples.CustomDictionaryStyle
         category: "Symbology",
         description: "Use a custom dictionary style (.stylx) to symbolize features using a variety of attribute values.",
         instructions: "Pan and zoom the map to see the symbology from the custom dictionary style.",
-        tags: new[] { "dictionary", "military", "renderer", "style", "stylx", "unique value", "visualization", "Featured" })]
+        tags: new[] { "ArcGIS Online", "dictionary", "military", "renderer", "style", "stylx", "unique value", "visualization", "web styles", "Featured" })]
     [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("751138a2e0844e06853522d54103222a")]
     public partial class CustomDictionaryStyle : ContentPage
     {
@@ -29,6 +30,13 @@ namespace ArcGISRuntimeXamarin.Samples.CustomDictionaryStyle
 
         // Uri for the restaurants feature service.
         private readonly Uri _restaurantUri = new Uri("https://services2.arcgis.com/ZQgQTuoyBrtmoGdP/arcgis/rest/services/Redlands_Restaurants/FeatureServer/0");
+
+        // Hold a reference to the custom dictionary styles for use in the event handlers. 
+        private DictionarySymbolStyle _localRestaurantStyle;
+        private DictionarySymbolStyle _restaurantStyleFromPortal;
+
+        // Hold a reference to the feature layer for use in the event handlers.
+        private FeatureLayer _restaurantLayer;
 
         public CustomDictionaryStyle()
         {
@@ -40,35 +48,63 @@ namespace ArcGISRuntimeXamarin.Samples.CustomDictionaryStyle
         {
             try
             {
-                // Create a new map with a streets basemap.
-                Map map = new Map(BasemapStyle.ArcGISStreets);
+                // Create a new map with a topographic basemap.
+                Map map = new Map(BasemapStyle.ArcGISTopographic);
 
                 // Create the restaurants layer and add it to the map.
-                FeatureLayer restaurantLayer = new FeatureLayer(_restaurantUri);
-                map.OperationalLayers.Add(restaurantLayer);
+                _restaurantLayer = new FeatureLayer(_restaurantUri);
+                map.OperationalLayers.Add(_restaurantLayer);
 
                 // Load the feature table for the restaurants layer.
-                await restaurantLayer.FeatureTable.LoadAsync();
+                await _restaurantLayer.FeatureTable.LoadAsync();
 
-                // Open the custom style file.
-                DictionarySymbolStyle restaurantStyle = await DictionarySymbolStyle.CreateFromFileAsync(_stylxPath);
+                // Open the local custom style file.
+                _localRestaurantStyle = await DictionarySymbolStyle.CreateFromFileAsync(_stylxPath);
 
-                // Create the dictionary renderer with the style file and the field overrides.
-                DictionaryRenderer dictRenderer = new DictionaryRenderer(restaurantStyle);
+                // Open a portal item containing a dictionary symbol style.
+                ArcGISPortal portal = await ArcGISPortal.CreateAsync();
+                PortalItem portalItem = await PortalItem.CreateAsync(portal, "adee951477014ec68d7cf0ea0579c800");
 
-                // Set the restaurant layer renderer to the dictionary renderer.
-                restaurantLayer.Renderer = dictRenderer;
+                // Open the portal custom style file.
+                _restaurantStyleFromPortal = await DictionarySymbolStyle.OpenAsync(portalItem);
 
                 // Set the map's initial extent to that of the restaurants.
-                map.InitialViewpoint = new Viewpoint(restaurantLayer.FullExtent);
+                map.InitialViewpoint = new Viewpoint(_restaurantLayer.FullExtent);
 
                 // Set the map to the map view.
                 MyMapView.Map = map;
+
+                // Set the local style button to be clicked.  
+                LocalStyleButton.IsEnabled = true;
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+        }
+
+        private void LocalStyleButtonClicked(object sender, EventArgs e)
+        {
+            // Create the dictionary renderer with the style file and the field overrides.
+            DictionaryRenderer dictionaryRenderer = new DictionaryRenderer(_localRestaurantStyle);
+
+            // Set the restaurant layer renderer to the dictionary renderer.
+            _restaurantLayer.Renderer = dictionaryRenderer;
+
+            LocalStyleButton.IsEnabled = false;
+            WebStyleButton.IsEnabled = true;
+        }
+
+        private void WebStyleButtonClicked(object sender, EventArgs e)
+        {
+            // Create the dictionary renderer with the style file and the field overrides.
+            DictionaryRenderer dictionaryRenderer = new DictionaryRenderer(_restaurantStyleFromPortal);
+
+            // Set the restaurant layer renderer to the dictionary renderer.
+            _restaurantLayer.Renderer = dictionaryRenderer;
+
+            LocalStyleButton.IsEnabled = true;
+            WebStyleButton.IsEnabled = false;
         }
     }
 }

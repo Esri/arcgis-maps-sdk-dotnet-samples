86c0bf95b6c968ef8b6d5539a8fda3108b17bac3
Armando Santana <69874675+asantana917@users.noreply.github.com>
2022-04-01 09:54:37 -0700

    Implemented the changes on WPF

--- src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Data/ToggleBetweenFeatureRequestModes/ToggleBetweenFeatureRequestModes.xaml.cs
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Data/ToggleBetweenFeatureRequestModes/ToggleBetweenFeatureRequestModes.xaml.cs
@@ -8,21 +8,9 @@
 // language governing permissions and limitations under the License.
 
 using Esri.ArcGISRuntime.Data;
-using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
-using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
-using Esri.ArcGISRuntime.UI.Controls;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Media;
 
 namespace ArcGISRuntime.WPF.Samples.ToggleBetweenFeatureRequestModes
 {
@@ -31,17 +19,87 @@ namespace ArcGISRuntime.WPF.Samples.ToggleBetweenFeatureRequestModes
         "Data",
         "Use different feature request modes to populate the map from a service feature table.",
         "")]
-    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
     public partial class ToggleBetweenFeatureRequestModes
     {
+        private ServiceFeatureTable _treeFeatureTable;
+        private FeatureLayer _treeFeatureLayer;
+
         public ToggleBetweenFeatureRequestModes()
         {
             InitializeComponent();
-            _ = Initialize();
+            Initialize();
         }
 
-        private async Task Initialize()
+        private void Initialize()
         {
+            // Create new Map with basemap
+            Map myMap = new Map(BasemapStyle.ArcGISTopographic);
+            MyMapView.Map = myMap;
+
+            // Set intial map location.
+            MyMapView.Map.InitialViewpoint = new Viewpoint(45.5266, -122.6219, 6000);
+
+            // Create uri to the used feature service.
+            Uri serviceUri = new Uri(
+               "https://services2.arcgis.com/ZQgQTuoyBrtmoGdP/arcgis/rest/services/Trees_of_Portland/FeatureServer/0");
+
+            // Create a feature table for the trees of Portland feature service.
+            _treeFeatureTable = new ServiceFeatureTable(serviceUri)
+            {
+                FeatureRequestMode = FeatureRequestMode.OnInteractionCache
+            };
+
+            // Create FeatureLayer that uses the created table
+            _treeFeatureLayer = new FeatureLayer(_treeFeatureTable);
+
+            // Add created layer to the map
+            MyMapView.Map.OperationalLayers.Add(_treeFeatureLayer);
+        }
+
+        // Use this method for manual cache.
+        private async void FetchCacheManually()
+        {
+            // Create new query object that contains parameters to query specific request types
+            QueryParameters queryParameters = new QueryParameters()
+            {
+                WhereClause = "Condition < '4'",
+                Geometry = MyMapView.VisibleArea
+            };
+
+            // Create list of the fields that are returned from the service
+            string[] outputFields = { "*" };
+
+            try
+            {
+                // Populate feature table with the data based on query
+                await _treeFeatureTable.PopulateFromServiceAsync(queryParameters, true, outputFields);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error");
+            }
+        }
+
+        private void PopulateButtonClick(object sender, RoutedEventArgs e)
+        {
+            // Populates the map with server feature table request mode cache.
+            if ((bool)Cache.IsChecked)
+            {
+                _treeFeatureTable.FeatureRequestMode = FeatureRequestMode.OnInteractionCache;
+            }
+
+            // Populates the map with server feature table request mode no cache.
+            if ((bool)NoCache.IsChecked)
+            {
+                _treeFeatureTable.FeatureRequestMode = FeatureRequestMode.OnInteractionNoCache;
+            }
+
+            // Populates the map with server feature table request mode manual cache.
+            if ((bool)ManualCache.IsChecked)
+            {
+                _treeFeatureTable.FeatureRequestMode = FeatureRequestMode.ManualCache;
+                FetchCacheManually();
+            }
         }
     }
 }

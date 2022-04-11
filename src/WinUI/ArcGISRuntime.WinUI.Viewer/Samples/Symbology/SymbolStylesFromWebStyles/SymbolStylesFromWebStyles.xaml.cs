5c2e2dbc76f3841fa2e8783c2dc8a9fda1db8771
Hamish Duff <hduff@esri.com>
2022-04-11 12:57:20 +0100

    Generated template files and ran sample_sync

---
+++ src/WinUI/ArcGISRuntime.WinUI.Viewer/Samples/Symbology/SymbolStylesFromWebStyles/SymbolStylesFromWebStyles.xaml.cs
@@ -0,0 +1,46 @@
+// Copyright 2022 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.Tasks.Offline;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.ArcGISServices;
+using Esri.ArcGISRuntime.UI.Controls;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Input;
+
+namespace ArcGISRuntime.WinUI.Samples.SymbolStylesFromWebStyles
+{
+    [ArcGISRuntime.Samples.Shared.Attributes.Sample(
+        name: "Create symbol styles from web styles",
+        category: "Symbology",
+        description: "Create symbol styles from a style file hosted on a portal.",
+        instructions: "The sample displays a map with a set of symbols that represent the categories of the features within the dataset. Pan and zoom on the map and view the legend to explore the appearance and names of the different symbols from the selected symbol style.",
+        tags: new[] { "renderer", "symbol", "symbology", "web style" })]
+    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
+    public partial class SymbolStylesFromWebStyles
+    {
+        public SymbolStylesFromWebStyles()
+        {
+            InitializeComponent();
+            _ = Initialize();
+        }
+
+        private async Task Initialize()
+        {
+        }
+    }
+}

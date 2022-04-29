71856935a0f6deee6a2f07109dabd6f06f630300
Armando Santana <69874675+asantana917@users.noreply.github.com>
2022-04-29 13:03:35 -0700

    Implemented Forms code

--- src/WinUI/ArcGISRuntime.WinUI.Viewer/Samples/Symbology/RenderMultilayerSymbols/RenderMultilayerSymbols.xaml.cs
+++ src/WinUI/ArcGISRuntime.WinUI.Viewer/Samples/Symbology/RenderMultilayerSymbols/RenderMultilayerSymbols.xaml.cs
@@ -16,7 +16,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Reflection;
-using System.Windows;
 
 namespace ArcGISRuntime.WinUI.Samples.RenderMultilayerSymbols
 {

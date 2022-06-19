87a5ac5544796114e8ac078fb01f3e271312eebe
Zack Allen <zack.m.allen@gmail.com>
2022-06-19 15:38:01 -0700

    Update async_void_replace.py

--- tools/async_void_replace.py
+++ tools/async_void_replace.py
@@ -5,6 +5,53 @@ from pathlib import Path
 
 import os
 
+def replace(platform_path):
+    for path in Path(platform_path).rglob('*.cs'):
+        try:
+            i = 0
+            task_count = 0
+            with open(path, 'r') as f:
+                lines = f.readlines()
+
+                # Use an indexed while loop so we can delete sections of lines
+                while i < len(lines):
+                    line = lines[i]
+
+                    # Check if the line is the start of the attributes
+                    if "System.Threading.Tasks" in line:
+                        task_count +=1
+                        
+                    i=i+1
+                f.close()
+            
+            if task_count>1:
+                print(path)
+                i = 0
+                with open(path, 'r') as f:
+                    lines = f.readlines()
+
+                    # Use an indexed while loop so we can delete sections of lines
+                    while i < len(lines):
+                        line = lines[i]
+
+                        # Check if the line is the start of the attributes
+                        if "using System; using System.Threading.Tasks;" in line:
+                            lines[i] = "using System;\n"
+                            # print("beans")
+                            # lines.remove(i)
+                            # lines.insert(i, "using System;")
+                            
+                        i=i+1
+                    f.close()
+                
+                # Rewrite the file with updated attributes.
+                with open(path, "w") as file:
+                    file.seek(0)
+                    file.write(''.join(lines))
+                    file.close()
+        except:
+            print("error with file: "+str(path))
+
 def main():
     '''
     Usage: python async_void_replace.py {path_to_samples (ends in src)} (optional)
@@ -19,26 +66,11 @@ def main():
     winUI_path = os.path.join(sample_root, "WinUI", "ArcGISRuntime.WinUI.Viewer", "Samples")
     forms_path = os.path.join(sample_root, "Forms", "Shared", "Samples")
 
-    for path in Path(wpf_path).rglob('*.cs'):
-        try:
-            with open(path, 'r') as f:
-                lines = f.readlines()
-                i = 0
-                task_count = 0
+    replace(wpf_path)
+    replace(winUI_path)
+    replace(forms_path)
 
-                # Use an indexed while loop so we can delete sections of lines
-                while i < len(lines):
-                    line = lines[i]
-
-                    # Check if the line is the start of the attributes
-                    if "System.Threading.Tasks" in line:
-                        task_count +=1
-                        if task_count>1:
-                            print(path)
-                    i=i+1
-                f.close()
-        except:
-            print("error with file: "+str(path))
+    
 
 if __name__ == "__main__":
     main()

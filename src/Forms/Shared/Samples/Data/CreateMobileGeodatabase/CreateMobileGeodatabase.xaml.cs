b5c9f4ff25e81318c7cc264c223af9cc3eddeeeb
Zack Allen <zack.m.allen@gmail.com>
2022-06-22 16:02:03 -0700

    started forms

--- src/Forms/Shared/Samples/Data/CreateMobileGeodatabase/CreateMobileGeodatabase.xaml.cs
+++ src/Forms/Shared/Samples/Data/CreateMobileGeodatabase/CreateMobileGeodatabase.xaml.cs
@@ -27,9 +27,14 @@ namespace ArcGISRuntimeXamarin.Samples.CreateMobileGeodatabase
         description: "Create and share a mobile geodatabase.",
         instructions: "Tap on the map to add a feature symbolizing the user's location. Tap \"View table\" to view the contents of the geodatabase feature table. Once you have added the location points to the map, tap on \"Close geodatabase\" to retrieve the `.geodatabase` file which can then be imported into ArcGIS Pro or opened in an ArcGIS Runtime application.",
         tags: new[] { "arcgis pro", "database", "feature", "feature table", "geodatabase", "mobile geodatabase", "sqlite" })]
-    [ArcGISRuntime.Samples.Shared.Attributes.OfflineData()]
     public partial class CreateMobileGeodatabase : ContentPage
     {
+        private GeodatabaseFeatureTable _featureTable;
+        private Geodatabase _geodatabase;
+
+        private string _gdbPath;
+        private string _directoryPath;
+
         public CreateMobileGeodatabase()
         {
             InitializeComponent();
@@ -38,6 +43,154 @@ namespace ArcGISRuntimeXamarin.Samples.CreateMobileGeodatabase
 
         private async Task Initialize()
         {
+            // Create a map.
+            MyMapView.Map = new Map(BasemapStyle.ArcGISTopographic);
+            MyMapView.SetViewpoint(new Viewpoint(39.323845, -77.733201, 10000.0));
+
+            await CreateGeodatabase();
+
+            Unloaded += SampleUnloaded;
+        }
+
+        private async Task CreateGeodatabase()
+        {
+            try
+            {
+                // Create a new randomly named directory for the geodatabase.
+                _directoryPath = Path.Combine(Environment.ExpandEnvironmentVariables("%TEMP%"), "CreateMobileGeodatabase", Guid.NewGuid().ToString());
+                if (!Directory.Exists(_directoryPath))
+                {
+                    Directory.CreateDirectory(_directoryPath);
+                }
+
+                // Create the geodatabase file.
+                _gdbPath = Path.Combine(_directoryPath, "LocationHistory.geodatabase");
+
+                // Delete existing file if present from previous sample run.
+                if (File.Exists(_gdbPath))
+                {
+                    File.Delete(_gdbPath);
+                }
+
+                _geodatabase = await Geodatabase.CreateAsync(_gdbPath);
+
+                // Construct a table description which stores features as points on map.
+                var tableDescription = new TableDescription("LocationHistory", SpatialReferences.Wgs84, GeometryType.Point)
+                {
+                    HasAttachments = false,
+                    HasM = false,
+                    HasZ = false
+                };
+
+                // Set up the fields for the table,
+                // FieldType.OID is the primary key of the SQLite table
+                // FieldType.Date is a date column used to store a Calendar date
+                // FieldDescriptions can be a SHORT, INTEGER, GUID, FLOAT, DOUBLE, DATE, TEXT, OID, GLOBALID, BLOB, GEOMETRY, RASTER, or XML.
+                tableDescription.FieldDescriptions.Add(new FieldDescription("oid", FieldType.OID));
+                tableDescription.FieldDescriptions.Add(new FieldDescription("collection_timestamp", FieldType.Date));
+
+                // Add a new table to the geodatabase by creating one from the table description.
+                _featureTable = await _geodatabase.CreateTableAsync(tableDescription);
+
+                // Clear the table.
+                await UpdateTable();
+
+                // Create a feature layer for the map.
+                FeatureLayer featureLayer = new FeatureLayer(_featureTable);
+                MyMapView.Map.OperationalLayers.Add(featureLayer);
+
+                // Add an event handler for new feature taps.
+                MyMapView.GeoViewTapped += MyMapView_GeoViewTapped;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().Name, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void MyMapView_GeoViewTapped(object sender, GeoViewInputEventArgs e)
+        {
+            _ = AddFeature(e.Location);
+        }
+
+        private async Task AddFeature(MapPoint location)
+        {
+            // Create an attributes dictionary for the feature.
+            var attributes = new Dictionary<string, object>();
+
+            // Add a timestamp of when the feature was created.
+            attributes["collection_timestamp"] = DateTime.Now;
+
+            // Create the feature.
+            Feature feature = _featureTable.CreateFeature(attributes, location);
+            try
+            {
+                // Add the feature to the feature table.
+                await _featureTable.AddFeatureAsync(feature);
+
+                // Update the UI.
+                await UpdateTable();
+                FeaturesLabel.Content = $"Number of features added: {_featureTable.NumberOfFeatures}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().Name, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private async Task UpdateTable()
+        {
+            // Query all of the features in the feature table.
+            FeatureQueryResult queryFeatureResult = await _featureTable.QueryFeaturesAsync(new QueryParameters());
+
+            // Set the items source for the data grid with the updated query result.
+            TableDataGrid.ItemsSource = queryFeatureResult;
+        }
+
+        private void ViewTable(object sender, RoutedEventArgs e)
+        {
+            TableBorder.Visibility = Visibility.Visible;
+        }
+
+        private void CloseTable(object sender, RoutedEventArgs e)
+        {
+            TableBorder.Visibility = Visibility.Hidden;
+        }
+
+        private void CloseGeodatabaseClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Clear the UI.
+                MyMapView.GeoViewTapped -= MyMapView_GeoViewTapped;
+                CloseGdbButton.IsEnabled = false;
+                CreateGdbButton.IsEnabled = true;
+                MyMapView.Map.OperationalLayers.Clear();
+                FeaturesLabel.Content = $"Number of features added:";
+
+                // Close the geodatabase.
+                _geodatabase.Close();
+
+                // Open the file explorer to the directory containing the geodatabase.
+                Process.Start("explorer.exe", _directoryPath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().Name, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void CreateGdbButton_Click(object sender, RoutedEventArgs e)
+        {
+            _ = CreateGeodatabase();
+            CloseGdbButton.IsEnabled = true;
+            CreateGdbButton.IsEnabled = false;
+        }
+
+        private void SampleUnloaded(object sender, RoutedEventArgs e)
+        {
+            // Close the geodatabase.
+            _geodatabase?.Close();
         }
     }
 }

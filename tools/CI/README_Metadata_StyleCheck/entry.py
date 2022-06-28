6f9b34886fb55933d8b349b1f2e9ed776bc59754
Hamish Duff <hduff@esri.com>
2022-06-28 14:28:17 +0100

    Update entry.py

--- tools/CI/README_Metadata_StyleCheck/entry.py
+++ tools/CI/README_Metadata_StyleCheck/entry.py
@@ -90,6 +90,7 @@ def main():
 
         path_parts = os.path.normpath(f).split(os.path.sep)
         print(path_parts)
+        print(len(path_parts))
         if len(path_parts) < 7:
             # A file not in samples folder, omit.
             # E.g. might be in the root folder or other unrelated folders.

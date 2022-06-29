6a2961c6c5949935efdb23cd9824184281c50e4f
Hamish Duff <hduff@esri.com>
2022-06-29 17:59:56 +0100

    implement sample_sync check

--- tools/CI/README_Metadata_SampleSyncCheck/sample_sync.py
+++ tools/CI/README_Metadata_SampleSyncCheck/sample_sync.py
@@ -6,10 +6,10 @@ def main():
     '''
     Usage: python sample_sync.py 
     '''
-
-    script_location = os.path.dirname(os.path.realpath(__file__))
-    readme_script_path = os.path.join(script_location, "readme_copy.py")
-    metadata_script_path = os.path.join(script_location, "process_metadata.py")
+    args = sys.argv[1]
+    
+    readme_script_path = os.path.join(args,"tools", "readme_copy", "readme_copy.py")
+    metadata_script_path = os.path.join(args, "tools", "metadata_tools", "process_metadata.py")
 
     print("Copying readmes")
     subprocess.run(["python", readme_script_path])

09c4e8ee94f0aa5ed4cb1558d2826fd8829180a4
Hamish Duff <hduff@esri.com>
2022-06-30 17:09:04 +0100

    Update metadata_style_checker.py

--- tools/CI/README_Metadata_StyleCheck/metadata_style_checker.py
+++ tools/CI/README_Metadata_StyleCheck/metadata_style_checker.py
@@ -189,6 +189,7 @@ class MetadataCreator:
             keywords = parse_tags(readme_parts[tags_section_index])
             # De-duplicate API names in README's Tags section.
             self.keywords = [w for w in keywords if w not in self.relevant_apis]
+            self.offline_data = ''
             if readme_parts.__contains__('Offline data'):
                 offline_data_section_index = readme_parts.index('Offline data') + 1
                 self.offline_data = parse_offline_data(readme_parts[offline_data_section_index])
@@ -222,7 +223,8 @@ class MetadataCreator:
         data["ignore"] = self.ignore
         data["images"] = self.images
         data["keywords"] = self.keywords
-        data["offline_data"] = self.offline_data
+        if self.offline_data.__len__() > 0:
+            data["offline_data"] = self.offline_data
         data["redirect_from"] = self.redirect_from
         data["relevant_apis"] = self.relevant_apis
         data["snippets"] = self.snippets

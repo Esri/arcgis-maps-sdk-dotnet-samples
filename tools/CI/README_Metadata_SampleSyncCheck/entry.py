85ede8af932f8fadbd758ff769ebaa425ca81c67
Hamish Duff <hduff@esri.com>
2022-07-01 12:53:32 +0100

    updated scripts

--- tools/CI/README_Metadata_SampleSyncCheck/entry.py
+++ tools/CI/README_Metadata_SampleSyncCheck/entry.py
@@ -1,17 +1,9 @@
-#!/usr/bin/env python3
-
-import argparse
-from pathlib import Path
 import subprocess as sp
 
 def main():
-    msg = 'Entry point of the docker to run the sample-sync script.'
-    parser = argparse.ArgumentParser(description=msg)
-    parser.add_argument('-s', '--string', help='A JSON array of file paths.')
-    args = parser.parse_args()
-    
+
     print("** Starting sample sync **")
-    code2 = sp.call(f'python3 /sample_sync.py',  shell=True)
+    sp.call(f'python3 /samplesync_change_checker.py',  shell=True)
 
 
 if __name__ == '__main__':
--- tools/CI/README_Metadata_SampleSyncCheck/samplesync_change_checker.py
+++ tools/CI/README_Metadata_SampleSyncCheck/samplesync_change_checker.py
@@ -1,13 +1,12 @@
-import sys
 import subprocess
 import os
 
 def main():
+
     '''
     Usage: python sample_sync.py 
     '''
-    # args = sys.argv[1]
-    
+
     readme_script_path = os.path.join(".","tools", "readme_copy", "readme_copy.py")
     metadata_script_path = os.path.join(".", "tools", "metadata_tools", "process_metadata.py")
 

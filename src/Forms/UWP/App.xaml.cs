142415a1b8676d5428cf7d53dd8175c0986802b7
Hamish Duff <hduff@esri.com>
2022-08-03 12:47:17 +0100

    Fixed issue caused by code cleanup

--- src/Forms/UWP/App.xaml.cs
+++ src/Forms/UWP/App.xaml.cs
@@ -21,7 +21,7 @@ using Windows.UI.Xaml.Navigation;
 
 namespace ArcGISRuntime.UWP
 {
-    internal sealed partial class App : Application
+    sealed partial class App : Application
     {
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code

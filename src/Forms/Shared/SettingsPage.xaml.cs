d8de4f1c517537d619ad39a1b520f107064b025f
Zack Allen <zack.m.allen@gmail.com>
2022-08-10 16:11:11 -0700

    Update Forms version number for settings page

--- src/Forms/Shared/SettingsPage.xaml.cs
+++ src/Forms/Shared/SettingsPage.xaml.cs
@@ -50,7 +50,7 @@ namespace ArcGISRuntime
             // Precise version number cant be used while running in release mode.
             catch (Exception)
             {
-                versionNumber = "100.14.1";
+                versionNumber = "100.15.0";
             }
 
             // Set up offline data.

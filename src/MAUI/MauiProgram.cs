618be5ae287255e7240006d0e3cce6ea2b2da0c2
Zack Allen <zack.m.allen@gmail.com>
2022-08-25 14:46:14 -0700

    Morten additions

---
+++ src/MAUI/MauiProgram.cs
@@ -0,0 +1,20 @@
+﻿namespace ArcGISRuntime.Samples.Maui;
+using Esri.ArcGISRuntime.Maui;
+using Microsoft.Maui.Controls.Compatibility.Hosting;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			}).UseArcGISRuntime().UseMauiCompatibility();
+
+        return builder.Build();
+	}
+}

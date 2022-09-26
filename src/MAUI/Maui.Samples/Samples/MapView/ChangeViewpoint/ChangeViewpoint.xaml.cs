26b2f81e3337328bdc75c6545fbf0be1f65624e2
Hamish Duff <hduff@esri.com>
2022-09-26 15:20:58 +0100

    updated PR feedback

--- src/MAUI/Maui.Samples/Samples/MapView/ChangeViewpoint/ChangeViewpoint.xaml.cs
+++ src/MAUI/Maui.Samples/Samples/MapView/ChangeViewpoint/ChangeViewpoint.xaml.cs
@@ -76,7 +76,7 @@ namespace ArcGISRuntime.Samples.ChangeViewpoint
             {
                 // Show sheet and get title from the selection
                 string selectedMapTitle =
-                    await ((Page)Parent).DisplayActionSheet("Select viewpoint", "Cancel", null, titles);
+                    await Application.Current.MainPage.DisplayActionSheet("Select viewpoint", "Cancel", null, titles);
 
                 // If selected cancel do nothing
                 if (selectedMapTitle == "Cancel") return;

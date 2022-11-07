21d8deb83199cfb2c538c3059ddfa519956324ac
Zack Allen <zack.m.allen@gmail.com>
2022-11-07 12:15:49 -0800

    Codemaid

--- src/MAUI/Maui.Samples/Samples/Security/OAuth/OAuth.xaml.cs
+++ src/MAUI/Maui.Samples/Samples/Security/OAuth/OAuth.xaml.cs
@@ -7,10 +7,10 @@
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
+using ArcGISRuntime.Samples.Shared.Managers;
+using ArcGISRuntimeMaui.Helpers;
 using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Portal;
-using ArcGISRuntimeMaui.Helpers;
-using ArcGISRuntime.Samples.Shared.Managers;
 
 namespace ArcGISRuntime.Samples.OAuth
 {

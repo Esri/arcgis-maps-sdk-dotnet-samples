fb4b06bfd7fe7cf69971cbc821f7a3a44ac1d557
Zack Allen <zack.m.allen@gmail.com>
2022-11-07 11:57:04 -0800

    Disposable for other named user samples

--- src/MAUI/Maui.Samples/Samples/Security/OAuth/OAuth.xaml.cs
+++ src/MAUI/Maui.Samples/Samples/Security/OAuth/OAuth.xaml.cs
@@ -10,12 +10,7 @@
 using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Portal;
 using ArcGISRuntimeMaui.Helpers;
-
-#if ANDROID
-
-using Application = Microsoft.Maui.Controls.Application;
-
-#endif
+using ArcGISRuntime.Samples.Shared.Managers;
 
 namespace ArcGISRuntime.Samples.OAuth
 {
@@ -26,7 +21,7 @@ namespace ArcGISRuntime.Samples.OAuth
         instructions: "When you run the sample, the app will load a web map which contains premium content. You will be challenged for an ArcGIS Online login to view the private layers. Enter a user name and password for an ArcGIS Online named user account (such as your ArcGIS for Developers account). If you authenticate successfully, the traffic layer will display, otherwise the map will contain only the public basemap layer.",
         tags: new[] { "OAuth", "OAuth2", "authentication", "cloud", "credential", "portal", "security" })]
     [ArcGISRuntime.Samples.Shared.Attributes.ClassFile("Helpers\\ArcGISLoginPrompt.cs")]
-    public partial class OAuth : ContentPage
+    public partial class OAuth : ContentPage, IDisposable
     {
         // - The URL of the portal to authenticate with
         private const string ServerUrl = "https://www.arcgis.com/sharing/rest";
@@ -65,6 +60,12 @@ namespace ArcGISRuntime.Samples.OAuth
             {
                 await Application.Current.MainPage.DisplayAlert("Error", e.ToString(), "OK");
             }
+        }
+
+        public void Dispose()
+        {
+            // Re-enable the API key in the viewer when exiting this sample.
+            ApiKeyManager.EnableKey();
         }
     }
 }

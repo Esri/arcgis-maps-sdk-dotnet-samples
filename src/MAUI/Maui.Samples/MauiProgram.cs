b5e8407a6f9c5748c4618a9e7d9f7a5da4bc8fb4
Zack Allen <zack.m.allen@gmail.com>
2022-11-16 16:05:01 -0800

    Update MauiProgram.cs

--- src/MAUI/Maui.Samples/MauiProgram.cs
+++ src/MAUI/Maui.Samples/MauiProgram.cs
@@ -14,7 +14,7 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            }).UseArcGISRuntime().UseMauiCompatibility();
+            }).UseArcGISRuntime();
 
         return builder.Build();
     }

6abc3796bbca90e366a13ad78c9aa2826e4ed74f
Zack Allen <zallen@esri.com>
2022-12-13 22:08:54 -0800

    Fix resource path in `Route around barriers`

--- src/MAUI/Maui.Samples/Samples/NetworkAnalysis/RouteAroundBarriers/RouteAroundBarriers.xaml.cs
+++ src/MAUI/Maui.Samples/Samples/NetworkAnalysis/RouteAroundBarriers/RouteAroundBarriers.xaml.cs
@@ -317,7 +317,7 @@ namespace ArcGIS.Samples.RouteAroundBarriers
 
             // Get image as a stream from the resources
             // Picture is defined as EmbeddedResource and DoNotCopy
-            Stream resourceStream = currentAssembly.GetManifestResourceStream("ArcGISMapsSDK.Resources.PictureMarkerSymbols.pin_blue.png");
+            Stream resourceStream = currentAssembly.GetManifestResourceStream("ArcGIS.Resources.PictureMarkerSymbols.pin_blue.png");
 
             // Create new symbol using asynchronous factory method from stream
             PictureMarkerSymbol pinSymbol = await PictureMarkerSymbol.CreateAsync(resourceStream);

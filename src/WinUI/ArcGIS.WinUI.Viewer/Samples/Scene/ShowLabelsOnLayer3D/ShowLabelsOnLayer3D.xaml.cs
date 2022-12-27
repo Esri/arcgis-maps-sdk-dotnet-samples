02e08ccada4bf6cdb3bc9fcc3b105268275861c1
William Bohrmann <wbohrmann@esri.com>
2022-12-27 15:01:09 -0800

    WinUI implementation

--- src/WinUI/ArcGIS.WinUI.Viewer/Samples/Scene/ShowLabelsOnLayer3D/ShowLabelsOnLayer3D.xaml.cs
+++ src/WinUI/ArcGIS.WinUI.Viewer/Samples/Scene/ShowLabelsOnLayer3D/ShowLabelsOnLayer3D.xaml.cs
@@ -21,6 +21,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Input;
+using Esri.ArcGISRuntime.Mapping.Labeling;
+using Esri.ArcGISRuntime.Portal;
 
 namespace ArcGIS.WinUI.Samples.ShowLabelsOnLayer3D
 {
@@ -32,6 +34,9 @@ namespace ArcGIS.WinUI.Samples.ShowLabelsOnLayer3D
     [ArcGIS.Samples.Shared.Attributes.OfflineData()]
     public partial class ShowLabelsOnLayer3D
     {
+        // Hold the ID of the portal item, which is a web scene.
+        private const string ItemId = "850dfee7d30f4d9da0ebca34a533c169";
+
         public ShowLabelsOnLayer3D()
         {
             InitializeComponent();
@@ -40,6 +45,52 @@ namespace ArcGIS.WinUI.Samples.ShowLabelsOnLayer3D
 
         private async Task Initialize()
         {
+            try
+            {
+                // Try to load the default portal, which will be ArcGIS Online.
+                ArcGISPortal portal = await ArcGISPortal.CreateAsync();
+
+                // Create the portal item.
+                PortalItem websceneItem = await PortalItem.CreateAsync(portal, ItemId);
+
+                // Create and show the scene.
+                MySceneView.Scene = new Scene(websceneItem);
+
+                // Load scene to access properties.
+                await MySceneView.Scene.LoadAsync();
+
+                // Find the gas layer.
+                Layer gasLayer = MySceneView.Scene.OperationalLayers.Single(l => l.Name.Equals("Gas"));
+
+                // Find the main gas sublayer.
+                FeatureLayer gasMainLayer = gasLayer.SublayerContents.Single(l => l.Name.Equals("Gas Main")) as FeatureLayer;
+
+                // Clear any existing labels.
+                gasMainLayer.LabelDefinitions.Clear();
+
+                // Enable labelling.
+                gasMainLayer.LabelsEnabled = true;
+
+                // Create a text symbol for the label definition.
+                var textSymbol = new TextSymbol
+                {
+                    Color = System.Drawing.Color.Orange,
+                    HaloColor = System.Drawing.Color.White,
+                    HaloWidth = 2,
+                    Size = 16
+                };
+
+                // Create a label defintion from an arcade label expression and the text symbol.
+                var labelDefinition = new LabelDefinition(new ArcadeLabelExpression("Text($feature.INSTALLATIONDATE, `DD MMM YY`)"),
+                    textSymbol);
+
+                gasMainLayer.LabelDefinitions.Add(labelDefinition);
+
+            }
+            catch (Exception e)
+            {
+                await new MessageDialog2(e.ToString(), "Error").ShowAsync();
+            }
         }
     }
 }

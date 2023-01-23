2df0c01384549563ec02ba5d0a84a208f3a1b626
Hamish Duff <hduff@esri.com>
2023-01-23 17:40:51 +0000

    Added converter and updated sample page list

---
+++ src/MAUI/Maui.Samples/Converters/SampleToBitmapConverter.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Reflection;
+
+namespace ArcGIS.Converters
+{
+    public class SampleToBitmapConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value != null && File.Exists(value.ToString()))
+                {
+                    ImageSource image;
+                    image = ImageSource.FromFile(value.ToString());
+
+                    if (image != null)
+                    {
+                        return image;
+                    };
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

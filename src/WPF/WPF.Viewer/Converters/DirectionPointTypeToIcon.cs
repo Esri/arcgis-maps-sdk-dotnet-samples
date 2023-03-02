e7018f0c43f3072bd7331edfe176a40c41e94c2d
William Bohrmann <wbohrmann@esri.com>
2023-03-02 15:58:15 -0800

    WPF - calcite font

--- src/WPF/WPF.Viewer/Converters/DirectionPointTypeToIcon.cs
+++ src/WPF/WPF.Viewer/Converters/DirectionPointTypeToIcon.cs
@@ -10,11 +10,12 @@ namespace ArcGIS.Converters
 {
     internal class DirectionPointTypeToIconConverter : IValueConverter
     {
-        // Create a dictionary of direction point type IDs and the corresponding file.
+        // Create a dictionary of direction point type and corresponding icons.
         private Dictionary<int, string> _directionIcons = new Dictionary<int, string>()
         {
-            {51, "startpoint.png"}, {52, "straight.png"}, {301, "forkright.png"}, {200, "forkleft.png"}, {305, "right.png"}, {205, "left"},
-            {50, "endpoint.png"}
+            {51, "calcite_glyphicon_RouteFrom"}, {52, "calcite_glyphicon_Straight"}, {301, "calcite_glyphicon_ForkRight"}, 
+            {200, "calcite_glyphicon_ForkLeft"}, {305, "calcite_glyphicon_Right"}, {205, "calcite_glyphicon_Left"}, 
+            {50, "calcite_glyphicon_RouteTo"}
         };
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)

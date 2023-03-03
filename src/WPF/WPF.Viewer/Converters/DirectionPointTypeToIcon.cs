bbbf32e127e4d27e3c5fe31f04765f71332dbc7b
William Bohrmann <wbohrmann@esri.com>
2023-03-03 15:21:57 -0800

    C# icon syntax

--- src/WPF/WPF.Viewer/Converters/DirectionPointTypeToIcon.cs
+++ src/WPF/WPF.Viewer/Converters/DirectionPointTypeToIcon.cs
@@ -10,8 +10,8 @@ namespace ArcGIS.Converters
         // Create a dictionary of direction point type and corresponding icons.
         private Dictionary<int, string> _directionIcons = new Dictionary<int, string>()
         {
-            {51, "\xe259"}, {52, "\xe2aa"}, {301, "\xe124"}, {200, "\xe122"}, {305, "&#xe24e;"}, 
-            {205, "&#xe199;"}, {50, "&#xe25a;"}
+            {51, "\xe259"}, {52, "\xe2aa"}, {301, "\xe124"}, {200, "\xe122"}, {305, "\xe24e"}, 
+            {205, "\xe199"}, {50, "\xe25a"}
         };
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)

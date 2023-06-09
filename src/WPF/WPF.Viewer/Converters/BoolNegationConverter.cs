290005365d253df4b50c94c31fb1c33c67be0509
William Bohrmann <wbohrmann@esri.com>
2023-06-09 10:26:21 -0700

    feedback implemented - WPF

---
+++ src/WPF/WPF.Viewer/Converters/BoolNegationConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace ArcGIS.WPF.Viewer.Converters
+{
+    internal class BoolNegationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

c5714edcfeeea5ba114300fa69f98ee20b2bdbda
Hamish Duff <hduff@esri.com>
2023-07-19 21:34:59 +0100

    Generate sample files and implement MAUI sample

---
+++ src/MAUI/Maui.Samples/Samples/Layers/AddCustomDynamicEntityDataSource/CustomFileSource.cs
@@ -0,0 +1,217 @@
+﻿using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.RealTime;
+using System.Diagnostics;
+using System.Text.Json;
+
+
+namespace ArcGIS.Samples.AddCustomDynamicEntityDataSource
+{
+    public class CustomFileSource : DynamicEntityDataSource
+    {
+        // Hold a reference to the file stream reader, the process task, and the cancellation token source.
+        private Task? _processTask;
+        private StreamReader? _streamReader;
+        private CancellationTokenSource? _cancellationTokenSource;
+        private TaskCompletionSource<object?>? _pauseTask;
+
+        public CustomFileSource(string filePath, string entityIdField, TimeSpan delay)
+        {
+            FilePath = filePath;
+            EntityIdField = entityIdField;
+            Delay = delay;
+        }
+
+        #region Properties
+
+        // Expose the file path, entity ID field, and delay length as properties.
+        public string FilePath { get; }
+        public string EntityIdField { get; }
+        public TimeSpan Delay { get; } 
+        public SpatialReference SpatialReference { get; set; } = SpatialReferences.Wgs84;
+
+        #endregion
+
+        protected override async Task<DynamicEntityDataSourceInfo> OnLoadAsync()
+        {
+            // Derive schema from the first row in the custom data source.
+            var fields = await InferSchemaAsync();
+
+            // Open the file for processing.
+            Stream stream = await FileSystem.OpenAppPackageFileAsync(FilePath);
+            _streamReader = new StreamReader(stream);
+
+            // Create a new DynamicEntityDataSourceInfo using the entity ID field and the fields derived from the attributes of each observation in the custom data source.
+            return new DynamicEntityDataSourceInfo(EntityIdField, fields) { SpatialReference = SpatialReferences.Wgs84 };
+        }
+
+        protected override Task OnConnectAsync(CancellationToken cancellationToken)
+        {
+            // On connecting to the custom data source begin processing the file. 
+            _cancellationTokenSource = new();
+            _processTask = Task.Run(() => ObservationProcessLoopAsync(), _cancellationTokenSource.Token);
+            return Task.CompletedTask;
+        }
+
+        protected override async Task OnDisconnectAsync()
+        {
+            // On disconnecting from the custom data source, stop processing the file.
+            _cancellationTokenSource?.Cancel();
+            _pauseTask?.TrySetResult(null);
+
+            if (_processTask is not null) await _processTask;
+
+            _cancellationTokenSource = null;
+            _pauseTask = null;
+            _processTask = null;
+        }
+
+        private async Task ObservationProcessLoopAsync()
+        {
+            try
+            {
+                while (!_cancellationTokenSource!.IsCancellationRequested)
+                {
+                    // If the pause task is not null, wait for it to complete before processing the next observation.
+                    if (_pauseTask is not null)
+                    {
+                        await _pauseTask.Task;
+                    }
+
+                    // Process the next observation.
+                    var processed = await ProcessNextObservation();
+
+                    // If the observation was not processed, continue to the next observation.
+                    if (!processed) continue;
+
+                    // If there is no delay, yield to the UI thread otherwise delay for the specified amount of time.
+                    if (Delay == TimeSpan.Zero)
+                    {
+                        await Task.Yield();
+                    }
+                    else
+                    {
+                        await Task.Delay(Delay, _cancellationTokenSource.Token);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        private async Task<bool> ProcessNextObservation()
+        {
+            // If we cannot read from the file, throw an exception.
+            if (_streamReader is null)
+            {
+                throw new ArgumentNullException("File stream not available.");
+            }
+
+            // Read the next observation from the file.
+            var json = await _streamReader.ReadLineAsync();
+            if (string.IsNullOrEmpty(json)) return false;
+
+            try
+            {
+                // Parse the observation from the file using the JsonGeoElementParser.
+                // This will return a graphic holding a  MapPoint and a dictionary of attributes.
+                var graphic = JsonGeoElementParser.ParseGraphicFromJson(json);
+                if (graphic?.Geometry is not MapPoint point) return false;
+
+                // Check to see if the spatial reference of the observation matches the spatial reference of the custom data source.
+                // If not, project the observation to the spatial reference of the custom data source.
+                if (point.SpatialReference?.Equals(SpatialReference) != true)
+                {
+                    graphic.Geometry = GeometryEngine.Project(graphic.Geometry, SpatialReference);
+                }
+
+                // Add the observation to the dynamic entity layer for presentation on the map.
+                AddObservation(graphic.Geometry, graphic.Attributes);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                return false;
+            }
+        }
+
+        private async Task<List<Field>> InferSchemaAsync()
+        {
+            var fields = new List<Field>();
+
+            try
+            {
+                // Load the file we are using for our custom data source.
+                using var stream = await FileSystem.OpenAppPackageFileAsync(FilePath);
+                if (stream != null)
+                {
+                    using var streamReader = new StreamReader(stream);
+                    
+                    // Read the first line of the file.
+                    var json = await streamReader.ReadLineAsync();
+
+                    // If the first line is empty the scheme cannot be inferred.
+                    if (string.IsNullOrEmpty(json))
+                    {
+                        throw new InvalidOperationException("Could not infer schema (empty json line at the start of the file).");
+                    }
+
+                    // Parse the first line of the file using the JsonGeoElementParser.
+                    var jsonGeoElement = JsonSerializer.Deserialize<JsonGeoElementParser.JsonGeoElement>(json);
+
+                    // If no attributes are present in the JsonGeoElement, the scheme cannot be inferred.
+                    if (jsonGeoElement?.attributes is null)
+                    {
+                        throw new InvalidOperationException("Could not infer schema (parsing error).");
+                    }
+
+                    // Loop through the attributes in the JsonGeoElement and create fields for each attribute based on the field type and length.
+                    foreach (var attr in jsonGeoElement.attributes)
+                    {
+                        // Infer the field type and length from the attribute value.
+                        var fieldType = Type.GetTypeCode(attr.Value?.GetType()) switch
+                        {
+                            TypeCode.Boolean or TypeCode.Int16 or TypeCode.UInt16 or TypeCode.Single => FieldType.Int16,
+                            TypeCode.Int32 or TypeCode.UInt32 => FieldType.Int32,
+                            TypeCode.DateTime => FieldType.Date,
+                            TypeCode.Decimal => FieldType.Float32,
+                            TypeCode.Double or TypeCode.Int64 or TypeCode.UInt64 => FieldType.Float64,
+                            _ => FieldType.Text,
+                        };
+
+                        var length = Type.GetTypeCode(attr.Value?.GetType()) switch
+                        {
+                            TypeCode.Boolean or TypeCode.Int16 or TypeCode.UInt16 or TypeCode.Single => 2,
+                            TypeCode.Int32 or TypeCode.UInt32 or TypeCode.Decimal => 4,
+                            TypeCode.Double or TypeCode.Int64 or TypeCode.UInt64 => 8,
+                            _ => 256,
+                        };
+
+                        // Create a field from the attribute type, name and length and add it to the list of fields.
+                        var field = new Field(fieldType, attr.Key, string.Empty, length);
+                        fields.Add(field);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+
+            return fields;
+        }
+
+        public async Task StepAsync()
+        {
+            bool processed;
+            do
+            {
+                processed = await ProcessNextObservation();
+            }
+            while (!processed);
+        }
+    }
+}
---
+++ src/MAUI/Maui.Samples/Samples/Layers/AddCustomDynamicEntityDataSource/JsonGeoElementParser.cs
@@ -0,0 +1,152 @@
+﻿using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.UI;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ArcGIS.Samples.AddCustomDynamicEntityDataSource
+{
+    public class JsonGeoElementParser
+    {
+        public static Graphic? ParseGraphicFromJson(string json)
+        {
+            // Deserialize the json into a JsonGeoElement.
+            var jsonGeoElement = JsonSerializer.Deserialize<JsonGeoElement>(json);
+
+            // If the jsonGeoElement cannot be parsed return.
+            if (jsonGeoElement is null) return null;
+
+            // Get the geometry from the jsonGeoElement.
+            JsonGeometry geometry = jsonGeoElement.geometry;
+
+            // Get the spatial reference from the geometry, if this is null return the default spatial reference (4326).
+            var spatialReference = SpatialReference.Create(geometry.spatialReference?.wkid ?? 4326);
+
+            // Create a MapPoint from the geometry and spatial reference.
+            var point = new MapPoint(geometry.x, geometry.y, geometry.z, spatialReference);
+
+            // Return a graphic with the created  MapPoint and attributes from the jsonGeoElement.
+            return new Graphic(point, jsonGeoElement.attributes);
+        }
+
+        #region JsonGeoElement
+
+        // Create an object structure used to deserialize each of the observations from the json file used as the custom data source.
+        public class JsonGeoElement
+        {
+            public JsonGeometry? geometry { get; set; }
+
+            [JsonConverter(typeof(DictionaryStringObjectJsonConverter))]
+            public Dictionary<string, object?>? attributes { get; set; }
+        }
+
+        public class JsonGeometry
+        {
+            public double x { get; set; }
+
+            public double y { get; set; }
+
+            public double z { get; set; }
+
+            public JsonSpatialReference? spatialReference { get; set; }
+        }
+
+        public class JsonSpatialReference
+        {
+            public int wkid { get; set; }
+        }
+
+        public class DictionaryStringObjectJsonConverter : JsonConverter<Dictionary<string, object?>>
+        {
+            public override bool CanConvert(Type typeToConvert)
+            {
+                return typeToConvert == typeof(Dictionary<string, object>)
+                       || typeToConvert == typeof(Dictionary<string, object?>);
+            }
+
+            // Override the default serialization logic to handle the Dictionary<string, object?>.
+            public override Dictionary<string, object?> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            {
+                if (reader.TokenType != JsonTokenType.StartObject)
+                {
+                    throw new JsonException($"JsonTokenType was of type {reader.TokenType}, only objects are supported");
+                }
+
+                var dictionary = new Dictionary<string, object?>();
+                while (reader.Read())
+                {
+                    if (reader.TokenType == JsonTokenType.EndObject)
+                    {
+                        return dictionary;
+                    }
+
+                    if (reader.TokenType != JsonTokenType.PropertyName)
+                    {
+                        throw new JsonException("JsonTokenType was not PropertyName");
+                    }
+
+                    var propertyName = reader.GetString();
+
+                    if (string.IsNullOrWhiteSpace(propertyName))
+                    {
+                        throw new JsonException("Failed to get property name");
+                    }
+
+                    reader.Read();
+
+                    dictionary.Add(propertyName!, ExtractValue(ref reader, options));
+                }
+
+                return dictionary;
+            }
+
+            public override void Write(
+                Utf8JsonWriter writer, Dictionary<string, object?> value, JsonSerializerOptions options)
+            {
+                // We don't need any custom serialization logic for writing the json.
+                // Ideally, this method should not be called at all. It's only called if you
+                // supply JsonSerializerOptions that contains this JsonConverter in it's Converters list.
+                // Don't do that, you will lose performance because of the cast needed below.
+                // Cast to avoid infinite loop: https://github.com/dotnet/docs/issues/19268
+                JsonSerializer.Serialize(writer, (IDictionary<string, object?>)value, options);
+            }
+
+            private object? ExtractValue(ref Utf8JsonReader reader, JsonSerializerOptions options)
+            {
+                switch (reader.TokenType)
+                {
+                    case JsonTokenType.String:
+                        if (reader.TryGetDateTime(out var date))
+                        {
+                            return date;
+                        }
+                        return reader.GetString();
+                    case JsonTokenType.False:
+                        return false;
+                    case JsonTokenType.True:
+                        return true;
+                    case JsonTokenType.Null:
+                        return null;
+                    case JsonTokenType.Number:
+                        if (reader.TryGetInt64(out var result))
+                        {
+                            return result;
+                        }
+                        return reader.GetDouble();
+                    case JsonTokenType.StartObject:
+                        return Read(ref reader, null!, options);
+                    case JsonTokenType.StartArray:
+                        var list = new List<object?>();
+                        while (reader.Read() && reader.TokenType != JsonTokenType.EndArray)
+                        {
+                            list.Add(ExtractValue(ref reader, options));
+                        }
+                        return list;
+                    default:
+                        throw new JsonException($"'{reader.TokenType}' is not supported");
+                }
+            }
+        }
+
+        #endregion
+    }
+}

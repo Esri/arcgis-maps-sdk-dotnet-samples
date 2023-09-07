5b0cac45dadba9dea8acd0187ed5396bc4e15965
Matvei Stefarov <mstefarov@esri.com>
2023-09-07 07:57:23 -0700

    winforms integration example

---
+++ src/WPF/WinFormsNetFx/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+
+namespace WinFormsNetFx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = "";
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.Initialize();
+            InitializeComponent();
+            var host = new ElementHost();
+            host.Dock = DockStyle.Fill;
+            host.Child = new ArcGIS.WPF.Samples.CreateAndEditGeometries.CreateAndEditGeometries();
+            this.Controls.Add(host);
+        }
+    }
+}

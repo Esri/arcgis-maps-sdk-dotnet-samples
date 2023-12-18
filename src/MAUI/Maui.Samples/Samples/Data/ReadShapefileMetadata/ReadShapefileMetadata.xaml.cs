d3835f3b9fb8c6599645d59943cdabcf033bdda4
William Bohrmann <118313312+williambohrmann3@users.noreply.github.com>
2023-12-18 08:44:00 -0800

    Bring in .NET MAUI sample fixes into `main` (#1343)

--- src/MAUI/Maui.Samples/Samples/Data/ReadShapefileMetadata/ReadShapefileMetadata.xaml.cs
+++ src/MAUI/Maui.Samples/Samples/Data/ReadShapefileMetadata/ReadShapefileMetadata.xaml.cs
@@ -26,71 +26,65 @@ namespace ArcGIS.Samples.ReadShapefileMetadata
         {
             InitializeComponent();
 
-            // Open a shapefile stored locally and add it to the map as a feature layer
+            // Open a shapefile stored locally and add it to the map as a feature layer.
             _ = Initialize();
         }
 
         private async Task Initialize()
         {
-            // Create a new map to display in the map view with a streets basemap
-            Map streetMap = new Map(BasemapStyle.ArcGISStreets);
+            // Create a new map to display in the map view with a streets basemap.
+            var streetMap = new Map(BasemapStyle.ArcGISStreets);
 
-            // Get the path to the downloaded shapefile
+            // Get the path to the downloaded shapefile.
             string filepath = GetShapefilePath();
 
             try
             {
-                // Open the shapefile
+                // Open the shapefile.
                 ShapefileFeatureTable myShapefile = await ShapefileFeatureTable.OpenAsync(filepath);
 
-                // Read metadata about the shapefile and display it in the UI
+                // Read metadata about the shapefile and display it in the UI.
                 ShapefileInfo fileInfo = myShapefile.Info;
-                InfoGrid.BindingContext = fileInfo;
+                InfoList.BindingContext = fileInfo;
 
-                // Read the thumbnail image data into a byte array
+                // Read the thumbnail image data into a byte array.
                 Stream imageStream = await fileInfo.Thumbnail.GetEncodedBufferAsync();
                 byte[] imageData = new byte[imageStream.Length];
                 imageStream.Read(imageData, 0, imageData.Length);
 
-                // Create a new image source from the thumbnail data
+                // Create a new image source from the thumbnail data.
                 ImageSource streamImageSource = ImageSource.FromStream(() => new MemoryStream(imageData));
 
-                // Create a new image to display the thumbnail
-                Image image = new Image()
+                // Create a new image to display the thumbnail.
+                var image = new Image()
                 {
                     Source = streamImageSource,
                     Margin = new Thickness(10)
                 };
 
-                // Show the thumbnail image in a UI control
+                // Show the thumbnail image in a UI control.
                 ShapefileThumbnailImage.Source = image.Source;
 
-                // Create a feature layer to display the shapefile
-                FeatureLayer newFeatureLayer = new FeatureLayer(myShapefile);
+                // Create a feature layer to display the shapefile.
+                var newFeatureLayer = new FeatureLayer(myShapefile);
                 await newFeatureLayer.LoadAsync();
 
-                // Zoom the map to the extent of the shapefile
+                // Zoom the map to the extent of the shapefile.
                 MyMapView.SpatialReferenceChanged += async (s, e) =>
                 {
                     await MyMapView.SetViewpointGeometryAsync(newFeatureLayer.FullExtent);
                 };
 
-                // Add the feature layer to the map
+                // Add the feature layer to the map.
                 streetMap.OperationalLayers.Add(newFeatureLayer);
 
-                // Show the map in the MapView
+                // Show the map in the MapView.
                 MyMapView.Map = streetMap;
             }
             catch (Exception e)
             {
                 await Application.Current.MainPage.DisplayAlert("Error", e.ToString(), "OK");
             }
-        }
-
-        private void ShowMetadataClicked(object sender, System.EventArgs e)
-        {
-            // Toggle the visibility of the metadata panel
-            MetadataScrollView.IsVisible = !MetadataScrollView.IsVisible;
         }
 
         private static string GetShapefilePath()

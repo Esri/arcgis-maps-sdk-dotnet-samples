49e4e6ab25dc6a16d780d3546a35c115e6a05721
Hamish Duff <99325835+duffh@users.noreply.github.com>
2024-01-19 04:34:02 -0800

    Improve MAUI sample viewer flyout menu (#1373)

--- src/MAUI/Maui.Samples/AppShell.xaml.cs
+++ src/MAUI/Maui.Samples/AppShell.xaml.cs
@@ -1,49 +1,31 @@
 using ArcGIS.Samples.Managers;
 using ArcGIS.Samples.Shared.Managers;
-using ArcGIS.Samples.Shared.Models;
 
 namespace ArcGIS;
 
 public partial class AppShell : Shell
 {
-	public AppShell()
-	{
+    public AppShell()
+    {
         InitializeComponent();
 
         Initialize();
-	}
+    }
 
     private void Initialize()
     {
         this.Appearing += FirstLoaded;
 
-        SampleManager.Current.Initialize();
-
-        var samplesCategories = SampleManager.Current.FullTree.Items.OfType<SearchableTreeNode>().ToList();
-        var allSamples = SampleManager.Current.AllSamples.ToList();
-
-        List<FlyoutItem> flyoutItems = new List<FlyoutItem>();
-
-        foreach (var category in samplesCategories)
-        {
-            FlyoutItem flyoutItem = new FlyoutItem();
-            flyoutItem.Title = category.Name;
-
-            ShellContent shellContent = new ShellContent();
-            //shellContent.Title = category.Name;
-            shellContent.Content = new CategoryPage(category);
-            shellContent.Route = $"{nameof(CategoryPage)}_{category.Name}";
-
-            flyoutItem.Items.Add(shellContent);
-
-            this.Items.Add(flyoutItem);
-        }
+        Routing.RegisterRoute(nameof(CategoryPage), typeof(CategoryPage));
+        Routing.RegisterRoute(nameof(SamplePage), typeof(SamplePage));
     }
 
     #region Check API Key
     private void FirstLoaded(object sender, EventArgs e)
     {
         this.Appearing -= FirstLoaded;
+
+        SampleManager.Current.Initialize();
 
         _ = CheckApiKey();
     }
---
+++ src/MAUI/Maui.Samples/ViewModels/FlyoutMenuViewModel.cs
@@ -0,0 +1,94 @@
+﻿using ArcGIS.Samples.Managers;
+using ArcGIS.Samples.Shared.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using System.Collections.ObjectModel;
+
+namespace ArcGIS.ViewModels
+{
+    public partial class FlyoutMenuViewModel : ObservableObject
+    {
+        private readonly Dictionary<string, char> _categoryIcons = new Dictionary<string, char>()
+        {
+            { "Featured", (char)0xe0dc },
+            { "Favorites", (char)0xe2a9 },
+            { "Analysis", (char)0xe003 },
+            { "Data", (char)0xe0b7 },
+            { "Geometry", (char)0xe190 },
+            { "Geoprocessing", (char)0xe180 },
+            { "GraphicsOverlay", (char)0xe16a },
+            { "Hydrography", (char)0xe0e1 },
+            { "Layers", (char)0xe192 },
+            { "Location", (char)0xe092 },
+            { "Map", (char)0xe164 },
+            { "MapView", (char)0xe011 },
+            { "Network analysis", (char)0xe2ce },
+            { "Scene", (char)0xe139 },
+            { "SceneView", (char)0xe018 },
+            { "Search", (char)0xe18e },
+            { "Security", (char)0xe1b6 },
+            { "Symbology", (char)0xe1de },
+            { "Utility network", (char)0xe26b },
+        };
+
+        public FlyoutMenuViewModel()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            SampleManager.Current.Initialize();
+
+            var samplesCategories = SampleManager.Current.FullTree.Items.OfType<SearchableTreeNode>().ToList();
+
+            var categories = new List<FlyoutCategoryViewModel>();
+
+            foreach (var category in samplesCategories)
+            {
+                if (_categoryIcons.TryGetValue(category.Name, out char categoryIcon))
+                {
+                    var flyoutCategory = new FlyoutCategoryViewModel(category.Name, categoryIcon);
+                    flyoutCategory.IsSelected = category.Name == "Featured";
+                    categories.Add(flyoutCategory);
+                }
+                else
+                {
+                    var flyoutCategory = new FlyoutCategoryViewModel(category.Name, (char)0xe1e2);
+                    flyoutCategory.IsSelected = category.Name == "Featured";
+                    categories.Add(flyoutCategory);
+                }
+            }
+
+            Categories = new ObservableCollection<FlyoutCategoryViewModel>(categories);
+
+        }
+
+        [ObservableProperty]
+        private ObservableCollection<FlyoutCategoryViewModel> _categories;
+
+        public void CategorySelected(string categoryName)
+        {
+            WeakReferenceMessenger.Default.Send(categoryName);
+        }
+    }
+
+    public partial class FlyoutCategoryViewModel : ObservableObject
+    {
+        public FlyoutCategoryViewModel(string categoryName, char categoryIcon)
+        {
+            CategoryName = categoryName;
+            CategoryIcon = categoryIcon;
+        }
+
+        [ObservableProperty]
+        private string _categoryName;
+
+        [ObservableProperty]
+        private char _categoryIcon;
+
+        [ObservableProperty]
+        private bool _isSelected;
+    }
+}

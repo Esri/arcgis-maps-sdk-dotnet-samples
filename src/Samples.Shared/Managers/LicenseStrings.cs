2f11c60df526ee82622fe13e725f8ef9abe78085
Morten Nielsen <mn@iter.dk>
2024-01-22 11:54:52 -0800

    CI Build changes (#1362)
    Co-authored-by: Hamish Duff <99325835+duffh@users.noreply.github.com>
    Co-authored-by: William Bohrmann <118313312+williambohrmann3@users.noreply.github.com>
    Co-authored-by: Andy Weis <aweis@esri.com>
    Co-authored-by: Zack Allen <16495151+ZackAllen@users.noreply.github.com>
    Co-authored-by: William Bohrmann <wbohrmann@esri.com>
    Co-authored-by: Preeti <pmaske@esri.com>
    Co-authored-by: Greg De Stigter <gdestigter@esri.com>
    Co-authored-by: Teja Balu <83402878+tejabalu@users.noreply.github.com>
    Co-authored-by: Morten Nielsen <mort5161@esri.com>
    Co-authored-by: Hamish Duff <hduff@esri.com>

---
+++ src/Samples.Shared/Managers/LicenseStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArcGIS.Samples.Shared.Managers
+{
+    internal static partial class LicenseStrings
+    {
+        // Optional license strings
+        public static string ArcGISLicenseKey { get; } = null; // ArcGIS SDK License Key
+
+        public static string ArcGISUtilityNetworkLicenseKey { get; } = null; // Utility Network Extension License Key
+
+        public static string ArcGISAnalysisLicenseKey { get; } = null; // Spatial Analyst Extension License Key
+
+        public static string[] ExtensionLicenses => new[] { ArcGISUtilityNetworkLicenseKey, ArcGISAnalysisLicenseKey }.Where(l => !string.IsNullOrEmpty(l)).ToArray();
+    }
+}

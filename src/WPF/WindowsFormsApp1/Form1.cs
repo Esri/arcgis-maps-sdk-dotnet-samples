8e74486e052ca66215caff612dced98d179c38cc
Jennifer Nery <jnery@esri.com>
2024-03-28 14:05:22 -0700

    cert: winforms

---
+++ src/WPF/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.Initialize();
+            InitializeComponent();
+            var host = new ElementHost();
+            host.Dock = DockStyle.Fill;
+            //host.Child = new ArcGIS.WPF.Samples.ShowCallout.ShowCallout();
+            //host.Child = new ArcGIS.WPF.Samples.CreateAndEditGeometries.CreateAndEditGeometries();
+            host.Child = new ArcGIS.WPF.Samples.GetElevationAtPoint.GetElevationAtPoint();
+            this.Controls.Add(host);
+        }
+    }
+}

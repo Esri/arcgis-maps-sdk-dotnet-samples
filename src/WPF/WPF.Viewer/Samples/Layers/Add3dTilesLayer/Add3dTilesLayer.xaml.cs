5d25971653cfad55f3b15b2e0cf8948ccc6336ea
William Bohrmann <wbohrmann@esri.com>
2024-05-30 12:31:20 -0700

    Add3dTilesLayer

--- src/WPF/WPF.Viewer/Samples/Layers/Add3dTilesLayer/Add3dTilesLayer.xaml.cs
+++ src/WPF/WPF.Viewer/Samples/Layers/Add3dTilesLayer/Add3dTilesLayer.xaml.cs
@@ -7,22 +7,9 @@
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
-using Esri.ArcGISRuntime.Data;
-using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
-using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
-using Esri.ArcGISRuntime.UI.Controls;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Media;
 
 namespace ArcGIS.WPF.Samples.Add3dTilesLayer
 {
@@ -34,6 +21,21 @@ namespace ArcGIS.WPF.Samples.Add3dTilesLayer
     [ArcGIS.Samples.Shared.Attributes.OfflineData()]
     public partial class Add3dTilesLayer
     {
+        // Create a new elevation source from Terrain3D REST service.
+        private readonly ArcGISTiledElevationSource _elevationSource = new ArcGISTiledElevationSource
+            (new Uri("https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"));
+
+        // Create a new scene with a dark gray basemap.
+        private readonly Scene _scene = new Scene(BasemapStyle.ArcGISDarkGray);
+
+        // Create a new 3D tiles layer from a REST endpoint.
+        private readonly Ogc3DTilesLayer _3dTilesLayer = new Ogc3DTilesLayer
+            (new Uri("https://tiles.arcgis.com/tiles/ZQgQTuoyBrtmoGdP/arcgis/rest/services/Stuttgart/3DTilesServer/tileset.json"));
+
+        // Create a camera with an initial viewpoint.
+        // Camera constructor parameters: latitude, longitude, altitude, heading, pitch, and roll.
+        private readonly Camera sceneCamera = new Camera(48.8418, 9.1536, 1325.0, 48.35, 57.84, 0.0);
+
         public Add3dTilesLayer()
         {
             InitializeComponent();
@@ -42,6 +44,17 @@ namespace ArcGIS.WPF.Samples.Add3dTilesLayer
 
         private async Task Initialize()
         {
+            // Add the elevation source to the scene.
+            _scene.BaseSurface.ElevationSources.Add(_elevationSource);
+
+            // Add the 3D tiles layer to the scene.
+            _scene.OperationalLayers.Add(_3dTilesLayer);
+
+            // Set the scene on the SceneView to visualize the 3D tiles layer.
+            MySceneView.Scene = _scene;
+
+            // Set the viewpoint with the camera.
+            await MySceneView.SetViewpointCameraAsync(sceneCamera);
         }
     }
 }

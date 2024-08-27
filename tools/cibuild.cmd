fc16b15adf658f93d27ab8aeb6f722be532fd1ba
William Bohrmann <wbohrmann@esri.com>
2024-08-27 09:37:06 -0700

    build params

--- tools/cibuild.cmd
+++ tools/cibuild.cmd
@@ -12,7 +12,7 @@ IF "%VSINSTALLDIR%"=="" (
 )
 
 IF "%RELEASE_VERSION%" == "" (
-  SET RELEASE_VERSION=200.5.0
+  SET RELEASE_VERSION=200.6.0
 )
 
 REM Configure NuGet
@@ -55,5 +55,5 @@ IF "%ARCGIS_API_KEY%" NEQ "" (
 )
 
 
-msbuild /t:BuildAll %~dp0GenerateApps.msbuild /p:BUILD_NUM=%BUILD_NUM% /p:RELEASE_VERSION=%RELEASE_VERSION% /p:PUBLISHER="%PUBLISHER%" /p:PFXSignaturePassword=%PFXSignaturePassword% /p:PFXSignatureFile=%PFXSignatureFile% /p:PackageCertificateThumbprint=%PackageCertificateThumbprint%
+msbuild /t:BuildAll %~dp0GenerateApps.msbuild /p:BUILD_NUM=%BUILD_NUM% /p:RELEASE_VERSION=%RELEASE_VERSION% /p:PUBLISHER="%PUBLISHER%" /p:PFXSignaturePassword=%PFXSignaturePassword% /p:PFXSignatureFile=%PFXSignatureFile% /p:PackageCertificateThumbprint=%PackageCertificateThumbprint% /p:KeyStoreFile=%KeyStoreFile% /p:KeyPass=%KeyPass% /p:KeyAlias=%KeyAlias%
 

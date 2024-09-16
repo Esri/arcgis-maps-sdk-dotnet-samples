c7466a5438ccda36d3f9ac7b32f08c36afbc2a36
William Bohrmann <118313312+williambohrmann3@users.noreply.github.com>
2024-09-16 10:15:34 -0700

    New sample: Style geometry types with symbols (#1524)

--- src/MAUI/Maui.Samples/Converters/ColorConverter.cs
+++ src/MAUI/Maui.Samples/Converters/ColorConverter.cs
@@ -7,8 +7,28 @@ namespace ArcGIS.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value == null || value.GetType() != typeof(string)) { return null; }
-            return Microsoft.Maui.Graphics.Color.FromArgb((string)value);
+            if (value == null) return null;
+
+            // Determine the object type.
+            Type type = value.GetType();
+
+            if (type == typeof(string))
+            {
+                string colorString = value as string;
+
+                // Color needs to be in hex format or else it will throw.
+                return Color.FromArgb(colorString);
+            }
+            else if (type == typeof(System.Drawing.Color))
+            {
+                var color = (System.Drawing.Color)value;
+
+                return new Color(color.R, color.G, color.B, color.A);
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
--- src/WPF/WPF.Viewer/Converters/ColorToSolidBrushConverter.cs
+++ src/WPF/WPF.Viewer/Converters/ColorToSolidBrushConverter.cs
@@ -13,17 +13,28 @@ namespace ArcGIS.WPF.Viewer.Converters
             // Get the input value as a System.Drawing.Color.
             Color inColor = (Color)value;
 
+            // Create a solid color brush using the color and return it.
+            return Convert(inColor);
+        }
+
+        public static WinMedia.SolidColorBrush Convert(Color color)
+        {  
             // Convert the input System.Drawing.Color to a System.Windows.Media.Color.
-            WinMedia.Color outColor = WinMedia.Color.FromArgb(inColor.A, inColor.R, inColor.G, inColor.B);
+            WinMedia.Color outColor = WinMedia.Color.FromArgb(color.A, color.R, color.G, color.B);
 
             // Create a solid color brush using the color and return it.
-            WinMedia.SolidColorBrush brush = new WinMedia.SolidColorBrush(outColor);
-            return brush;
+            return new WinMedia.SolidColorBrush(outColor);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             throw new NotImplementedException();
+        }
+
+        public static Color ConvertBack(WinMedia.SolidColorBrush brush)
+        {
+            // Convert the input System.Windows.Media.Color to a System.Drawing.Color.
+            return Color.FromArgb(brush.Color.A, brush.Color.R, brush.Color.G, brush.Color.B);
         }
     }
 }

70f346123913b5421e2731552fbcd36820f4813e
imalcolm <imalcolm@esri.com>
2025-10-23 15:18:44 -0700

    Ran sample gen script

---
+++ src/WPF/WPF.Viewer/Samples/Scene/DisplayLocalScene/DisplayLocalScene.xaml.cs
@@ -0,0 +1,47 @@
+﻿// Copyright 2025 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.Tasks.Offline;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.ArcGISServices;
+using Esri.ArcGISRuntime.UI.Controls;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ArcGIS.WPF.Samples.DisplayLocalScene
+{
+    [ArcGIS.Samples.Shared.Attributes.Sample(
+        "Display local scene",
+        "Scene",
+        "Display a local scene with a topographic surface and 3D scene layer clipped to a local area.",
+        "")]
+    [ArcGIS.Samples.Shared.Attributes.OfflineData("61da8dc1a7bc4eea901c20ffb3f8b7af")]
+    public partial class DisplayLocalScene
+    {
+        public DisplayLocalScene()
+        {
+            InitializeComponent();
+            _ = Initialize();
+        }
+
+        private async Task Initialize()
+        {
+        }
+    }
+}

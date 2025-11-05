1f7a757915aaeee657be00e1c008988710694e8e
praveenaa <praveenaa2408@gmail.com>
2025-11-05 09:42:58 -0800

    new sample code

---
+++ src/MAUI/Maui.Samples/Samples/Layers/AddBuildingSceneLayer/readme.md
@@ -0,0 +1,33 @@
+# Add building scene layer
+
+Add a layer to a local scene to visualize and interact with 3D building models developed using Building Information Modeling (BIM) tools.
+
+![Image of add building scene layer](addbuildingscenelayer.jpg)
+
+## Use case
+
+Building Scene Layers allow you to display and analyze detailed building models created from 3D BIM data. Unlike 3D object scene layers, which represent all features within a single layer, Building Scene Layers are organized into a hierarchy of sublayers representing individual building components such as walls, light fixtures, and mechanical systems. These sublayers are often grouped by disciplines like Architectural, Mechanical, or Structural. This structure enables deeper interaction and analysis of both interior and exterior features, providing insight into how a building is designed, used, and situated in its spatial context.
+
+## How to use the sample
+
+When loaded, the sample displays a scene with a Building Scene Layer. By default, the Overview sublayer is visible, showing the building's exterior shell. Use the "Full Model" toggle to switch to the Full Model sublayer, which reveals the building's components. Pan around and zoom in to observe the detailed features such as walls, light fixtures, mechanical systems, and more, both outside and inside the building.
+
+## How it works
+
+1. Create a local scene object with the `Scene.Scene(BasemapStyle, SceneViewingMode)` constructor.
+2. Create an `ArcGISTiledElevationSource` object and add it to the local scene's base surface.
+3. Create a `BuildingSceneLayer` and add it to the local scene's operational layers.
+4. Create a `LocalSceneView` object to display the scene.
+5. Set the local scene to the `LocalSceneView`.
+
+## Relevant API
+
+* ArcGISTiledElevationSource
+* BuildingSceneLayer
+* BuildingSublayer
+* LocalSceneView
+* Scene
+
+## Tags
+
+3D, buildings, elevation, layers, scene, surface
---
+++ src/WPF/WPF.Viewer/Samples/Layers/AddBuildingSceneLayer/AddBuildingSceneLayer.xaml.cs
@@ -0,0 +1,93 @@
+﻿// Copyright 2025 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArcGIS.WPF.Samples.AddBuildingSceneLayer
+{
+    [ArcGIS.Samples.Shared.Attributes.Sample(
+        "Add building scene layer",
+        "Layers",
+        "Add a layer to a local scene to visualize and interact with 3D building models developed using Building Information Modeling (BIM) tools.",
+        "")]
+    [ArcGIS.Samples.Shared.Attributes.OfflineData()]
+    public partial class AddBuildingSceneLayer
+    {
+        private BuildingSublayer _overviewSublayer;
+        private BuildingSublayer _fullModelSublayer;
+
+        public AddBuildingSceneLayer()
+        {
+            InitializeComponent();
+            _ = Initialize();
+        }
+
+        private async Task Initialize()
+        {
+            var scene = new Scene(BasemapStyle.ArcGISTopographic, SceneViewingMode.Local);
+
+            var elevationSource = new ArcGISTiledElevationSource(
+                new Uri("https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"));
+            scene.BaseSurface.ElevationSources.Add(elevationSource);
+
+            var buildingSceneLayer = new BuildingSceneLayer(
+                new Uri("https://3dcities.maps.arcgis.com/home/item.html?id=ebae5f766aac41ba9ec588f66028a6a9"));
+            buildingSceneLayer.AltitudeOffset = 2;
+
+            await buildingSceneLayer.LoadAsync();
+            scene.OperationalLayers.Add(buildingSceneLayer);
+            MySceneView.Scene = scene;
+
+            var camera = new Camera(
+                           new MapPoint(-13045148, 4036775, 454, SpatialReferences.WebMercator),
+                           heading: 343,
+                           pitch: 64,
+                           roll: 0);
+            MySceneView.SetViewpointCamera(camera);
+
+            var sublayers = buildingSceneLayer.Sublayers;
+            _overviewSublayer = sublayers.FirstOrDefault(s =>
+                string.Equals(s.Name, "Overview", StringComparison.OrdinalIgnoreCase));
+            _fullModelSublayer = sublayers.FirstOrDefault(s =>
+                string.Equals(s.Name, "Full Model", StringComparison.OrdinalIgnoreCase));
+
+            if (_fullModelSublayer != null)
+            {
+                FullModelCheckBox.IsEnabled = true;
+            }
+            else
+            {
+                FullModelCheckBox.IsEnabled = false;
+            }
+
+            UpdateSublayerVisibility(false);
+
+            FullModelCheckBox.Checked += OnCheckBoxChanged;
+            FullModelCheckBox.Unchecked += OnCheckBoxChanged;
+        }
+
+        private void OnCheckBoxChanged(object sender, System.Windows.RoutedEventArgs e)
+        {
+            UpdateSublayerVisibility(FullModelCheckBox.IsChecked == true);
+        }
+
+        private void UpdateSublayerVisibility(bool showFullModel)
+        {
+            if (_fullModelSublayer != null)
+                _fullModelSublayer.IsVisible = showFullModel;
+
+            if (_overviewSublayer != null)
+                _overviewSublayer.IsVisible = !showFullModel;
+        }
+    }
+}

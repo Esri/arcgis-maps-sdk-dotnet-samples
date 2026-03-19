131434a8a11f6f5e21a5b37e2c24cf544e7b9e78
imalcolm <imalcolm@esri.com>
2026-03-18 18:35:38 -0700

    Made Maui converter

---
+++ src/MAUI/Maui.Samples/Converters/RuntimeImageToImageSourceConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using Esri.ArcGISRuntime.UI;
+using Microsoft.Maui.Controls;
+
+namespace ArcGIS.Converters;
+
+internal class RuntimeImageToImageSourceConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is RuntimeImage runtimeImage)
+        {
+            return Esri.ArcGISRuntime.Maui.RuntimeImageExtensions.ToImageSource(runtimeImage);
+        }
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}

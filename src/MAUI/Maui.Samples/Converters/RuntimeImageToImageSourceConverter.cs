1ba68354a7c02e1a1cd200a375535e84e6ebe1ce
Ian Malcolm <imalcolm@esri.com>
2026-03-27 10:52:05 -0700

    [Maui, WinUI] Best practices for converting RuntimeImage to ImageSource (#1768)

---
+++ src/MAUI/Maui.Samples/Converters/RuntimeImageToImageSourceConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using Esri.ArcGISRuntime.UI;
+
+namespace ArcGIS.Converters;
+
+internal class RuntimeImageToImageSourceConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is RuntimeImage runtimeImage)
+        {
+            return Esri.ArcGISRuntime.Maui.RuntimeImageExtensions.ToImageSource(runtimeImage);
+        }
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}

2018242691633bcee1e39ec15187a44cacbea573
Ian Malcolm <imalcolm@esri.com>
2026-04-30 15:24:46 -0700

    Update Windows CI scripts (#1797)

--- tools/cibuild.cmd
+++ tools/cibuild.cmd
@@ -1,29 +1,40 @@
 REM @ECHO OFF
-ECHO SEARCHING FOR VISUAL STUDIO...
-"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -prerelease -version [17.8,18.0) -sort -requires Microsoft.Component.MSBuild -products * -property InstallationPath > %TEMP%\vsinstalldir.txt
-SET /p _VSINSTALLDIR=<%TEMP%\vsinstalldir.txt
-DEL %TEMP%\vsinstalldir.txt	
-IF "%_VSINSTALLDIR%"=="" (
-  ECHO ERROR: VISUAL STUDIO NOT FOUND
-  EXIT /B 1
+
+REM Default environment variables
+IF "%DOTNET_VERSION%" == "" (
+  set DOTNET_VERSION=10.0.203
 )
-IF "%VSINSTALLDIR%"=="" (
-  CALL "%_VSINSTALLDIR%\Common7\Tools\VsDevCmd.bat"
+IF "%RELEASE_VERSION%" == "" (
+  SET RELEASE_VERSION=300.1.0
 )
 
-IF "%RELEASE_VERSION%" == "" (
-  SET RELEASE_VERSION=300.0.0
+REM Install the latest dotnet version if not already cached to the build machine
+if "%DOTNET_CACHE_FOLDER%" == "" (
+  set DOTNET_INSTALL_FOLDER=%WORKSPACE%\.dotnet
+) ELSE (
+  set DOTNET_INSTALL_FOLDER=%DOTNET_CACHE_FOLDER%\%DOTNET_VERSION%
 )
+
+if NOT EXIST "%DOTNET_INSTALL_FOLDER%\dotnet.exe" (
+  curl -L https://dot.net/v1/dotnet-install.ps1 -o %WORKSPACE%\dotnet-install.ps1
+  powershell -File %WORKSPACE%\dotnet-install.ps1 -version %DOTNET_VERSION% -InstallDir %DOTNET_INSTALL_FOLDER%
+)
+
+SET DOTNET_EXE=%DOTNET_INSTALL_FOLDER%\dotnet.exe
+ECHO Installed dotnet at %DOTNET_EXE%
 
 REM Configure NuGet
-dotnet new nugetconfig --force -o ../
+%DOTNET_EXE% new nugetconfig --force -o ../
 IF "%NUGET_REPO%" NEQ "" IF EXIST "%NUGET_REPO%" (
-dotnet nuget add source %NUGET_REPO%
+%DOTNET_EXE% nuget add source %NUGET_REPO%
 )
 SET NUGET_PACKAGES=%~dp0..\.nuget\packages
 SET NUGET_HTTP_CACHE_PATH=%~dp0..\.nuget\cache
 md %NUGET_PACKAGES%
 md %NUGET_HTTP_CACHE_PATH%
+
+REM Install maui workload
+%DOTNET_EXE% workload install maui --version %DOTNET_VERSION%
 
 SET licenseFile=%~dp0..\src\Samples.Shared\Managers\LicenseStrings.generated.cs
 IF "%ArcGISLicenseKey%" NEQ "" (
@@ -54,6 +65,5 @@ IF "%ARCGIS_API_KEY%" NEQ "" (
   ECHO ^}^}^} >>%keyFile%
 )
 
-
-msbuild /t:BuildAll %~dp0GenerateApps.msbuild /p:BUILD_NUM=%BUILD_NUM% /p:RELEASE_VERSION=%RELEASE_VERSION% /p:PUBLISHER="%PUBLISHER%" /p:PFXSignaturePassword=%PFXSignaturePassword% /p:PFXSignatureFile=%PFXSignatureFile% /p:PackageCertificateThumbprint=%PackageCertificateThumbprint% /p:KeyStoreFile=%KeyStoreFile% /p:KeyPass=%KeyPass% /p:KeyAlias=%KeyAlias%
-
+set PUBLISHER=%PUBLISHER:\"="%
+%DOTNET_EXE% msbuild %~dp0GenerateApps.msbuild -t:BuildAll -p:BUILD_NUM=%BUILD_NUM% -p:RELEASE_VERSION=%RELEASE_VERSION% -p:PFXSignaturePassword=%PFXSignaturePassword% -p:PFXSignatureFile=%PFXSignatureFile% -p:PackageCertificateThumbprint=%PackageCertificateThumbprint% -p:KeyStoreFile=%KeyStoreFile% -p:KeyPass=%KeyPass% -p:KeyAlias=%KeyAlias%
